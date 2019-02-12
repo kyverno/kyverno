88c90eaa4dff8b8781a382f79377af45b91cef6b
belyshevdenis <belyshev.denis@apriorit.com>
2019-02-12 16:30:40 +0200

    NK-8: Implemented basic HTTPS server with stub for mutation webhook. Implemented script for generating TLS key and certificate. Created MutatingWebhookConfiguration.yaml with declaration of future service.

--- main.go
+++ main.go
@@ -2,23 +2,31 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"log"
+	"os"
 	"time"
 
 	"github.com/nirmata/kube-policy/server"
 )
 
-var (
-	kubeConfigFile string
-)
-
 func main() {
-	server := server.NewWebhookServer()
-	fmt.Println("WebHook server is running!")
+	var cert = flag.String("cert", "", "TLS certificate")
+	var key = flag.String("key", "", "TLS key in PEM format")
+	flag.Parse()
+
+	if *cert == "" || *key == "" {
+		log.Fatal("TLS certificate or/and key is not set")
+	}
+
+	logger := log.New(os.Stdout, "http: ", log.LstdFlags|log.Lshortfile)
+	logger.Printf("! Server is starting...")
+	server := server.NewWebhookServer(*cert, *key, logger)
+	logger.Printf("! WebHook server is running!")
 
 	server.RunAsync()
-	time.Sleep(5 * time.Second)
+	time.Sleep(500500 * time.Second)
 
 	server.Stop()
-	fmt.Println("WebHook server is stopped.")
+	logger.Printf("! WebHook server is stopped.")
 }
