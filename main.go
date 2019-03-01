4e184091d998d4c252ceda3ac5e7636465f3b4c8
belyshevdenis <belyshev.denis@apriorit.com>
2019-03-01 14:16:20 +0200

    NK-22: Fixed build error with Selector pointer. Added comments. Changed tab to 4 spaces identation. Added unit tests for LabelSelector.

--- main.go
+++ main.go
@@ -1,58 +1,58 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"log"
-	"os"
+    "flag"
+    "fmt"
+    "log"
+    "os"
 
-	"github.com/nirmata/kube-policy/controller"
-	"github.com/nirmata/kube-policy/server"
+    "github.com/nirmata/kube-policy/controller"
+    "github.com/nirmata/kube-policy/server"
 
-	"k8s.io/sample-controller/pkg/signals"
+    "k8s.io/sample-controller/pkg/signals"
 )
 
 var (
-	masterURL  string
-	kubeconfig string
-	cert       string
-	key        string
+    masterURL  string
+    kubeconfig string
+    cert       string
+    key        string
 )
 
 func main() {
-	flag.Parse()
+    flag.Parse()
 
-	if cert == "" || key == "" {
-		log.Fatal("TLS certificate or/and key is not set")
-	}
+    if cert == "" || key == "" {
+        log.Fatal("TLS certificate or/and key is not set")
+    }
 
-	crdcLogger := log.New(os.Stdout, "Policy Controller: ", log.LstdFlags|log.Lshortfile)
-	controller, err := controller.NewPolicyController(masterURL, kubeconfig, crdcLogger)
-	if err != nil {
-		fmt.Printf("Error creating PolicyController! Error: %s\n", err)
-		return
-	}
+    crdcLogger := log.New(os.Stdout, "Policy Controller: ", log.LstdFlags|log.Lshortfile)
+    controller, err := controller.NewPolicyController(masterURL, kubeconfig, crdcLogger)
+    if err != nil {
+        fmt.Printf("Error creating PolicyController! Error: %s\n", err)
+        return
+    }
 
-	httpLogger := log.New(os.Stdout, "HTTPS Server: ", log.LstdFlags|log.Lshortfile)
-	server := server.NewWebhookServer(cert, key, controller, httpLogger)
-	server.RunAsync()
+    httpLogger := log.New(os.Stdout, "HTTPS Server: ", log.LstdFlags|log.Lshortfile)
+    server := server.NewWebhookServer(cert, key, controller, httpLogger)
+    server.RunAsync()
 
-	stopCh := signals.SetupSignalHandler()
-	controller.Run(stopCh)
+    stopCh := signals.SetupSignalHandler()
+    controller.Run(stopCh)
 
-	if err != nil {
-		fmt.Printf("Error running PolicyController! Error: %s\n", err)
-	}
+    if err != nil {
+        fmt.Printf("Error running PolicyController! Error: %s\n", err)
+    }
 
-	fmt.Println("Policy Controller has started")
-	<-stopCh
-	server.Stop()
-	fmt.Println("Policy Controller has stopped")
+    fmt.Println("Policy Controller has started")
+    <-stopCh
+    server.Stop()
+    fmt.Println("Policy Controller has stopped")
 }
 
 func init() {
-	flag.StringVar(&kubeconfig, "kubeconfig", "", "Path to a kubeconfig. Only required if out-of-cluster.")
-	flag.StringVar(&masterURL, "master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
-	flag.StringVar(&cert, "cert", "", "TLS certificate used in connection with cluster.")
-	flag.StringVar(&key, "key", "", "Key, used in TLS connection.")
+    flag.StringVar(&kubeconfig, "kubeconfig", "", "Path to a kubeconfig. Only required if out-of-cluster.")
+    flag.StringVar(&masterURL, "master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
+    flag.StringVar(&cert, "cert", "", "TLS certificate used in connection with cluster.")
+    flag.StringVar(&key, "key", "", "Key, used in TLS connection.")
 }
