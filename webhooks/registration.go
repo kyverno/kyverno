b6542ed877431505f8eef1cdee035f2f2db7d204
belyshevdenis <belyshev.denis@apriorit.com>
2019-03-21 15:57:30 +0200

    NK-31: Put constants in separate file. Updated install.yaml definition to create Service and DaemonSet. Fixed bug with webhook registration.

--- webhooks/registration.go
+++ webhooks/registration.go
@@ -1,58 +1,44 @@
 package webhooks
+
 import (
 	"io/ioutil"
+
+	"github.com/nirmata/kube-policy/constants"
 
 	rest "k8s.io/client-go/rest"
 	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
 	adm "k8s.io/api/admissionregistration/v1beta1"
-	types "k8s.io/api/admissionregistration/v1beta1"
 	admreg "k8s.io/client-go/kubernetes/typed/admissionregistration/v1beta1"
 )
 
-const (
-	webhookName = "nirmata-kube-policy-webhook-cfg"
-	mutationWebhookName = "webhook.nirmata.kube-policy"
-	webhookServiceNamespace = "default"
-	webhookServiceName = "kube-policy-svc"
-)
-
-var (
-	webhookPath = "mutate"
-	webhookLabels = map[string]string {
-	    "app": "kube-policy",
-    }
-)
-
-func RegisterMutationWebhook(config *rest.Config) (*types.MutatingWebhookConfiguration, error) {
-var result *types.MutatingWebhookConfiguration = nil
-
+func RegisterMutationWebhook(config *rest.Config) error {
 	registrationClient, err := admreg.NewForConfig(config)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	result, err = registrationClient.MutatingWebhookConfigurations().Create(constructWebhookConfig(config))
+	_, err = registrationClient.MutatingWebhookConfigurations().Create(constructWebhookConfig(config))
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return result, nil
+	return nil
 }
 
 func constructWebhookConfig(config *rest.Config) *adm.MutatingWebhookConfiguration {
 	return &adm.MutatingWebhookConfiguration {
 		ObjectMeta: meta.ObjectMeta {
-			Name: webhookName,
-			Labels: webhookLabels,
+			Name: constants.WebhookConfigName,
+			Labels: constants.WebhookConfigLabels,
 		},
 		Webhooks: []adm.Webhook {
 			adm.Webhook {
-				Name: mutationWebhookName,
+				Name: constants.MutationWebhookName,
 				ClientConfig: adm.WebhookClientConfig {
                     Service: &adm.ServiceReference {
-						Namespace: webhookServiceNamespace,
-						Name: webhookServiceName,
-						Path: &webhookPath,
+						Namespace: constants.WebhookServiceNamespace,
+						Name: constants.WebhookServiceName,
+						Path: &constants.WebhookServicePath,
 					},
 					CABundle: ExtractCA(config),
 				},
