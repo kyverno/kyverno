5ba0b0b8427ec4b790ca418c7485ef0fb0f85858
shuting <shuting@nirmata.com>
2019-04-29 19:27:59 -0700

    modify applyPOolicyRule to make it reusable for a certain raw resource

--- webhooks/admission.go
+++ webhooks/admission.go
@@ -44,12 +44,44 @@ func AdmissionIsRequired(request *v1beta1.AdmissionRequest) bool {
 
 // Checks requests kind, name and labels to fit the policy
 func IsRuleApplicableToRequest(policyResource types.PolicyResource, request *v1beta1.AdmissionRequest) bool {
-	if policyResource.Kind != request.Kind.Kind {
+	return IsRuleApplicableToResource(request.Kind.Kind, request.Object.Raw, policyResource)
+	// if policyResource.Kind != request.Kind.Kind {
+	// 	return false
+	// }
+
+	// if request.Object.Raw != nil {
+	// 	meta := parseMetadataFromObject(request.Object.Raw)
+	// 	name := parseNameFromMetadata(meta)
+
+	// 	if policyResource.Name != nil && *policyResource.Name != name {
+	// 		return false
+	// 	}
+
+	// 	if policyResource.Selector != nil {
+	// 		selector, err := metav1.LabelSelectorAsSelector(policyResource.Selector)
+
+	// 		if err != nil {
+	// 			return false
+	// 		}
+
+	// 		labelMap := parseLabelsFromMetadata(meta)
+
+	// 		if !selector.Matches(labelMap) {
+	// 			return false
+	// 		}
+	// 	}
+	// }
+	// return true
+}
+
+// kind is the type of object being manipulated
+func IsRuleApplicableToResource(kind string, resourceRaw []byte, policyResource types.PolicyResource) bool {
+	if policyResource.Kind != kind {
 		return false
 	}
 
-	if request.Object.Raw != nil {
-		meta := parseMetadataFromObject(request.Object.Raw)
+	if resourceRaw != nil {
+		meta := parseMetadataFromObject(resourceRaw)
 		name := parseNameFromMetadata(meta)
 
 		if policyResource.Name != nil && *policyResource.Name != name {
