419f488d49d61e68b91d41603e65cbd6e26d9d20
Jim Bugwadia <jim@nirmata.com>
2019-05-12 04:16:31 -0700

    Publishing to gh-pages (publish.sh)

---
+++ gh-pages/content/features.md
@@ -0,0 +1,17 @@
+# Features
+
+* Policy definitions as Kubernetes resources
+
+* Validate, mutate, or generate Kubernetes resources
+
+* Match resources using label selectors and wildcards
+
+* Mutate using overlays (like Kustomize) or JSON Patch
+
+* Validate using overlays and powerful conditionals
+
+* Policy violation reporting for existing resources
+
+* Kubernetes events for resource changes and policy enforcement 
+
+* Test using kubectl
---
+++ gh-pages/publish-to-gh-pages.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+DIR=$(dirname "$0")
+
+cd $DIR/..
+
+if [[ $(git status -s) ]]
+then
+	echo "The working directory is dirty. Please commit any pending changes."
+	exit 1;
+fi
+
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
+
+echo "Checking out gh-pages branch into public"
+git worktree add -B gh-pages public upstream/gh-pages
+
+echo "Removing existing files"
+rm -rf public/*
+
+echo "Generating site"
+hugo
+
+echo "Updating gh-pages branch"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+
+`	
