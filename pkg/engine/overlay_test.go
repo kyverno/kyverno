8e73131f029017689b4a332bfc80af3645c6adec
kacejot <kacejot@fex.net>
2019-05-23 17:31:34 +0300

    Fixed minor issue in overlay logic. Added tests

--- pkg/engine/overlay_test.go
+++ pkg/engine/overlay_test.go
@@ -10,8 +10,48 @@ import (
 )
 
 func TestApplyOverlay_NestedListWithAnchor(t *testing.T) {
-	resourceRaw := []byte(`{ "apiVersion": "v1", "kind": "Endpoints", "metadata": { "name": "test-endpoint", "labels": { "label": "test" } }, "subsets": [ { "addresses": [ { "ip": "192.168.10.171" } ], "ports": [ { "name": "secure-connection", "port": 443, "protocol": "TCP" } ] } ] }`)
-	overlayRaw := []byte(`{ "subsets": [ { "ports": [ { "(name)": "secure-connection", "port": 444, "protocol": "UDP" } ] } ] }`)
+	resourceRaw := []byte(`
+	 {  
+		"apiVersion":"v1",
+		"kind":"Endpoints",
+		"metadata":{  
+		   "name":"test-endpoint",
+		   "labels":{  
+			  "label":"test"
+		   }
+		},
+		"subsets":[  
+		   {  
+			  "addresses":[  
+				 {  
+					"ip":"192.168.10.171"
+				 }
+			  ],
+			  "ports":[  
+				 {  
+					"name":"secure-connection",
+					"port":443,
+					"protocol":"TCP"
+				 }
+			  ]
+		   }
+		]
+	 }`)
+
+	overlayRaw := []byte(`
+	 {  
+		"subsets":[  
+		   {  
+			  "ports":[  
+				 {  
+					"(name)":"secure-connection",
+					"port":444,
+					"protocol":"UDP"
+				 }
+			  ]
+		   }
+		]
+	 }`)
 
 	var resource, overlay interface{}
 
@@ -36,8 +76,55 @@ func TestApplyOverlay_NestedListWithAnchor(t *testing.T) {
 }
 
 func TestApplyOverlay_InsertIntoArray(t *testing.T) {
-	resourceRaw := []byte(`{ "apiVersion": "v1", "kind": "Endpoints", "metadata": { "name": "test-endpoint", "labels": { "label": "test" } }, "subsets": [ { "addresses": [ { "ip": "192.168.10.171" } ], "ports": [ { "name": "secure-connection", "port": 443, "protocol": "TCP" } ] } ] }`)
-	overlayRaw := []byte(`{ "subsets": [ { "addresses": [ { "ip": "192.168.10.172" }, { "ip": "192.168.10.173" } ], "ports": [ { "name": "insecure-connection", "port": 80, "protocol": "UDP" } ] } ] }`)
+	resourceRaw := []byte(`
+	 {  
+		"apiVersion":"v1",
+		"kind":"Endpoints",
+		"metadata":{  
+		   "name":"test-endpoint",
+		   "labels":{  
+			  "label":"test"
+		   }
+		},
+		"subsets":[  
+		   {  
+			  "addresses":[  
+				 {  
+					"ip":"192.168.10.171"
+				 }
+			  ],
+			  "ports":[  
+				 {  
+					"name":"secure-connection",
+					"port":443,
+					"protocol":"TCP"
+				 }
+			  ]
+		   }
+		]
+	 }`)
+	overlayRaw := []byte(`
+	 {  
+		"subsets":[  
+		   {  
+			  "addresses":[  
+				 {  
+					"ip":"192.168.10.172"
+				 },
+				 {  
+					"ip":"192.168.10.173"
+				 }
+			  ],
+			  "ports":[  
+				 {  
+					"name":"insecure-connection",
+					"port":80,
+					"protocol":"UDP"
+				 }
+			  ]
+		   }
+		]
+	 }`)
 
 	var resource, overlay interface{}
 
@@ -63,8 +150,59 @@ func TestApplyOverlay_InsertIntoArray(t *testing.T) {
 }
 
 func TestApplyOverlay_TestInsertToArray(t *testing.T) {
-	overlayRaw := []byte(`{ "spec": { "template": { "spec": { "containers": [ { "name": "pi1", "image": "vasylev.perl" } ] } } } }`)
-	resourceRaw := []byte(`{ "apiVersion": "batch/v1", "kind": "Job", "metadata": { "name": "pi" }, "spec": { "template": { "spec": { "containers": [ { "name": "piv0", "image": "perl", "command": [ "perl" ] }, { "name": "pi", "image": "perl", "command": [ "perl" ] }, { "name": "piv1", "image": "perl", "command": [ "perl" ] } ], "restartPolicy": "Never" } }, "backoffLimit": 4 } }`)
+	overlayRaw := []byte(`
+	 {  
+		"spec":{  
+		   "template":{  
+			  "spec":{  
+				 "containers":[  
+					{  
+					   "name":"pi1",
+					   "image":"vasylev.perl"
+					}
+				 ]
+			  }
+		   }
+		}
+	 }`)
+	resourceRaw := []byte(`{  
+		"apiVersion":"batch/v1",
+		"kind":"Job",
+		"metadata":{  
+		   "name":"pi"
+		},
+		"spec":{  
+		   "template":{  
+			  "spec":{  
+				 "containers":[  
+					{  
+					   "name":"piv0",
+					   "image":"perl",
+					   "command":[  
+						  "perl"
+					   ]
+					},
+					{  
+					   "name":"pi",
+					   "image":"perl",
+					   "command":[  
+						  "perl"
+					   ]
+					},
+					{  
+					   "name":"piv1",
+					   "image":"perl",
+					   "command":[  
+						  "perl"
+					   ]
+					}
+				 ],
+				 "restartPolicy":"Never"
+			  }
+		   },
+		   "backoffLimit":4
+		}
+	 }`)
 
 	var resource, overlay interface{}
 
