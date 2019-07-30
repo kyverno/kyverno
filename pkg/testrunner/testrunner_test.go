d339455f687027622a24d71e07550a3972ba412f
Shuting Zhao <shutting06@gmail.com>
2019-07-29 19:30:12 -0700

    fix unit test

--- pkg/testrunner/testrunner_test.go
+++ pkg/testrunner/testrunner_test.go
@@ -2,14 +2,6 @@ package testrunner
 
 import "testing"
 
-func TestMutate(t *testing.T) {
-	runner(t, "/test/scenarios/mutate")
-}
-
 func TestCLI(t *testing.T) {
 	runner(t, "/test/scenarios/cli")
-}
-
-func TestGenerate(t *testing.T) {
-	runner(t, "/test/scenarios/generate")
 }
