e7082153102a4c24b66bf6de9d20433a356bbed3
shivkumar dudhani <shivkumar@nirmata.com>
2019-08-20 23:43:30 -0700

    add Policy kind as known Kind

--- pkg/api/kyverno/v1alpha1/register.go
+++ pkg/api/kyverno/v1alpha1/register.go
@@ -29,6 +29,8 @@ var (
 // Adds the list of known types to Scheme.
 func addKnownTypes(scheme *runtime.Scheme) error {
 	scheme.AddKnownTypes(SchemeGroupVersion,
+		&Policy{},
+		&PolicyList{},
 		&PolicyViolation{},
 		&PolicyViolationList{},
 	)
