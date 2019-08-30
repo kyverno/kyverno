0a132054e11ed01c9bcd0598626d7f3990a8cc44
shivkumar dudhani <shivkumar@nirmata.com>
2019-08-30 14:06:47 -0700

    fixes + support generate policies

--- pkg/testrunner/testrunner_test.go
+++ pkg/testrunner/testrunner_test.go
@@ -2,11 +2,6 @@ package testrunner
 
 import "testing"
 
-// func TestCLI(t *testing.T) {
-// 	//https://github.com/nirmata/kyverno/issues/301
-// 	runner(t, "/test/scenarios/cli")
-// }
-
 func Test_Mutate_EndPoint(t *testing.T) {
 	testScenario(t, "/test/scenarios/test/scenario_mutate_endPpoint.yaml")
 }
@@ -35,4 +30,6 @@ func Test_validate_nonRootUsers(t *testing.T) {
 	testScenario(t, "/test/scenarios/test/scenario_validate_nonRootUser.yaml")
 }
 
-//TODO add tests for Generation
+func Test_generate_networkPolicy(t *testing.T) {
+	testScenario(t, "/test/scenarios/test/scenario_generate_networkPolicy.yaml")
+}
--- pkg/testrunner/utils.go
+++ pkg/testrunner/utils.go
@@ -1,18 +1,11 @@
 package testrunner
 
 import (
-	"bytes"
-	"encoding/json"
 	"io/ioutil"
 	"os"
 
 	"github.com/golang/glog"
-	client "github.com/nirmata/kyverno/pkg/dclient"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	kscheme "k8s.io/client-go/kubernetes/scheme"
 )
 
 const (
@@ -28,58 +21,6 @@ func LoadFile(path string) ([]byte, error) {
 	return ioutil.ReadFile(path)
 }
 
-type resourceInfo struct {
-	rawResource []byte
-	gvk         *metav1.GroupVersionKind
-}
-
-func (ri resourceInfo) isSame(other resourceInfo) bool {
-	// compare gvk
-	if *ri.gvk != *other.gvk {
-		return false
-	}
-	// compare rawResource
-	return bytes.Equal(ri.rawResource, other.rawResource)
-}
-
-// compare patched resources
-func compareResource(er *resourceInfo, pr *resourceInfo) bool {
-	if !er.isSame(*pr) {
-		return false
-	}
-	return true
-}
-
-func createClient(resources []*resourceInfo) (*client.Client, error) {
-	scheme := runtime.NewScheme()
-	objects := []runtime.Object{}
-	// registered group versions
-	regResources := []schema.GroupVersionResource{}
-
-	for _, r := range resources {
-		// registered gvr
-		gv := schema.GroupVersion{Group: r.gvk.Group, Version: r.gvk.Version}
-		gvr := gv.WithResource(getResourceFromKind(r.gvk.Kind))
-		regResources = append(regResources, gvr)
-		decode := kscheme.Codecs.UniversalDeserializer().Decode
-		obj, _, err := decode([]byte(r.rawResource), nil, nil)
-		rdata, err := runtime.DefaultUnstructuredConverter.ToUnstructured(&obj)
-		if err != nil {
-			glog.Errorf("failed to load resource. err %s", err)
-		}
-		unstr := unstructured.Unstructured{Object: rdata}
-		objects = append(objects, &unstr)
-	}
-	// Mock Client
-	c, err := client.NewMockClient(scheme, objects...)
-	if err != nil {
-		return nil, err
-	}
-	c.SetDiscovery(client.NewFakeDiscoveryClient(regResources))
-
-	return c, nil
-}
-
 var kindToResource = map[string]string{
 	"ConfigMap":     "configmaps",
 	"Endpoints":     "endpoints",
@@ -92,32 +33,6 @@ var kindToResource = map[string]string{
 func getResourceFromKind(kind string) string {
 	if resource, ok := kindToResource[kind]; ok {
 		return resource
-	}
-	return ""
-}
-
-//ParseNameFromObject extracts resource name from JSON obj
-func ParseNameFromObject(bytes []byte) string {
-	var objectJSON map[string]interface{}
-	json.Unmarshal(bytes, &objectJSON)
-
-	meta := objectJSON["metadata"].(map[string]interface{})
-
-	if name, ok := meta["name"].(string); ok {
-		return name
-	}
-	return ""
-}
-
-// ParseNamespaceFromObject extracts the namespace from the JSON obj
-func ParseNamespaceFromObject(bytes []byte) string {
-	var objectJSON map[string]interface{}
-	json.Unmarshal(bytes, &objectJSON)
-
-	meta := objectJSON["metadata"].(map[string]interface{})
-
-	if namespace, ok := meta["namespace"].(string); ok {
-		return namespace
 	}
 	return ""
 }
