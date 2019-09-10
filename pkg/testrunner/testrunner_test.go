e6a5b1ceb8105421878e1670d8a682b64d697492
Shuting Zhao <shutting06@gmail.com>
2019-09-10 12:27:21 -0700

    add namespace_quota testrunner

--- pkg/testrunner/testrunner_test.go
+++ pkg/testrunner/testrunner_test.go
@@ -100,9 +100,9 @@ func Test_validate_not_readonly_rootfilesystem(t *testing.T) {
 	testScenario(t, "test/scenarios/test/scenario_validate_not_readonly_rootfilesystem.yaml")
 }
 
-// func Test_validate_namespace_quota(t *testing.T) {
-// 	testScenario(t, "test/scenarios/test/scenario_validate_namespace_quota.yaml")
-// }
+func Test_validate_namespace_quota(t *testing.T) {
+	testScenario(t, "test/scenarios/test/scenario_validate_namespace_quota.yaml")
+}
 
 func Test_validate_disallow_node_port(t *testing.T) {
 	testScenario(t, "test/scenarios/test/scenario_validate_disallow_node_port.yaml")
