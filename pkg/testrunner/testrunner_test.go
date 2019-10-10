24f3b8ac969abcd034281c6cf12a68869f92ca93
Shuting Zhao <shutting06@gmail.com>
2019-10-10 12:29:48 -0700

    disallow automountServiceAccountToken

--- pkg/testrunner/testrunner_test.go
+++ pkg/testrunner/testrunner_test.go
@@ -76,8 +76,8 @@ func Test_validate_require_image_tag_not_latest_pass(t *testing.T) {
 	testScenario(t, "test/scenarios/test/scenario_valiadate_require_image_tag_not_latest_pass.yaml")
 }
 
-func Test_mutate_pod_disable_automoutingapicred_pass(t *testing.T) {
-	testScenario(t, "test/scenarios/test/scenario_mutate_pod_disable_automountingapicred.yaml")
+func Test_validate_disallow_automoutingapicred_pass(t *testing.T) {
+	testScenario(t, "test/scenarios/test/scenario_validate_disallow_automountingapicred.yaml")
 }
 
 func Test_validate_disallow_default_namespace(t *testing.T) {
