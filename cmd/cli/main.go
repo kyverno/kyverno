2b899e28f43e1171b6164bdcd66231ba1235e73c
shivkumar dudhani <shivkumar@nirmata.com>
2019-11-14 13:43:42 -0800

    add cli main

---
+++ cmd/cli/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	goflag "flag"
+	"fmt"
+	"os"
+
+	"github.com/nirmata/kyverno/pkg/config"
+	kyverno "github.com/nirmata/kyverno/pkg/kyverno"
+	flag "github.com/spf13/pflag"
+)
+
+func main() {
+	cmd := kyverno.NewDefaultKyvernoCommand()
+	if err := cmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	flag.CommandLine.AddGoFlagSet(goflag.CommandLine)
+	config.LogDefaultFlags()
+	flag.Parse()
+}
