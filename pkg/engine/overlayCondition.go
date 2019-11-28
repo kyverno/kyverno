261560eafbb067c79eaeb9af2c1491c78ada8b8b
Shuting Zhao <shutting06@gmail.com>
2019-11-27 16:07:15 -0800

    mutate rule: do not ignore empty key in resource if overlay has nested anchor

--- pkg/engine/overlayCondition.go
+++ pkg/engine/overlayCondition.go
@@ -156,11 +156,13 @@ func validateNonAnchorOverlayMap(resourceMap, overlayWithoutAnchor map[string]in
 		curPath := path + key + "/"
 		resourceValue, ok := resourceMap[key]
 		if !ok {
-			// policy: 		"(image)": "*:latest",
-			//				"imagePullPolicy": "IfNotPresent",
-			// resource:	"(image)": "*:latest",
-			// the above case should be allowed
-			continue
+			if !hasNestedAnchors(overlayValue) {
+				// policy: 		"(image)": "*:latest",
+				//				"imagePullPolicy": "IfNotPresent",
+				// resource:	"(image)": "*:latest",
+				// the above case should be allowed
+				continue
+			}
 		}
 		if newPath, err := checkConditions(resourceValue, overlayValue, curPath); !reflect.DeepEqual(err, overlayError{}) {
 			return newPath, err
