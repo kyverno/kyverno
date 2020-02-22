3cfa70bbba8910518ba10f0ebb67913cf6b511f9
shravan <shravan.ss@zopsmart.com>
2020-02-22 19:05:54 +0530

    527 untested prototype

--- pkg/policy/status2.go
+++ pkg/policy/status2.go
@@ -15,17 +15,6 @@ import (
 	v1 "github.com/nirmata/kyverno/pkg/api/kyverno/v1"
 )
 
-func NewStatusSync(client *versioned.Clientset, stopCh <-chan struct{}, pMetaStore *policystore.PolicyStore) *StatSync {
-	return &StatSync{
-		cache: &statusCache{
-			mu:   sync.RWMutex{},
-			data: make(map[string]v1.PolicyStatus),
-		},
-		stop:   stopCh,
-		client: client,
-	}
-}
-
 type statusCache struct {
 	mu   sync.RWMutex
 	data map[string]v1.PolicyStatus
@@ -36,6 +25,18 @@ type StatSync struct {
 	stop        <-chan struct{}
 	client      *versioned.Clientset
 	policyStore *policystore.PolicyStore
+}
+
+func NewStatusSync(client *versioned.Clientset, stopCh <-chan struct{}, pMetaStore *policystore.PolicyStore) *StatSync {
+	return &StatSync{
+		cache: &statusCache{
+			mu:   sync.RWMutex{},
+			data: make(map[string]v1.PolicyStatus),
+		},
+		stop:        stopCh,
+		client:      client,
+		policyStore: pMetaStore,
+	}
 }
 
 func (s *StatSync) Start() {
@@ -59,8 +60,14 @@ func (s *StatSync) updateStats() {
 
 func (s *StatSync) UpdateStatusWithMutateStats(response response.EngineResponse) {
 	s.cache.mu.Lock()
-	policyStatus := s.cache.data[response.PolicyResponse.Policy]
-	s.policyStore.ListAll()
+	var policyStatus v1.PolicyStatus
+	policyStatus, exist := s.cache.data[response.PolicyResponse.Policy]
+	if !exist {
+		policy, _ := s.policyStore.Get(response.PolicyResponse.Policy)
+		if policy != nil {
+			policyStatus = policy.Status
+		}
+	}
 
 	var nameToRule = make(map[string]v1.RuleStats, 0)
 	for _, rule := range policyStatus.Rules {
@@ -85,6 +92,112 @@ func (s *StatSync) UpdateStatusWithMutateStats(response response.EngineResponse)
 			policyStatus.ResourcesMutatedCount++
 			ruleStat.AppliedCount++
 			ruleStat.ResourcesMutatedCount++
+		} else {
+			policyStatus.ViolationCount++
+			ruleStat.ViolationCount++
+		}
+
+		nameToRule[rule.Name] = ruleStat
+	}
+
+	var ruleStats = make([]v1.RuleStats, 0, len(nameToRule))
+	for _, ruleStat := range nameToRule {
+		ruleStats = append(ruleStats, ruleStat)
+	}
+
+	policyStatus.AvgExecutionTime = policyAverageExecutionTime.String()
+	policyStatus.Rules = ruleStats
+
+	s.cache.data[response.PolicyResponse.Policy] = policyStatus
+	s.cache.mu.Unlock()
+}
+
+func (s *StatSync) UpdateStatusWithValidateStats(response response.EngineResponse) {
+	s.cache.mu.Lock()
+	var policyStatus v1.PolicyStatus
+	policyStatus, exist := s.cache.data[response.PolicyResponse.Policy]
+	if !exist {
+		policy, _ := s.policyStore.Get(response.PolicyResponse.Policy)
+		if policy != nil {
+			policyStatus = policy.Status
+		}
+	}
+
+	var nameToRule = make(map[string]v1.RuleStats, 0)
+	for _, rule := range policyStatus.Rules {
+		nameToRule[rule.Name] = rule
+	}
+
+	var policyAverageExecutionTime time.Duration
+	for _, rule := range response.PolicyResponse.Rules {
+		ruleStat := nameToRule[rule.Name]
+		ruleStat.Name = rule.Name
+
+		averageOver := int64(ruleStat.AppliedCount + ruleStat.ViolationCount)
+		newAverageExecutionTime := updateAverageTime(
+			rule.ProcessingTime,
+			ruleStat.ExecutionTime,
+			averageOver)
+		policyAverageExecutionTime += newAverageExecutionTime
+		ruleStat.ExecutionTime = newAverageExecutionTime.String()
+
+		if rule.Success {
+			policyStatus.RulesAppliedCount++
+			policyStatus.ResourcesBlockedCount++
+			ruleStat.AppliedCount++
+			ruleStat.ResourcesBlockedCount++
+		} else {
+			policyStatus.ViolationCount++
+			ruleStat.ViolationCount++
+		}
+
+		nameToRule[rule.Name] = ruleStat
+	}
+
+	var ruleStats = make([]v1.RuleStats, 0, len(nameToRule))
+	for _, ruleStat := range nameToRule {
+		ruleStats = append(ruleStats, ruleStat)
+	}
+
+	policyStatus.AvgExecutionTime = policyAverageExecutionTime.String()
+	policyStatus.Rules = ruleStats
+
+	s.cache.data[response.PolicyResponse.Policy] = policyStatus
+	s.cache.mu.Unlock()
+}
+
+func (s *StatSync) UpdateStatusWithGenerateStats(response response.EngineResponse) {
+	s.cache.mu.Lock()
+	var policyStatus v1.PolicyStatus
+	policyStatus, exist := s.cache.data[response.PolicyResponse.Policy]
+	if !exist {
+		policy, _ := s.policyStore.Get(response.PolicyResponse.Policy)
+		if policy != nil {
+			policyStatus = policy.Status
+		}
+	}
+
+	var nameToRule = make(map[string]v1.RuleStats, 0)
+	for _, rule := range policyStatus.Rules {
+		nameToRule[rule.Name] = rule
+	}
+
+	var policyAverageExecutionTime time.Duration
+	for _, rule := range response.PolicyResponse.Rules {
+		ruleStat := nameToRule[rule.Name]
+		ruleStat.Name = rule.Name
+
+		averageOver := int64(ruleStat.AppliedCount + ruleStat.ViolationCount)
+		newAverageExecutionTime := updateAverageTime(
+			rule.ProcessingTime,
+			ruleStat.ExecutionTime,
+			averageOver)
+		policyAverageExecutionTime += newAverageExecutionTime
+		ruleStat.ExecutionTime = newAverageExecutionTime.String()
+
+		if rule.Success {
+			policyStatus.RulesAppliedCount++
+			ruleStat.AppliedCount++
 		} else {
 			policyStatus.ViolationCount++
 			ruleStat.ViolationCount++
