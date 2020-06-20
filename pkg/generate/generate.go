b5bdb8711eaab32adb86a4ee583917ee439e2d0c
Yuvraj <yuvraj.yad001@gmail.com>
2020-06-19 17:23:35 -0700

    uid conflict resolve

--- pkg/generate/generate.go
+++ pkg/generate/generate.go
@@ -273,12 +273,18 @@ func applyRule(log logr.Logger, client *dclient.Client, rule kyverno.Rule, resou
 		if rule.Generation.Synchronize {
 			logger.V(4).Info("updating existing resource")
 			// Update the resource
-			_, err := client.UpdateResource(genKind, genNamespace, newResource, false)
-			if err != nil {
-				// Failed to update resource
-				return noGenResource, err
+			orignalResource, err := client.GetResource(genKind, genNamespace, genName)
+			if err == nil {
+				newResource.SetUID(orignalResource.GetUID())
+				_, err := client.UpdateResource(genKind, genNamespace, newResource, false)
+				if err != nil {
+					// Failed to update resource
+					return noGenResource, err
+				}
+				logger.V(4).Info("updated new resource")
 			}
-			logger.V(4).Info("updated new resource")
+		} else {
+			logger.V(4).Info("Synchronize resource is disabled")
 		}
 	}
 
