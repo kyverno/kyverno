1e093b33068305e5a2b6931aeb7e197d06bc37f1
Yuvraj <10830562+evalsocket@users.noreply.github.com>
2020-07-30 01:05:37 +0530

    Update ci.sh

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,6 +2,17 @@
 set -e
 
 pwd=$(pwd)
+hash=$(git describe --dirty --always --tags)
+
+## Install Kind
+curl -Lo $pwd/kind https://kind.sigs.k8s.io/dl/v0.8.1/kind-linux-amd64
+chmod a+x $pwd/kind
+
+## Create Kind Cluster
+$pwd/kind create cluster --name e2e
+$pwd/kind load nirmata/kyverno:$hash
+
+pwd=$(pwd)
 hash=sha-$(git rev-parse --short HEAD)
 echo $hash
 cd $pwd/definitions
@@ -12,3 +23,4 @@ echo "Kustomize image edit"
 $pwd/definitions/kustomize edit set image kyverno=evalsocket/kyverno:$hash
 $pwd/definitions/kustomize edit set image kyvernopre=evalsocket/kyvernopre:$hash
 $pwd/definitions/kustomize build $pwd/definitions/ > $pwd/definitions/install.yaml
+
