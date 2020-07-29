4c705d596292d6bdd8f09e6809000206844978b1
evalsocket <yuvraj.yad001@gmail.com>
2020-07-29 15:02:34 -0700

    work

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -3,7 +3,7 @@ set -e
 
 pwd=$(pwd)
 hash=$(git describe --dirty --always --tags)
-
+#
 ## Install Kind
 curl -Lo $pwd/kind https://kind.sigs.k8s.io/dl/v0.8.1/kind-linux-amd64
 chmod a+x $pwd/kind
@@ -18,10 +18,10 @@ pwd=$(pwd)
 echo $hash
 cd $pwd/definitions
 echo "Installing kustomize"
-curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
+#curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
 chmod a+x $pwd/definitions/kustomize
 echo "Kustomize image edit"
-$pwd/definitions/kustomize edit set image kyverno=evalsocket/kyverno:$hash
-$pwd/definitions/kustomize edit set image kyvernopre=evalsocket/kyvernopre:$hash
+$pwd/definitions/kustomize edit set image evalsocket/kyverno:$hash
+$pwd/definitions/kustomize edit set image evalsocket/kyvernopre:$hash
 $pwd/definitions/kustomize build $pwd/definitions/ > $pwd/definitions/install.yaml
 
