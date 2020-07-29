805fd1700d934839b2619a713f0130667e29ec6d
evalsocket <yuvraj.yad001@gmail.com>
2020-07-29 14:10:05 -0700

    test case fixed

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -11,8 +11,8 @@ chmod a+x $pwd/kind
 ## Create Kind Cluster
 $pwd/kind create cluster --name e2e
 docker images
-$pwd/kind load docker-image evalsocket/kyverno:$hash
-$pwd/kind load docker-image evalsocket/kyvernopre:$hash
+$pwd/kind load docker-image evalsocket/kyverno:$hash --name kind-e2e
+$pwd/kind load docker-image evalsocket/kyvernopre:$hash --name kind-e2e
 
 pwd=$(pwd)
 echo $hash
