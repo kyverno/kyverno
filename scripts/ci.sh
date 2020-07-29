407501e400c9dad50ca05990fc10832f6cbb3bd7
evalsocket <yuvraj.yad001@gmail.com>
2020-07-28 22:37:03 -0700

    rbac added for testing

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,14 +2,13 @@
 set -e
 
 pwd=$(pwd)
-hash="sha-$(git rev-parse --short HEAD)"
+hash=sha-$(git rev-parse --short HEAD)
 echo $hash
-
 cd $pwd/definitions
 echo "Installing kustomize"
 curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
 chmod a+x $pwd/definitions/kustomize
 echo "Kustomize image edit"
-$pwd/definitions/kustomize edit set image nirmata/kyverno:$hash
-$pwd/definitions/kustomize edit set image nirmata/kyvernopre:$hash
-$pwd/definitions/kustomize build . > $pwd/definitions/install.yaml
+$pwd/definitions/kustomize edit set image kyverno=evalsocket/kyverno:$hash
+$pwd/definitions/kustomize edit set image kyvernopre=evalsocket/kyvernopre:$hash
+$pwd/definitions/kustomize build $pwd/definitions/ > $pwd/definitions/install.yaml
