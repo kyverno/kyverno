e75c42966aef2a3b7d7f11fe386d561ef5d39d29
evalsocket <yuvraj.yad001@gmail.com>
2020-07-28 22:24:20 -0700

    docker changes

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -9,6 +9,6 @@ echo "Installing kustomize"
 curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
 chmod a+x $pwd/definitions/kustomize
 echo "Kustomize image edit"
-$pwd/definitions/kustomize edit set image kyverno=nirmata/kyverno:$hash
-$pwd/definitions/kustomize edit set image kyvernopre=nirmata/kyvernopre:$hash
+$pwd/definitions/kustomize edit set image kyverno=evalsocket/kyverno:$hash
+$pwd/definitions/kustomize edit set image kyvernopre=evalsocket/kyvernopre:$hash
 $pwd/definitions/kustomize build . > $pwd/definitions/install.yaml
