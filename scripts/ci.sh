0063793604d3323e628b95c590b18d45c9447a18
evalsocket <yuvraj.yad001@gmail.com>
2020-07-29 15:02:40 -0700

    work

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -18,7 +18,7 @@ pwd=$(pwd)
 echo $hash
 cd $pwd/definitions
 echo "Installing kustomize"
-#curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
+curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
 chmod a+x $pwd/definitions/kustomize
 echo "Kustomize image edit"
 $pwd/definitions/kustomize edit set image evalsocket/kyverno:$hash
