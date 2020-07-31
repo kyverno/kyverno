54c663b3ea33cd3cbe77c8372918d934531bf0bc
evalsocket <yuvraj.yad001@gmail.com>
2020-07-31 07:25:08 -0700

    fix kind setup

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -3,16 +3,17 @@ set -e
 
 pwd=$(pwd)
 hash=$(git describe --dirty --always --tags)
-
+#
 ## Install Kind
 curl -Lo $pwd/kind https://kind.sigs.k8s.io/dl/v0.8.1/kind-linux-amd64
 chmod a+x $pwd/kind
 
 ## Create Kind Cluster
 $pwd/kind create cluster
-$pwd/kind load docker-image nirmata/kyverno:$hash
-$pwd/kind load docker-image nirmata/kyvernopre:$hash
+$pwd/kind load docker-image evalsocket/kyverno:$hash
+$pwd/kind load docker-image evalsocket/kyvernopre:$hash
 
+pwd=$(pwd)
 cd $pwd/definitions
 echo "Installing kustomize"
 curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
