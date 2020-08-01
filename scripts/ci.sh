fa85ce4d396bea1d86e7ff92b592ebbc04750459
evalsocket <yuvraj.yad001@gmail.com>
2020-08-01 06:18:13 -0700

    fix with e2e test cases

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,3 +1,4 @@
+
 #!/bin/sh
 set -e
 
@@ -10,8 +11,8 @@ chmod a+x $pwd/kind
 
 ## Create Kind Cluster
 $pwd/kind create cluster
-$pwd/kind load docker-image nirmata/kyverno:$hash
-$pwd/kind load docker-image nirmata/kyvernopre:$hash
+$pwd/kind load docker-image evalsocket/kyverno:$hash
+$pwd/kind load docker-image evalsocket/kyvernopre:$hash
 
 pwd=$(pwd)
 cd $pwd/definitions
@@ -19,7 +20,6 @@ echo "Installing kustomize"
 curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
 chmod a+x $pwd/definitions/kustomize
 echo "Kustomize image edit"
-$pwd/definitions/kustomize edit set image nirmata/kyverno:$hash
-$pwd/definitions/kustomize edit set image nirmata/kyvernopre:$hash
+$pwd/definitions/kustomize edit set image evalsocket/kyverno:$hash
+$pwd/definitions/kustomize edit set image evalsocket/kyvernopre:$hash
 $pwd/definitions/kustomize build $pwd/definitions/ > $pwd/definitions/install.yaml
-
