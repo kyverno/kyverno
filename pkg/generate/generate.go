846dbb7e4888918a24269ade0726d617a74a9ad5
Yuvraj <yuvraj.yad001@gmail.com>
2020-08-28 01:41:08 +0530

    added logic for delete gr

--- pkg/generate/generate.go
+++ pkg/generate/generate.go
@@ -117,9 +117,9 @@ func (c *Controller) applyGenerate(resource unstructured.Unstructured, gr kyvern
 			}
 			for _, v := range grList.Items {
 				if engineResponse.PolicyResponse.Policy == v.Spec.Policy && engineResponse.PolicyResponse.Resource.Name == v.Spec.Resource.Name && engineResponse.PolicyResponse.Resource.Kind == v.Spec.Resource.Kind && engineResponse.PolicyResponse.Resource.Namespace == v.Spec.Resource.Namespace{
-					err := c.kyvernoClient.KyvernoV1().GenerateRequests(config.KubePolicyNamespace).Delete(v.ObjectMeta.Name, &metav1.DeleteOptions{})
+					err :=c.kyvernoClient.KyvernoV1().GenerateRequests(config.KubePolicyNamespace).Delete(v.GetName(),&metav1.DeleteOptions{})
 					if err != nil {
-
+						logger.Error(err, "failed to update gr")
 					}
 				}
 			}
