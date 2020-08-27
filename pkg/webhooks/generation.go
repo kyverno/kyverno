06e1b5c5fcdd48a9061017a7fbc803e82cffa703
Yuvraj <yuvraj.yad001@gmail.com>
2020-08-27 19:46:21 +0530

    remove extra logs

--- pkg/webhooks/generation.go
+++ pkg/webhooks/generation.go
@@ -54,13 +54,13 @@ func (ws *WebhookServer) HandleGenerate(request *v1beta1.AdmissionRequest, polic
 			if !rule.Success {
 				grList, err := ws.kyvernoClient.KyvernoV1().GenerateRequests(config.KubePolicyNamespace).List(metav1.ListOptions{})
 				if err != nil {
-					logger.Error(err, "failed to convert RAR resource to unstructured format")
+					logger.Error(err, "failed to list generate request")
 				}
 				for _, v := range grList.Items {
 					if engineResponse.PolicyResponse.Policy == v.Spec.Policy && engineResponse.PolicyResponse.Resource.Name == v.Spec.Resource.Name && engineResponse.PolicyResponse.Resource.Kind == v.Spec.Resource.Kind && engineResponse.PolicyResponse.Resource.Namespace == v.Spec.Resource.Namespace {
 						err := ws.kyvernoClient.KyvernoV1().GenerateRequests(config.KubePolicyNamespace).Delete(v.ObjectMeta.Name, &metav1.DeleteOptions{})
 						if err != nil {
-							logger.Error(err, "failed to convert RAR resource to unstructured format")
+							logger.Error(err, "failed to delete grlist")
 						}
 					}
 				}
