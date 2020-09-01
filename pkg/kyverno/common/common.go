e0f617b383f6fb0dafa3ecafb89cd36998c3d02e
shuting <shutting06@gmail.com>
2020-09-01 09:11:20 -0700

    810 support cronJob for auto-gen (#1089)
    * add watch policy to clusterrole kyverno:customresources
    
    * - improve auto-gen policy application logic - remove unused code
    
    * move method to common util
    
    * auto-gen rule for cronJob
    
    * update doc
    
    * set CronJob as default auto-gen pod controller
    
    * - update doc; - fix test
    
    * remove unused code

--- pkg/kyverno/common/common.go
+++ pkg/kyverno/common/common.go
@@ -1,11 +1,8 @@
 package common
 
 import (
-	"bufio"
-	"bytes"
 	"encoding/json"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -21,7 +18,7 @@ import (
 	"github.com/nirmata/kyverno/pkg/kyverno/sanitizedError"
 	"github.com/nirmata/kyverno/pkg/openapi"
 	"github.com/nirmata/kyverno/pkg/policymutation"
-	"k8s.io/apimachinery/pkg/util/yaml"
+	"github.com/nirmata/kyverno/pkg/utils"
 	log "sigs.k8s.io/controller-runtime/pkg/log"
 )
 
@@ -56,7 +53,7 @@ func GetPolicies(paths []string) (policies []*v1.ClusterPolicy, error error) {
 
 			policies = append(policies, policiesFromDir...)
 		} else {
-			getPolicies, getErrors := GetPolicy(path)
+			getPolicies, getErrors := utils.GetPolicy(path)
 			var errString string
 			for _, err := range getErrors {
 				if err != nil {
@@ -74,62 +71,6 @@ func GetPolicies(paths []string) (policies []*v1.ClusterPolicy, error error) {
 	}
 
 	return policies, nil
-}
-
-// GetPolicy - Extracts policies from a YAML
-func GetPolicy(path string) (clusterPolicies []*v1.ClusterPolicy, errors []error) {
-	file, err := ioutil.ReadFile(path)
-	if err != nil {
-		errors = append(errors, fmt.Errorf(fmt.Sprintf("failed to load file: %v. error: %v", path, err)))
-		return clusterPolicies, errors
-	}
-
-	policies, err := SplitYAMLDocuments(file)
-	if err != nil {
-		errors = append(errors, err)
-		return clusterPolicies, errors
-	}
-
-	for _, thisPolicyBytes := range policies {
-		policyBytes, err := yaml.ToJSON(thisPolicyBytes)
-		if err != nil {
-			errors = append(errors, fmt.Errorf(fmt.Sprintf("failed to convert json. error: %v", err)))
-			continue
-		}
-
-		policy := &v1.ClusterPolicy{}
-		if err := json.Unmarshal(policyBytes, policy); err != nil {
-			errors = append(errors, fmt.Errorf(fmt.Sprintf("failed to decode policy in %s. error: %v", path, err)))
-			continue
-		}
-
-		if policy.TypeMeta.Kind != "ClusterPolicy" {
-			errors = append(errors, fmt.Errorf(fmt.Sprintf("resource %v is not a cluster policy", policy.Name)))
-			continue
-		}
-		clusterPolicies = append(clusterPolicies, policy)
-	}
-
-	return clusterPolicies, errors
-}
-
-// SplitYAMLDocuments reads the YAML bytes per-document, unmarshals the TypeMeta information from each document
-// and returns a map between the GroupVersionKind of the document and the document bytes
-func SplitYAMLDocuments(yamlBytes []byte) (policies [][]byte, error error) {
-	buf := bytes.NewBuffer(yamlBytes)
-	reader := yaml.NewYAMLReader(bufio.NewReader(buf))
-	for {
-		// Read one YAML document at a time, until io.EOF is returned
-		b, err := reader.Read()
-		if err == io.EOF || len(b) == 0 {
-			break
-		} else if err != nil {
-			return policies, fmt.Errorf("unable to read yaml")
-		}
-
-		policies = append(policies, b)
-	}
-	return policies, error
 }
 
 //GetPoliciesValidation - validating policies
---
+++ pkg/policymutation/cronjob.go
@@ -0,0 +1,96 @@
+package policymutation
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/go-logr/logr"
+	kyverno "github.com/nirmata/kyverno/pkg/api/kyverno/v1"
+	"github.com/nirmata/kyverno/pkg/engine"
+)
+
+func generateCronJobRule(rule kyverno.Rule, controllers string, log logr.Logger) kyvernoRule {
+	logger := log.WithName("handleCronJob")
+
+	hasCronJob := strings.Contains(controllers, engine.PodControllerCronJob) || strings.Contains(controllers, "all")
+	if !hasCronJob {
+		return kyvernoRule{}
+	}
+
+	logger.V(3).Info("")
+	jobRule := generateRuleForControllers(rule, "Job", logger)
+
+	cronJobRule := &jobRule
+	cronJobRule.Name = fmt.Sprintf("autogen-cronjob-%s", rule.Name)
+	cronJobRule.MatchResources.Kinds = []string{engine.PodControllerCronJob}
+	if (jobRule.ExcludeResources) != nil && (len(jobRule.ExcludeResources.Kinds) > 0) {
+		cronJobRule.ExcludeResources.Kinds = []string{engine.PodControllerCronJob}
+	}
+
+	if (jobRule.Mutation != nil) && (jobRule.Mutation.Overlay != nil) {
+		newMutation := &kyverno.Mutation{
+			Overlay: map[string]interface{}{
+				"spec": map[string]interface{}{
+					"jobTemplate": jobRule.Mutation.Overlay,
+				},
+			},
+		}
+
+		cronJobRule.Mutation = newMutation.DeepCopy()
+		return *cronJobRule
+	}
+
+	if (jobRule.Validation != nil) && (jobRule.Validation.Pattern != nil) {
+		newValidate := &kyverno.Validation{
+			Message: rule.Validation.Message,
+			Pattern: map[string]interface{}{
+				"spec": map[string]interface{}{
+					"jobTemplate": jobRule.Validation.Pattern,
+				},
+			},
+		}
+		cronJobRule.Validation = newValidate.DeepCopy()
+		return *cronJobRule
+	}
+
+	if (jobRule.Validation != nil) && (len(jobRule.Validation.AnyPattern) != 0) {
+		var patterns []interface{}
+		for _, pattern := range jobRule.Validation.AnyPattern {
+			newPattern := map[string]interface{}{
+				"spec": map[string]interface{}{
+					"jobTemplate": pattern,
+				},
+			}
+
+			patterns = append(patterns, newPattern)
+		}
+
+		cronJobRule.Validation = &kyverno.Validation{
+			Message:    rule.Validation.Message,
+			AnyPattern: patterns,
+		}
+		return *cronJobRule
+	}
+
+	return kyvernoRule{}
+}
+
+// stripCronJob removes CronJob from controllers
+func stripCronJob(controllers string) string {
+	var newControllers []string
+
+	controllerArr := strings.Split(controllers, ",")
+	for _, c := range controllerArr {
+		if c == engine.PodControllerCronJob {
+			continue
+		}
+
+		newControllers = append(newControllers, c)
+	}
+
+	if len(newControllers) == 0 {
+		return ""
+	}
+
+	return strings.Join(newControllers, ",")
+}
