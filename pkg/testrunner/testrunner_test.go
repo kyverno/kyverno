bb5fd8bf55d4d18b9fcbe20b20030e6e7cd89d20
NoSkillGirl <singhpooja240393@gmail.com>
2020-09-17 19:48:44 +0530

    Strategic Merge TestCase

--- pkg/testrunner/testrunner_test.go
+++ pkg/testrunner/testrunner_test.go
@@ -10,6 +10,10 @@ func Test_Mutate_Validate_qos(t *testing.T) {
 	testScenario(t, "/test/scenarios/other/scenario_mutate_validate_qos.yaml")
 }
 
+func Test_Mutate_Validate_qos_sm(t *testing.T) {
+	testScenario(t, "/test/scenarios/other/scenario_mutate_validate_qos_sm.yaml")
+}
+
 func Test_disallow_root_user(t *testing.T) {
 	testScenario(t, "test/scenarios/samples/best_practices/disallow_root_user.yaml")
 }
