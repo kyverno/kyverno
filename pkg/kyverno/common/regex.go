72ba013e9bbd1a1704477a9f897eb1fbe087a5c4
Jim Bugwadia <jim@nirmata.com>
2020-10-19 02:16:42 -0700

    fix regex

--- pkg/kyverno/common/regex.go
+++ pkg/kyverno/common/regex.go
@@ -2,11 +2,7 @@ package common
 
 import (
 	"regexp"
-	"strings"
 )
 
 var REGEX_VARIABLES = regexp.MustCompile(`\{\{[^{}]*\}\}`)
-
-var allowedList = []string{`request\..*`, `serviceAccountName`, `serviceAccountNamespace`}
-var regexStr = `\{\{\s*(` + strings.Join(allowedList, "|") + `)[^{}]\s*\}\}`
-var ALLOWED_VARIABLES = regexp.MustCompile(regexStr)
+var ALLOWED_VARIABLES = regexp.MustCompile(`\{\{\s*[request\.|serviceAccountName|serviceAccountNamespace][^{}]*\}\}`)
