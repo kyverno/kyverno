370828afec25dd89c1f5504288bc3d5af9ed5298
shuting <shutting06@gmail.com>
2020-11-30 23:26:49 -0800

    Fix typo, add short names (#1344)
    * fix typo
    
    * add short names for report change request

--- pkg/api/kyverno/v1alpha1/reportchangerequest_types.go
+++ pkg/api/kyverno/v1alpha1/reportchangerequest_types.go
@@ -25,6 +25,7 @@ import (
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
+// ReportChangeRequest is the Schema for the ReportChangeRequests API
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 // +kubebuilder:object:root=true
@@ -36,8 +37,7 @@ import (
 // +kubebuilder:printcolumn:name="Error",type=integer,JSONPath=`.summary.error`
 // +kubebuilder:printcolumn:name="Skip",type=integer,JSONPath=`.summary.skip`
 // +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
-
-// ReportChangeRequest is the Schema for the ReportChangeRequests API
+// +kubebuilder:resource:shortName=rcr
 type ReportChangeRequest struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
