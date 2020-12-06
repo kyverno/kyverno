709c90976fd13321cbdc15d643813a152a549972
Jim Bugwadia <jim@nirmata.com>
2020-12-06 11:17:28 -0800

    improve validation message

--- pkg/kyverno/sanitizedError/error.go
+++ pkg/kyverno/sanitizedError/error.go
@@ -1,6 +1,9 @@
 package sanitizederror
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type customError struct {
 	message string
@@ -10,9 +13,14 @@ func (c customError) Error() string {
 	return c.message
 }
 
-// New creates a new sanitized error with given message
-func New(message string) error {
-	return customError{message: message}
+func NewWithErrors(message string, errors []error) error {
+	bldr := strings.Builder{}
+	bldr.WriteString(message + "\n")
+	for _, err := range errors {
+		bldr.WriteString(err.Error() + "\n")
+	}
+
+	return fmt.Errorf(bldr.String())
 }
 
 // NewWithError creates a new sanitized error with given message and error
