68ece5720cf2ffb953cee8aa061a8a1c2796b9b5
evalsocket <yuvraj.yad001@gmail.com>
2020-12-13 19:30:12 +0530

    More changes

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -9,10 +9,10 @@ hash=$(git describe --always --tags)
 curl -Lo $pwd/kind https://kind.sigs.k8s.io/dl/v0.8.1/kind-linux-amd64
 chmod a+x $pwd/kind
 
-## Create Kind Cluster
+# Create Kind Cluster
 $pwd/kind create cluster
-$pwd/kind load docker-image ghcr.io/kyverno/kyverno:$hash
-$pwd/kind load docker-image ghcr.io/kyverno/kyvernopre:$hash
+kind load docker-image ghcr.io/kyverno/kyverno:$hash
+kind load docker-image ghcr.io/kyverno/kyvernopre:$hash
 
 pwd=$(pwd)
 cd $pwd/definitions
@@ -20,4 +20,4 @@ echo "Installing kustomize"
 curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
 kustomize edit set image ghcr.io/kyverno/kyverno:$hash
 kustomize edit set image ghcr.io/kyverno/kyvernopre:$hash
-kustomize build $pwd/definitions/ > $pwd/definitions/install.yaml
+kustomize build $pwd/definitions/ -o $pwd/definitions/install.yaml
