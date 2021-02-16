8f5ac668518abc82009815a28a74f380a006756d
vyankatesh <vyankatesh@neualto.com>
2021-02-16 22:55:03 +0530

    update kyverno cli test cmd
    Signed-off-by: vyankatesh <vyankatesh@neualto.com>

--- pkg/kyverno/test/git.go
+++ pkg/kyverno/test/git.go
@@ -1,11 +1,12 @@
 package test
 
 import (
+	"os"
+	"path/filepath"
+
 	"github.com/go-git/go-billy/v5"
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/storage/memory"
-	"os"
-	"path/filepath"
 )
 
 func clone(path string, fs billy.Filesystem) (*git.Repository, error) {
@@ -25,6 +26,12 @@ func listYAMLs(fs billy.Filesystem, path string) ([]string, error) {
 	for _, fi := range fis {
 		name := filepath.Join(path, fi.Name())
 		if fi.IsDir() {
+			moreYAMLs, err := listYAMLs(fs, name)
+			if err != nil {
+				return nil, err
+			}
+
+			yamls = append(yamls, moreYAMLs...)
 			continue
 		}
 
