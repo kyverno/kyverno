f80929c2a464e10cc494128cd106a756f332f137
Vyankatesh Kudtarkar <vyankateshkd@gmail.com>
2021-04-29 23:07:09 +0530

    Bugfix :  Make match.resources.kinds required (#1843)
    * Fix Dev setup
    
    * make kind required in MatchResources
    
    * add test cases
    
    Co-authored-by: vyankatesh <vyankatesh@neualto.com>

--- pkg/policy/validate_test.go
+++ pkg/policy/validate_test.go
@@ -1237,3 +1237,50 @@ func Test_doesMatchExcludeConflict(t *testing.T) {
 		}
 	}
 }
+
+func Test_Validate_Kind(t *testing.T) {
+	rawPolicy := []byte(`
+	{
+		"apiVersion": "kyverno.io/v1",
+		"kind": "ClusterPolicy",
+		"metadata": {
+		  "name": "policy-to-monitor-root-user-access"
+		},
+		"spec": {
+		  "validationFailureAction": "audit",
+		  "rules": [
+			{
+			  "name": "monitor-annotation-for-root-user-access",
+			  "match": {
+				"resources": {
+				  "selector": {
+					"matchLabels": {
+					  "AllowRootUserAccess": "true"
+					}
+				  }
+				}
+			  },
+			  "validate": {
+				"message": "Label provisioner.wg.net/cloudprovider is required",
+				"pattern": {
+				  "metadata": {
+					"labels": {
+					  "provisioner.wg.net/cloudprovider": "*"
+					}
+				  }
+				}
+			  }
+			}
+		  ]
+		}
+	  }
+	`)
+
+	var policy *kyverno.ClusterPolicy
+	err := json.Unmarshal(rawPolicy, &policy)
+	assert.NilError(t, err)
+
+	openAPIController, _ := openapi.NewOpenAPIController()
+	err = Validate(policy, nil, true, openAPIController)
+	assert.Assert(t, err != nil)
+}
