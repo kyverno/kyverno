873d1a76dbb4269be4334edd96d089a6b5546ce4
NoSkillGirl <singhpooja240393@gmail.com>
2021-05-07 17:20:48 +0530

    added test cases
    Signed-off-by: NoSkillGirl <singhpooja240393@gmail.com>

--- pkg/kyverno/validate/commmand_test.go
+++ pkg/kyverno/validate/commmand_test.go
@@ -13,86 +13,130 @@ func Test_validateUsingPolicyCRD(t *testing.T) {
 	type TestCase struct {
 		rawPolicy   []byte
 		errorDetail string
+		detail      string
 	}
 
 	testcases := []TestCase{
-		// {
-		// 	rawPolicy: []byte(`
-		// {
-		// 	"apiVersion": "kyverno.io/v1",
-		// 	"kind": "ClusterPolicy",
-		// 	"metadata": {
-		// 	  "name": "add-requests"
-		// 	},
-		// 	"spec": {
-		// 	  "rules": [
-		// 		{
-		// 		  "name": "Set memory and/or cpu requests for all pods in namespaces labeled 'myprivatelabel'",
-		// 		  "match": {
-		// 			"resources": {
-		// 			  "kinds": [
-		// 				"Pod"
-		// 			  ]
-		// 			}
-		// 		  },
-		// 		  "mutate": {
-		// 			"overlay": {
-		// 			  "spec": {
-		// 				"containers": [
-		// 				  {
-		// 					"(name)": "*",
-		// 					"resources": {
-		// 					  "requests": {
-		// 						"cpu": "1000m"
-		// 					  }
-		// 					}
-		// 				  }
-		// 				]
-		// 			  }
-		// 			}
-		// 		  }
-		// 		}
-		// 	  ]
-		// 	}
-		//   }
-		// `),
-		// 	errorDetail: "spec.rules.name in body should be at most 63 chars long",
-		// },
-		// {
-		// 	rawPolicy: []byte(`
-		// 		{
-		// 			"apiVersion": "kyverno.io/v1",
-		// 			"kind": "ClusterPolicy",
-		// 			"metadata": {
-		// 			  "name": "min-replicas-clusterpolicy"
-		// 			},
-		// 			"spec": {
-		// 			  "validationFailureAction": "audit",
-		// 			  "rules": [
-		// 				{
-		// 				  "name": "check-min-replicas",
-		// 				  "match": {
-		// 					"resources": {
-		// 					  "kinds": [
-		// 						"Deployment"
-		// 					  ]
-		// 					}
-		// 				  },
-		// 				  "validate": {
-		// 					"message": "should have at least 2 replicas",
-		// 					"pattern": {
-		// 					  "spec": {
-		// 						"replicas": 2
-		// 					  }
-		// 					}
-		// 				  }
-		// 				}
-		// 			  ]
-		// 			}
-		// 		  }
-		// `),
-		// 	errorDetail: "",
-		// },
+		{
+			rawPolicy: []byte(`
+		{
+			"apiVersion": "kyverno.io/v1",
+			"kind": "ClusterPolicy",
+			"metadata": {
+			  "name": "add-requests"
+			},
+			"spec": {
+			  "rules": [
+				{
+				  "name": "Set memory and/or cpu requests for all pods in namespaces labeled 'myprivatelabel'",
+				  "match": {
+					"resources": {
+					  "kinds": [
+						"Pod"
+					  ]
+					}
+				  },
+				  "mutate": {
+					"overlay": {
+					  "spec": {
+						"containers": [
+						  {
+							"(name)": "*",
+							"resources": {
+							  "requests": {
+								"cpu": "1000m"
+							  }
+							}
+						  }
+						]
+					  }
+					}
+				  }
+				}
+			  ]
+			}
+		  }
+		`),
+			errorDetail: "spec.rules.name in body should be at most 63 chars long",
+			detail:      "Test: char count for rule name",
+		},
+
+		{
+			rawPolicy: []byte(`
+				{
+					"apiVersion": "kyverno.io/v1",
+					"kind": "ClusterPolicy",
+					"metadata": {
+					  "name": "min-replicas-clusterpolicy"
+					},
+					"spec": {
+					  "validationFailureAction": "audit",
+					  "rules": [
+						{
+						  "name": "check-min-replicas",
+						  "match": {
+							"resources": {
+							  "kinds": [
+								"Deployment"
+							  ]
+							}
+						  },
+						  "validate": {
+							"message": "should have at least 2 replicas",
+							"pattern": {
+							  "spec": {
+								"replicas": 2
+							  }
+							}
+						  }
+						}
+					  ]
+					}
+				  }
+		`),
+			errorDetail: "",
+			detail:      "Test: basic vaild policy",
+		},
+
+		{
+			rawPolicy: []byte(`
+				{
+					"apiVersion": "kyverno.io/v1",
+					"kind": "ClusterPolicy",
+					"metadata": {
+					  "name": "disallow-singleton"
+					},
+					"spec": {
+					  "validationFailureAction": "audit",
+					  "rules": [
+						{
+						  "name": "validate-replicas",
+						  "match": {
+							"resources": {
+							  "kinds": [
+								"Deployment"
+							  ],
+							  "annotations": {
+								"singleton": "true"
+							  }
+							}
+						  },
+						  "validate": {
+							"message": "Replicasets require at least 2 replicas.",
+							"pattern": {
+							  "spec": {
+								"replicas": ">1"
+							  }
+							}
+						  }
+						}
+					  ]
+					}
+				  }
+		`),
+			errorDetail: "",
+			detail:      "Test: schema validation for spec.rules.match.resources.annotations",
+		},
 
 		{
 			rawPolicy: []byte(`
@@ -100,40 +144,32 @@ func Test_validateUsingPolicyCRD(t *testing.T) {
 				"apiVersion": "kyverno.io/v1",
 				"kind": "ClusterPolicy",
 				"metadata": {
-				  "name": "pod-requirements",
-				  "namespace": "kyverno",
-				  "annotations": {
-					"pod-policies.kyverno.io/autogen-controllers": "none"
-				  }
+				  "name": "disallow-singleton"
 				},
 				"spec": {
-				  "background": true,
-				  "validationFailureAction": "enforce",
+				  "validationFailureAction": "audit",
 				  "rules": [
 					{
-					  "name": "pods-require-lifetime",
+					  "name": "validate-replicas",
 					  "match": {
 						"resources": {
 						  "kinds": [
-							"Pod"
-						  ],
-						  "namespaces": [
-							"user-?*"
-						  ],
-						  "selector": {
-							"matchLabels": {
-							  "app.kubernetes.io/managed-by": "open-ondemand"
-							}
+							"Deployment"
+						  ]
+						}
+					  },
+					  "exclude": {
+						"resources": {
+						  "annotations": {
+							"singleton": "true"
 						  }
 						}
 					  },
 					  "validate": {
-						"message": "User pods must include an lifetime",
+						"message": "Replicasets require at least 2 replicas.",
 						"pattern": {
-						  "metadata": {
-							"annotations": {
-							  "pod.kubernetes.io/lifetime": "*?"
-							}
+						  "spec": {
+							"replicas": ">1"
 						  }
 						}
 					  }
@@ -143,6 +179,187 @@ func Test_validateUsingPolicyCRD(t *testing.T) {
 			  }
 		`),
 			errorDetail: "",
+			detail:      "Test: schema validation for spec.rules.exclude.resources.annotations",
+		},
+
+		{
+			rawPolicy: []byte(`
+			{
+				"apiVersion": "kyverno.io/v1",
+				"kind": "ClusterPolicy",
+				"metadata": {
+				  "name": "enforce-pod-name"
+				},
+				"spec": {
+				  "validationFailureAction": "audit",
+				  "background": true,
+				  "rules": [
+					{
+					  "name": "validate-name",
+					  "match": {
+						"resources": {
+						  "kinds": [
+							"Pod"
+						  ],
+						  "namespaceSelector": {
+							"matchLabels": {
+							  "app-namespace": "true"
+							}
+						  }
+						}
+					  },
+					  "validate": {
+						"message": "The Pod must end with -nginx",
+						"pattern": {
+						  "metadata": {
+							"name": "*-nginx"
+						  }
+						}
+					  }
+					}
+				  ]
+				}
+			  }
+		`),
+			errorDetail: "",
+			detail:      "Test: schema validation for spec.rules.match.resources.namespaceSelector.matchLabels",
+		},
+
+		{
+			rawPolicy: []byte(`
+			{
+				"apiVersion": "kyverno.io/v1",
+				"kind": "ClusterPolicy",
+				"metadata": {
+				  "name": "enforce-pod-name"
+				},
+				"spec": {
+				  "validationFailureAction": "audit",
+				  "background": true,
+				  "rules": [
+					{
+					  "name": "validate-name",
+					  "match": {
+						"resources": {
+						  "kinds": [
+							"Pod"
+						  ]
+						}
+					  },
+					  "exclude": {
+						"resources": {
+						  "namespaceSelector": {
+							"matchLabels": {
+							  "app-namespace": "true"
+							}
+						  }
+						}
+					  },
+					  "validate": {
+						"message": "The Pod must end with -nginx",
+						"pattern": {
+						  "metadata": {
+							"name": "*-nginx"
+						  }
+						}
+					  }
+					}
+				  ]
+				}
+			  }
+		`),
+			errorDetail: "",
+			detail:      "Test: schema validation for spec.rules.exclude.resources.namespaceSelector.matchLabels",
+		},
+
+		{
+			rawPolicy: []byte(`
+			{
+				"apiVersion": "kyverno.io/v1",
+				"kind": "ClusterPolicy",
+				"metadata": {
+				  "name": "enforce-pod-name"
+				},
+				"spec": {
+				  "validationFailureAction": "audit",
+				  "background": true,
+				  "rules": [
+					{
+					  "name": "validate-name",
+					  "match": {
+						"resources": {
+						  "kinds": [
+							"Pod"
+						  ],
+						  "selector": {
+							"matchLabels": {
+							  "app-namespace": "true"
+							}
+						  }
+						}
+					  },
+					  "validate": {
+						"message": "The Pod must end with -nginx",
+						"pattern": {
+						  "metadata": {
+							"name": "*-nginx"
+						  }
+						}
+					  }
+					}
+				  ]
+				}
+			  }
+		`),
+			errorDetail: "",
+			detail:      "Test: schema validation for spec.rules.match.resources.selector.matchLabels",
+		},
+
+		{
+			rawPolicy: []byte(`
+			{
+				"apiVersion": "kyverno.io/v1",
+				"kind": "ClusterPolicy",
+				"metadata": {
+				  "name": "enforce-pod-name"
+				},
+				"spec": {
+				  "validationFailureAction": "audit",
+				  "background": true,
+				  "rules": [
+					{
+					  "name": "validate-name",
+					  "match": {
+						"resources": {
+						  "kinds": [
+							"Pod"
+						  ]
+						}
+					  },
+					  "exclude": {
+						"resources": {
+						  "selector": {
+							"matchLabels": {
+							  "app-namespace": "true"
+							}
+						  }
+						}
+					  },
+					  "validate": {
+						"message": "The Pod must end with -nginx",
+						"pattern": {
+						  "metadata": {
+							"name": "*-nginx"
+						  }
+						}
+					  }
+					}
+				  ]
+				}
+			  }
+		`),
+			errorDetail: "",
+			detail:      "Test: schema validation for spec.rules.exclude.resources.selector.matchLabels",
 		},
 	}
 
@@ -155,7 +372,7 @@ func Test_validateUsingPolicyCRD(t *testing.T) {
 		assert.NilError(t, err)
 
 		_, errorList := validatePolicyAccordingToPolicyCRD(&policy, v1crd)
-		fmt.Println("errorList:  ", errorList)
+		fmt.Println(tc.detail)
 		for _, e := range errorList {
 			assert.Assert(t, tc.errorDetail == e.Detail)
 		}
