c1cfdb9a521a2e4c628ff61a69a70db0183b8a40
Shuting Zhao <shutting06@gmail.com>
2021-06-14 23:20:35 -0700

    add checks for registered webhook configs
    Signed-off-by: Shuting Zhao <shutting06@gmail.com>

--- scripts/verify-deployment.sh
+++ scripts/verify-deployment.sh
@@ -122,3 +122,13 @@ while [[ ${updated_replicas} -lt ${specified_replicas} || ${current_replicas} -g
 done
 
 echo "Deployment ${deployment} successful. All ${available_replicas} replicas are ready."
+
+mutatingwebhookconfigurations=$(kubectl get mutatingwebhookconfigurations | wc -l)
+validatingwebhookconfigurations=$(kubectl get validatingwebhookconfigurations | wc -l)
+while [[ ${mutatingwebhookconfigurations} -lt 4 || ${validatingwebhookconfigurations} -lt 3 ]]; do
+  sleep 5
+  echo "registed webhooks:"
+  kubectl get mutatingwebhookconfigurations,validatingwebhookconfigurations
+done
+
+echo "All webhooks are registered."
