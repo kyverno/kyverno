2a375fa1b5bb024877604c976d9a5ea3078ef35a
Max Goncharenko <goncharenko.maxim@apriorit.com>
2021-09-08 22:33:41 +0300

    Remove contains function (#2346)
    * remove contains function
    
    Signed-off-by: Maxim Goncharenko <goncharenko.maxim@apriorit.com>
    
    * added test for contains issue case
    
    Signed-off-by: Maxim Goncharenko <goncharenko.maxim@apriorit.com>

--- test/e2e/validate/config.go
+++ test/e2e/validate/config.go
@@ -1,7 +1,12 @@
 package validate
 
-// ValidateTests is E2E Test Config for validation
-var ValidateTests = []struct {
+import (
+	"github.com/kyverno/kyverno/test/e2e"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+// FluxValidateTests is E2E Test Config for validation
+var FluxValidateTests = []struct {
 	//TestName - Name of the Test
 	TestName string
 	// PolicyRaw - The Yaml file of the ClusterPolicy
@@ -26,5 +31,38 @@ var ValidateTests = []struct {
 		ResourceRaw:       kyverno_2241_FluxKustomization,
 		ResourceNamespace: "test-validate",
 		MustSucceed:       true,
+	},
+}
+
+var podGVR = e2e.GetGVR("", "v1", "pods")
+
+var ValidateTests = []struct {
+	//TestDescription - Description of the Test
+	TestDescription string
+	// PolicyName - Name of the Policy
+	PolicyName string
+	// PolicyRaw - The Yaml file of the ClusterPolicy
+	PolicyRaw []byte
+	// ResourceName - Name of the Resource
+	ResourceName string
+	// ResourceNamespace - Namespace of the Resource
+	ResourceNamespace string
+	// ResourceGVR - GVR of the Resource
+	ResourceGVR schema.GroupVersionResource
+	// ResourceRaw - The Yaml file of the ClusterPolicy
+	ResourceRaw []byte
+	// MustSucceed - indicates if validation must succeed
+	MustSucceed bool
+}{
+	{
+		// Case for https://github.com/kyverno/kyverno/issues/2345 issue
+		TestDescription:   "checks that contains function works properly with string list",
+		PolicyName:        "drop-cap-net-raw",
+		PolicyRaw:         kyverno_2345_policy,
+		ResourceName:      "test",
+		ResourceNamespace: "test-validate1",
+		ResourceGVR:       podGVR,
+		ResourceRaw:       kyverno_2345_resource,
+		MustSucceed:       false,
 	},
 }
