e6380f835ee074ed203c749e2e9843a41bfa3999
Marcel Mueller <marcel.mueller1@rwth-aachen.de>
2021-09-09 21:08:35 +0200

    Check Any and All ResourceFilters during policymutation
    Signed-off-by: Marcel Mueller <marcel.mueller1@rwth-aachen.de>

--- pkg/policymutation/policymutation.go
+++ pkg/policymutation/policymutation.go
@@ -470,8 +470,22 @@ func generateRuleForControllers(rule kyverno.Rule, controllers string, log logr.
 
 	match := rule.MatchResources
 	exclude := rule.ExcludeResources
-	if !utils.ContainsString(match.ResourceDescription.Kinds, "Pod") ||
-		(len(exclude.ResourceDescription.Kinds) != 0 && !utils.ContainsString(exclude.ResourceDescription.Kinds, "Pod")) {
+	matchResourceDescriptionsKinds := match.ResourceDescription.Kinds
+	for _, value := range match.All {
+		matchResourceDescriptionsKinds = append(matchResourceDescriptionsKinds, value.ResourceDescription.Kinds...)
+	}
+	for _, value := range match.Any {
+		matchResourceDescriptionsKinds = append(matchResourceDescriptionsKinds, value.ResourceDescription.Kinds...)
+	}
+	excludeResourceDescriptionsKinds := exclude.ResourceDescription.Kinds
+	for _, value := range exclude.All {
+		excludeResourceDescriptionsKinds = append(excludeResourceDescriptionsKinds, value.ResourceDescription.Kinds...)
+	}
+	for _, value := range exclude.Any {
+		excludeResourceDescriptionsKinds = append(excludeResourceDescriptionsKinds, value.ResourceDescription.Kinds...)
+	}
+	if !utils.ContainsString(matchResourceDescriptionsKinds, "Pod") ||
+		(len(excludeResourceDescriptionsKinds) != 0 && !utils.ContainsString(excludeResourceDescriptionsKinds, "Pod")) {
 		return kyvernoRule{}
 	}
 
