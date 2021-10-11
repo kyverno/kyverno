6b521870ab30f1c77c0f9d260e18a88b05511532
Marcel Mueller <marcel.mueller1@rwth-aachen.de>
2021-10-11 18:56:35 +0200

    Remove dead code in various packages
    Signed-off-by: Marcel Mueller <marcel.mueller1@rwth-aachen.de>

--- pkg/engine/mutate/utils.go
+++ pkg/engine/mutate/utils.go
@@ -33,20 +33,6 @@ func removeAnchor(key string) string {
 	return key
 }
 
-func getRawKeyIfWrappedWithAttributes(str string) string {
-	if len(str) < 2 {
-		return str
-	}
-
-	if str[0] == '(' && str[len(str)-1] == ')' {
-		return str[1 : len(str)-1]
-	} else if (str[0] == '$' || str[0] == '^' || str[0] == '+' || str[0] == '=') && (str[1] == '(' && str[len(str)-1] == ')') {
-		return str[2 : len(str)-1]
-	} else {
-		return str
-	}
-}
-
 // getAnchorAndElementsFromMap gets the condition anchor map and resource map without anchor
 func getAnchorAndElementsFromMap(anchorsMap map[string]interface{}) (map[string]interface{}, map[string]interface{}) {
 	anchors := make(map[string]interface{})
