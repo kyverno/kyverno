83aa35b97b62d132cb96d4241a399a38a38f93fa
Marcus Noble <AverageMarcus@users.noreply.github.com>
2021-10-12 15:25:56 +0100

    Update pkg/kyverno/common/regex.go
    Co-authored-by: Bricktop <marcel.mueller1@rwth-aachen.de>

--- pkg/kyverno/common/regex.go
+++ pkg/kyverno/common/regex.go
@@ -7,7 +7,7 @@ import (
 // RegexVariables represents regex for '{{}}'
 var RegexVariables = regexp.MustCompile(`\{\{[^{}]*\}\}`)
 
-// AllowedVariables represents regex for {{request.}}, {{serviceAccountName}}, {{serviceAccountNamespace}}, {{@}} and functions e.g. {{divide(<num>,<num>))}} (#2409)
+// AllowedVariables represents regex for {{request.}}, {{serviceAccountName}}, {{serviceAccountNamespace}}, {{@}} and functions e.g. {{divide(<num>,<num>))}} 
 var AllowedVariables = regexp.MustCompile(`\{\{\s*(request\.|serviceAccountName|serviceAccountNamespace|@|([a-z_]+\())[^{}]*\}\}`)
 
 // IsHttpRegex represents regex for starts with http:// or https://
