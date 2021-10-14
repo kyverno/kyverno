2089767c8547db793b249cebe011c6e551dc5e02
Vyankatesh Kudtarkar <vyankateshkd@gmail.com>
2021-10-14 12:45:32 +0530

    wildcard support (#2485)
    * wildcard support
    
    * remove console
    
    * fix issue
    
    * fix deny condition
    
    * fix comments
    
    * fix regex issue
    
    * remove UserInfo

--- pkg/kyverno/common/regex.go
+++ pkg/kyverno/common/regex.go
@@ -10,5 +10,8 @@ var RegexVariables = regexp.MustCompile(`\{\{[^{}]*\}\}`)
 // AllowedVariables represents regex for {{request.}}, {{serviceAccountName}}, {{serviceAccountNamespace}}, {{@}} and functions e.g. {{divide(<num>,<num>))}}
 var AllowedVariables = regexp.MustCompile(`\{\{\s*(request\.|serviceAccountName|serviceAccountNamespace|@|([a-z_]+\())[^{}]*\}\}`)
 
+// AllowedVariables represents regex for {{request.}}, {{serviceAccountName}}, {{serviceAccountNamespace}}
+var WildCardAllowedVariables = regexp.MustCompile(`\{\{\s*(request\.|serviceAccountName|serviceAccountNamespace)[^{}]*\}\}`)
+
 // IsHttpRegex represents regex for starts with http:// or https://
 var IsHttpRegex = regexp.MustCompile("^(http|https)://")
--- pkg/webhooks/server.go
+++ pkg/webhooks/server.go
@@ -298,10 +298,10 @@ func (ws *WebhookServer) resourceMutation(request *v1beta1.AdmissionRequest) *v1
 
 	logger.V(4).Info("received an admission request in mutating webhook")
 	requestTime := time.Now().Unix()
-
-	mutatePolicies := ws.pCache.GetPolicies(policycache.Mutate, request.Kind.Kind, request.Namespace)
-	generatePolicies := ws.pCache.GetPolicies(policycache.Generate, request.Kind.Kind, request.Namespace)
-	verifyImagesPolicies := ws.pCache.GetPolicies(policycache.VerifyImages, request.Kind.Kind, request.Namespace)
+	kind := request.Kind.Kind
+	mutatePolicies := ws.pCache.GetPolicies(policycache.Mutate, kind, request.Namespace)
+	generatePolicies := ws.pCache.GetPolicies(policycache.Generate, kind, request.Namespace)
+	verifyImagesPolicies := ws.pCache.GetPolicies(policycache.VerifyImages, kind, request.Namespace)
 
 	if len(mutatePolicies) == 0 && len(generatePolicies) == 0 && len(verifyImagesPolicies) == 0 {
 		logger.V(4).Info("no policies matched admission request")
@@ -488,7 +488,6 @@ func (ws *WebhookServer) resourceValidation(request *v1beta1.AdmissionRequest) *
 	if request.Operation == v1beta1.Delete {
 		ws.handleDelete(request)
 	}
-
 	if excludeKyvernoResources(request.Kind.Kind) {
 		return successResponse(nil)
 	}
@@ -496,10 +495,10 @@ func (ws *WebhookServer) resourceValidation(request *v1beta1.AdmissionRequest) *
 	logger.V(6).Info("received an admission request in validating webhook")
 	// timestamp at which this admission request got triggered
 	admissionRequestTimestamp := time.Now().Unix()
-
-	policies := ws.pCache.GetPolicies(policycache.ValidateEnforce, request.Kind.Kind, "")
+	kind := request.Kind.Kind
+	policies := ws.pCache.GetPolicies(policycache.ValidateEnforce, kind, "")
 	// Get namespace policies from the cache for the requested resource namespace
-	nsPolicies := ws.pCache.GetPolicies(policycache.ValidateEnforce, request.Kind.Kind, request.Namespace)
+	nsPolicies := ws.pCache.GetPolicies(policycache.ValidateEnforce, kind, request.Namespace)
 	policies = append(policies, nsPolicies...)
 
 	var roles, clusterRoles []string
