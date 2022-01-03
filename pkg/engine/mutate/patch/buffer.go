03a49b2996d7400a3fd378c7ed51b07ef0495e79
Jim Bugwadia <jim@nirmata.com>
2022-01-03 15:00:17 -0800

    add comments to strategicMergePatch buffer
    Signed-off-by: Jim Bugwadia <jim@nirmata.com>

--- pkg/engine/mutate/patch/buffer.go
+++ pkg/engine/mutate/patch/buffer.go
@@ -2,10 +2,13 @@ package patch
 
 import "bytes"
 
+// buffer is a wrapper around a slice of bytes used for JSON
+// marshal and unmarshal operations for a strategic merge patch
 type buffer struct {
 	*bytes.Buffer
 }
 
+// UnmarshalJSON writes the slice of bytes to an internal buffer
 func (buff buffer) UnmarshalJSON(b []byte) error {
 	buff.Reset()
 	_, err := buff.Write(b)
@@ -15,6 +18,8 @@ func (buff buffer) UnmarshalJSON(b []byte) error {
 	return nil
 }
 
+// MarshalJSON returns the buffered slice of bytes. The returned slice
+// is valid for use only until the next buffer modification.
 func (buff buffer) MarshalJSON() ([]byte, error) {
 	return buff.Bytes(), nil
 }
