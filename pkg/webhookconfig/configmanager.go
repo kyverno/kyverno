800a11ad1ce74a940bc56257ec37e9adeb16fd73
Charles-Edouard Brétéché <charled.breteche@gmail.com>
2022-03-02 11:54:34 +0100

    feat: add autogen controllers to policy status
    Signed-off-by: Charles-Edouard Brétéché <charled.breteche@gmail.com>

--- pkg/webhookconfig/configmanager.go
+++ pkg/webhookconfig/configmanager.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/go-logr/logr"
 	kyverno "github.com/kyverno/kyverno/api/kyverno/v1"
+	"github.com/kyverno/kyverno/pkg/autogen"
 	kyvernoclient "github.com/kyverno/kyverno/pkg/client/clientset/versioned"
 	kyvernoinformer "github.com/kyverno/kyverno/pkg/client/informers/externalversions/kyverno/v1"
 	kyvernolister "github.com/kyverno/kyverno/pkg/client/listers/kyverno/v1"
@@ -368,6 +369,8 @@ func (m *webhookConfigManager) reconcileWebhook(namespace, name string) error {
 	}
 
 	ready := true
+	// TODO: not sure it should be there or in the policy controller
+	autogenControllers := policy.Status.AutogenControllers
 	// build webhook only if auto-update is enabled, otherwise directly update status to ready
 	if m.autoUpdateWebhooks {
 		webhooks, err := m.buildWebhooks(namespace)
@@ -380,13 +383,15 @@ func (m *webhookConfigManager) reconcileWebhook(namespace, name string) error {
 			logger.Error(err, "failed to update webhook configurations for policy")
 		}
 
+		autogenControllers = autogen.GetControllers(policy.ObjectMeta, &policy.Spec, m.log)
+
 		// DELETION of the policy
 		if policy == nil {
 			return nil
 		}
 	}
 
-	if err := m.updateStatus(policy, ready); err != nil {
+	if err := m.updateStatus(policy, ready, autogenControllers); err != nil {
 		return errors.Wrapf(err, "failed to update policy status %s/%s", namespace, name)
 	}
 
@@ -677,9 +682,10 @@ func (m *webhookConfigManager) compareAndUpdateWebhook(webhookKind, webhookName 
 	return nil
 }
 
-func (m *webhookConfigManager) updateStatus(policy *kyverno.ClusterPolicy, status bool) error {
+func (m *webhookConfigManager) updateStatus(policy *kyverno.ClusterPolicy, status bool, autogenControllers []string) error {
 	policyCopy := policy.DeepCopy()
 	policyCopy.Status.Ready = status
+	policyCopy.Status.AutogenControllers = autogenControllers
 	if policy.GetNamespace() == "" {
 		_, err := m.kyvernoClient.KyvernoV1().ClusterPolicies().UpdateStatus(context.TODO(), policyCopy, v1.UpdateOptions{})
 		return err
