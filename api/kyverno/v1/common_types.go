8b00e7e52d67961baaf38689128d17b8eeeabca4
Charles-Edouard Brétéché <charled.breteche@gmail.com>
2022-03-11 14:26:12 +0100

    feat: add conditions support
    Signed-off-by: Charles-Edouard Brétéché <charled.breteche@gmail.com>

--- api/kyverno/v1/common_types.go
+++ api/kyverno/v1/common_types.go
@@ -7,6 +7,7 @@ import (
 	rbacv1 "k8s.io/api/rbac/v1"
 	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions"
 	apiextv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -808,15 +809,45 @@ type CloneFrom struct {
 	Name string `json:"name,omitempty" yaml:"name,omitempty"`
 }
 
+const (
+	// Ready means that the policy is ready
+	PolicyConditionReady = "Ready"
+)
+
+const (
+	// PolicyReasonSucceeded is the reason set when the policy is ready
+	PolicyReasonSucceeded = "Succeeded"
+	// PolicyReasonSucceeded is the reason set when the policy is not ready
+	PolicyReasonFailed = "Failed"
+)
+
 // PolicyStatus mostly contains runtime information related to policy execution.
 // Deprecated. Policy metrics are now available via the "/metrics" endpoint.
 // See: https://kyverno.io/docs/monitoring-kyverno-with-prometheus-metrics/
 type PolicyStatus struct {
 	// Ready indicates if the policy is ready to serve the admission request
 	Ready bool `json:"ready" yaml:"ready"`
+	// Conditions is a list of conditions that apply to the policy
+	// +optional
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
 	// Autogen contains autogen status information
 	// +optional
 	Autogen AutogenStatus `json:"autogen" yaml:"autogen"`
+}
+
+func (status *PolicyStatus) SetReady(ready bool) {
+	condition := metav1.Condition{
+		Type: PolicyConditionReady,
+	}
+	if ready {
+		condition.Status = metav1.ConditionTrue
+		condition.Reason = PolicyReasonSucceeded
+	} else {
+		condition.Status = metav1.ConditionFalse
+		condition.Reason = PolicyReasonFailed
+	}
+	status.Ready = ready
+	meta.SetStatusCondition(&status.Conditions, condition)
 }
 
 // AutogenStatus contains autogen status information.
