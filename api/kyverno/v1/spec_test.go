4ce5c972ee7ebd0f0f3c5e8311ed63780e9091bb
Charles-Edouard Brétéché <charled.breteche@gmail.com>
2022-03-17 18:02:35 +0100

    refactor: Policy name validation (#3409)
    * refactor: UserInfo validation
    
    Signed-off-by: Charles-Edouard Brétéché <charled.breteche@gmail.com>
    
    * refactor: Rule type validation
    
    Signed-off-by: Charles-Edouard Brétéché <charled.breteche@gmail.com>
    
    * refactor: Rule names validation
    
    Signed-off-by: Charles-Edouard Brétéché <charled.breteche@gmail.com>
    
    * refactor: Policy name validation
    
    Signed-off-by: Charles-Edouard Brétéché <charled.breteche@gmail.com>

--- api/kyverno/v1/spec_test.go
+++ api/kyverno/v1/spec_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"gotest.tools/assert"
+	apiextv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 )
 
@@ -18,6 +19,12 @@ func Test_Validate_UniqueRuleName(t *testing.T) {
 					},
 				},
 			},
+			Validation: Validation{
+				Message: "message",
+				RawAnyPattern: &apiextv1.JSON{
+					Raw: []byte("{"),
+				},
+			},
 		}, {
 			Name: "deny-privileged-disallowpriviligedescalation",
 			MatchResources: MatchResources{
@@ -27,11 +34,17 @@ func Test_Validate_UniqueRuleName(t *testing.T) {
 					},
 				},
 			},
+			Validation: Validation{
+				Message: "message",
+				RawAnyPattern: &apiextv1.JSON{
+					Raw: []byte("{"),
+				},
+			},
 		}},
 	}
 	path := field.NewPath("dummy")
 	errs := subject.Validate(path)
-	assert.Assert(t, len(errs) == 1)
+	assert.Equal(t, len(errs), 1)
 	assert.Equal(t, errs[0].Field, "dummy.rules[1].name")
 	assert.Equal(t, errs[0].Type, field.ErrorTypeInvalid)
 	assert.Equal(t, errs[0].Detail, "Duplicate rule name: 'deny-privileged-disallowpriviligedescalation'")
