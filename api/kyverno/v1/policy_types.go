bdcecf9882e9e188c9b5e0a3db69e6e8cc6f2271
Charles-Edouard Brétéché <charled.breteche@gmail.com>
2022-03-21 09:53:12 +0100

    refactor: ValidationFailureActionOverrides validation (#3421)
    Signed-off-by: Charles-Edouard Brétéché <charled.breteche@gmail.com>

--- api/kyverno/v1/policy_types.go
+++ api/kyverno/v1/policy_types.go
@@ -86,11 +86,13 @@ func (p *Policy) IsReady() bool {
 	return p.Status.IsReady()
 }
 
-// Validate implements programmatic validation
-func (p *Policy) Validate() field.ErrorList {
+// Validate implements programmatic validation.
+// namespaced means that the policy is bound to a namespace and therefore
+// should not filter/generate cluster wide resources.
+func (p *Policy) Validate(namespaced bool) field.ErrorList {
 	var errs field.ErrorList
 	errs = append(errs, ValidatePolicyName(field.NewPath("name"), p.Name)...)
-	errs = append(errs, p.Spec.Validate(field.NewPath("spec"))...)
+	errs = append(errs, p.Spec.Validate(field.NewPath("spec"), namespaced)...)
 	return errs
 }
 
