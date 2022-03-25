c17d35a443e31dd8561d6216e245bb4524bdfb7a
Mritunjay Kumar Sharma <mritunjaysharma394@gmail.com>
2022-03-25 19:12:01 +0530

    adds lease objects for storing last-request-time and set-status annotations in deployment (#3447)
    * funcs to patch last request time and status
    
    Signed-off-by: Mritunjay Sharma <mritunjaysharma394@gmail.com>
    
    * instead of patch, updating status
    
    Signed-off-by: Mritunjay Sharma <mritunjaysharma394@gmail.com>
    
    * added lease object appraoch
    
    Signed-off-by: Mritunjay Sharma <mritunjaysharma394@gmail.com>
    
    * cleanup
    
    Signed-off-by: Mritunjay Sharma <mritunjaysharma394@gmail.com>
    
    * attempt to solve panic issue
    
    Signed-off-by: Mritunjay Sharma <mritunjaysharma394@gmail.com>
    
    * fixes lease updates for both annotations
    
    Signed-off-by: Mritunjay Sharma <mritunjaysharma394@gmail.com>
    
    * minor cleanups in log messages
    
    Signed-off-by: Mritunjay Sharma <mritunjaysharma394@gmail.com>
    
    * clean up
    
    Signed-off-by: Mritunjay Sharma <mritunjaysharma394@gmail.com>
    
    * add object selector
    
    Signed-off-by: Mritunjay Sharma <mritunjaysharma394@gmail.com>
    
    * fixed leases and object selector
    
    Signed-off-by: Mritunjay Sharma <mritunjaysharma394@gmail.com>

--- pkg/webhookconfig/monitor.go
+++ pkg/webhookconfig/monitor.go
@@ -1,16 +1,19 @@
 package webhookconfig
 
 import (
+	"context"
 	"fmt"
 	"sync"
 	"time"
 
 	"github.com/go-logr/logr"
+	"github.com/kyverno/kyverno/pkg/config"
 	"github.com/kyverno/kyverno/pkg/event"
 	"github.com/kyverno/kyverno/pkg/tls"
 	"github.com/pkg/errors"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	coordinationv1 "k8s.io/client-go/kubernetes/typed/coordination/v1"
 )
 
 //maxRetryCount defines the max deadline count
@@ -39,6 +42,9 @@ const (
 // not compare other details like the webhook settings.
 //
 type Monitor struct {
+	// leaseClient is used to manage Kyverno lease
+	leaseClient coordinationv1.LeaseInterface
+
 	// lastSeenRequestTime records the timestamp
 	// of the latest received admission request
 	lastSeenRequestTime time.Time
@@ -50,6 +56,7 @@ type Monitor struct {
 // NewMonitor returns a new instance of webhook monitor
 func NewMonitor(kubeClient kubernetes.Interface, log logr.Logger) (*Monitor, error) {
 	monitor := &Monitor{
+		leaseClient:         kubeClient.CoordinationV1().Leases(config.KyvernoNamespace),
 		lastSeenRequestTime: time.Now(),
 		log:                 log,
 	}
@@ -76,8 +83,8 @@ func (t *Monitor) SetTime(tm time.Time) {
 func (t *Monitor) Run(register *Register, certRenewer *tls.CertRenewer, eventGen event.Interface, stopCh <-chan struct{}) {
 	logger := t.log.WithName("webhookMonitor")
 
-	logger.V(4).Info("starting webhook monitor", "interval", idleCheckInterval.String())
-	status := newStatusControl(register, eventGen, t.log.WithName("WebhookStatusControl"))
+	logger.V(3).Info("starting webhook monitor", "interval", idleCheckInterval.String())
+	status := newStatusControl(t.leaseClient, eventGen, logger.WithName("WebhookStatusControl"))
 
 	ticker := time.NewTicker(tickerInterval)
 	defer ticker.Stop()
@@ -113,7 +120,7 @@ func (t *Monitor) Run(register *Register, certRenewer *tls.CertRenewer, eventGen
 			}
 
 			timeDiff := time.Since(t.Time())
-			lastRequestTimeFromAnn := lastRequestTimeFromAnnotation(register, t.log.WithName("lastRequestTimeFromAnnotation"))
+			lastRequestTimeFromAnn := lastRequestTimeFromAnnotation(t.leaseClient, t.log.WithName("lastRequestTimeFromAnnotation"))
 			if lastRequestTimeFromAnn == nil {
 				if err := status.UpdateLastRequestTimestmap(t.Time()); err != nil {
 					logger.Error(err, "failed to annotate deployment for lastRequestTime")
@@ -188,27 +195,22 @@ func registerWebhookIfNotPresent(register *Register, logger logr.Logger) error {
 	return nil
 }
 
-func lastRequestTimeFromAnnotation(register *Register, logger logr.Logger) *time.Time {
-	_, deploy, err := register.GetKubePolicyDeployment()
+func lastRequestTimeFromAnnotation(leaseClient coordinationv1.LeaseInterface, logger logr.Logger) *time.Time {
+
+	lease, err := leaseClient.Get(context.TODO(), "kyverno", metav1.GetOptions{})
 	if err != nil {
-		logger.Info("unable to get Kyverno deployment", "reason", err.Error())
-		return nil
+		logger.Info("Lease 'kyverno' not found. Starting clean-up...")
 	}
 
-	timeStamp, ok, err := unstructured.NestedString(deploy.UnstructuredContent(), "metadata", "annotations", annLastRequestTime)
-	if err != nil {
-		logger.Info("unable to get annotation", "reason", err.Error())
-		return nil
-	}
-
-	if !ok {
+	timeStamp := lease.GetAnnotations()
+	if timeStamp == nil {
 		logger.Info("timestamp not set in the annotation, setting")
 		return nil
 	}
 
-	annTime, err := time.Parse(time.RFC3339, timeStamp)
+	annTime, err := time.Parse(time.RFC3339, timeStamp[annLastRequestTime])
 	if err != nil {
-		logger.Error(err, "failed to parse timestamp annotation", "timeStamp", timeStamp)
+		logger.Error(err, "failed to parse timestamp annotation", "timeStamp", timeStamp[annLastRequestTime])
 		return nil
 	}
 
