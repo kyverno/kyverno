66aac0abb219b8ea4eb16884ba5aeceb79011bca
Prateek Pandey <prateek.pandey@nirmata.com>
2022-03-29 17:57:21 +0530

    feat: generate support for namespace policy (#3498)
    * feat: generate support for namespace policy
    
    Signed-off-by: prateekpandey14 <prateek.pandey@nirmata.com>
    
    * use policy spec instead
    
    Signed-off-by: prateekpandey14 <prateek.pandey@nirmata.com>
    
    * refactor the changes as per the 1.6 branch
    
    Signed-off-by: prateekpandey14 <prateek.pandey@nirmata.com>
    Signed-off-by: ShutingZhao <shuting@nirmata.com>

--- pkg/engine/generation.go
+++ pkg/engine/generation.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/kyverno/kyverno/pkg/engine/common"
+	"k8s.io/client-go/tools/cache"
 
 	kyverno "github.com/kyverno/kyverno/api/kyverno/v1"
 	"github.com/kyverno/kyverno/pkg/engine/response"
@@ -18,6 +19,54 @@ import (
 func Generate(policyContext *PolicyContext) (resp *response.EngineResponse) {
 	policyStartTime := time.Now()
 	return filterRules(policyContext, policyStartTime)
+}
+
+// GenerateResponse checks for validity of generate rule on the resource
+func GenerateResponse(policyContext *PolicyContext, gr kyverno.GenerateRequest) (resp *response.EngineResponse) {
+	policyStartTime := time.Now()
+	return filterGenerateRules(policyContext, gr.Spec.Policy, policyStartTime)
+}
+
+func filterGenerateRules(policyContext *PolicyContext, policyNameKey string, startTime time.Time) *response.EngineResponse {
+	kind := policyContext.NewResource.GetKind()
+	name := policyContext.NewResource.GetName()
+	namespace := policyContext.NewResource.GetNamespace()
+	apiVersion := policyContext.NewResource.GetAPIVersion()
+	pNamespace, pName, err := cache.SplitMetaNamespaceKey(policyNameKey)
+	if err != nil {
+		log.Log.Error(err, "failed to spilt name and namespace", policyNameKey)
+	}
+
+	resp := &response.EngineResponse{
+		PolicyResponse: response.PolicyResponse{
+			Policy: response.PolicySpec{
+				Name:      pName,
+				Namespace: pNamespace,
+			},
+			PolicyStats: response.PolicyStats{
+				PolicyExecutionTimestamp: startTime.Unix(),
+			},
+			Resource: response.ResourceSpec{
+				Kind:       kind,
+				Name:       name,
+				Namespace:  namespace,
+				APIVersion: apiVersion,
+			},
+		},
+	}
+
+	if policyContext.ExcludeResourceFunc(kind, namespace, name) {
+		log.Log.WithName("Generate").Info("resource excluded", "kind", kind, "namespace", namespace, "name", name)
+		return resp
+	}
+
+	for _, rule := range policyContext.Policy.Spec.Rules {
+		if ruleResp := filterRule(rule, policyContext); ruleResp != nil {
+			resp.PolicyResponse.Rules = append(resp.PolicyResponse.Rules, *ruleResp)
+		}
+	}
+
+	return resp
 }
 
 func filterRules(policyContext *PolicyContext, startTime time.Time) *response.EngineResponse {
