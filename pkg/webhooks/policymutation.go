9fc65fa5a7bc2721d1d5e79fd0ea49718ed264c9
Charles-Edouard Brétéché <charled.breteche@gmail.com>
2022-03-31 14:25:54 +0200

    refactor: use policy interface and introduce admission utils package (#3512)
    * refactor: use more policy interface
    
    Signed-off-by: Charles-Edouard Brétéché <charled.breteche@gmail.com>
    
    * refactor: migrate to policy interface
    
    Signed-off-by: Charles-Edouard Brétéché <charled.breteche@gmail.com>

--- pkg/webhooks/policymutation.go
+++ pkg/webhooks/policymutation.go
@@ -1,97 +1,47 @@
 package webhooks
 
 import (
-	"encoding/json"
 	"fmt"
 	"reflect"
 	"strings"
 	"time"
 
-	logr "github.com/go-logr/logr"
 	kyverno "github.com/kyverno/kyverno/api/kyverno/v1"
 	"github.com/kyverno/kyverno/pkg/policymutation"
+	admissionutils "github.com/kyverno/kyverno/pkg/utils/admission"
 	v1beta1 "k8s.io/api/admission/v1beta1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func (ws *WebhookServer) policyMutation(request *v1beta1.AdmissionRequest) *v1beta1.AdmissionResponse {
 	logger := ws.log.WithValues("action", "policy mutation", "uid", request.UID, "kind", request.Kind, "namespace", request.Namespace, "name", request.Name, "operation", request.Operation, "gvk", request.Kind.String())
-	var policy *kyverno.ClusterPolicy
-	raw := request.Object.Raw
-
-	if err := json.Unmarshal(raw, &policy); err != nil {
-		logger.Error(err, "failed to unmarshal policy admission request")
-		return &v1beta1.AdmissionResponse{
-			Allowed: true,
-			Result: &metav1.Status{
-				Message: fmt.Sprintf("failed to default value, check kyverno controller logs for details: %v", err),
-			},
-		}
+	policy, oldPolicy, err := admissionutils.GetPolicies(request)
+	if err != nil {
+		logger.Error(err, "failed to unmarshal policies from admission request")
+		return admissionutils.ResponseWithMessage(true, fmt.Sprintf("failed to default value, check kyverno controller logs for details: %v", err))
 	}
-
-	if request.Operation == v1beta1.Update {
-		admissionResponse := hasPolicyChanged(policy, request.OldObject.Raw, logger)
-		if admissionResponse != nil {
-			logger.V(4).Info("skip policy mutation on status update")
-			return admissionResponse
-		}
+	if oldPolicy != nil && isStatusUpdate(oldPolicy, policy) {
+		logger.V(4).Info("skip policy mutation on status update")
+		return admissionutils.Response(true)
 	}
-
 	startTime := time.Now()
 	logger.V(3).Info("start policy change mutation")
 	defer logger.V(3).Info("finished policy change mutation", "time", time.Since(startTime).String())
-
 	// Generate JSON Patches for defaults
-	patches, updateMsgs := policymutation.GenerateJSONPatchesForDefaults(policy, logger)
-	if len(patches) != 0 {
-		patchType := v1beta1.PatchTypeJSONPatch
-		return &v1beta1.AdmissionResponse{
-			Allowed: true,
-			Result: &metav1.Status{
-				Message: strings.Join(updateMsgs, "'"),
-			},
-			Patch:     patches,
-			PatchType: &patchType,
-		}
+	if patches, updateMsgs := policymutation.GenerateJSONPatchesForDefaults(policy, logger); len(patches) != 0 {
+		return admissionutils.ResponseWithMessageAndPatch(true, strings.Join(updateMsgs, "'"), patches)
 	}
-
-	return &v1beta1.AdmissionResponse{
-		Allowed: true,
-	}
+	return admissionutils.Response(true)
 }
 
-func hasPolicyChanged(policy *kyverno.ClusterPolicy, oldRaw []byte, logger logr.Logger) *v1beta1.AdmissionResponse {
-	var oldPolicy *kyverno.ClusterPolicy
-	if err := json.Unmarshal(oldRaw, &oldPolicy); err != nil {
-		logger.Error(err, "failed to unmarshal old policy admission request")
-		return &v1beta1.AdmissionResponse{
-			Allowed: true,
-			Result: &metav1.Status{
-				Message: fmt.Sprintf("failed to validate policy, check kyverno controller logs for details: %v", err),
-			},
-		}
-
-	}
-
-	if isStatusUpdate(oldPolicy, policy) {
-		return &v1beta1.AdmissionResponse{Allowed: true}
-	}
-
-	return nil
-}
-
-func isStatusUpdate(old, new *kyverno.ClusterPolicy) bool {
+func isStatusUpdate(old, new kyverno.PolicyInterface) bool {
 	if !reflect.DeepEqual(old.GetAnnotations(), new.GetAnnotations()) {
 		return false
 	}
-
 	if !reflect.DeepEqual(old.GetLabels(), new.GetLabels()) {
 		return false
 	}
-
-	if !reflect.DeepEqual(old.Spec, new.Spec) {
+	if !reflect.DeepEqual(old.GetSpec(), new.GetSpec()) {
 		return false
 	}
-
 	return true
 }
