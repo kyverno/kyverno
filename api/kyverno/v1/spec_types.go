a388583d08a80fb4bbd446f987a9dfb07629f580
Charles-Edouard Brétéché <charled.breteche@gmail.com>
2022-04-04 15:17:04 +0200

    refactor: use GetFailurePolicy method
    Signed-off-by: Charles-Edouard Brétéché <charled.breteche@gmail.com>

--- api/kyverno/v1/spec_types.go
+++ api/kyverno/v1/spec_types.go
@@ -132,6 +132,14 @@ func (s *Spec) BackgroundProcessingEnabled() bool {
 	return *s.Background
 }
 
+// GetFailurePolicy returns the failure policy to be applied
+func (s *Spec) GetFailurePolicy() FailurePolicyType {
+	if s.FailurePolicy == nil {
+		return Fail
+	}
+	return *s.FailurePolicy
+}
+
 // ValidateRuleNames checks if the rule names are unique across a policy
 func (s *Spec) ValidateRuleNames(path *field.Path) (errs field.ErrorList) {
 	names := sets.NewString()
