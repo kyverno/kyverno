374117e6a291bcacb25ad170f8fa655a6cb14f0c
Jim Bugwadia <jim@nirmata.com>
2022-04-12 11:52:33 -0700

    update msg
    Signed-off-by: Jim Bugwadia <jim@nirmata.com>

--- pkg/policy/validate.go
+++ pkg/policy/validate.go
@@ -238,9 +238,9 @@ func Validate(policy kyverno.PolicyInterface, client *dclient.Client, mock bool,
 		var podOnlyMap = make(map[string]bool) //Validate that Kind is only Pod
 		podOnlyMap["Pod"] = true
 		if reflect.DeepEqual(common.GetKindsFromRule(rule), podOnlyMap) && podControllerAutoGenExclusion(policy) {
-			msg := "Policies that match a pod apply to all pods including pods managed by controllers excluded " +
-				"from autogen. Use preconditions to exclude the pods managed by excluded controllers. See: " +
-				"https://kyverno.io/docs/writing-policies/autogen/ for more information."
+			msg := "Policies that match Pods apply to all Pods including those managed by controllers excluded " +
+				"in the autogen annotation. Use preconditions to exclude Pods managed controllers excluded in " +
+				"the autogen annotation. Refer to https://kyverno.io/docs/writing-policies/autogen/ for details."
 
 			return &admissionv1.AdmissionResponse{
 				Allowed:  true,
