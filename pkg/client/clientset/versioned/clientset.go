2482ee9747ad4706e1453cbec65d97523a5ade8e
ShutingZhao <shuting@nirmata.com>
2022-04-18 16:18:51 +0800

    fix v1beta1 client registration
    Signed-off-by: ShutingZhao <shuting@nirmata.com>

--- pkg/client/clientset/versioned/clientset.go
+++ pkg/client/clientset/versioned/clientset.go
@@ -53,7 +53,7 @@ func (c *Clientset) KyvernoV1() kyvernov1.KyvernoV1Interface {
 	return c.kyvernoV1
 }
 
-// KyvernoV1 retrieves the KyvernoV1Client
+// KyvernoV1beta1 retrieves the KyvernoV1beta1Client
 func (c *Clientset) KyvernoV1beta1() kyvernov1beta1.KyvernoV1beta1Interface {
 	return c.kyvernoV1beta1
 }
