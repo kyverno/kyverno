570e96d648c27d6e6feeb231853f05cbfa2c8147
Vyankatesh Kudtarkar <vyankateshkd@gmail.com>
2022-04-19 17:29:27 +0530

    add  RWMutex  lock

--- pkg/policycache/policy_cache.go
+++ pkg/policycache/policy_cache.go
@@ -87,6 +87,8 @@ func (pc *policyCache) remove(p kyverno.PolicyInterface) {
 }
 
 func (pc *policyCache) update(oldP kyverno.PolicyInterface, newP kyverno.PolicyInterface) {
+	pc.pMap.lock.Lock()
+	defer pc.pMap.lock.Unlock()
 	pc.pMap.remove(oldP)
 	pc.pMap.add(newP)
 	pc.logger.V(4).Info("policy is updated from cache", "name", newP.GetName())
