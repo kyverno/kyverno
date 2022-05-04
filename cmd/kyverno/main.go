5be6a4e2b0a453bc474661484c2c5c3ba0a447f4
Prateek Nandle <56027872+Prateeknandle@users.noreply.github.com>
2022-05-05 00:41:21 +0530

    removed kubeconfig flags (#3744)
    Signed-off-by: Prateeknandle <prateeknandle@gmail.com>
    
    Co-authored-by: Vyankatesh Kudtarkar <vyankateshkd@gmail.com>
    Co-authored-by: Charles-Edouard Brétéché <charled.breteche@gmail.com>

--- cmd/kyverno/main.go
+++ cmd/kyverno/main.go
@@ -74,7 +74,6 @@ func main() {
 	log.SetLogger(klogr.New().WithCallDepth(1))
 	flag.IntVar(&webhookTimeout, "webhookTimeout", int(webhookconfig.DefaultWebhookTimeout), "Timeout for webhook configurations.")
 	flag.IntVar(&genWorkers, "genWorkers", 10, "Workers for generate controller")
-	flag.StringVar(&kubeconfig, "kubeconfig", "", "Path to a kubeconfig. Only required if out-of-cluster.")
 	flag.StringVar(&serverIP, "serverIP", "", "IP address where Kyverno controller runs. Only required if out-of-cluster.")
 	flag.BoolVar(&profile, "profile", false, "Set this flag to 'true', to enable profiling.")
 	flag.StringVar(&profilePort, "profilePort", "6060", "Enable profiling at given port, defaults to 6060.")
@@ -98,6 +97,7 @@ func main() {
 
 	cleanUp := make(chan struct{})
 	stopCh := signal.SetupSignalHandler()
+	kubeconfig = ""
 	debug := serverIP != ""
 
 	// clients
