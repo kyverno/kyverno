2df63d039ff1ebbdf00603f3538137ff38feb2f3
Jim Bugwadia <jim@nirmata.com>
2022-05-11 10:08:50 -0700

    increase timeout to 30s to match webhook timeout
    Signed-off-by: Jim Bugwadia <jim@nirmata.com>

--- pkg/webhooks/server.go
+++ pkg/webhooks/server.go
@@ -170,8 +170,8 @@ func NewWebhookServer(
 			MinVersion: tls.VersionTLS12,
 		},
 		Handler:      mux,
-		ReadTimeout:  15 * time.Second,
-		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 30 * time.Second,
 	}
 	return ws, nil
 }
