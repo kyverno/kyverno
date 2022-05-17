000c90d424224210db841f1a7e6481a85dcc96af
Prateek Pandey <prateek.pandey@nirmata.com>
2022-05-17 10:29:11 +0530

    fix: use patch to update handler status in UR (#3927)
    Signed-off-by: prateekpandey14 <prateek.pandey@nirmata.com>

--- pkg/background/common/util.go
+++ pkg/background/common/util.go
@@ -2,6 +2,7 @@ package common
 
 import (
 	"context"
+	"time"
 
 	urkyverno "github.com/kyverno/kyverno/api/kyverno/v1beta1"
 	kyvernoclient "github.com/kyverno/kyverno/pkg/client/clientset/versioned"
@@ -9,7 +10,15 @@ import (
 	jsonutils "github.com/kyverno/kyverno/pkg/utils/json"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/wait"
 )
+
+var DefaultRetry = wait.Backoff{
+	Steps:    5,
+	Duration: 30 * time.Millisecond,
+	Factor:   1.0,
+	Jitter:   0.1,
+}
 
 // PatchUpdateRequest patches a update request object
 func PatchUpdateRequest(ur *urkyverno.UpdateRequest, patch jsonutils.Patch, client kyvernoclient.Interface, subresources ...string) (*urkyverno.UpdateRequest, error) {
