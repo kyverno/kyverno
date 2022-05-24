f29afbab352becd86b6c8e55ab670b652084963f
Jim Bugwadia <jim@nirmata.com>
2022-05-23 21:21:20 -0700

    make codegen
    Signed-off-by: Jim Bugwadia <jim@nirmata.com>

--- api/policyreport/v1alpha2/zz_generated.deepcopy.go
+++ api/policyreport/v1alpha2/zz_generated.deepcopy.go
@@ -20,7 +20,7 @@ limitations under the License.
 package v1alpha2
 
 import (
-	v1 "k8s.io/api/core/v1"
+	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 )
