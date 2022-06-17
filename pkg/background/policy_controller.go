55a7b55c62900ace41543090b3ef8be5ed8a7857
anushkamittal20 <anumittal4641@gmail.com>
2022-06-17 14:49:04 +0530

    Modified policy_controller in background pkg
    Signed-off-by: anushkamittal20 <anumittal4641@gmail.com>

--- pkg/background/policy_controller.go
+++ pkg/background/policy_controller.go
@@ -2,7 +2,8 @@ package background
 
 import (
 	kubeutils "github.com/kyverno/kyverno/pkg/utils/kube"
-	//"k8s.io/apimachinery/pkg/api/errors"
+
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/client-go/tools/cache"
 )
 
@@ -16,6 +17,9 @@ func (c *controller) addPolicy(obj interface{}) {
 		if err != nil {
 			logger.Error(err, "failed to list update requests for policy", "key", key)
 			return
+		}
+		if c.sync(key) != nil {
+			// how to deal with errors here if necessary?
 		}
 		// re-evaluate the UR as the policy was added
 		for _, ur := range urs {
@@ -34,6 +38,9 @@ func (c *controller) updatePolicy(_, obj interface{}) {
 		if err != nil {
 			logger.Error(err, "failed to list update requests for policy", "key", key)
 			return
+		}
+		if c.sync(key) != nil {
+			// how to deal with errors here if necessary?
 		}
 		// re-evaluate the UR as the policy was updated
 		for _, ur := range urs {
@@ -54,6 +61,9 @@ func (c *controller) deletePolicy(obj interface{}) {
 			logger.Error(err, "failed to list update requests for policy", "key", key)
 			return
 		}
+		if c.sync(key) != nil {
+			// how to deal with errors here if necessary?
+		}
 		// re-evaluate the UR as the policy was updated
 		for _, ur := range urs {
 			c.enqueueUpdateRequest(ur)
@@ -61,20 +71,18 @@ func (c *controller) deletePolicy(obj interface{}) {
 	}
 }
 
-// func sync(err error) {
-// 	if err != nil {
-// 		if errors.IsNotFound(err) {
-// 			// here only takes care of mutateExisting policies
-// 			// generate cleanup controller handles policy deletion
-// 			mutateURs := pc.listMutateURs(key, nil)
-// 			deleteUR(pc.kyvernoClient, key, mutateURs, logger)
-// 			return nil
-// 		}
-// 		return err
-// 	} else {
-// 		err = pc.updateURs(key, policy)
-// 		if err != nil {
-// 			logger.Error(err, "failed to updateUR on Policy update")
-// 		}
-// 	}
-// }
+func (c *controller) sync(key string) error {
+	policy, err := c.getPolicy(key)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return nil
+		}
+		return err
+	} else {
+		err = c.updateURs(key, policy)
+		if err != nil {
+			logger.Error(err, "failed to updateUR on Policy update")
+		}
+		return nil
+	}
+}
