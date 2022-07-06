aa4547a4f68fc76cfa1af65e5ae4dd23b0c804cf
Hyokil KIM <treqh@outlook.com>
2022-07-06 16:08:57 +0200

    test for HostPorts control

--- pkg/pss/evaluate.go
+++ pkg/pss/evaluate.go
@@ -28,12 +28,11 @@ func EvaluatePSS(lv api.LevelVersion, podMetadata *metav1.ObjectMeta, podSpec *c
 		}
 
 		// check version
-
 		for _, versionCheck := range check.Versions {
 			res := versionCheck.CheckPod(podMetadata, podSpec)
 			// fmt.Printf("%v, res: %v\n", versionCheck, res)
 			if !res.Allowed {
-				fmt.Printf("check error: %v\n", res)
+				fmt.Printf("[Check Error]: %v\n", res)
 				results = append(results, res)
 			}
 		}
@@ -62,11 +61,13 @@ func ExemptProfile(rule *v1.PodSecurity, podSpec *corev1.PodSpec, podObjectMeta 
 			}
 		}
 
-		fmt.Printf("Restricted Field: %v\n", exclude.RestrictedField)
 		value, err := ctx.Query(exclude.RestrictedField)
 		if err != nil {
 			return false, errors.Wrap(err, fmt.Sprintf("failed to query value with the given RestrictedField %s", exclude.RestrictedField))
 		}
+
+		fmt.Printf("[Exclude]: %v\n", exclude)
+		fmt.Printf("[Restricted Field Value]: %v\n", value)
 
 		if !allowedValues(value, exclude) {
 			return false, nil
@@ -86,6 +87,14 @@ func imagesMatched(podSpec *corev1.PodSpec, images []string) bool {
 	return false
 }
 
+// default setting of the encoding/json decoder when unmarshaling JSON numbers into interface{} values.
+// -----------------------------------------
+// JSON booleans: bool
+// JSON numbers: float64
+// JSON strings: string
+// JSON arrays: []interface{}
+// JSON objects: map[string]interface{}
+// JSON null: nil
 func allowedValues(resourceValue interface{}, exclude *v1.PodSecurityStandard) bool {
 	// When resourceValue is a bool (Host Namespaces control)
 	if reflect.TypeOf(resourceValue).Kind() == reflect.Bool {
@@ -98,26 +107,32 @@ func allowedValues(resourceValue interface{}, exclude *v1.PodSecurityStandard) b
 
 	excludeValues := resourceValue.([]interface{})
 
-	fmt.Println(excludeValues)
-
-	for k, values := range excludeValues {
+	for _, values := range excludeValues {
 		rt := reflect.TypeOf(values)
 		kind := rt.Kind()
 
 		if kind == reflect.Slice {
-			fmt.Println(k, "is a slice with element type", rt.Elem())
+			fmt.Println(values, "is a slice with element type", rt.Elem())
 			for _, value := range values.([]interface{}) {
 				fmt.Printf("value: %s\n", value)
-				fmt.Printf("exclude values %v,  value: %v\n", exclude.Values, value)
-				if !utils.ContainsString(exclude.Values, value.(string)) {
-					return false
+
+				// Check value type
+				fmt.Printf("type: %s\n", reflect.TypeOf(value).Kind())
+				if reflect.TypeOf(value).Kind() == reflect.Float64 {
+					fmt.Println(fmt.Sprint((value.(float64))))
+					if !utils.ContainsString(exclude.Values, fmt.Sprint((value.(float64)))) {
+						return false
+					}
+				} else if reflect.TypeOf(value).Kind() == reflect.String {
+					fmt.Printf("exclude values %v,  value: %v\n", exclude.Values, value)
+					if !utils.ContainsString(exclude.Values, value.(string)) {
+						return false
+					}
 				}
 			}
-		} else if kind == reflect.Array {
-			fmt.Println(k, "is an array with element type", rt.Elem())
 		} else if kind == reflect.Map {
 			// For Volume Types control
-			fmt.Println("is a map with element type", rt.Elem())
+			fmt.Println(values, "is a map with element type", rt.Elem())
 			for key, value := range values.(map[string]interface{}) {
 				// `Volume`` has 2 fields: `Name` and a `Volume Source` (inline json)
 				// Ignore `Name` field because we want to look at `Volume Source`'s key
@@ -131,7 +146,7 @@ func allowedValues(resourceValue interface{}, exclude *v1.PodSecurityStandard) b
 				}
 			}
 		} else {
-			fmt.Println(k, "is something else entirely")
+			fmt.Println(values, "is something else entirely")
 		}
 	}
 	return true
