c40bef6a4cb740554a80740a56fe17c17e91ea85
Jim Bugwadia <jim@nirmata.com>
2022-07-10 13:31:48 -0700

    fix patch join
    Signed-off-by: Jim Bugwadia <jim@nirmata.com>

--- pkg/utils/json/utils.go
+++ pkg/utils/json/utils.go
@@ -1,18 +1,38 @@
 package json
 
+import (
+	"regexp"
+	"strings"
+)
+
+var space = regexp.MustCompile(`\s+`)
+
 // JoinPatches joins array of serialized JSON patches to the single JSONPatch array
+// It accepts patch operations and patches (arrays of patch operations) and returns
+// a single combined patch.
 func JoinPatches(patches ...[]byte) []byte {
-	var result []byte
 	if len(patches) == 0 {
-		return result
+		return nil
 	}
-	result = append(result, []byte("[\n")...)
-	for index, patch := range patches {
-		result = append(result, patch...)
-		if index != len(patches)-1 {
-			result = append(result, []byte(",\n")...)
+
+	var patchOperations []string
+	for _, patch := range patches {
+		patch = space.ReplaceAll(patch, []byte(""))
+		if len(patch) == 0 {
+			continue
 		}
+
+		if string(patch[0]) == "[" {
+			patch = patch[1 : len(patch)-1]
+		}
+
+		patchOperations = append(patchOperations, string(patch))
 	}
-	result = append(result, []byte("\n]")...)
-	return result
+
+	if len(patchOperations) == 0 {
+		return nil
+	}
+
+	result := "[" + strings.Join(patchOperations, ",\n") + "]"
+	return []byte(result)
 }
