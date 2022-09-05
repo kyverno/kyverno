7e074746386fe110c0b492bda8af3913c1533988
Charles-Edouard Brétéché <charled.breteche@gmail.com>
2022-09-05 14:50:10 +0200

    fix linters
    Signed-off-by: Charles-Edouard Brétéché <charled.breteche@gmail.com>

--- pkg/webhooks/resource/generation.go
+++ pkg/webhooks/resource/generation.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/go-logr/logr"
 	kyvernov1beta1 "github.com/kyverno/kyverno/api/kyverno/v1beta1"
-
 	// kyvernov2beta1 "github.com/kyverno/kyverno/api/kyverno/v2beta1"
 	kyvernov2beta1 "github.com/kyverno/kyverno/api/kyverno/v2beta1"
 	"github.com/kyverno/kyverno/pkg/autogen"
