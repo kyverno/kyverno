f0622a8a3b0bcdeb9b6784871cadc4b5fcc47264
Charles-Edouard Brétéché <charled.breteche@gmail.com>
2022-09-07 14:56:38 +0200

    refactor: use generics in client wrappers (#4525)
    Signed-off-by: Charles-Edouard Brétéché <charled.breteche@gmail.com>
    
    Signed-off-by: Charles-Edouard Brétéché <charled.breteche@gmail.com>

--- pkg/clients/wrappers/kyverno/v1alpha2/clusterreportchangerequest.go
+++ pkg/clients/wrappers/kyverno/v1alpha2/clusterreportchangerequest.go
@@ -6,7 +6,6 @@ import (
 	kyvernov1alpha2 "github.com/kyverno/kyverno/api/kyverno/v1alpha2"
 	"github.com/kyverno/kyverno/pkg/client/clientset/versioned/typed/kyverno/v1alpha2"
 	"github.com/kyverno/kyverno/pkg/clients/wrappers/utils"
-	"github.com/kyverno/kyverno/pkg/metrics"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/watch"
@@ -24,42 +23,34 @@ func wrapClusterReportChangeRequests(c v1alpha2.ClusterReportChangeRequestInterf
 	}
 }
 
-func (c *clusterReportChangeRequest) Create(ctx context.Context, clusterReportChangeRequest *kyvernov1alpha2.ClusterReportChangeRequest, opts metav1.CreateOptions) (*kyvernov1alpha2.ClusterReportChangeRequest, error) {
-	c.clientQueryMetric.Record(metrics.ClientCreate, metrics.KyvernoClient, "ClusterReportChangeRequest", "")
-	return c.inner.Create(ctx, clusterReportChangeRequest, opts)
+func (c *clusterReportChangeRequest) Create(ctx context.Context, o *kyvernov1alpha2.ClusterReportChangeRequest, opts metav1.CreateOptions) (*kyvernov1alpha2.ClusterReportChangeRequest, error) {
+	return utils.Create(ctx, c.clientQueryMetric, "ClusterReportChangeRequest", "", o, opts, c.inner.Create)
 }
 
-func (c *clusterReportChangeRequest) Update(ctx context.Context, clusterReportChangeRequest *kyvernov1alpha2.ClusterReportChangeRequest, opts metav1.UpdateOptions) (*kyvernov1alpha2.ClusterReportChangeRequest, error) {
-	c.clientQueryMetric.Record(metrics.ClientUpdate, metrics.KyvernoClient, "ClusterReportChangeRequest", "")
-	return c.inner.Update(ctx, clusterReportChangeRequest, opts)
+func (c *clusterReportChangeRequest) Update(ctx context.Context, o *kyvernov1alpha2.ClusterReportChangeRequest, opts metav1.UpdateOptions) (*kyvernov1alpha2.ClusterReportChangeRequest, error) {
+	return utils.Update(ctx, c.clientQueryMetric, "ClusterReportChangeRequest", "", o, opts, c.inner.Update)
 }
 
 func (c *clusterReportChangeRequest) Delete(ctx context.Context, name string, opts metav1.DeleteOptions) error {
-	c.clientQueryMetric.Record(metrics.ClientDelete, metrics.KyvernoClient, "ClusterReportChangeRequest", "")
-	return c.inner.Delete(ctx, name, opts)
+	return utils.Delete(ctx, c.clientQueryMetric, "ClusterReportChangeRequest", "", name, opts, c.inner.Delete)
 }
 
 func (c *clusterReportChangeRequest) DeleteCollection(ctx context.Context, opts metav1.DeleteOptions, listOpts metav1.ListOptions) error {
-	c.clientQueryMetric.Record(metrics.ClientDeleteCollection, metrics.KyvernoClient, "ClusterReportChangeRequest", "")
-	return c.inner.DeleteCollection(ctx, opts, listOpts)
+	return utils.DeleteCollection(ctx, c.clientQueryMetric, "ClusterReportChangeRequest", "", opts, listOpts, c.inner.DeleteCollection)
 }
 
 func (c *clusterReportChangeRequest) Get(ctx context.Context, name string, opts metav1.GetOptions) (*kyvernov1alpha2.ClusterReportChangeRequest, error) {
-	c.clientQueryMetric.Record(metrics.ClientGet, metrics.KyvernoClient, "ClusterReportChangeRequest", "")
-	return c.inner.Get(ctx, name, opts)
+	return utils.Get(ctx, c.clientQueryMetric, "ClusterReportChangeRequest", "", name, opts, c.inner.Get)
 }
 
 func (c *clusterReportChangeRequest) List(ctx context.Context, opts metav1.ListOptions) (*kyvernov1alpha2.ClusterReportChangeRequestList, error) {
-	c.clientQueryMetric.Record(metrics.ClientList, metrics.KyvernoClient, "ClusterReportChangeRequest", "")
-	return c.inner.List(ctx, opts)
+	return utils.List(ctx, c.clientQueryMetric, "ClusterReportChangeRequest", "", opts, c.inner.List)
 }
 
 func (c *clusterReportChangeRequest) Watch(ctx context.Context, opts metav1.ListOptions) (watch.Interface, error) {
-	c.clientQueryMetric.Record(metrics.ClientWatch, metrics.KyvernoClient, "ClusterReportChangeRequest", "")
-	return c.inner.Watch(ctx, opts)
+	return utils.Watch(ctx, c.clientQueryMetric, "ClusterReportChangeRequest", "", opts, c.inner.Watch)
 }
 
-func (c *clusterReportChangeRequest) Patch(ctx context.Context, name string, pt types.PatchType, data []byte, opts metav1.PatchOptions, subresources ...string) (result *kyvernov1alpha2.ClusterReportChangeRequest, err error) {
-	c.clientQueryMetric.Record(metrics.ClientPatch, metrics.KyvernoClient, "ClusterReportChangeRequest", "")
-	return c.inner.Patch(ctx, name, pt, data, opts, subresources...)
+func (c *clusterReportChangeRequest) Patch(ctx context.Context, name string, pt types.PatchType, data []byte, opts metav1.PatchOptions, subresources ...string) (*kyvernov1alpha2.ClusterReportChangeRequest, error) {
+	return utils.Patch(ctx, c.clientQueryMetric, "ClusterReportChangeRequest", "", name, pt, data, opts, c.inner.Patch, subresources...)
 }
--- pkg/clients/wrappers/policyreport/v1alpha2/policyreport.go
+++ pkg/clients/wrappers/policyreport/v1alpha2/policyreport.go
@@ -6,7 +6,6 @@ import (
 	policyreportv1alpha2 "github.com/kyverno/kyverno/api/policyreport/v1alpha2"
 	"github.com/kyverno/kyverno/pkg/client/clientset/versioned/typed/policyreport/v1alpha2"
 	"github.com/kyverno/kyverno/pkg/clients/wrappers/utils"
-	"github.com/kyverno/kyverno/pkg/metrics"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/watch"
@@ -26,42 +25,34 @@ func wrapPolicyReports(c v1alpha2.PolicyReportInterface, m utils.ClientQueryMetr
 	}
 }
 
-func (c *policyReports) Create(ctx context.Context, policyReport *policyreportv1alpha2.PolicyReport, opts metav1.CreateOptions) (*policyreportv1alpha2.PolicyReport, error) {
-	c.clientQueryMetric.Record(metrics.ClientCreate, metrics.PolicyReportClient, "PolicyReport", c.ns)
-	return c.inner.Create(ctx, policyReport, opts)
+func (c *policyReports) Create(ctx context.Context, o *policyreportv1alpha2.PolicyReport, opts metav1.CreateOptions) (*policyreportv1alpha2.PolicyReport, error) {
+	return utils.Create(ctx, c.clientQueryMetric, "PolicyReport", c.ns, o, opts, c.inner.Create)
 }
 
-func (c *policyReports) Update(ctx context.Context, policyReport *policyreportv1alpha2.PolicyReport, opts metav1.UpdateOptions) (*policyreportv1alpha2.PolicyReport, error) {
-	c.clientQueryMetric.Record(metrics.ClientUpdate, metrics.PolicyReportClient, "PolicyReport", c.ns)
-	return c.inner.Update(ctx, policyReport, opts)
+func (c *policyReports) Update(ctx context.Context, o *policyreportv1alpha2.PolicyReport, opts metav1.UpdateOptions) (*policyreportv1alpha2.PolicyReport, error) {
+	return utils.Update(ctx, c.clientQueryMetric, "PolicyReport", c.ns, o, opts, c.inner.Update)
 }
 
 func (c *policyReports) Delete(ctx context.Context, name string, opts metav1.DeleteOptions) error {
-	c.clientQueryMetric.Record(metrics.ClientDelete, metrics.PolicyReportClient, "PolicyReport", c.ns)
-	return c.inner.Delete(ctx, name, opts)
+	return utils.Delete(ctx, c.clientQueryMetric, "PolicyReport", c.ns, name, opts, c.inner.Delete)
 }
 
 func (c *policyReports) DeleteCollection(ctx context.Context, opts metav1.DeleteOptions, listOpts metav1.ListOptions) error {
-	c.clientQueryMetric.Record(metrics.ClientDeleteCollection, metrics.PolicyReportClient, "PolicyReport", c.ns)
-	return c.inner.DeleteCollection(ctx, opts, listOpts)
+	return utils.DeleteCollection(ctx, c.clientQueryMetric, "PolicyReport", c.ns, opts, listOpts, c.inner.DeleteCollection)
 }
 
 func (c *policyReports) Get(ctx context.Context, name string, opts metav1.GetOptions) (*policyreportv1alpha2.PolicyReport, error) {
-	c.clientQueryMetric.Record(metrics.ClientGet, metrics.PolicyReportClient, "PolicyReport", c.ns)
-	return c.inner.Get(ctx, name, opts)
+	return utils.Get(ctx, c.clientQueryMetric, "PolicyReport", c.ns, name, opts, c.inner.Get)
 }
 
 func (c *policyReports) List(ctx context.Context, opts metav1.ListOptions) (*policyreportv1alpha2.PolicyReportList, error) {
-	c.clientQueryMetric.Record(metrics.ClientList, metrics.PolicyReportClient, "PolicyReport", c.ns)
-	return c.inner.List(ctx, opts)
+	return utils.List(ctx, c.clientQueryMetric, "PolicyReport", c.ns, opts, c.inner.List)
 }
 
 func (c *policyReports) Watch(ctx context.Context, opts metav1.ListOptions) (watch.Interface, error) {
-	c.clientQueryMetric.Record(metrics.ClientWatch, metrics.PolicyReportClient, "PolicyReport", c.ns)
-	return c.inner.Watch(ctx, opts)
+	return utils.Watch(ctx, c.clientQueryMetric, "PolicyReport", c.ns, opts, c.inner.Watch)
 }
 
-func (c *policyReports) Patch(ctx context.Context, name string, pt types.PatchType, data []byte, opts metav1.PatchOptions, subresources ...string) (result *policyreportv1alpha2.PolicyReport, err error) {
-	c.clientQueryMetric.Record(metrics.ClientPatch, metrics.PolicyReportClient, "PolicyReport", c.ns)
-	return c.inner.Patch(ctx, name, pt, data, opts, subresources...)
+func (c *policyReports) Patch(ctx context.Context, name string, pt types.PatchType, data []byte, opts metav1.PatchOptions, subresources ...string) (*policyreportv1alpha2.PolicyReport, error) {
+	return utils.Patch(ctx, c.clientQueryMetric, "PolicyReport", c.ns, name, pt, data, opts, c.inner.Patch, subresources...)
 }
