fbc7c33427b15db1df2c6be41cb0db6aa9090b92
viveksahu26 <vivekkumarsahu650@gmail.com>
2022-09-11 04:54:47 +0530

    fixed tests
    Signed-off-by: viveksahu26 <vivekkumarsahu650@gmail.com>

--- cmd/cli/kubectl-kyverno/apply/apply_command.go
+++ cmd/cli/kubectl-kyverno/apply/apply_command.go
@@ -239,22 +239,22 @@ func (c *ApplyCommandConfig) applyCommandHelper() (rc *common.ResultCounts, reso
 
 	gitURL.Path = strings.Join([]string{pathElems[0], pathElems[1]}, "/")
 	repoURL := gitURL.String()
-	gitBranch, gitPathToYamls := common.GetGitBranchOrPolicyPaths(c.GitBranch, repoURL, c.PolicyPaths)
-
-	_, cloneErr := test.Clone(repoURL, fs, gitBranch)
-	if cloneErr != nil {
-		fmt.Printf("Error: failed to clone repository \nCause: %s\n", cloneErr)
-		log.Log.V(3).Info(fmt.Sprintf("failed to clone repository  %v as it is not valid", repoURL), "error", cloneErr)
-		os.Exit(1)
+	var gitPathToYamls string
+	if isGit {
+		c.GitBranch, gitPathToYamls = common.GetGitBranchOrPolicyPaths(c.GitBranch, repoURL, c.PolicyPaths)
+		_, cloneErr := test.Clone(repoURL, fs, c.GitBranch)
+		if cloneErr != nil {
+			fmt.Printf("Error: failed to clone repository \nCause: %s\n", cloneErr)
+			log.Log.V(3).Info(fmt.Sprintf("failed to clone repository  %v as it is not valid", repoURL), "error", cloneErr)
+			os.Exit(1)
+		}
+		policyYamls, err := test.ListYAMLs(fs, gitPathToYamls)
+		if err != nil {
+			return rc, resources, skipInvalidPolicies, pvInfos, sanitizederror.NewWithError("failed to list YAMLs in repository", err)
+		}
+		c.PolicyPaths = policyYamls
+		sort.Strings(policyYamls)
 	}
-	policyYamls, err := test.ListYAMLs(fs, gitPathToYamls)
-	if err != nil {
-		return rc, resources, skipInvalidPolicies, pvInfos, sanitizederror.NewWithError("failed to list YAMLs in repository", err)
-	}
-
-	c.PolicyPaths = policyYamls
-	sort.Strings(policyYamls)
-
 	policies, err = common.GetPoliciesFromPaths(fs, c.PolicyPaths, isGit, "")
 	if err != nil {
 		fmt.Printf("Error: failed to load policies\nCause: %s\n", err)
