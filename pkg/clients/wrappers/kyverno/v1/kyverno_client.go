160259d9546f61b17a00beb93f12412d8683c25c
Charles-Edouard Brétéché <charled.breteche@gmail.com>
2022-09-26 18:54:29 +0200

    fix: missing client wrapper (#4703)
    * fix: missing client wrapper
    
    Signed-off-by: Charles-Edouard Brétéché <charled.breteche@gmail.com>
    
    * v1beta1
    
    Signed-off-by: Charles-Edouard Brétéché <charled.breteche@gmail.com>
    
    * v1alpha2
    
    Signed-off-by: Charles-Edouard Brétéché <charled.breteche@gmail.com>
    
    * policy report
    
    Signed-off-by: Charles-Edouard Brétéché <charled.breteche@gmail.com>
    
    Signed-off-by: Charles-Edouard Brétéché <charled.breteche@gmail.com>

--- pkg/clients/wrappers/kyverno/v1/kyverno_client.go
+++ pkg/clients/wrappers/kyverno/v1/kyverno_client.go
@@ -3,19 +3,28 @@ package v1
 import (
 	v1 "github.com/kyverno/kyverno/pkg/client/clientset/versioned/typed/kyverno/v1"
 	"github.com/kyverno/kyverno/pkg/clients/wrappers/utils"
+	"k8s.io/client-go/rest"
 )
 
 type client struct {
-	v1.KyvernoV1Interface
+	inner             v1.KyvernoV1Interface
 	clientQueryMetric utils.ClientQueryMetric
 }
 
 func (c *client) ClusterPolicies() v1.ClusterPolicyInterface {
-	return wrapClusterPolicies(c.KyvernoV1Interface.ClusterPolicies(), c.clientQueryMetric)
+	return wrapClusterPolicies(c.inner.ClusterPolicies(), c.clientQueryMetric)
 }
 
 func (c *client) Policies(namespace string) v1.PolicyInterface {
-	return wrapPolicies(c.KyvernoV1Interface.Policies(namespace), c.clientQueryMetric, namespace)
+	return wrapPolicies(c.inner.Policies(namespace), c.clientQueryMetric, namespace)
+}
+
+func (c *client) GenerateRequests(namespace string) v1.GenerateRequestInterface {
+	return wrapGenerateRequests(c.inner.GenerateRequests(namespace), c.clientQueryMetric, namespace)
+}
+
+func (c *client) RESTClient() rest.Interface {
+	return c.inner.RESTClient()
 }
 
 func Wrap(inner v1.KyvernoV1Interface, m utils.ClientQueryMetric) v1.KyvernoV1Interface {
