a96d376b434ed60fb547901fa31dd2cace144507
damilola olayinka <holayinkajr@gmail.com>
2022-09-29 15:59:37 +0100

    small fixes
    Signed-off-by: damilola olayinka <holayinkajr@gmail.com>

--- pkg/logging/log.go
+++ pkg/logging/log.go
@@ -11,8 +11,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
-type Logger = logr.Logger
-
 const (
 	JSONFormat = "json"
 	TextFormat = "text"
@@ -20,44 +18,42 @@ const (
 
 //Confugures the logger with the supplied log format
 func Setup(logFormat string) error {
-	var setupErr error
 
 	switch logFormat {
 	case TextFormat:
 		// in text mode we use FormatSerialize format
 		log.SetLogger(klogr.New())
+		return nil
 	case JSONFormat:
 		zapLog, err := zap.NewProduction()
 		if err != nil {
-			setupErr = errors.New("JSON logger could not be initialized")
+			return errors.New("JSON logger could not be initialized")
 		}
 		klog.SetLogger(zapr.NewLogger(zapLog))
 
 		// in json mode we use FormatKlog format
 		log.SetLogger(klog.NewKlogr())
-		return setupErr
-	default:
-		setupErr = errors.New("log format not recognized, pass `text` for text mode or `json` to enable JSON logging")
+		return nil
 	}
-	return setupErr
+	return errors.New("log format not recognized, pass `text` for text mode or `json` to enable JSON logging")
 }
 
 //Returns the global logger as configured in main
-func GlobalLogger() Logger {
+func GlobalLogger() logr.Logger {
 	return log.Log
 }
 
 //Return a new Logger instance with the specified name element added to the Logger's name
-func WithName(name string) Logger {
+func WithName(name string) logr.Logger {
 	return GlobalLogger().WithName(name)
 }
 
 //WithValues returns a new Logger instance with additional key/value pairs
-func WithValues(keysAndValues ...interface{}) Logger {
+func WithValues(keysAndValues ...interface{}) logr.Logger {
 	return GlobalLogger().WithValues(keysAndValues...)
 }
 
 //V returns a new Logger instance for a specific verbosity level
-func V(level int) Logger {
+func V(level int) logr.Logger {
 	return GlobalLogger().V(level)
 }
