5033ec5ef07e8493bb091056dccae83379e74ab6
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2022-10-07 11:28:36 +0200

    remove unused
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/controllers/certmanager/controller.go
+++ pkg/controllers/certmanager/controller.go
@@ -10,7 +10,6 @@ import (
 	"github.com/kyverno/kyverno/pkg/controllers"
 	"github.com/kyverno/kyverno/pkg/tls"
 	controllerutils "github.com/kyverno/kyverno/pkg/utils/controller"
-	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	corev1informers "k8s.io/client-go/informers/core/v1"
 	corev1listers "k8s.io/client-go/listers/core/v1"
@@ -49,26 +48,6 @@ func NewController(secretInformer corev1informers.SecretInformer, certRenewer *t
 func (c *controller) Run(ctx context.Context, workers int) {
 	go c.ticker(ctx)
 	controllerutils.Run(ctx, ControllerName, logger.V(3), c.queue, workers, maxRetries, c.reconcile)
-}
-
-func (m *controller) GetTLSPemPair() ([]byte, []byte, error) {
-	secret, err := m.secretLister.Secrets(config.KyvernoNamespace()).Get(tls.GenerateTLSPairSecretName())
-	if err != nil {
-		return nil, nil, err
-	}
-	return secret.Data[corev1.TLSCertKey], secret.Data[corev1.TLSPrivateKeyKey], nil
-}
-
-func (m *controller) GetCAPem() ([]byte, error) {
-	secret, err := m.secretLister.Secrets(config.KyvernoNamespace()).Get(tls.GenerateRootCASecretName())
-	if err != nil {
-		return nil, err
-	}
-	result := secret.Data[corev1.TLSCertKey]
-	if len(result) == 0 {
-		result = secret.Data[tls.RootCAKey]
-	}
-	return result, nil
 }
 
 func (c *controller) reconcile(ctx context.Context, logger logr.Logger, key, namespace, name string) error {
