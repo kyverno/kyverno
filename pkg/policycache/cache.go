17ba925490432f8d7d396345d5ef0e1b1f5daf45
Sandesh More <34198712+sandeshlmore@users.noreply.github.com>
2022-10-13 13:29:10 +0530

    add filter for validation policies when ValidationFailureActionOverrides is used (#4809)
    Signed-off-by: Sandesh More <sandesh.more@infracloud.io>
    
    Signed-off-by: Sandesh More <sandesh.more@infracloud.io>

--- pkg/policycache/cache.go
+++ pkg/policycache/cache.go
@@ -2,6 +2,7 @@ package policycache
 
 import (
 	kyvernov1 "github.com/kyverno/kyverno/api/kyverno/v1"
+	kyvernoutils "github.com/kyverno/kyverno/pkg/utils"
 )
 
 // Cache get method use for to get policy names and mostly use to test cache testcases
@@ -42,5 +43,47 @@ func (c *cache) GetPolicies(pkey PolicyType, kind, nspace string) []kyvernov1.Po
 		result = append(result, c.store.get(pkey, kind, nspace)...)
 		result = append(result, c.store.get(pkey, "*", nspace)...)
 	}
+
+	if pkey == ValidateAudit { // also get policies with ValidateEnforce
+		result = append(result, c.store.get(ValidateEnforce, kind, "")...)
+		result = append(result, c.store.get(ValidateEnforce, "*", "")...)
+	}
+
+	if pkey == ValidateAudit || pkey == ValidateEnforce {
+		result = filterPolicies(pkey, result, nspace, kind)
+	}
+
 	return result
+}
+
+// Filter cluster policies using validationFailureAction override
+func filterPolicies(pkey PolicyType, result []kyvernov1.PolicyInterface, nspace, kind string) []kyvernov1.PolicyInterface {
+	var policies []kyvernov1.PolicyInterface
+	for _, policy := range result {
+		keepPolicy := true
+		switch pkey {
+		case ValidateAudit:
+			keepPolicy = checkValidationFailureActionOverrides(kyvernov1.Audit, nspace, policy)
+		case ValidateEnforce:
+			keepPolicy = checkValidationFailureActionOverrides(kyvernov1.Enforce, nspace, policy)
+		}
+		if keepPolicy { // add policy to result
+			policies = append(policies, policy)
+		}
+	}
+	return policies
+}
+
+func checkValidationFailureActionOverrides(requestedAction kyvernov1.ValidationFailureAction, ns string, policy kyvernov1.PolicyInterface) bool {
+	validationFailureAction := policy.GetSpec().ValidationFailureAction
+	validationFailureActionOverrides := policy.GetSpec().ValidationFailureActionOverrides
+	if validationFailureAction != requestedAction && (ns == "" || len(validationFailureActionOverrides) == 0) {
+		return false
+	}
+	for _, action := range validationFailureActionOverrides {
+		if action.Action != requestedAction && kyvernoutils.ContainsNamepace(action.Namespaces, ns) {
+			return false
+		}
+	}
+	return true
 }
