93f55923ea4a896206835e346d289a9d38969fa3
Vishal Choudhary <contactvishaltech@gmail.com>
2022-10-15 21:13:02 +0530

    added RequestTypes to MatchResources in v2beta1
    Signed-off-by: Vishal Choudhary <contactvishaltech@gmail.com>

--- api/kyverno/v2beta1/match_resources_types.go
+++ api/kyverno/v2beta1/match_resources_types.go
@@ -9,6 +9,12 @@ import (
 // MatchResources is used to specify resource and admission review request data for
 // which a policy rule is applicable.
 type MatchResources struct {
+	// RequestTypes can contain values ["CREATE, "UPDATE", "CONNECT", "DELETE"], which are used to match a specific action.
+	// +kubebuilder:validation:MaxItems=3
+	// +kubebuilder:validation:MinItems=1
+	// +kubebuilder:validation:Optional
+	RequestTypes []string `json:"requestTypes,omitempty" yaml:"requestTypes,omitempty"`
+	// Any allows specifying resources which will be ORed
 	// Any allows specifying resources which will be ORed
 	// +optional
 	Any kyvernov1.ResourceFilters `json:"any,omitempty" yaml:"any,omitempty"`
