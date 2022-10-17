5e018c7f35e9fc89649e54be05a952b2d556127c
Vyankatesh Kudtarkar <vyankateshkd@gmail.com>
2022-10-17 19:02:27 +0530

    update

--- pkg/webhooks/utils/policy_context_builder.go
+++ pkg/webhooks/utils/policy_context_builder.go
@@ -57,11 +57,6 @@ func (b *policyContextBuilder) Build(request *admissionv1.AdmissionRequest, poli
 	userRequestInfo := kyvernov1beta1.RequestInfo{
 		AdmissionUserInfo: *request.UserInfo.DeepCopy(),
 	}
-	var err error
-	userRequestInfo.Roles, userRequestInfo.ClusterRoles, err = userinfo.GetRoleRef(b.rbLister, b.crbLister, request, b.configuration)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to fetch RBAC information for request")
-	}
 
 	ctx, err := newVariablesContext(request, &userRequestInfo)
 	if err != nil {
@@ -74,6 +69,11 @@ func (b *policyContextBuilder) Build(request *admissionv1.AdmissionRequest, poli
 	if err := ctx.AddImageInfos(&newResource); err != nil {
 		return nil, errors.Wrap(err, "failed to add image information to the policy rule context")
 	}
+	userRequestInfo.Roles, userRequestInfo.ClusterRoles, err = userinfo.GetRoleRef(b.rbLister, b.crbLister, request, b.configuration)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to fetch RBAC information for request")
+	}
+
 	policyContext := &engine.PolicyContext{
 		NewResource:         newResource,
 		OldResource:         oldResource,
