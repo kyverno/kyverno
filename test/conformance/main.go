151d7b232f4af4701db2c35c49d96358db1e8bf0
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2022-10-19 22:00:50 +0200

    improvements
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- test/conformance/main.go
+++ test/conformance/main.go
@@ -1,30 +1,98 @@
 package main
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
-	"io/fs"
 	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
-	"path/filepath"
+	"strings"
+
+	"go.uber.org/multierr"
+	"gopkg.in/yaml.v3"
 )
 
-func getManifests(folder string) ([]fs.FileInfo, error) {
-	return ioutil.ReadDir(folder)
+type CommandExpectation struct {
+	ExitCode *int
+	StdOut   *string
+	StdErr   *string
 }
 
-func createCommand(name string, arg ...string) *exec.Cmd {
+func (x CommandExpectation) Verify(stdout []byte, stderr []byte, err error) error {
+	exitcode := 0
+	if err != nil {
+		exitError := err.(*exec.ExitError)
+		exitcode = exitError.ExitCode()
+	}
+	if x.ExitCode != nil {
+		if exitcode != *x.ExitCode {
+			return errors.New(fmt.Sprint("unexpected exit code, expected: ", *x.ExitCode, " - actual: ", exitcode))
+		}
+	}
+	if x.StdOut != nil {
+		if trim(*x.StdOut, "\n", " ") != trim(string(stdout), "\n", " ") {
+			return errors.New(fmt.Sprint("unexpected stdout, expected: ", *x.StdOut, " - actual: ", string(stdout)))
+		}
+	}
+	if x.StdErr != nil {
+		if trim(*x.StdErr, "\n", " ") != trim(string(stderr), "\n", " ") {
+			return errors.New(fmt.Sprint("unexpected stderr, expected: ", *x.StdErr, " - actual: ", string(stderr)))
+		}
+	}
+	return nil
+}
+
+type KubectlTest struct {
+	Args   []string
+	Expect *CommandExpectation
+}
+
+func (kt KubectlTest) Run(name string) error {
+	stdout, stderr, err := runCommand("kubectl", kt.Args...)
+	if kt.Expect != nil {
+		return kt.Expect.Verify(stdout, stderr, err)
+	}
+	return nil
+}
+
+type Test struct {
+	Kubectl *KubectlTest
+}
+
+func (t Test) Run(name string) error {
+	if t.Kubectl != nil {
+		return t.Kubectl.Run(name)
+	}
+	return errors.New("no test defined")
+}
+
+func trim(in string, s ...string) string {
+	for _, s := range s {
+		in = strings.TrimSuffix(in, s)
+	}
+	return in
+}
+
+func runCommand(name string, arg ...string) ([]byte, []byte, error) {
 	cmd := exec.Command(name, arg...)
-	// cmd.Stdin = os.Stdin
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	return stdout.Bytes(), stderr.Bytes(), err
+}
+
+func stdCommand(name string, arg ...string) *exec.Cmd {
+	cmd := exec.Command(name, arg...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	return cmd
 }
 
 func makeCluster() error {
-	cmd := createCommand("make", "kind-create-cluster", "kind-deploy-kyverno")
+	cmd := stdCommand("make", "kind-create-cluster", "kind-deploy-kyverno")
 	if err := cmd.Run(); err != nil {
 		return err
 	}
@@ -32,46 +100,51 @@ func makeCluster() error {
 }
 
 func makeDeleteCluster() error {
-	cmd := createCommand("make", "kind-delete-cluster")
+	cmd := stdCommand("make", "kind-delete-cluster")
 	if err := cmd.Run(); err != nil {
 		return err
 	}
 	return nil
 }
 
-func kubectl(file string) error {
-	cmd := createCommand("kubectl", "create", "-f", file)
-	if err := cmd.Run(); err != nil {
-		return err
+func loadTests() (map[string][]Test, error) {
+	data, err := ioutil.ReadFile("./test/conformance/tests.yaml")
+	if err != nil {
+		return nil, err
 	}
-	return nil
+	tests := map[string][]Test{}
+	if err := yaml.Unmarshal(data, tests); err != nil {
+		return nil, err
+	}
+	return tests, nil
 }
 
 func main() {
-	runner := func(index int, path string, file fs.FileInfo) error {
-		name := fmt.Sprint("test-", index)
-		os.Setenv("KIND_NAME", name)
-		defer func(name string) {
-			if err := makeDeleteCluster(); err != nil {
-				log.Fatal(err)
-			}
-		}(name)
-		if err := makeCluster(); err != nil {
-			return err
-		}
-		if err := kubectl(filepath.Join(path, file.Name())); err != nil {
-			return err
-		}
-		return nil
-	}
-	shouldFailPath := "./test/conformance/manifests/should-fail"
-	files, err := getManifests(shouldFailPath)
+	tests, err := loadTests()
 	if err != nil {
 		log.Fatal(err)
 	}
-	for i, file := range files {
-		if err := runner(i, shouldFailPath, file); err == nil {
-			log.Fatal(errors.New(fmt.Sprint("no error returned but one was expected when applying manifest", file.Name())))
+	for cluster, tests := range tests {
+		runner := func(name string, tests []Test) error {
+			os.Setenv("KIND_NAME", name)
+			defer func(name string) {
+				if err := makeDeleteCluster(); err != nil {
+					log.Fatal(err)
+				}
+			}(name)
+			if err := makeCluster(); err != nil {
+				return err
+			}
+			var errs []error
+			for _, test := range tests {
+				if err := test.Run(name); err != nil {
+					errs = append(errs, err)
+				}
+			}
+			return multierr.Combine(errs...)
+		}
+		if err := runner(cluster, tests); err != nil {
+			log.Fatal(err)
 		}
 	}
 }
