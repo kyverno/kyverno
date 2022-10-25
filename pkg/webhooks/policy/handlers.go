e4bf66e756ab0196c8c2acaa6b4c2a320669c656
Charles-Edouard Brétéché <charled.breteche@gmail.com>
2022-10-26 01:43:46 +0200

    feat: remove policy mutation for auto-gen rules (#5123)
    * feat: remove policy mutation code
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    
    * fix
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    
    * Fix
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    
    * changelog
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    Co-authored-by: shuting <shuting@nirmata.com>

--- pkg/webhooks/policy/handlers.go
+++ pkg/webhooks/policy/handlers.go
@@ -2,15 +2,12 @@ package policy
 
 import (
 	"fmt"
-	"strings"
 	"time"
 
 	"github.com/go-logr/logr"
 	"github.com/kyverno/kyverno/pkg/clients/dclient"
 	"github.com/kyverno/kyverno/pkg/openapi"
 	policyvalidate "github.com/kyverno/kyverno/pkg/policy"
-	"github.com/kyverno/kyverno/pkg/policymutation"
-	"github.com/kyverno/kyverno/pkg/toggle"
 	admissionutils "github.com/kyverno/kyverno/pkg/utils/admission"
 	"github.com/kyverno/kyverno/pkg/webhooks"
 	admissionv1 "k8s.io/api/admission/v1"
@@ -50,20 +47,5 @@ func (h *handlers) Validate(logger logr.Logger, request *admissionv1.AdmissionRe
 }
 
 func (h *handlers) Mutate(logger logr.Logger, request *admissionv1.AdmissionRequest, _ time.Time) *admissionv1.AdmissionResponse {
-	if toggle.AutogenInternals.Enabled() {
-		return admissionutils.Response(true)
-	}
-	if request.SubResource != "" {
-		logger.V(4).Info("skip policy validation on status update")
-		return admissionutils.Response(true)
-	}
-	policy, _, err := admissionutils.GetPolicies(request)
-	if err != nil {
-		logger.Error(err, "failed to unmarshal policies from admission request")
-		return admissionutils.ResponseWithMessage(true, fmt.Sprintf("failed to default value, check kyverno controller logs for details: %v", err))
-	}
-	if patches, updateMsgs := policymutation.GenerateJSONPatchesForDefaults(policy, logger); len(patches) != 0 {
-		return admissionutils.ResponseWithMessageAndPatch(true, strings.Join(updateMsgs, "'"), patches)
-	}
 	return admissionutils.Response(true)
 }
