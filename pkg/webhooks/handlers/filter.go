63bbcd40af49e5c8eb6993b50e5d25fcfdaa8789
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2022-11-07 11:25:39 +0100

    refactor: move all middlewares in handlers sub package
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

---
+++ pkg/webhooks/handlers/filter.go
@@ -0,0 +1,18 @@
+package handlers
+
+import (
+	"time"
+
+	"github.com/go-logr/logr"
+	"github.com/kyverno/kyverno/pkg/config"
+	admissionv1 "k8s.io/api/admission/v1"
+)
+
+func Filter(c config.Configuration, inner AdmissionHandler) AdmissionHandler {
+	return func(logger logr.Logger, request *admissionv1.AdmissionRequest, startTime time.Time) *admissionv1.AdmissionResponse {
+		if c.ToFilter(request.Kind.Kind, request.Namespace, request.Name) {
+			return nil
+		}
+		return inner(logger, request, startTime)
+	}
+}
