e67735f19f904423b75b33467a9241df44c22c38
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2022-11-14 14:20:10 +0100

    fixes
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- cmd/cleanup-controller/main.go
+++ cmd/cleanup-controller/main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func setupSignals() (context.Context, context.CancelFunc) {
+	return signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+}
+
 func main() {
+	// setup signals
+	signalCtx, signalCancel := setupSignals()
+	defer signalCancel()
+	// wait for termination signal
+	<-signalCtx.Done()
 }
