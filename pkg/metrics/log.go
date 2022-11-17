0462f78c5a8135f16338241fbfc3ef88732a66bb
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2022-11-17 21:01:24 +0100

    chore: remove obsolete metrics client code
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/metrics/log.go
+++ pkg/metrics/log.go
@@ -1,12 +1,7 @@
 package metrics
 
 import (
-	"github.com/go-logr/logr"
 	"github.com/kyverno/kyverno/pkg/logging"
 )
 
 var logger = logging.WithName("metrics")
-
-func Logger() logr.Logger {
-	return logger
-}
