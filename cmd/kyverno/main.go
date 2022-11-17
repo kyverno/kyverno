ee54672cab2abfadb8265e8b7393aa465ac69b01
yinka <holayinkajr@gmail.com>
2022-11-17 22:42:26 +0000

    add os.Exit (#5402)
    Signed-off-by: damilola olayinka <holayinkajr@gmail.com>
    
    Signed-off-by: damilola olayinka <holayinkajr@gmail.com>
    Co-authored-by: Charles-Edouard Brétéché <charled.breteche@gmail.com>

--- cmd/kyverno/main.go
+++ cmd/kyverno/main.go
@@ -253,6 +253,7 @@ func setupMetrics(logger logr.Logger, kubeClient kubernetes.Interface) (*metrics
 			}
 			if err := metricsServer.ListenAndServe(); err != nil {
 				logger.Error(err, "failed to enable metrics", "address", metricsAddr)
+				os.Exit(1)
 			}
 		}()
 	}
