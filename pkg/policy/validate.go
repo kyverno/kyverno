4d6ee041f7b84364bb0d59069d21adc2dcbbf6dc
shuting <shuting@nirmata.com>
2022-11-17 21:37:14 +0800

    fix: enable policy validation for the verifyImage rule (#5383)
    * enable policy validation for the verifyImage rule
    
    Signed-off-by: ShutingZhao <shuting@nirmata.com>
    
    * update policy to fix test
    
    Signed-off-by: ShutingZhao <shuting@nirmata.com>
    
    * add rekor url
    
    Signed-off-by: ShutingZhao <shuting@nirmata.com>
    
    Signed-off-by: ShutingZhao <shuting@nirmata.com>

--- pkg/policy/validate.go
+++ pkg/policy/validate.go
@@ -246,13 +246,16 @@ func Validate(policy kyvernov1.PolicyInterface, client dclient.Interface, mock b
 				}
 			}
 
-			if rule.HasVerifyImages() {
-				verifyImagePath := rulePath.Child("verifyImages")
-				for index, i := range rule.VerifyImages {
-					errs = append(errs, i.Validate(verifyImagePath.Index(index))...)
-				}
+			if len(errs) != 0 {
+				return warnings, errs.ToAggregate()
 			}
+		}
 
+		if rule.HasVerifyImages() {
+			verifyImagePath := rulePath.Child("verifyImages")
+			for index, i := range rule.VerifyImages {
+				errs = append(errs, i.Validate(verifyImagePath.Index(index))...)
+			}
 			if len(errs) != 0 {
 				return warnings, errs.ToAggregate()
 			}
