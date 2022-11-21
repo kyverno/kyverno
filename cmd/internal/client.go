97710576757913aff05175b02ee9cb26a323722b
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2022-11-21 21:01:23 +0100

    metadata
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- cmd/internal/client.go
+++ cmd/internal/client.go
@@ -6,9 +6,11 @@ import (
 	dyn "github.com/kyverno/kyverno/pkg/clients/dynamic"
 	kube "github.com/kyverno/kyverno/pkg/clients/kube"
 	kyverno "github.com/kyverno/kyverno/pkg/clients/kyverno"
+	meta "github.com/kyverno/kyverno/pkg/clients/metadata"
 	"github.com/kyverno/kyverno/pkg/config"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/metadata"
 	"k8s.io/client-go/rest"
 )
 
@@ -39,5 +41,13 @@ func CreateDynamicClient(logger logr.Logger, opts ...dyn.NewOption) dynamic.Inte
 	logger.Info("create dynamic client...", "kubeconfig", kubeconfig, "qps", clientRateLimitQPS, "burst", clientRateLimitBurst)
 	client, err := dyn.NewForConfig(CreateClientConfig(logger), opts...)
 	checkError(logger, err, "failed to create dynamic client")
+	return client
+}
+
+func CreateMetadataClient(logger logr.Logger, opts ...meta.NewOption) metadata.Interface {
+	logger = logger.WithName("metadata-client")
+	logger.Info("create metadata client...", "kubeconfig", kubeconfig, "qps", clientRateLimitQPS, "burst", clientRateLimitBurst)
+	client, err := meta.NewForConfig(CreateClientConfig(logger), opts...)
+	checkError(logger, err, "failed to create metadata client")
 	return client
 }
