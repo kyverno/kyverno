7afecd52bd06c9b7992495a3097c5d361aef8522
Charles-Edouard Brétéché <charled.breteche@gmail.com>
2022-11-22 15:26:49 +0100

    feat: add discovery support in instrumented clients (#5437)
    * feat: add discovery support in instrumented clients
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    
    * fix tracing
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    Signed-off-by: Brandt Keller <brandt.keller@defenseunicorns.com>

--- pkg/clients/kyverno/clientset.generated.go
+++ pkg/clients/kyverno/clientset.generated.go
@@ -7,17 +7,18 @@ import (
 	github_com_kyverno_kyverno_pkg_client_clientset_versioned_typed_kyverno_v1alpha2 "github.com/kyverno/kyverno/pkg/client/clientset/versioned/typed/kyverno/v1alpha2"
 	github_com_kyverno_kyverno_pkg_client_clientset_versioned_typed_kyverno_v1beta1 "github.com/kyverno/kyverno/pkg/client/clientset/versioned/typed/kyverno/v1beta1"
 	github_com_kyverno_kyverno_pkg_client_clientset_versioned_typed_policyreport_v1alpha2 "github.com/kyverno/kyverno/pkg/client/clientset/versioned/typed/policyreport/v1alpha2"
+	discovery "github.com/kyverno/kyverno/pkg/clients/kyverno/discovery"
 	kyvernov1 "github.com/kyverno/kyverno/pkg/clients/kyverno/kyvernov1"
 	kyvernov1alpha1 "github.com/kyverno/kyverno/pkg/clients/kyverno/kyvernov1alpha1"
 	kyvernov1alpha2 "github.com/kyverno/kyverno/pkg/clients/kyverno/kyvernov1alpha2"
 	kyvernov1beta1 "github.com/kyverno/kyverno/pkg/clients/kyverno/kyvernov1beta1"
 	wgpolicyk8sv1alpha2 "github.com/kyverno/kyverno/pkg/clients/kyverno/wgpolicyk8sv1alpha2"
 	"github.com/kyverno/kyverno/pkg/metrics"
-	"k8s.io/client-go/discovery"
+	k8s_io_client_go_discovery "k8s.io/client-go/discovery"
 )
 
 type clientset struct {
-	inner               github_com_kyverno_kyverno_pkg_client_clientset_versioned.Interface
+	discovery           k8s_io_client_go_discovery.DiscoveryInterface
 	kyvernov1           github_com_kyverno_kyverno_pkg_client_clientset_versioned_typed_kyverno_v1.KyvernoV1Interface
 	kyvernov1alpha1     github_com_kyverno_kyverno_pkg_client_clientset_versioned_typed_kyverno_v1alpha1.KyvernoV1alpha1Interface
 	kyvernov1alpha2     github_com_kyverno_kyverno_pkg_client_clientset_versioned_typed_kyverno_v1alpha2.KyvernoV1alpha2Interface
@@ -25,8 +26,8 @@ type clientset struct {
 	wgpolicyk8sv1alpha2 github_com_kyverno_kyverno_pkg_client_clientset_versioned_typed_policyreport_v1alpha2.Wgpolicyk8sV1alpha2Interface
 }
 
-func (c *clientset) Discovery() discovery.DiscoveryInterface {
-	return c.inner.Discovery()
+func (c *clientset) Discovery() k8s_io_client_go_discovery.DiscoveryInterface {
+	return c.discovery
 }
 func (c *clientset) KyvernoV1() github_com_kyverno_kyverno_pkg_client_clientset_versioned_typed_kyverno_v1.KyvernoV1Interface {
 	return c.kyvernov1
@@ -44,20 +45,20 @@ func (c *clientset) Wgpolicyk8sV1alpha2() github_com_kyverno_kyverno_pkg_client_
 	return c.wgpolicyk8sv1alpha2
 }
 
-func WrapWithMetrics(inner github_com_kyverno_kyverno_pkg_client_clientset_versioned.Interface, metrics metrics.MetricsConfigManager, clientType metrics.ClientType) github_com_kyverno_kyverno_pkg_client_clientset_versioned.Interface {
+func WrapWithMetrics(inner github_com_kyverno_kyverno_pkg_client_clientset_versioned.Interface, m metrics.MetricsConfigManager, clientType metrics.ClientType) github_com_kyverno_kyverno_pkg_client_clientset_versioned.Interface {
 	return &clientset{
-		inner:               inner,
-		kyvernov1:           kyvernov1.WithMetrics(inner.KyvernoV1(), metrics, clientType),
-		kyvernov1alpha1:     kyvernov1alpha1.WithMetrics(inner.KyvernoV1alpha1(), metrics, clientType),
-		kyvernov1alpha2:     kyvernov1alpha2.WithMetrics(inner.KyvernoV1alpha2(), metrics, clientType),
-		kyvernov1beta1:      kyvernov1beta1.WithMetrics(inner.KyvernoV1beta1(), metrics, clientType),
-		wgpolicyk8sv1alpha2: wgpolicyk8sv1alpha2.WithMetrics(inner.Wgpolicyk8sV1alpha2(), metrics, clientType),
+		discovery:           discovery.WithMetrics(inner.Discovery(), metrics.ClusteredClientQueryRecorder(m, "Discovery", clientType)),
+		kyvernov1:           kyvernov1.WithMetrics(inner.KyvernoV1(), m, clientType),
+		kyvernov1alpha1:     kyvernov1alpha1.WithMetrics(inner.KyvernoV1alpha1(), m, clientType),
+		kyvernov1alpha2:     kyvernov1alpha2.WithMetrics(inner.KyvernoV1alpha2(), m, clientType),
+		kyvernov1beta1:      kyvernov1beta1.WithMetrics(inner.KyvernoV1beta1(), m, clientType),
+		wgpolicyk8sv1alpha2: wgpolicyk8sv1alpha2.WithMetrics(inner.Wgpolicyk8sV1alpha2(), m, clientType),
 	}
 }
 
 func WrapWithTracing(inner github_com_kyverno_kyverno_pkg_client_clientset_versioned.Interface) github_com_kyverno_kyverno_pkg_client_clientset_versioned.Interface {
 	return &clientset{
-		inner:               inner,
+		discovery:           discovery.WithTracing(inner.Discovery(), "Discovery", ""),
 		kyvernov1:           kyvernov1.WithTracing(inner.KyvernoV1(), "KyvernoV1"),
 		kyvernov1alpha1:     kyvernov1alpha1.WithTracing(inner.KyvernoV1alpha1(), "KyvernoV1alpha1"),
 		kyvernov1alpha2:     kyvernov1alpha2.WithTracing(inner.KyvernoV1alpha2(), "KyvernoV1alpha2"),
