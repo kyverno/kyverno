2a174b1130a8d3b53be33f7dc2c66b970be74a0b
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2022-11-24 00:08:48 +0100

    move kyverno filtering
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/webhooks/handlers/filter.go
+++ pkg/webhooks/handlers/filter.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/go-logr/logr"
 	"github.com/kyverno/kyverno/pkg/config"
+	webhookutils "github.com/kyverno/kyverno/pkg/webhooks/utils"
 	admissionv1 "k8s.io/api/admission/v1"
 )
 
@@ -16,6 +17,9 @@ func (inner AdmissionHandler) WithFilter(configuration config.Configuration) Adm
 func (inner AdmissionHandler) withFilter(c config.Configuration) AdmissionHandler {
 	return func(ctx context.Context, logger logr.Logger, request *admissionv1.AdmissionRequest, startTime time.Time) *admissionv1.AdmissionResponse {
 		if c.ToFilter(request.Kind.Kind, request.Namespace, request.Name) {
+			return nil
+		}
+		if webhookutils.ExcludeKyvernoResources(request.Kind.Kind) {
 			return nil
 		}
 		return inner(ctx, logger, request, startTime)
