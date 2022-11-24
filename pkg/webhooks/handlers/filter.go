d22ca917f77d827fa5d0b682feb1cf4bbfcbc39b
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2022-11-24 21:53:23 +0100

    feat: add new filtering handlers
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/webhooks/handlers/filter.go
+++ pkg/webhooks/handlers/filter.go
@@ -8,10 +8,15 @@ import (
 	"github.com/kyverno/kyverno/pkg/config"
 	webhookutils "github.com/kyverno/kyverno/pkg/webhooks/utils"
 	admissionv1 "k8s.io/api/admission/v1"
+	"k8s.io/apimachinery/pkg/util/sets"
 )
 
 func (inner AdmissionHandler) WithFilter(configuration config.Configuration) AdmissionHandler {
 	return inner.withFilter(configuration).WithTrace("FILTER")
+}
+
+func (inner AdmissionHandler) WithOperationFilter(operations ...admissionv1.Operation) AdmissionHandler {
+	return inner.withOperationFilter(operations...).WithTrace("OPERATION")
 }
 
 func (inner AdmissionHandler) withFilter(c config.Configuration) AdmissionHandler {
@@ -23,5 +28,18 @@ func (inner AdmissionHandler) withFilter(c config.Configuration) AdmissionHandle
 			return nil
 		}
 		return inner(ctx, logger, request, startTime)
+	}
+}
+
+func (inner AdmissionHandler) withOperationFilter(operations ...admissionv1.Operation) AdmissionHandler {
+	ops := sets.NewString()
+	for _, operation := range operations {
+		ops.Insert(string(operation))
+	}
+	return func(ctx context.Context, logger logr.Logger, request *admissionv1.AdmissionRequest, startTime time.Time) *admissionv1.AdmissionResponse {
+		if ops.Has(string(request.Operation)) {
+			return inner(ctx, logger, request, startTime)
+		}
+		return nil
 	}
 }
