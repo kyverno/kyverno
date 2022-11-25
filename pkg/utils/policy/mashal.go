1ecab96fd4ad23ef58806c5940d19aa99fb9f892
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2022-11-25 11:14:06 +0100

    added comment
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/utils/policy/mashal.go
+++ pkg/utils/policy/mashal.go
@@ -7,10 +7,14 @@ import (
 	"sigs.k8s.io/yaml"
 )
 
+// ToJson marshals a policy into corresponding json bytes.
 func ToJson(policy kyvernov1.PolicyInterface) ([]byte, error) {
 	return json.Marshal(policy)
 }
 
+// ToYaml marshals a policy into corresponding yaml bytes.
+// If firsts converts the policy to json because some internal structures have
+// custom json marshalling functions, then it converts json to yaml.
 func ToYaml(policy kyvernov1.PolicyInterface) ([]byte, error) {
 	jsonBytes, err := ToJson(policy)
 	if err != nil {
