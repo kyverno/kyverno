a3a2a61b582a1e80a5f862cd25e8de6f5d76b97a
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2022-11-25 15:11:54 +0100

    feat: add dynamic client support to internal cmd package
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- cmd/internal/metrics.go
+++ cmd/internal/metrics.go
@@ -1,15 +1,54 @@
 package internal
 
 import (
+	"context"
+	"net/http"
+	"time"
+
 	"github.com/go-logr/logr"
 	"github.com/kyverno/kyverno/pkg/config"
+	"github.com/kyverno/kyverno/pkg/logging"
+	"github.com/kyverno/kyverno/pkg/metrics"
 	"k8s.io/client-go/kubernetes"
 )
 
 func GetMetricsConfiguration(logger logr.Logger, client kubernetes.Interface) config.MetricsConfiguration {
-	logger = logger.WithName("metrics")
 	logger.Info("load metrics configuration...")
 	metricsConfiguration, err := config.NewMetricsConfiguration(client)
 	checkError(logger, err, "failed to load metrics configuration")
 	return metricsConfiguration
+}
+
+func SetupMetrics(logger logr.Logger, kubeClient kubernetes.Interface) (metrics.MetricsConfigManager, context.CancelFunc) {
+	logger = logger.WithName("metrics")
+	logger.Info("setup metrics...", "otel", otel, "port", metricsPort, "collector", otelCollector, "creds", transportCreds)
+	metricsConfiguration := GetMetricsConfiguration(logger, kubeClient)
+	metricsAddr := ":" + metricsPort
+	metricsConfig, metricsServerMux, metricsPusher, err := metrics.InitMetrics(
+		disableMetricsExport,
+		otel,
+		metricsAddr,
+		otelCollector,
+		metricsConfiguration,
+		transportCreds,
+		kubeClient,
+		logging.WithName("metrics"),
+	)
+	checkError(logger, err, "failed to init metrics")
+	var cancel context.CancelFunc
+	if otel == "grpc" {
+		cancel = func() {
+			ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+			defer cancel()
+			metrics.ShutDownController(ctx, metricsPusher)
+		}
+	}
+	if otel == "prometheus" {
+		go func() {
+			if err := http.ListenAndServe(metricsAddr, metricsServerMux); err != nil {
+				logger.Error(err, "failed to enable metrics", "address", metricsAddr)
+			}
+		}()
+	}
+	return metricsConfig, cancel
 }
--- cmd/internal/setup.go
+++ cmd/internal/setup.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/go-logr/logr"
+	"github.com/kyverno/kyverno/pkg/metrics"
 )
 
 func shutdown(logger logr.Logger, sdowns ...context.CancelFunc) context.CancelFunc {
@@ -15,11 +16,13 @@ func shutdown(logger logr.Logger, sdowns ...context.CancelFunc) context.CancelFu
 	}
 }
 
-func Setup() (context.Context, logr.Logger, context.CancelFunc) {
+func Setup() (context.Context, logr.Logger, metrics.MetricsConfigManager, context.CancelFunc) {
 	logger := SetupLogger()
 	ShowVersion(logger)
 	sdownMaxProcs := SetupMaxProcs(logger)
 	SetupProfiling(logger)
+	client := CreateKubernetesClient(logger)
+	metricsManager, sdownMetrics := SetupMetrics(logger, client)
 	ctx, sdownSignals := SetupSignals(logger)
-	return ctx, logger, shutdown(logger.WithName("shutdown"), sdownMaxProcs, sdownSignals)
+	return ctx, logger, metricsManager, shutdown(logger.WithName("shutdown"), sdownMaxProcs, sdownMetrics, sdownSignals)
 }
