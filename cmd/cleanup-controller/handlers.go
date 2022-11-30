9263422747943b6f014fbcc176e097e50b721d35
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2022-11-30 13:43:46 +0100

    fix: cleanup policy validation
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- cmd/cleanup-controller/handlers.go
+++ cmd/cleanup-controller/handlers.go
@@ -5,9 +5,9 @@ import (
 	"time"
 
 	"github.com/go-logr/logr"
-	"github.com/kyverno/kyverno/cmd/cleanup-controller/validate"
 	"github.com/kyverno/kyverno/pkg/clients/dclient"
 	admissionutils "github.com/kyverno/kyverno/pkg/utils/admission"
+	validation "github.com/kyverno/kyverno/pkg/validation/cleanuppolicy"
 	admissionv1 "k8s.io/api/admission/v1"
 )
 
@@ -27,9 +27,9 @@ func (h *cleanupPolicyHandlers) Validate(ctx context.Context, logger logr.Logger
 		logger.Error(err, "failed to unmarshal policies from admission request")
 		return admissionutils.Response(err)
 	}
-	err = validate.ValidateCleanupPolicy(logger, policy, h.client, false)
-	if err != nil {
+	if err := validation.Validate(ctx, logger, h.client, policy); err != nil {
 		logger.Error(err, "policy validation errors")
+		return admissionutils.Response(err)
 	}
-	return admissionutils.Response(err)
+	return nil
 }
