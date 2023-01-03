a0d7b116571fb48adefdfbe9f5fc21a92f0e6b26
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-01-03 09:41:48 +0100

    delay
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/controllers/report/background/controller.go
+++ pkg/controllers/report/background/controller.go
@@ -38,7 +38,7 @@ const (
 	ControllerName         = "background-scan-controller"
 	maxRetries             = 10
 	annotationLastScanTime = "audit.kyverno.io/last-scan-time"
-	enqueueDelay           = 1 * time.Second
+	enqueueDelay           = 30 * time.Second
 )
 
 type controller struct {
