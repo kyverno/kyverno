a4cf04748e2b11cc06f9955349aca3c87854a010
ShutingZhao <shuting@nirmata.com>
2023-02-09 20:29:53 +0800

    refactor retry annotation updates
    Signed-off-by: ShutingZhao <shuting@nirmata.com>

---
+++ pkg/background/generate/log.go
@@ -0,0 +1,5 @@
+package generate
+
+import "github.com/kyverno/kyverno/pkg/logging"
+
+var logger = logging.WithName("background-generate")
---
+++ pkg/background/generate/utils.go
@@ -0,0 +1,34 @@
+package generate
+
+import (
+	"fmt"
+	"strconv"
+
+	kyvernov1beta1 "github.com/kyverno/kyverno/api/kyverno/v1beta1"
+)
+
+func increaseRetryAnnotation(ur *kyvernov1beta1.UpdateRequest) (int, map[string]string, error) {
+	urAnnotations := ur.Annotations
+	if len(urAnnotations) == 0 {
+		urAnnotations = map[string]string{
+			urAnnotations[kyvernov1beta1.URGenerateRetryCountAnnotation]: "1",
+		}
+	}
+
+	var retry = 1
+	val, ok := urAnnotations[kyvernov1beta1.URGenerateRetryCountAnnotation]
+	if !ok {
+		urAnnotations[kyvernov1beta1.URGenerateRetryCountAnnotation] = "1"
+	} else {
+		retryUint, err := strconv.ParseUint(val, 10, 64)
+		if err != nil {
+			return retry, urAnnotations, fmt.Errorf("unable to convert retry-count %v: %w", val, err)
+		}
+		retry = int(retryUint)
+		retry += 1
+		incrementedRetryString := strconv.Itoa(retry)
+		urAnnotations[kyvernov1beta1.URGenerateRetryCountAnnotation] = incrementedRetryString
+	}
+
+	return retry, urAnnotations, nil
+}
