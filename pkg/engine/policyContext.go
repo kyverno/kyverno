09e68973e9eca7f9240a14f9637cd663bc494b5b
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-02-24 21:10:36 +0100

    fix
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/engine/policyContext.go
+++ pkg/engine/policyContext.go
@@ -66,11 +66,6 @@ func (c *PolicyContext) Policy() kyvernov1.PolicyInterface {
 }
 
 func (c *PolicyContext) NewResource() unstructured.Unstructured {
-	// resource, err := c.jsonContext.Query("request.object")
-	// if err != nil || resource == nil {
-	// 	return c.newResource
-	// }
-	// return unstructured.Unstructured{Object: resource.(map[string]interface{})}
 	return c.newResource
 }
 
