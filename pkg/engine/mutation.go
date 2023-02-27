7e3a8dda383168f843aa6f7d0257a143d6221bca
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-02-27 15:21:06 +0100

    remove explicit AddResource call
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/engine/mutation.go
+++ pkg/engine/mutation.go
@@ -30,7 +30,6 @@ func (e *engine) mutate(
 	policy := policyContext.Policy()
 	resp = engineapi.NewEngineResponseFromPolicyContext(policyContext, nil)
 	matchedResource := policyContext.NewResource()
-	enginectx := policyContext.JSONContext()
 	var skippedRules []string
 
 	logger.V(4).Info("start mutate policy processing", "startTime", startTime)
@@ -76,15 +75,6 @@ func (e *engine) mutate(
 				}
 
 				logger.V(3).Info("processing mutate rule")
-				resource, err := policyContext.JSONContext().Query("request.object")
-				policyContext.JSONContext().Reset()
-				if err == nil && resource != nil {
-					if err := enginectx.AddResource(resource.(map[string]interface{})); err != nil {
-						logger.Error(err, "unable to update resource object")
-					}
-				} else {
-					logger.Error(err, "failed to query resource object")
-				}
 
 				if err := internal.LoadContext(ctx, e, policyContext, rule); err != nil {
 					if _, ok := err.(gojmespath.NotFoundError); ok {
