5c95c0d95f60831b27cde6d098b6cf46e3c5271f
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-03-09 12:42:05 +0100

    error
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/clients/dclient/discovery.go
+++ pkg/clients/dclient/discovery.go
@@ -166,12 +166,16 @@ func (c serverResources) findResource(groupVersion string, kind string) (apiReso
 ) {
 	serverPreferredResources, _ := c.cachedClient.ServerPreferredResources()
 	_, serverGroupsAndResources, err := c.cachedClient.ServerGroupsAndResources()
-
 	if err != nil && !strings.Contains(err.Error(), "Got empty response for") {
+		gv, err := schema.ParseGroupVersion(groupVersion)
+		if err != nil {
+			logger.Error(err, "failed to parse group/version", "groupVersion", groupVersion)
+			return nil, nil, schema.GroupVersionResource{}, err
+		}
 		if discovery.IsGroupDiscoveryFailedError(err) {
 			logDiscoveryErrors(err)
-			// } else if isMetricsServerUnavailable(groupVersion, err) {
-			// 	logger.V(3).Info("failed to find preferred resource version", "error", err.Error())
+		} else if isMetricsServerUnavailable(gv, err) {
+			logger.V(3).Info("failed to find preferred resource version", "error", err.Error())
 		} else {
 			logger.Error(err, "failed to find preferred resource version")
 			return nil, nil, schema.GroupVersionResource{}, err
