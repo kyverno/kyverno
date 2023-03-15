dbfeb75793f057966bd5550637e94aff7f2e1305
shuting <shuting@nirmata.com>
2023-03-15 20:27:28 +0800

    feat: ignore admission requests sent by the kyverno background controller (#6499)
    * ignore ARs sent by the kyverno background controller
    
    Signed-off-by: ShutingZhao <shuting@nirmata.com>
    
    * add a kuttl test
    
    Signed-off-by: ShutingZhao <shuting@nirmata.com>
    
    * add background controller sa to exclude usernames
    
    Signed-off-by: ShutingZhao <shuting@nirmata.com>
    
    ---------
    
    Signed-off-by: ShutingZhao <shuting@nirmata.com>

--- pkg/webhooks/resource/updaterequest.go
+++ pkg/webhooks/resource/updaterequest.go
@@ -11,6 +11,7 @@ import (
 	"github.com/kyverno/kyverno/pkg/engine"
 	engineapi "github.com/kyverno/kyverno/pkg/engine/api"
 	"github.com/kyverno/kyverno/pkg/event"
+	"github.com/kyverno/kyverno/pkg/utils/wildcard"
 	"github.com/kyverno/kyverno/pkg/webhooks/resource/generation"
 	webhookutils "github.com/kyverno/kyverno/pkg/webhooks/utils"
 	admissionv1 "k8s.io/api/admission/v1"
@@ -18,6 +19,11 @@ import (
 
 // handleBackgroundApplies applies generate and mutateExisting policies, and creates update requests for background reconcile
 func (h *handlers) handleBackgroundApplies(ctx context.Context, logger logr.Logger, request *admissionv1.AdmissionRequest, policyContext *engine.PolicyContext, generatePolicies, mutatePolicies []kyvernov1.PolicyInterface, ts time.Time) {
+	for _, username := range h.configuration.GetExcludedBackgroundUsernames() {
+		if wildcard.Match(username, policyContext.AdmissionInfo().AdmissionUserInfo.Username) {
+			return
+		}
+	}
 	go h.handleMutateExisting(ctx, logger, request, mutatePolicies, policyContext, ts)
 	h.handleGenerate(ctx, logger, request, generatePolicies, policyContext, ts)
 }
