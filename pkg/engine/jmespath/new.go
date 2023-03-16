ace46aa2510c31c2dcf0eecbac96c5b9524a143b
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-03-16 18:35:01 +0100

    fix
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/engine/jmespath/new.go
+++ pkg/engine/jmespath/new.go
@@ -2,11 +2,10 @@ package jmespath
 
 import (
 	"github.com/jmespath-community/go-jmespath"
-	"github.com/jmespath-community/go-jmespath/pkg/functions"
 )
 
 func New(query string) (jmespath.JMESPath, error) {
-	var funcs []functions.FunctionEntry
+	var funcs []jmespath.FunctionEntry
 	for _, f := range GetFunctions() {
 		funcs = append(funcs, f.FunctionEntry)
 	}
