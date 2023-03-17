96e11a9877a7200df1d031598921c02f21e7cafa
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-03-17 09:57:57 +0100

    fix
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/clients/dclient/discovery.go
+++ pkg/clients/dclient/discovery.go
@@ -11,8 +11,6 @@ import (
 	"github.com/kyverno/kyverno/pkg/utils/wildcard"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/apimachinery/pkg/version"
-	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/client-go/discovery"
 )
 
