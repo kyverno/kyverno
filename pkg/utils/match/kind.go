f1b3e0e0ec80894a3ef9ac0b0ed2a79ec8643dae
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-04-17 17:17:45 +0200

    fix: match on ephemeral containers (#6963)
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/utils/match/kind.go
+++ pkg/utils/match/kind.go
@@ -6,18 +6,18 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
+var podGVK = schema.GroupVersionKind{Group: "", Version: "v1", Kind: "Pod"}
+
 // CheckKind checks if the resource kind matches the kinds in the policy. If the policy matches on subresources, then those resources are
 // present in the subresourceGVKToAPIResource map. Set allowEphemeralContainers to true to allow ephemeral containers to be matched even when the
 // policy does not explicitly match on ephemeral containers and only matches on pods.
 func CheckKind(kinds []string, gvk schema.GroupVersionKind, subresource string, allowEphemeralContainers bool) bool {
 	for _, k := range kinds {
 		group, version, kind, sub := kubeutils.ParseKindSelector(k)
-		if wildcard.Match(group, gvk.Group) && wildcard.Match(version, gvk.Version) && wildcard.Match(kind, gvk.Kind) && wildcard.Match(sub, subresource) {
-			return true
-		}
-		if allowEphemeralContainers {
-			// TODO: we should check if GVK matches v1/Pod
-			if subresource == "ephemeralcontainers" {
+		if wildcard.Match(group, gvk.Group) && wildcard.Match(version, gvk.Version) && wildcard.Match(kind, gvk.Kind) {
+			if wildcard.Match(sub, subresource) {
+				return true
+			} else if allowEphemeralContainers && gvk == podGVK && subresource == "ephemeralcontainers" {
 				return true
 			}
 		}
