80926c02ff776a2fbd36d712fb0d766d63efc4ba
Mariam Fahmy <55502281+MariamFahmy98@users.noreply.github.com>
2023-05-10 11:12:53 +0300

    Supporting ValidatingAdmissionPolicy in kyverno cli (apply and test command) (#6656)
    * feat: add policy reporter to the dev lab
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    
    * refactor: remove obsolete structs from CLI
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    
    * more
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    
    * fix
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    
    * fix
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    
    * fix
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    
    * codegen
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    
    * Supporting ValidatingAdmissionPolicy in kyverno apply
    
    Signed-off-by: Mariam Fahmy <mariamfahmy66@gmail.com>
    
    * chore: bump k8s from v0.26.3 to v0.27.0-rc.0
    
    Signed-off-by: Mariam Fahmy <mariamfahmy66@gmail.com>
    
    * Support validating admission policy in kyverno apply
    
    Signed-off-by: Mariam Fahmy <mariamfahmy66@gmail.com>
    
    * Support validating admission policy in kyverno test
    
    Signed-off-by: Mariam Fahmy <mariamfahmy66@gmail.com>
    
    * refactoring
    
    Signed-off-by: Mariam Fahmy <mariamfahmy66@gmail.com>
    
    * Adding kyverno apply tests for validating admission policy
    
    Signed-off-by: Mariam Fahmy <mariamfahmy66@gmail.com>
    
    * fix
    
    Signed-off-by: Mariam Fahmy <mariamfahmy66@gmail.com>
    
    * fix
    
    Signed-off-by: Mariam Fahmy <mariamfahmy66@gmail.com>
    
    * running codegen-all
    
    Signed-off-by: Mariam Fahmy <mariamfahmy66@gmail.com>
    
    * fix
    
    Signed-off-by: Mariam Fahmy <mariamfahmy66@gmail.com>
    
    * Adding IsVap field in TestResults
    
    Signed-off-by: Mariam Fahmy <mariamfahmy66@gmail.com>
    
    * chore: bump k8s from v0.27.0-rc.0 to v0.27.1
    
    Signed-off-by: Mariam Fahmy <mariamfahmy66@gmail.com>
    
    * fix
    
    Signed-off-by: Mariam Fahmy <mariamfahmy66@gmail.com>
    
    * fix
    
    Signed-off-by: Mariam Fahmy <mariamfahmy66@gmail.com>
    
    * Fix vap in engine response
    
    Signed-off-by: Mariam Fahmy <mariamfahmy66@gmail.com>
    
    * codegen
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    
    ---------
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    Signed-off-by: Mariam Fahmy <mariamfahmy66@gmail.com>
    Co-authored-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    Co-authored-by: Jim Bugwadia <jim@nirmata.com>
    Signed-off-by: Vishal Choudhary <sendtovishalchoudhary@gmail.com>

--- pkg/utils/yaml/loadpolicy.go
+++ pkg/utils/yaml/loadpolicy.go
@@ -3,78 +3,98 @@ package yaml
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 
 	kyvernov1 "github.com/kyverno/kyverno/api/kyverno/v1"
 	log "github.com/kyverno/kyverno/pkg/logging"
+	"k8s.io/api/admissionregistration/v1alpha1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/yaml"
 )
 
 // GetPolicy extracts policies from YAML bytes
-func GetPolicy(bytes []byte) (policies []kyvernov1.PolicyInterface, err error) {
+func GetPolicy(bytes []byte) (policies []kyvernov1.PolicyInterface, validatingAdmissionPolicies []v1alpha1.ValidatingAdmissionPolicy, err error) {
 	documents, err := SplitDocuments(bytes)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 	for _, thisPolicyBytes := range documents {
 		policyBytes, err := yaml.ToJSON(thisPolicyBytes)
 		if err != nil {
-			return nil, fmt.Errorf("failed to convert to JSON: %v", err)
+			return nil, nil, fmt.Errorf("failed to convert to JSON: %v", err)
 		}
 		us := &unstructured.Unstructured{}
 
 		if err := json.Unmarshal(policyBytes, us); err != nil {
-			return nil, fmt.Errorf("failed to decode policy: %v", err)
+			return nil, nil, fmt.Errorf("failed to decode policy: %v", err)
 		}
 		if us.IsList() {
 			list, err := us.ToList()
 			if err != nil {
-				return nil, fmt.Errorf("failed to decode policy list: %v", err)
+				return nil, nil, fmt.Errorf("failed to decode policy list: %v", err)
 			}
 
 			for i := range list.Items {
 				item := list.Items[i]
-				if policies, err = addPolicy(policies, &item); err != nil {
-					return nil, err
+				if policies, validatingAdmissionPolicies, err = addPolicy(policies, validatingAdmissionPolicies, &item); err != nil {
+					return nil, nil, err
 				}
 			}
 		} else {
-			if policies, err = addPolicy(policies, us); err != nil {
-				return nil, err
+			if policies, validatingAdmissionPolicies, err = addPolicy(policies, validatingAdmissionPolicies, us); err != nil {
+				return nil, nil, err
 			}
 		}
 	}
-	return policies, nil
+	return policies, validatingAdmissionPolicies, nil
 }
 
-func addPolicy(policies []kyvernov1.PolicyInterface, us *unstructured.Unstructured) ([]kyvernov1.PolicyInterface, error) {
-	var policy kyvernov1.PolicyInterface
-	if us.GetKind() == "ClusterPolicy" {
-		policy = &kyvernov1.ClusterPolicy{}
-	} else {
-		policy = &kyvernov1.Policy{}
-	}
+func addPolicy(policies []kyvernov1.PolicyInterface, validatingAdmissionPolicies []v1alpha1.ValidatingAdmissionPolicy, us *unstructured.Unstructured) ([]kyvernov1.PolicyInterface, []v1alpha1.ValidatingAdmissionPolicy, error) {
+	kind := us.GetKind()
 
-	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(us.Object, policy); err != nil {
-		return nil, fmt.Errorf("failed to decode policy: %v", err)
-	}
+	if strings.Compare(kind, "ValidatingAdmissionPolicy") == 0 {
+		validatingAdmissionPolicy := &v1alpha1.ValidatingAdmissionPolicy{}
 
-	if policy.GetKind() == "" {
-		log.V(3).Info("skipping file as policy.TypeMeta.Kind not found")
-		return policies, nil
-	}
-	if policy.GetKind() != "ClusterPolicy" && policy.GetKind() != "Policy" {
-		return nil, fmt.Errorf("resource %s/%s is not a Policy or a ClusterPolicy", policy.GetKind(), policy.GetName())
-	}
-
-	if policy.GetKind() == "Policy" {
-		if policy.GetNamespace() == "" {
-			policy.SetNamespace("default")
+		if err := runtime.DefaultUnstructuredConverter.FromUnstructured(us.Object, validatingAdmissionPolicy); err != nil {
+			return policies, nil, fmt.Errorf("failed to decode policy: %v", err)
 		}
+
+		if validatingAdmissionPolicy.Kind == "" {
+			log.V(3).Info("skipping file as ValidatingAdmissionPolicy.Kind not found")
+			return policies, validatingAdmissionPolicies, nil
+		}
+
+		validatingAdmissionPolicies = append(validatingAdmissionPolicies, *validatingAdmissionPolicy)
 	} else {
-		policy.SetNamespace("")
+		var policy kyvernov1.PolicyInterface
+		if us.GetKind() == "ClusterPolicy" {
+			policy = &kyvernov1.ClusterPolicy{}
+		} else {
+			policy = &kyvernov1.Policy{}
+		}
+
+		if err := runtime.DefaultUnstructuredConverter.FromUnstructured(us.Object, policy); err != nil {
+			return nil, validatingAdmissionPolicies, fmt.Errorf("failed to decode policy: %v", err)
+		}
+
+		if policy.GetKind() == "" {
+			log.V(3).Info("skipping file as policy.TypeMeta.Kind not found")
+			return policies, validatingAdmissionPolicies, nil
+		}
+		if policy.GetKind() != "ClusterPolicy" && policy.GetKind() != "Policy" {
+			return nil, validatingAdmissionPolicies, fmt.Errorf("resource %s/%s is not a Policy or a ClusterPolicy", policy.GetKind(), policy.GetName())
+		}
+
+		if policy.GetKind() == "Policy" {
+			if policy.GetNamespace() == "" {
+				policy.SetNamespace("default")
+			}
+		} else {
+			policy.SetNamespace("")
+		}
+		policies = append(policies, policy)
 	}
-	policies = append(policies, policy)
-	return policies, nil
+
+	return policies, validatingAdmissionPolicies, nil
 }
