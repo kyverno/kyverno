0f2604e4c50ce89d53faacab82a59280ce2230e6
shuting <shuting@nirmata.com>
2023-05-15 22:27:45 +0800

    fix: panic for policy variable validation (#7079)
    * fix panic
    
    Signed-off-by: ShutingZhao <shuting@nirmata.com>
    
    * check errors
    
    Signed-off-by: ShutingZhao <shuting@nirmata.com>
    
    ---------
    
    Signed-off-by: ShutingZhao <shuting@nirmata.com>
    Co-authored-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    Signed-off-by: Vishal Choudhary <sendtovishalchoudhary@gmail.com>

--- pkg/validation/policy/validate.go
+++ pkg/validation/policy/validate.go
@@ -374,7 +374,10 @@ func Validate(policy, oldPolicy kyvernov1.PolicyInterface, client dclient.Interf
 }
 
 func ValidateVariables(p kyvernov1.PolicyInterface, backgroundMode bool) error {
-	vars := hasVariables(p)
+	vars, err := hasVariables(p)
+	if err != nil {
+		return err
+	}
 	if backgroundMode {
 		if err := containsUserVariables(p, vars); err != nil {
 			return fmt.Errorf("only select variables are allowed in background mode. Set spec.background=false to disable background mode for this policy rule: %s ", err)
@@ -425,7 +428,10 @@ func hasInvalidVariables(policy kyvernov1.PolicyInterface, background bool) erro
 }
 
 func ValidateOnPolicyUpdate(p kyvernov1.PolicyInterface, onPolicyUpdate bool) error {
-	vars := hasVariables(p)
+	vars, err := hasVariables(p)
+	if err != nil {
+		return err
+	}
 	if len(vars) == 0 {
 		return nil
 	}
@@ -469,11 +475,14 @@ func ruleForbiddenSectionsHaveVariables(rule *kyvernov1.Rule) error {
 }
 
 // hasVariables - check for variables in the policy
-func hasVariables(policy kyvernov1.PolicyInterface) [][]string {
-	policy = cleanup(policy)
-	policyRaw, _ := json.Marshal(policy)
+func hasVariables(policy kyvernov1.PolicyInterface) ([][]string, error) {
+	polCopy := cleanup(policy.CreateDeepCopy())
+	policyRaw, err := json.Marshal(polCopy)
+	if err != nil {
+		return nil, fmt.Errorf("failed to serialize the policy: %v", err)
+	}
 	matches := regex.RegexVariables.FindAllStringSubmatch(string(policyRaw), -1)
-	return matches
+	return matches, nil
 }
 
 func cleanup(policy kyvernov1.PolicyInterface) kyvernov1.PolicyInterface {
