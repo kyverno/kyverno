3c85c6eb3aeed88bc49cb7e4448d107743989fde
ShutingZhao <shuting@nirmata.com>
2023-05-17 17:09:00 +0800

    add rule name in ur for mutate existing
    Signed-off-by: ShutingZhao <shuting@nirmata.com>

--- pkg/background/mutate/mutate.go
+++ pkg/background/mutate/mutate.go
@@ -78,14 +78,14 @@ func (c *mutateExistingController) ProcessUR(ur *kyvernov1beta1.UpdateRequest) e
 	logger := c.log.WithValues("name", ur.GetName(), "policy", ur.Spec.GetPolicyKey(), "resource", ur.Spec.GetResource().String())
 	var errs []error
 
-	policy, err := c.getPolicy(ur.Spec.Policy)
+	policy, err := c.getPolicy(ur)
 	if err != nil {
 		logger.Error(err, "failed to get policy")
 		return err
 	}
 
 	for _, rule := range policy.GetSpec().Rules {
-		if !rule.IsMutateExisting() {
+		if !rule.IsMutateExisting() || ur.Spec.Rule != rule.Name {
 			continue
 		}
 
@@ -213,17 +213,25 @@ func (c *mutateExistingController) ProcessUR(ur *kyvernov1beta1.UpdateRequest) e
 	return updateURStatus(c.statusControl, *ur, err)
 }
 
-func (c *mutateExistingController) getPolicy(key string) (kyvernov1.PolicyInterface, error) {
-	pNamespace, pName, err := cache.SplitMetaNamespaceKey(key)
+func (c *mutateExistingController) getPolicy(ur *kyvernov1beta1.UpdateRequest) (policy kyvernov1.PolicyInterface, err error) {
+	pNamespace, pName, err := cache.SplitMetaNamespaceKey(ur.Spec.Policy)
 	if err != nil {
 		return nil, err
 	}
 
 	if pNamespace != "" {
-		return c.npolicyLister.Policies(pNamespace).Get(pName)
+		policy, err = c.npolicyLister.Policies(pNamespace).Get(pName)
 	}
 
-	return c.policyLister.Get(pName)
+	policy, err = c.policyLister.Get(pName)
+	if err != nil {
+		return nil, err
+	}
+
+	// newPol := policy.CreateDeepCopy()
+	// newPol.
+	// policy.GetSpec().Rules
+	return
 }
 
 func (c *mutateExistingController) report(err error, policy, rule string, target *unstructured.Unstructured) {
