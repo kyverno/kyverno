6048c38c52ac5e5b58ea5ac1cf027e7bca0f1bee
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-06-21 15:02:12 +0200

    fix: stop using lister in tls renewer
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- cmd/kyverno/main.go
+++ cmd/kyverno/main.go
@@ -244,7 +244,6 @@ func main() {
 	var wg sync.WaitGroup
 	certRenewer := tls.NewCertRenewer(
 		setup.KubeClient.CoreV1().Secrets(config.KyvernoNamespace()),
-		secretLister,
 		tls.CertRenewalInterval,
 		tls.CAValidityDuration,
 		tls.TLSValidityDuration,
