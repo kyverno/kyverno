1ba5a380bbf7fa6d1423999a5539d7bb87802db7
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-07-03 20:56:16 +0200

    fix
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- cmd/cli/kubectl-kyverno/utils/getter/getter.go
+++ cmd/cli/kubectl-kyverno/utils/getter/getter.go
@@ -1,19 +1,18 @@
 package getter
 
 import (
+	"fmt"
 	"os"
 
 	gogettter "github.com/hashicorp/go-getter"
 )
 
 func Get(src string) (string, error) {
-	if _, err := os.Stat(src); err == nil {
-		return src, nil
-	}
 	dst, err := os.MkdirTemp("", "kubectl-kyverno")
 	if err != nil {
 		return "", err
 	}
+	fmt.Println(src, dst)
 	return dst, gogettter.GetAny(dst, src, func(c *gogettter.Client) error {
 		pwd, err := os.Getwd()
 		if err != nil {
