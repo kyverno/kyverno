cc65d62db807ecfb9e643a4c3ad34ec998079625
Mike Bryant <mike.bryant@mettle.co.uk>
2023-07-11 13:58:19 +0100

    chore: run make codegen-all
    Signed-off-by: Mike Bryant <mike.bryant@mettle.co.uk>

--- pkg/client/applyconfigurations/kyverno/v1/spec.go
+++ pkg/client/applyconfigurations/kyverno/v1/spec.go
@@ -37,6 +37,7 @@ type SpecApplyConfiguration struct {
 	MutateExistingOnPolicyUpdate     *bool                                               `json:"mutateExistingOnPolicyUpdate,omitempty"`
 	GenerateExistingOnPolicyUpdate   *bool                                               `json:"generateExistingOnPolicyUpdate,omitempty"`
 	GenerateExisting                 *bool                                               `json:"generateExisting,omitempty"`
+	UseServerSideApply               *bool                                               `json:"useServerSideApply,omitempty"`
 }
 
 // SpecApplyConfiguration constructs an declarative configuration of the Spec type for use with
@@ -148,5 +149,13 @@ func (b *SpecApplyConfiguration) WithGenerateExistingOnPolicyUpdate(value bool) 
 // If called multiple times, the GenerateExisting field is set to the value of the last call.
 func (b *SpecApplyConfiguration) WithGenerateExisting(value bool) *SpecApplyConfiguration {
 	b.GenerateExisting = &value
+	return b
+}
+
+// WithUseServerSideApply sets the UseServerSideApply field in the declarative configuration to the given value
+// and returns the receiver, so that objects can be built by chaining "With" function invocations.
+// If called multiple times, the UseServerSideApply field is set to the value of the last call.
+func (b *SpecApplyConfiguration) WithUseServerSideApply(value bool) *SpecApplyConfiguration {
+	b.UseServerSideApply = &value
 	return b
 }
