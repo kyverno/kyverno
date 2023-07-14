35685aea5be04cbef6726db730232c522c3756c4
Vishal Choudhary <sendtovishalchoudhary@gmail.com>
2023-07-14 23:55:57 +0530

    fix: add severity and category to cpol report
    Signed-off-by: Vishal Choudhary <sendtovishalchoudhary@gmail.com>

--- cmd/cli/kubectl-kyverno/apply/report.go
+++ cmd/cli/kubectl-kyverno/apply/report.go
@@ -98,7 +98,9 @@ func buildPolicyResults(auditWarn bool, engineResponses ...engineapi.EngineRespo
 						UID:        engineResponse.Resource.GetUID(),
 					},
 				},
-				Scored: true,
+				Scored:   true,
+				Category: ann[kyverno.AnnotationPolicyCategory],
+				Severity: severityFromString(ann[kyverno.AnnotationPolicySeverity]),
 			}
 
 			if ruleResponse.Status() == engineapi.RuleStatusSkip {
@@ -130,4 +132,16 @@ func buildPolicyResults(auditWarn bool, engineResponses ...engineapi.EngineRespo
 	}
 
 	return results
+}
+
+func severityFromString(severity string) policyreportv1alpha2.PolicySeverity {
+	switch severity {
+	case policyreportv1alpha2.SeverityHigh:
+		return policyreportv1alpha2.SeverityHigh
+	case policyreportv1alpha2.SeverityMedium:
+		return policyreportv1alpha2.SeverityMedium
+	case policyreportv1alpha2.SeverityLow:
+		return policyreportv1alpha2.SeverityLow
+	}
+	return ""
 }
