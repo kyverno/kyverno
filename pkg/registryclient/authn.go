f673fbe0602bdee66d1a16a8bcc00cead859aa1a
Vishal Choudhary <sendtovishalchoudhary@gmail.com>
2023-07-25 12:44:34 +0530

    feat: add registry checking
    Signed-off-by: Vishal Choudhary <sendtovishalchoudhary@gmail.com>

--- pkg/registryclient/authn.go
+++ pkg/registryclient/authn.go
@@ -2,6 +2,9 @@ package registryclient
 
 import (
 	"context"
+	"net/url"
+	"regexp"
+	"strings"
 
 	"github.com/fluxcd/pkg/oci/auth/aws"
 	"github.com/fluxcd/pkg/oci/auth/azure"
@@ -9,6 +12,21 @@ import (
 	"github.com/google/go-containerregistry/pkg/authn"
 	"github.com/google/go-containerregistry/pkg/name"
 	corev1listers "k8s.io/client-go/listers/core/v1"
+)
+
+var acrRE = regexp.MustCompile(`.*\.azurecr\.io|.*\.azurecr\.cn|.*\.azurecr\.de|.*\.azurecr\.us`)
+var ecrPattern = regexp.MustCompile(`(^[a-zA-Z0-9][a-zA-Z0-9-_]*)\.dkr\.ecr(-fips)?\.([a-zA-Z0-9][a-zA-Z0-9-_]*)\.amazonaws\.com(\.cn)?$`)
+
+const (
+	mcrHostname   = "mcr.microsoft.com"
+	tokenUsername = "<token>"
+
+	ServiceECR          = "ecr"
+	ServiceECRPublic    = "ecr-public"
+	proxyEndpointScheme = "https://"
+	programName         = "docker-credential-ecr-login"
+	ecrPublicName       = "public.ecr.aws"
+	ecrPublicEndpoint   = proxyEndpointScheme + ecrPublicName
 )
 
 type autoRefreshSecrets struct {
@@ -36,12 +54,33 @@ type azurekeychain struct{}
 var AzureKeychain authn.Keychain = azurekeychain{}
 
 func (azurekeychain) Resolve(resource authn.Resource) (authn.Authenticator, error) {
+	if !isACRRegistry(resource.RegistryStr()) {
+		return authn.Anonymous, nil
+	}
+
 	ref, err := name.ParseReference(resource.String())
 	if err != nil {
 		return nil, err
 	}
+
 	azClient := azure.NewClient()
-	return azClient.Login(context.TODO(), true, resource.String(), ref)
+	auth, err := azClient.Login(context.TODO(), true, resource.String(), ref)
+	if err != nil {
+		return authn.Anonymous, nil
+	}
+	return auth, nil
+}
+
+func isACRRegistry(input string) bool {
+	serverURL, err := url.Parse("https://" + input)
+	if err != nil {
+		return false
+	}
+	if serverURL.Hostname() == mcrHostname {
+		return true
+	}
+	matches := acrRE.FindStringSubmatch(serverURL.Hostname())
+	return len(matches) != 0
 }
 
 type awskeychain struct{}
@@ -49,8 +88,28 @@ type awskeychain struct{}
 var AWSKeychain authn.Keychain = awskeychain{}
 
 func (awskeychain) Resolve(resource authn.Resource) (authn.Authenticator, error) {
+	if !isAWSRegistry(resource.RegistryStr()) {
+		return authn.Anonymous, nil
+	}
 	awsClient := aws.NewClient()
-	return awsClient.Login(context.TODO(), true, resource.String())
+	auth, err := awsClient.Login(context.TODO(), true, resource.String())
+	if err != nil {
+		return authn.Anonymous, nil
+	}
+	return auth, nil
+}
+
+func isAWSRegistry(input string) bool {
+	input = strings.TrimPrefix(input, proxyEndpointScheme)
+	serverURL, err := url.Parse(proxyEndpointScheme + input)
+	if err != nil {
+		return false
+	}
+	if serverURL.Hostname() == ecrPublicName {
+		return true
+	}
+	matches := ecrPattern.FindStringSubmatch(serverURL.Hostname())
+	return len(matches) >= 3
 }
 
 type gcpkeychain struct{}
@@ -58,10 +117,19 @@ type gcpkeychain struct{}
 var GCPKeychain authn.Keychain = gcpkeychain{}
 
 func (gcpkeychain) Resolve(resource authn.Resource) (authn.Authenticator, error) {
+	if !gcp.ValidHost(resource.RegistryStr()) {
+		return authn.Anonymous, nil
+	}
+
 	ref, err := name.ParseReference(resource.String())
 	if err != nil {
-		return nil, err
+		return authn.Anonymous, nil
 	}
+
 	gcpClient := gcp.NewClient()
-	return gcpClient.Login(context.TODO(), true, resource.String(), ref)
+	auth, err := gcpClient.Login(context.TODO(), true, resource.String(), ref)
+	if err != nil {
+		return authn.Anonymous, nil
+	}
+	return auth, nil
 }
