3f11a6e7f830072d78e91535c91c020e6331c2c0
shahbaz <shahbaz@shahbaz.myguest.virtualbox.org>
2023-07-26 11:45:31 +0530

    Increase code converage
    Signed-off-by: shahbaz <shahbaz@shahbaz.myguest.virtualbox.org>

--- pkg/policy/mutate/validate_test.go
+++ pkg/policy/mutate/validate_test.go
@@ -10,6 +10,9 @@ import (
 )
 
 func Test_Validate_PatchStrategicMerge_Has_Conditional_Anchors(t *testing.T) {
+	var mutation kyvernov1.Mutation
+	var err error
+	//case 1
 	rawPolicy := []byte(`{
 		"foreach": [{
 		  "patchStrategicMerge": {
@@ -23,11 +26,66 @@ func Test_Validate_PatchStrategicMerge_Has_Conditional_Anchors(t *testing.T) {
 		}]
 	}`)
 
-	var mutation kyvernov1.Mutation
-	err := json.Unmarshal(rawPolicy, &mutation)
+	err = json.Unmarshal(rawPolicy, &mutation)
 	assert.NilError(t, err)
 
 	checker := NewFakeMutate(mutation)
+	if _, err := checker.Validate(context.TODO()); err != nil {
+		assert.Assert(t, err != nil)
+	}
+
+	//case 2
+	rawPolicy = []byte(`{
+		"foreach": [{
+		  "patchStrategicMerge": {
+			"spec": {
+			  "template": {
+			    "spec": {
+				  "containers": {
+					"(name)": "*",
+					"image": "{{regex_replace_all('^([^/]+\\.[^/]+/)?(.*)$','{{@}}','myregistry.corp.com/$2')}}"
+				  }
+				}
+			  }
+			}
+		  }
+		}]
+	}`)
+
+	err = json.Unmarshal(rawPolicy, &mutation)
+	assert.NilError(t, err)
+
+	checker = NewFakeMutate(mutation)
+	if _, err := checker.Validate(context.TODO()); err != nil {
+		assert.Assert(t, err != nil)
+	}
+
+	//case 3
+	rawPolicy = []byte(`{
+		"foreach": [{
+		  "patchStrategicMerge": {
+			"spec": {
+			  "jobTemplate": {
+				"spec": {
+				  "template": {
+					"spec": {
+					  "containers": {
+						"(name)": "*",
+						"image": "{{regex_replace_all('^([^/]+\\.[^/]+/)?(.*)$','{{@}}','myregistry.corp.com/$2')}}"
+					  }
+					}
+				  }
+				}
+		  	  }
+			}
+		  }
+		}]
+	}`)
+
+	err = json.Unmarshal(rawPolicy, &mutation)
+	assert.NilError(t, err)
+
+	checker = NewFakeMutate(mutation)
 	if _, err := checker.Validate(context.TODO()); err != nil {
 		assert.Assert(t, err != nil)
 	}
