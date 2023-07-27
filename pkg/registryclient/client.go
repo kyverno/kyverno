cbddd59d36765d28a913c7f6585712f4a4c733e8
Amit kumar <amit9116260192@gmail.com>
2023-07-27 12:34:36 +0530

    updated comments (#7902)
    Signed-off-by: hackeramitkumar <amit9116260192@gmail.com>
    Co-authored-by: shuting <shuting@nirmata.com>

--- pkg/registryclient/client.go
+++ pkg/registryclient/client.go
@@ -118,7 +118,7 @@ func WithKeychainPullSecrets(lister corev1listers.SecretNamespaceLister, imagePu
 	}
 }
 
-// WithKeychainPullSecrets provides initialize registry client option that allows to use insecure registries.
+// WithCredentialProviders initialize registry client option by using registries credentials
 func WithCredentialProviders(credentialProviders ...string) Option {
 	return func(c *config) error {
 		var chains []authn.Keychain
@@ -143,7 +143,7 @@ func WithCredentialProviders(credentialProviders ...string) Option {
 	}
 }
 
-// WithKeychainPullSecrets provides initialize registry client option that allows to use insecure registries.
+// WithAllowInsecureRegistry initialize registry client option that allows to use insecure registries.
 func WithAllowInsecureRegistry() Option {
 	return func(c *config) error {
 		c.transport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true} //nolint:gosec
