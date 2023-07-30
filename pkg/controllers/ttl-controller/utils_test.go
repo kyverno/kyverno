de1a5150f6d6301e96e11a65fcfb678eabbbb0f3
Ved Ratan <vedratan8@gmail.com>
2023-07-30 11:56:38 +0530

    lint
    Signed-off-by: Ved Ratan <vedratan8@gmail.com>

--- pkg/controllers/ttl-controller/utils_test.go
+++ pkg/controllers/ttl-controller/utils_test.go
@@ -1,45 +1,43 @@
 package ttlcontroller
 
 import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"testing"
 	"time"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
-
 
 type mockMetaObj struct {
 	metav1.ObjectMeta
 }
 
-
 func TestParseDeletionTime(t *testing.T) {
 	// Test cases
 	tests := []struct {
-		creationTime        time.Time
-		ttlValue            string
+		creationTime         time.Time
+		ttlValue             string
 		expectedDeletionTime time.Time
-		expectError         bool
+		expectError          bool
 	}{
 		// Test case 1: ttlValue is a valid duration
 		{
-			creationTime:        time.Date(2023, 7, 18, 12, 0, 0, 0, time.UTC),
-			ttlValue:            "2h30m",
+			creationTime:         time.Date(2023, 7, 18, 12, 0, 0, 0, time.UTC),
+			ttlValue:             "2h30m",
 			expectedDeletionTime: time.Date(2023, 7, 18, 14, 30, 0, 0, time.UTC),
-			expectError:         false,
+			expectError:          false,
 		},
 		// Test case 2: ttlValue is in RFC3339 format
 		{
-			creationTime:        time.Date(2023, 7, 18, 12, 0, 0, 0, time.UTC),
-			ttlValue:            "2023-07-19T120000Z",
+			creationTime:         time.Date(2023, 7, 18, 12, 0, 0, 0, time.UTC),
+			ttlValue:             "2023-07-19T120000Z",
 			expectedDeletionTime: time.Date(2023, 7, 19, 12, 0, 0, 0, time.UTC),
-			expectError:         false,
+			expectError:          false,
 		},
 		// Test case 3: ttlValue is in custom date format
 		{
-			creationTime:        time.Date(2023, 7, 18, 12, 0, 0, 0, time.UTC),
-			ttlValue:            "2023-07-19",
+			creationTime:         time.Date(2023, 7, 18, 12, 0, 0, 0, time.UTC),
+			ttlValue:             "2023-07-19",
 			expectedDeletionTime: time.Date(2023, 7, 19, 0, 0, 0, 0, time.UTC),
-			expectError:         false,
+			expectError:          false,
 		},
 		// Test case 4: Invalid ttlValue
 		{
