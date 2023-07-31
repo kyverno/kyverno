acedb1b02d238784b923b8eb281ff1fd4e4c8e91
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-07-31 16:51:23 +0200

    chore: move more constants (#7944)
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- api/kyverno/constants.go
+++ api/kyverno/constants.go
@@ -2,8 +2,10 @@ package kyverno
 
 const (
 	// Well known labels
-	LabelAppManagedBy  = "app.kubernetes.io/managed-by"
-	LabelCertManagedBy = "cert.kyverno.io/managed-by"
+	LabelAppManagedBy     = "app.kubernetes.io/managed-by"
+	LabelAppComponent     = "app.kubernetes.io/component"
+	LabelCertManagedBy    = "cert.kyverno.io/managed-by"
+	LabelWebhookManagedBy = "webhook.kyverno.io/managed-by"
 	// Well known annotations
 	AnnotationAutogenControllers = "pod-policies.kyverno.io/autogen-controllers"
 	AnnotationPolicyCategory     = "policies.kyverno.io/category"
