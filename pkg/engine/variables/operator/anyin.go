a392e693939125d0c46278f46e984d0ef8001c64
Chandan-DK <chandandk468@gmail.com>
2023-08-17 14:30:27 +0000

    Lazy evaluate isAnyNotIn function
    Signed-off-by: Chandan-DK <chandandk468@gmail.com>

--- pkg/engine/variables/operator/anyin.go
+++ pkg/engine/variables/operator/anyin.go
@@ -200,16 +200,21 @@ func isAnyIn(key []string, value []string) bool {
 
 // isAnyNotIn checks if any of the values in S1 are not in S2
 func isAnyNotIn(key []string, value []string) bool {
-	found := 0
 	for _, valKey := range key {
+		matchFound := false
+
 		for _, valValue := range value {
 			if wildcard.Match(valKey, valValue) || wildcard.Match(valValue, valKey) {
-				found++
+				matchFound = true
 				break
 			}
 		}
+
+		if !matchFound {
+			return true
+		}
 	}
-	return found < len(key)
+	return false
 }
 
 func (anyin AnyInHandler) validateValueWithBoolPattern(_ bool, _ interface{}) bool {
