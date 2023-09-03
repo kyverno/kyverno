afb69b18cc459d06f06019bdf3e2a2c5aa08d65b
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-09-04 00:53:18 +0200

    chore: improve test coverage of source cli utils package
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- cmd/cli/kubectl-kyverno/utils/source/stdin.go
+++ cmd/cli/kubectl-kyverno/utils/source/stdin.go
@@ -4,10 +4,19 @@ import (
 	"os"
 )
 
-func IsStdin() bool {
-	fileInfo, err := os.Stdin.Stat()
+var defaultStater = (*os.File).Stat
+
+func isStdin(stater func(*os.File) (os.FileInfo, error)) bool {
+	if stater == nil {
+		stater = defaultStater
+	}
+	fileInfo, err := stater(os.Stdin)
 	if err != nil {
 		return false
 	}
 	return fileInfo.Mode()&os.ModeCharDevice == 0
+}
+
+func IsStdin() bool {
+	return isStdin(defaultStater)
 }
