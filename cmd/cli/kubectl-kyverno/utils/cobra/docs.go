e411eea188364c4c3da33eb9eca64ad1b0cd48c1
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-09-04 21:56:23 +0200

    refactor: add a cobra utils package to build commands doc (#8255)
    * refactor: add a cobra utils package to build commands doc
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    
    * codegen
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    
    ---------
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

---
+++ cmd/cli/kubectl-kyverno/utils/cobra/docs.go
@@ -0,0 +1,36 @@
+package cobra
+
+func FormatDescription(short bool, url string, lines ...string) string {
+	if len(lines) == 0 {
+		return ""
+	}
+	description := lines[0]
+	description += "\n"
+	if short {
+		return description
+	}
+	for _, line := range lines[1:] {
+		description += "  "
+		description += line
+		description += "\n"
+	}
+	if url != "" {
+		description += "\n"
+		description += "  "
+		description += "For more information visit " + url
+	}
+	return description
+}
+
+func FormatExamples(in ...[]string) string {
+	var examples string
+	for _, example := range in {
+		for _, line := range example {
+			examples += "  "
+			examples += line
+			examples += "\n"
+		}
+		examples += "\n"
+	}
+	return examples
+}
