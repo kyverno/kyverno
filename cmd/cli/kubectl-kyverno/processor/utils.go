045e955a6eb425ee7a2c0aaa41ef1859c7315209
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-09-12 14:07:12 +0200

    refactor: simplify cli processor (#8352)
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- cmd/cli/kubectl-kyverno/processor/utils.go
+++ cmd/cli/kubectl-kyverno/processor/utils.go
@@ -1,79 +1,14 @@
 package processor
 
 import (
-	"strings"
-
-	engineapi "github.com/kyverno/kyverno/pkg/engine/api"
+	kyvernov1 "github.com/kyverno/kyverno/api/kyverno/v1"
 )
 
-func combineRuleResponses(imageResponse engineapi.EngineResponse) engineapi.EngineResponse {
-	if imageResponse.PolicyResponse.RulesAppliedCount() == 0 {
-		return imageResponse
+func policyHasGenerate(policy kyvernov1.PolicyInterface) bool {
+	for _, rule := range policy.GetSpec().Rules {
+		if rule.HasGenerate() {
+			return true
+		}
 	}
-
-	completeRuleResponses := imageResponse.PolicyResponse.Rules
-	var combineRuleResponses []engineapi.RuleResponse
-
-	ruleNameType := make(map[string][]engineapi.RuleResponse)
-	for _, rsp := range completeRuleResponses {
-		key := rsp.Name() + ";" + string(rsp.RuleType())
-		ruleNameType[key] = append(ruleNameType[key], rsp)
-	}
-
-	for key, ruleResponses := range ruleNameType {
-		tokens := strings.Split(key, ";")
-		ruleName := tokens[0]
-		ruleType := tokens[1]
-		var failRuleResponses []engineapi.RuleResponse
-		var errorRuleResponses []engineapi.RuleResponse
-		var passRuleResponses []engineapi.RuleResponse
-		var skipRuleResponses []engineapi.RuleResponse
-
-		ruleMesssage := ""
-		for _, rsp := range ruleResponses {
-			if rsp.Status() == engineapi.RuleStatusFail {
-				failRuleResponses = append(failRuleResponses, rsp)
-			} else if rsp.Status() == engineapi.RuleStatusError {
-				errorRuleResponses = append(errorRuleResponses, rsp)
-			} else if rsp.Status() == engineapi.RuleStatusPass {
-				passRuleResponses = append(passRuleResponses, rsp)
-			} else if rsp.Status() == engineapi.RuleStatusSkip {
-				skipRuleResponses = append(skipRuleResponses, rsp)
-			}
-		}
-		if len(errorRuleResponses) > 0 {
-			for _, errRsp := range errorRuleResponses {
-				ruleMesssage += errRsp.Message() + ";"
-			}
-			errorResponse := engineapi.NewRuleResponse(ruleName, engineapi.RuleType(ruleType), ruleMesssage, engineapi.RuleStatusError)
-			combineRuleResponses = append(combineRuleResponses, *errorResponse)
-			continue
-		}
-
-		if len(failRuleResponses) > 0 {
-			for _, failRsp := range failRuleResponses {
-				ruleMesssage += failRsp.Message() + ";"
-			}
-			failResponse := engineapi.NewRuleResponse(ruleName, engineapi.RuleType(ruleType), ruleMesssage, engineapi.RuleStatusFail)
-			combineRuleResponses = append(combineRuleResponses, *failResponse)
-			continue
-		}
-
-		if len(passRuleResponses) > 0 {
-			for _, passRsp := range passRuleResponses {
-				ruleMesssage += passRsp.Message() + ";"
-			}
-			passResponse := engineapi.NewRuleResponse(ruleName, engineapi.RuleType(ruleType), ruleMesssage, engineapi.RuleStatusPass)
-			combineRuleResponses = append(combineRuleResponses, *passResponse)
-			continue
-		}
-
-		for _, skipRsp := range skipRuleResponses {
-			ruleMesssage += skipRsp.Message() + ";"
-		}
-		skipResponse := engineapi.NewRuleResponse(ruleName, engineapi.RuleType(ruleType), ruleMesssage, engineapi.RuleStatusSkip)
-		combineRuleResponses = append(combineRuleResponses, *skipResponse)
-	}
-	imageResponse.PolicyResponse.Rules = combineRuleResponses
-	return imageResponse
+	return false
 }
