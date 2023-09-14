8d683124af5adcdeeae3c65a441dd90f986271a7
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-09-14 10:50:21 +0200

    defaulting test
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- cmd/cli/kubectl-kyverno/experimental/experimental.go
+++ cmd/cli/kubectl-kyverno/experimental/experimental.go
@@ -6,8 +6,8 @@ import (
 )
 
 const (
-	experimentalEnv    = "KYVERNO_EXPERIMENTAL"
-	kubectlValidateEnv = "KYVERNO_KUBECTL_VALIDATE" //nolint:gosec
+	ExperimentalEnv    = "KYVERNO_EXPERIMENTAL"
+	KubectlValidateEnv = "KYVERNO_KUBECTL_VALIDATE" //nolint:gosec
 )
 
 func getBool(env string) bool {
@@ -18,9 +18,9 @@ func getBool(env string) bool {
 }
 
 func IsEnabled() bool {
-	return getBool(experimentalEnv)
+	return getBool(ExperimentalEnv)
 }
 
 func UseKubectlValidate() bool {
-	return getBool(kubectlValidateEnv)
+	return getBool(KubectlValidateEnv)
 }
