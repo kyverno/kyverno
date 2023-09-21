86201c716b803b59878b3ddec6e2c669aed00e72
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-09-22 00:32:07 +0200

    use loader to load policies
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- cmd/cli/kubectl-kyverno/resource/load.go
+++ cmd/cli/kubectl-kyverno/resource/load.go
@@ -3,8 +3,6 @@ package resource
 import (
 	"github.com/kyverno/kyverno/cmd/cli/kubectl-kyverno/resource/convert"
 	"github.com/kyverno/kyverno/cmd/cli/kubectl-kyverno/resource/loader"
-	yamlutils "github.com/kyverno/kyverno/pkg/utils/yaml"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
 func Load[T any](l loader.Loader, content []byte) (*T, error) {
@@ -17,20 +15,4 @@ func Load[T any](l loader.Loader, content []byte) (*T, error) {
 		return nil, err
 	}
 	return result, nil
-}
-
-func LoadResources(l loader.Loader, content []byte) ([]unstructured.Unstructured, error) {
-	documents, err := yamlutils.SplitDocuments(content)
-	if err != nil {
-		return nil, err
-	}
-	var resources []unstructured.Unstructured
-	for _, document := range documents {
-		_, untyped, err := l.Load(document)
-		if err != nil {
-			return nil, err
-		}
-		resources = append(resources, untyped)
-	}
-	return resources, nil
 }
