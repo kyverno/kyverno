15630ffaaa72555c16bf6c3a608e14b5f61030da
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-09-26 13:44:35 +0200

    fix: creating ClusterAdmissionReports fails for resources with colon in name (#8530)
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/utils/report/labels.go
+++ pkg/utils/report/labels.go
@@ -21,11 +21,11 @@ import (
 const (
 	LabelDomain = "kyverno.io"
 	//	resource labels
-	LabelResourceHash      = "audit.kyverno.io/resource.hash"
-	LabelResourceUid       = "audit.kyverno.io/resource.uid"
-	LabelResourceGVR       = "audit.kyverno.io/resource.gvr"
-	LabelResourceNamespace = "audit.kyverno.io/resource.namespace"
-	LabelResourceName      = "audit.kyverno.io/resource.name"
+	LabelResourceHash           = "audit.kyverno.io/resource.hash"
+	LabelResourceUid            = "audit.kyverno.io/resource.uid"
+	LabelResourceGVR            = "audit.kyverno.io/resource.gvr"
+	AnnotationResourceNamespace = "audit.kyverno.io/resource.namespace"
+	AnnotationResourceName      = "audit.kyverno.io/resource.name"
 	//	policy labels
 	LabelDomainClusterPolicy             = "cpol.kyverno.io"
 	LabelDomainPolicy                    = "pol.kyverno.io"
@@ -99,8 +99,8 @@ func SetResourceGVR(report kyvernov1alpha2.ReportInterface, gvr schema.GroupVers
 }
 
 func SetResourceNamespaceAndName(report kyvernov1alpha2.ReportInterface, namespace, name string) {
-	controllerutils.SetLabel(report, LabelResourceNamespace, namespace)
-	controllerutils.SetLabel(report, LabelResourceName, name)
+	controllerutils.SetAnnotation(report, AnnotationResourceNamespace, namespace)
+	controllerutils.SetAnnotation(report, AnnotationResourceName, name)
 }
 
 func CalculateResourceHash(resource unstructured.Unstructured) string {
@@ -152,7 +152,7 @@ func GetResourceGVR(report metav1.Object) schema.GroupVersionResource {
 }
 
 func GetResourceNamespaceAndName(report kyvernov1alpha2.ReportInterface) (string, string) {
-	return controllerutils.GetLabel(report, LabelResourceNamespace), controllerutils.GetLabel(report, LabelResourceName)
+	return controllerutils.GetAnnotation(report, AnnotationResourceNamespace), controllerutils.GetAnnotation(report, AnnotationResourceName)
 }
 
 func GetResourceHash(report metav1.Object) string {
