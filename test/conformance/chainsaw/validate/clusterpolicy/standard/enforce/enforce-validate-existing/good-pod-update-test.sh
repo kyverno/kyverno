fd86ec9b83a29b5103852fdb4057d063eed2b632
Vishal Choudhary <vishal.choudhary@nirmata.com>
2023-11-28 20:54:33 +0530

    feat: update test
    Signed-off-by: Vishal Choudhary <vishal.choudhary@nirmata.com>

--- test/conformance/chainsaw/validate/clusterpolicy/standard/enforce/enforce-validate-existing/good-pod-update-test.sh
+++ test/conformance/chainsaw/validate/clusterpolicy/standard/enforce/enforce-validate-existing/good-pod-update-test.sh
@@ -1,10 +1,12 @@
 if kubectl label po goodpod foo=bad1 --overwrite 2>&1 | grep -q  "validation error: rule check-labels" 
 then 
   echo "Output:"
-  kubectl label po goodpod foo=bad1 --overwrite
+  kubectl label po goodpod foo=bad1 --overwrite 2>&1
   echo "Test succeed, updating violating resource throws error"
   exit 0
 else 
+  echo "Output:"
+  kubectl label po goodpod foo=bad1 --overwrite 2>&1
   echo "Test failed, updating violating resource did not throw error"
   exit 1
 fi
