46f02a8ba7c9c3bf00854f4e8904712c52a19839
Jim Bugwadia <jim@nirmata.com>
2023-12-03 23:35:36 -0800

    optimize JSON context processing using in-memory maps (#8322)
    * optimize JSON context processing using in memory maps
    
    Signed-off-by: Jim Bugwadia <jim@nirmata.com>
    
    * fix excessive logs
    
    Signed-off-by: Jim Bugwadia <jim@nirmata.com>
    
    * fix mutate resource diff
    
    Signed-off-by: Jim Bugwadia <jim@nirmata.com>
    
    * uncomment tests
    
    Signed-off-by: Jim Bugwadia <jim@nirmata.com>
    
    * copy resource, as it can be modified
    
    Signed-off-by: Jim Bugwadia <jim@nirmata.com>
    
    * clear prior resource to prevent mutating original
    
    Signed-off-by: Jim Bugwadia <jim@nirmata.com>
    
    * linter fix
    
    Signed-off-by: Jim Bugwadia <jim@nirmata.com>
    
    * fix ImageInfo to unstructured conversion
    
    Signed-off-by: Jim Bugwadia <jim@nirmata.com>
    
    * fix custom image extractors
    
    Signed-off-by: Jim Bugwadia <jim@nirmata.com>
    
    * do not update mutated resource in JSON context
    
    Signed-off-by: Jim Bugwadia <jim@nirmata.com>
    
    * address review comments
    
    Signed-off-by: Jim Bugwadia <jim@nirmata.com>
    
    ---------
    
    Signed-off-by: Jim Bugwadia <jim@nirmata.com>
    Signed-off-by: shuting <shuting@nirmata.com>
    Co-authored-by: Vishal Choudhary <sendtovishalchoudhary@gmail.com>
    Co-authored-by: shuting <shuting@nirmata.com>

--- pkg/engine/validation_test.go
+++ pkg/engine/validation_test.go
@@ -6,7 +6,6 @@ import (
 	"strings"
 	"testing"
 
-	kyverno "github.com/kyverno/kyverno/api/kyverno/v1"
 	kyvernov1 "github.com/kyverno/kyverno/api/kyverno/v1"
 	kyvernov1beta1 "github.com/kyverno/kyverno/api/kyverno/v1beta1"
 	"github.com/kyverno/kyverno/pkg/config"
@@ -53,7 +52,7 @@ func testValidate(
 func newPolicyContext(
 	t *testing.T,
 	resource unstructured.Unstructured,
-	operation kyverno.AdmissionOperation,
+	operation kyvernov1.AdmissionOperation,
 	admissionInfo *kyvernov1beta1.RequestInfo,
 ) *PolicyContext {
 	t.Helper()
@@ -2145,7 +2144,7 @@ func executeTest(t *testing.T, test testCase) {
 		t.Fatal(err)
 	}
 
-	pc := newPolicyContext(t, newR, kyverno.AdmissionOperation(request.Operation), &userInfo).
+	pc := newPolicyContext(t, newR, kyvernov1.AdmissionOperation(request.Operation), &userInfo).
 		WithPolicy(&policy).
 		WithOldResource(oldR)
 
