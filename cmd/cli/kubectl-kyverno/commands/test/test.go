0c3244634ef34c0a61db984eef2fec28048deb97
Vishal Choudhary <vishal.choudhary@nirmata.com>
2024-01-05 13:32:20 +0530

    fix: update CLI to use store for fetching regclient
    Signed-off-by: Vishal Choudhary <vishal.choudhary@nirmata.com>

--- cmd/cli/kubectl-kyverno/commands/test/test.go
+++ cmd/cli/kubectl-kyverno/commands/test/test.go
@@ -23,7 +23,6 @@ import (
 	"github.com/kyverno/kyverno/pkg/clients/dclient"
 	"github.com/kyverno/kyverno/pkg/config"
 	engineapi "github.com/kyverno/kyverno/pkg/engine/api"
-	"github.com/kyverno/kyverno/pkg/registryclient"
 	policyvalidation "github.com/kyverno/kyverno/pkg/validation/policy"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
@@ -142,7 +141,7 @@ func runTest(out io.Writer, testCase test.TestCase, auditWarn bool) ([]engineapi
 			Client:                    dClient,
 			Subresources:              vars.Subresources(),
 			Out:                       out,
-			RegistryClient:            registryclient.NewOrDie(),
+			RegistryClient:            store.GetRegistryClient(),
 		}
 		ers, err := processor.ApplyPoliciesOnResource()
 		if err != nil {
