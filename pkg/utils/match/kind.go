7e6e9447b3c9bee0e948f25bd7f97042a10b5867
root <root@pop-os.localdomain>
2024-01-11 12:50:35 +0530

    Updated func CheckKind

--- pkg/utils/match/kind.go
+++ pkg/utils/match/kind.go
@@ -1,6 +1,8 @@
 package match
 
 import (
+	"fmt"
+
 	"github.com/kyverno/kyverno/ext/wildcard"
 	kubeutils "github.com/kyverno/kyverno/pkg/utils/kube"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -12,9 +14,11 @@ var podGVK = schema.GroupVersionKind{Group: "", Version: "v1", Kind: "Pod"}
 // present in the subresourceGVKToAPIResource map. Set allowEphemeralContainers to true to allow ephemeral containers to be matched even when the
 // policy does not explicitly match on ephemeral containers and only matches on pods.
 func CheckKind(kinds []string, gvk schema.GroupVersionKind, subresource string, allowEphemeralContainers bool) bool {
+	fmt.Print("CheckKind was called\n")
 	for _, k := range kinds {
 		group, version, kind, sub := kubeutils.ParseKindSelector(k)
-		if wildcard.Match(group, gvk.Group) && wildcard.Match(version, gvk.Version) && wildcard.Match(kind, gvk.Kind) {
+		matchKind := wildcard.Match(kind, gvk.Kind) || gvk.Kind == "GenericJSONResource"
+		if wildcard.Match(group, gvk.Group) && wildcard.Match(version, gvk.Version) && matchKind {
 			if wildcard.Match(sub, subresource) {
 				return true
 			} else if allowEphemeralContainers && gvk == podGVK && subresource == "ephemeralcontainers" {
