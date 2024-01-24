89ec1cc19d75a73749caba802c8b89557f5a8748
ShutingZhao <shuting@nirmata.com>
2024-01-24 23:54:37 +0800

    add policy validations
    Signed-off-by: ShutingZhao <shuting@nirmata.com>

--- pkg/validation/policy/validate.go
+++ pkg/validation/policy/validate.go
@@ -123,6 +123,10 @@ func Validate(policy, oldPolicy kyvernov1.PolicyInterface, client dclient.Interf
 	spec := policy.GetSpec()
 	background := spec.BackgroundProcessingEnabled()
 	mutateExistingOnPolicyUpdate := spec.GetMutateExistingOnPolicyUpdate()
+	if policy.GetSpec().CustomWebhookConfigurations() &&
+		!kubeutils.HigherThanKubernetesVersion(client.GetKubeClient().Discovery(), logging.GlobalLogger(), 1, 27, 0) {
+		return warnings, fmt.Errorf("custom webhook configurations are only supported in kubernetes version 1.27. and above")
+	}
 
 	warnings = append(warnings, checkValidationFailureAction(spec)...)
 	var errs field.ErrorList
