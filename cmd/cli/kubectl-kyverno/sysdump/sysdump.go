3f0655d945f7687a94b8109b4a433426af60ffb7
Chandan-DK <chandandk468@gmail.com>
2024-01-30 13:21:04 +0000

    initialize clients
    Signed-off-by: Chandan-DK <chandandk468@gmail.com>

--- cmd/cli/kubectl-kyverno/sysdump/sysdump.go
+++ cmd/cli/kubectl-kyverno/sysdump/sysdump.go
@@ -1,7 +1,10 @@
 package sysdump
 
 import (
+	"fmt"
+
 	kyverno "github.com/kyverno/kyverno/pkg/client/clientset/versioned"
+	"github.com/kyverno/kyverno/pkg/config"
 	"github.com/spf13/cobra"
 	"k8s.io/client-go/kubernetes"
 )
@@ -12,6 +15,8 @@ type client struct {
 }
 
 type sysdumpConfig struct {
+	kubeConfig              string
+	context                 string
 	includePolicies         bool
 	includePolicyReports    bool
 	includePolicyExceptions bool
@@ -23,8 +28,31 @@ func Command() *cobra.Command {
 		Use:   "sysdump",
 		Short: "Collect and package information for troubleshooting",
 		RunE: func(cmd *cobra.Command, args []string) error {
+			clients, err := initClients(sysdumpConfiguration.kubeConfig, sysdumpConfiguration.context)
+			if err != nil {
+				return err
+			}
 			return nil
 		},
 	}
+	cmd.Flags().StringVar(&sysdumpConfiguration.kubeConfig, "kubeconfig", "", "path to kubeconfig file with authorization and master location information")
+	cmd.Flags().StringVar(&sysdumpConfiguration.context, "context", "", "The name of the kubeconfig context to use")
 	return cmd
+}
+
+func initClients(kubeConfig string, context string) (*client, error) {
+	var clients client
+	clientConfig, err := config.CreateClientConfigWithContext(kubeConfig, context)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create client config: %v", err)
+	}
+	clients.kubernetesClientSet, err = kubernetes.NewForConfig(clientConfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create kubernetes clientset: %v", err)
+	}
+	clients.kyvernoClientSet, err = kyverno.NewForConfig(clientConfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create kyverno clientset: %v", err)
+	}
+	return &clients, nil
 }
