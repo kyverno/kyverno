769bde01c992a243400a50adf7547c5b39d5c5d4
mohamedasifs123 <142201466+mohamedasifs123@users.noreply.github.com>
2024-02-03 11:35:38 +0530

    Update test.go
    Signed-off-by: mohamedasifs123 <142201466+mohamedasifs123@users.noreply.github.com>

--- cmd/cli/kubectl-kyverno/commands/test/test.go
+++ cmd/cli/kubectl-kyverno/commands/test/test.go
@@ -106,7 +106,10 @@ func runTest(out io.Writer, testCase test.TestCase, registryAccess bool, auditWa
 		ruleSame := false
 		policySame := false
 		for _, policy := range policies {
-			if res.Policy == policy.GetName() || res.Policy == "default/"+policy.GetName() {
+			if policy.IsNamespaced() {
+				ns := policy.GetNamespace()
+				}
+			if res.Policy == policy.GetName() || res.Policy == "default/"+policy.GetName() || res.Policy == ns+"/"+policy.GetName() {
 				policySame = true
 				for _, rule := range autogen.ComputeRules(policy) {
 					if res.Rule == rule.Name {
