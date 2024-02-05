107a97c089fbd8e9e7434cb39920a644c2faf929
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2024-02-05 12:38:07 +0000

    feat: add root command to process generic json resources (#9639)
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

---
+++ cmd/cli/kubectl-kyverno/commands/json/command.go
@@ -0,0 +1,21 @@
+package json
+
+import (
+	"github.com/kyverno/kyverno/cmd/cli/kubectl-kyverno/command"
+	"github.com/spf13/cobra"
+)
+
+func Command() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:          "json",
+		Short:        command.FormatDescription(true, websiteUrl, true, description...),
+		Long:         command.FormatDescription(false, websiteUrl, true, description...),
+		Example:      command.FormatExamples(examples...),
+		Args:         cobra.NoArgs,
+		SilenceUsage: true,
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			return cmd.Help()
+		},
+	}
+	return cmd
+}
