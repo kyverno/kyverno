e54aaab1130b403c59b4d46f93f237bde81bb5e0
sivasathyaseeelan <dnsiva.sathyseelan.chy21@iitbhu.ac.in>
2024-03-28 22:28:24 +0000

    added Validate in ImageVerification
    Signed-off-by: sivasathyaseeelan <dnsiva.sathyseelan.chy21@iitbhu.ac.in>

--- api/kyverno/v1/common_types.go
+++ api/kyverno/v1/common_types.go
@@ -507,6 +507,18 @@ type Validation struct {
 	Assert AssertionTree `json:"assert"`
 }
 
+// ValidateImageVerification checks conditions across multiple image 
+// verification attestations or context entries
+type ValidateImageVerification struct {
+	// Message specifies a custom message to be displayed on failure.
+	// +optional
+	Message string `json:"message,omitempty" yaml:"message,omitempty"`
+
+	// Deny defines conditions used to pass or fail a validation rule.
+	// +optional
+	Deny *Deny `json:"deny,omitempty" yaml:"deny,omitempty"`
+}
+
 // PodSecurity applies exemptions for Kubernetes Pod Security admission
 // by specifying exclusions for Pod Security Standards controls.
 type PodSecurity struct {
