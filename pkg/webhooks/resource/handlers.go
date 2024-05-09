02ece5ed3b1ecf7d6e001369c293f8b09c47e317
Vishal Choudhary <vishal.choudhary@nirmata.com>
2024-05-09 16:42:25 +0530

    feat: add warnings for validation
    Signed-off-by: Vishal Choudhary <vishal.choudhary@nirmata.com>

--- pkg/webhooks/resource/handlers.go
+++ pkg/webhooks/resource/handlers.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"fmt"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/alitto/pond"
@@ -114,22 +113,22 @@ func (h *resourceHandlers) Validate(ctx context.Context, logger logr.Logger, req
 		return errorResponse(logger, request.UID, err, "failed to fetch policy with key")
 	}
 
-	if len(policies) == 0 && len(mutatePolicies) == 0 && len(generatePolicies) == 0 {
+	if len(policies) == 0 && len(mutatePolicies) == 0 && len(generatePolicies) == 0 && len(auditWarnPolicies) == 0 {
 		logger.V(4).Info("no policies matched admission request")
 	}
 
 	logger.V(4).Info("processing policies for validate admission request", "validate", len(policies), "mutate", len(mutatePolicies), "generate", len(generatePolicies))
 
 	vh := validation.NewValidationHandler(logger, h.kyvernoClient, h.engine, h.pCache, h.pcBuilder, h.eventGen, h.admissionReports, h.metricsConfig, h.configuration, h.nsLister)
-	var wg sync.WaitGroup
+	// var wg sync.WaitGroup
 	var ok bool
 	var msg string
 	var warnings []string
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		ok, msg, warnings = vh.HandleValidationEnforce(ctx, request, policies, auditWarnPolicies, startTime)
-	}()
+	// wg.Add(1)
+	// go func() {
+	// 	defer wg.Done()
+	ok, msg, warnings = vh.HandleValidationEnforce(ctx, request, policies, auditWarnPolicies, startTime)
+	// }()
 
 	go h.auditPool.Submit(func() {
 		vh.HandleValidationAudit(ctx, request)
@@ -137,11 +136,11 @@ func (h *resourceHandlers) Validate(ctx context.Context, logger logr.Logger, req
 	if !admissionutils.IsDryRun(request.AdmissionRequest) {
 		h.handleBackgroundApplies(ctx, logger, request, generatePolicies, mutatePolicies, startTime, nil)
 	}
-	if len(policies) == 0 {
+	if len(policies) == 0 && len(auditWarnPolicies) == 0 {
 		return admissionutils.ResponseSuccess(request.UID)
 	}
 
-	wg.Wait()
+	// wg.Wait()
 	if !ok {
 		logger.Info("admission request denied")
 		return admissionutils.Response(request.UID, errors.New(msg), warnings...)
