f1c5a4b64d8c616a2b52c58f63bfed93b1a4a21a
sivasathyaseeelan <dnsiva.sathyaseelan.chy21@iitbhu.ac.in>
2024-05-13 17:45:33 +0530

    some logical changes
    Signed-off-by: sivasathyaseeelan <dnsiva.sathyaseelan.chy21@iitbhu.ac.in>

--- pkg/engine/validation.go
+++ pkg/engine/validation.go
@@ -32,7 +32,8 @@ func (e *engine) validate(
 		handlerFactory := func() (handlers.Handler, error) {
 			hasValidate := rule.HasValidate()
 			hasVerifyImageChecks := rule.HasVerifyImageChecks()
-			if !hasValidate && !hasVerifyImageChecks {
+			HasValidateImageVerification := rule.HasValidateImageVerification()
+			if !hasValidate && !hasVerifyImageChecks && !HasValidateImageVerification {
 				return nil, nil
 			}
 			if hasValidate {
@@ -58,6 +59,8 @@ func (e *engine) validate(
 					rule,
 					e.configuration,
 				)
+			} else if HasValidateImageVerification {
+				return validation.NewValidateImageVerificationHandler()
 			}
 			return nil, nil
 		}
