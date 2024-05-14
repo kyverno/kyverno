0f94789583fb856c484d00594cda515804460c0e
sivasathyaseeelan <dnsiva.sathyaseelan.chy21@iitbhu.ac.in>
2024-05-14 13:26:46 +0530

    some changes
    Signed-off-by: sivasathyaseeelan <dnsiva.sathyaseelan.chy21@iitbhu.ac.in>

--- pkg/validation/policy/validate.go
+++ pkg/validation/policy/validate.go
@@ -709,8 +709,8 @@ func buildContext(rule *kyvernov1.Rule, background bool, target bool) *enginecon
 	re := getAllowedVariables(background, target)
 
 	ctx := enginecontext.NewMockContext(re)
-	addContextVariables(rule.Context, ctx)
 
+	addContextVariables(rule.Context, ctx)
 	addImageVerifyVariables(rule, ctx)
 
 	for _, fe := range rule.Validation.ForEachValidation {
