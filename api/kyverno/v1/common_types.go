9acca232a24fbc9d874d4dbf3f1a00cd23fed870
Mariam Fahmy <mariam.fahmy@nirmata.com>
2024-06-13 20:41:46 +0700

    feat: add generateExisting field under the generate rule (#10441)
    Signed-off-by: Mariam Fahmy <mariam.fahmy@nirmata.com>
    Signed-off-by: pipo02mix <fernando@giantswarm.io>

--- api/kyverno/v1/common_types.go
+++ api/kyverno/v1/common_types.go
@@ -656,6 +656,11 @@ func (v *ForEachValidation) SetAnyPattern(in apiextensions.JSON) {
 
 // Generation defines how new resources should be created and managed.
 type Generation struct {
+	// GenerateExisting controls whether to trigger the rule in existing resources
+	// If is set to "true" the rule will be triggered and applied to existing matched resources.
+	// +optional
+	GenerateExisting *bool `json:"generateExisting,omitempty" yaml:"generateExisting,omitempty"`
+
 	// ResourceSpec contains information to select the resource.
 	ResourceSpec `json:",omitempty" yaml:",omitempty"`
 
@@ -688,6 +693,10 @@ type Generation struct {
 	// CloneList specifies the list of source resource used to populate each generated resource.
 	// +optional
 	CloneList CloneList `json:"cloneList,omitempty" yaml:"cloneList,omitempty"`
+}
+
+func (g *Generation) IsGenerateExisting() *bool {
+	return g.GenerateExisting
 }
 
 type CloneList struct {
