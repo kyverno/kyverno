cde36a3ed5580d411e0776dfb516c00d9664919e
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2024-06-26 10:13:17 +0200

    codegen
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/client/applyconfigurations/utils.go
+++ pkg/client/applyconfigurations/utils.go
@@ -179,8 +179,6 @@ func ForKind(kind schema.GroupVersionKind) interface{} {
 		return &kyvernov2.ConditionApplyConfiguration{}
 	case v2.SchemeGroupVersion.WithKind("Exception"):
 		return &kyvernov2.ExceptionApplyConfiguration{}
-	case v2.SchemeGroupVersion.WithKind("MatchResources"):
-		return &kyvernov2.MatchResourcesApplyConfiguration{}
 	case v2.SchemeGroupVersion.WithKind("PolicyException"):
 		return &kyvernov2.PolicyExceptionApplyConfiguration{}
 	case v2.SchemeGroupVersion.WithKind("PolicyExceptionSpec"):
