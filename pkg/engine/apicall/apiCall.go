abcd379cce098f4b146529af56a268a812b0841c
sivasathyaseeelan <dnsiva.sathyaseelan.chy21@iitbhu.ac.in>
2024-07-03 03:37:42 +0530

    logical changes
    Signed-off-by: sivasathyaseeelan <dnsiva.sathyaseelan.chy21@iitbhu.ac.in>

--- pkg/engine/apicall/apiCall.go
+++ pkg/engine/apicall/apiCall.go
@@ -82,6 +82,15 @@ func (a *apiCall) Execute(ctx context.Context, call *kyvernov1.APICall) ([]byte,
 }
 
 func (a *apiCall) transformAndStore(jsonData []byte) ([]byte, error) {
+	if jsonData == nil {
+		jsonData = a.entry.APICall.Default.Raw
+		err := a.jsonCtx.AddContextEntry(a.entry.Name, jsonData)
+		if err != nil {
+			return nil, fmt.Errorf("failed to add resource data to context entry %s: %w", a.entry.Name, err)
+		}
+
+		return jsonData, nil
+	}
 	if a.entry.APICall.JMESPath == "" {
 		err := a.jsonCtx.AddContextEntry(a.entry.Name, jsonData)
 		if err != nil {
