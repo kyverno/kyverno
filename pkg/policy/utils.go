aa2e68e657ae8bb4d55554b0f2c5aaf2a0e8c3c6
Mariam Fahmy <mariam.fahmy@nirmata.com>
2024-08-28 16:38:30 +0300

    fix: match wildcard names for generateExisting policies
    Signed-off-by: Mariam Fahmy <mariam.fahmy@nirmata.com>

--- pkg/policy/utils.go
+++ pkg/policy/utils.go
@@ -2,29 +2,39 @@ package policy
 
 import (
 	kyvernov1 "github.com/kyverno/kyverno/api/kyverno/v1"
+	"github.com/kyverno/kyverno/ext/wildcard"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
 func resourceMatches(match kyvernov1.ResourceDescription, res unstructured.Unstructured, isNamespacedPolicy bool) bool {
-	if match.Name != "" && res.GetName() != match.Name {
+	if match.Name != "" && !wildcard.Match(match.Name, res.GetName()) {
 		return false
 	}
-	if len(match.Names) > 0 && !contains(match.Names, res.GetName()) {
-		return false
-	}
-	if !isNamespacedPolicy && len(match.Namespaces) > 0 && !contains(match.Namespaces, res.GetNamespace()) {
-		return false
-	}
-	return true
-}
 
-func contains(slice []string, item string) bool {
-	for _, s := range slice {
-		if s == item {
-			return true
+	isMatch := false
+	for _, name := range match.Names {
+		if wildcard.Match(name, res.GetName()) {
+			isMatch = true
+			break
 		}
 	}
-	return false
+	if !isMatch {
+		return false
+	}
+
+	if !isNamespacedPolicy && len(match.Namespaces) > 0 {
+		isMatch = false
+		for _, namespace := range match.Namespaces {
+			if wildcard.Match(namespace, res.GetNamespace()) {
+				isMatch = true
+				break
+			}
+		}
+		if !isMatch {
+			return false
+		}
+	}
+	return true
 }
 
 func castPolicy(p interface{}) kyvernov1.PolicyInterface {
