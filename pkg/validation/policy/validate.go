00831540bd00d79dd5be9aea592b3929275aefe8
ShutingZhao <shuting@nirmata.com>
2024-09-23 18:28:52 +0800

    fix: restrict rule spec changes except for generate pattern
    Signed-off-by: ShutingZhao <shuting@nirmata.com>

--- pkg/validation/policy/validate.go
+++ pkg/validation/policy/validate.go
@@ -227,9 +227,9 @@ func Validate(policy, oldPolicy kyvernov1.PolicyInterface, client dclient.Interf
 		}
 	}
 
-	if err := immutableGenerateFields(policy, oldPolicy); err != nil {
-		warnings = append(warnings, fmt.Sprintf("no synchronization will be performed to the old target resource upon policy updates: %s", err.Error()))
-		return warnings, nil
+	if warning, err := immutableGenerateFields(policy, oldPolicy); warning != "" || err != nil {
+		warnings = append(warnings, fmt.Sprintf("no synchronization will be performed to the old target resource upon policy updates: %s", warning))
+		return warnings, err
 	}
 
 	rules := autogen.ComputeRules(policy, "")
