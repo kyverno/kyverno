ac513ff6d365a3349f5f73825ed8f6143e973dbb
ShivamJha2436 <shivamkumar87148@gmail.com>
2024-10-04 15:37:43 +0530

    Fixed the linter issue
    Signed-off-by: ShivamJha2436 <shivamkumar87148@gmail.com>

--- pkg/controllers/ttl/controller_test.go
+++ pkg/controllers/ttl/controller_test.go
@@ -7,7 +7,6 @@ import (
 	"github.com/kyverno/kyverno/api/kyverno"
 	"github.com/stretchr/testify/assert"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	
 )
 
 // TestDeterminePropagationPolicy tests the determinePropagationPolicy function
