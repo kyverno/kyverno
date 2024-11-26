58a1bf2acd2ac553d0a26420ffb15cf7cc274681
Tomas Aschan <tomasl@spotify.com>
2024-11-26 15:42:42 +0100

    feat: Show textual diff when generate test fails
    Signed-off-by: Tomas Aschan <tomasl@spotify.com>

--- cmd/cli/kubectl-kyverno/commands/test/compare.go
+++ cmd/cli/kubectl-kyverno/commands/test/compare.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 
 	"github.com/go-git/go-billy/v5"
+	"github.com/kyverno/kyverno/cmd/cli/kubectl-kyverno/log"
 	"github.com/kyverno/kyverno/cmd/cli/kubectl-kyverno/resource"
+	"github.com/sergi/go-diff/diffmatchpatch"
+	"gopkg.in/yaml.v2"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
@@ -35,6 +38,12 @@ func getAndCompareResource(actualResources []*unstructured.Unstructured, fs bill
 			return false, fmt.Errorf("error: failed to compare resources (%s)", err)
 		}
 		if !equals {
+			log.Log.V(8).Info("Resource diff", "expected", expectedResourcesMap[r.GetNamespace()+"/"+r.GetName()], "actual", r)
+			es, _ := yaml.Marshal(expectedResourcesMap[r.GetNamespace()+"/"+r.GetName()])
+			as, _ := yaml.Marshal(r)
+			dmp := diffmatchpatch.New()
+			diffs := dmp.DiffMain(string(es), string(as), false)
+			log.Log.V(8).Info("\n" + dmp.DiffPrettyText(diffs) + "\n")
 			return false, nil
 		}
 	}
