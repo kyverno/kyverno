9928432eeac06c0324357b3b4f7ea9be79a05b94
aerosouund <aerosound161@gmail.com>
2024-12-12 19:51:47 +0200

    fix: Dont check duplicate strings for field that is an array of any
    Signed-off-by: aerosouund <aerosound161@gmail.com>

--- cmd/cli/kubectl-kyverno/test/load.go
+++ cmd/cli/kubectl-kyverno/test/load.go
@@ -84,20 +84,6 @@ func LoadTest(fs billy.Filesystem, path string) TestCase {
 func cleanTest(test *v1alpha1.Test) {
 	test.Policies = removeDuplicateStrings(test.Policies)
 	test.Resources = removeDuplicateStrings(test.Resources)
-	for index, result := range test.Results {
-		stringArr := []string{}
-		for _, res := range result.Resources {
-			stringArr = append(stringArr, res.(string))
-		}
-		stringArr = removeDuplicateStrings(stringArr)
-
-		anyArr := []any{}
-		for _, res := range stringArr {
-			stringArr = append(stringArr, res)
-		}
-
-		test.Results[index].Resources = anyArr
-	}
 }
 
 func removeDuplicateStrings(strings []string) []string {
