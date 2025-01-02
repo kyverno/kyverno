e037a43056735fdf14b90c3c95d69aa7b89c3a8e
Mariam Fahmy <mariam.fahmy@nirmata.com>
2025-01-02 15:55:52 +0200

    fix the result column for Kyverno test
    Signed-off-by: Mariam Fahmy <mariam.fahmy@nirmata.com>

--- cmd/cli/kubectl-kyverno/commands/test/output.go
+++ cmd/cli/kubectl-kyverno/commands/test/output.go
@@ -255,8 +255,7 @@ func printTestResult(
 								continue
 							}
 
-							success := ok || (!ok && test.Result == policyreportv1alpha2.StatusFail)
-							resourceRows := createRowsAccordingToResults(test, rc, &testCount, success, message, reason, strings.Replace(resource, ",", "/", -1))
+							resourceRows := createRowsAccordingToResults(test, rc, &testCount, ok, message, reason, strings.Replace(resource, ",", "/", -1))
 							rows = append(rows, resourceRows...)
 						} else {
 							generatedResources := rule.GeneratedResources()
