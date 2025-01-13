84ce12fef0583e12ca0b6c8ab40ac6a044ee0bb3
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2025-01-13 12:26:30 +0100

    feat: add validating policy compiler
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

---
+++ pkg/cel/policy/policy.go
@@ -0,0 +1,13 @@
+package policy
+
+import (
+	"github.com/google/cel-go/cel"
+	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
+)
+
+type CompiledPolicy struct {
+	failurePolicy   admissionregistrationv1.FailurePolicyType
+	matchConditions []cel.Program
+	variables       map[string]cel.Program
+	validations     []cel.Program
+}
