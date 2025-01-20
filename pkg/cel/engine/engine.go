215cc32dbc1ee3738b56be00eceb5e2cbd594576
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2025-01-20 10:43:05 +0100

    feat: use policy provider (#11947)
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    Signed-off-by: pipo02mix <fernando@giantswarm.io>

--- pkg/cel/engine/engine.go
+++ pkg/cel/engine/engine.go
@@ -3,7 +3,13 @@ package engine
 import (
 	"context"
 
+	kyvernov1 "github.com/kyverno/kyverno/api/kyverno/v1"
+	kyvernov2alpha1 "github.com/kyverno/kyverno/api/kyverno/v2alpha1"
 	"github.com/kyverno/kyverno/pkg/cel/policy"
+	engineapi "github.com/kyverno/kyverno/pkg/engine/api"
+	"github.com/kyverno/kyverno/pkg/engine/handlers"
+	admissionregistrationv1beta1 "k8s.io/api/admissionregistration/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
@@ -12,26 +18,112 @@ type EngineRequest struct {
 	NamespaceLabels map[string]map[string]string
 }
 
-type EngineResponse struct{}
+type EngineResponse struct {
+	Resource *unstructured.Unstructured
+	Policies []PolicyResponse
+}
+
+type PolicyResponse struct {
+	Policy kyvernov2alpha1.ValidatingPolicy
+	Rules  []engineapi.RuleResponse
+}
+
+type ValidatingPolicy struct {
+	policy kyvernov2alpha1.ValidatingPolicy
+}
+
+func (p *ValidatingPolicy) AsKyvernoPolicy() kyvernov1.PolicyInterface {
+	return nil
+}
+
+func (p *ValidatingPolicy) AsValidatingAdmissionPolicy() *admissionregistrationv1beta1.ValidatingAdmissionPolicy {
+	return nil
+}
+
+func (p *ValidatingPolicy) GetType() engineapi.PolicyType {
+	return engineapi.ValidatingAdmissionPolicyType
+}
+
+func (p *ValidatingPolicy) GetAPIVersion() string {
+	return "admissionregistration.k8s.io/v1beta1"
+}
+
+func (p *ValidatingPolicy) GetName() string {
+	return p.policy.GetName()
+}
+
+func (p *ValidatingPolicy) GetNamespace() string {
+	return p.policy.GetNamespace()
+}
+
+func (p *ValidatingPolicy) GetKind() string {
+	return "ValidatingAdmissionPolicy"
+}
+
+func (p *ValidatingPolicy) GetResourceVersion() string {
+	return p.policy.GetResourceVersion()
+}
+
+func (p *ValidatingPolicy) GetAnnotations() map[string]string {
+	return p.policy.GetAnnotations()
+}
+
+func (p *ValidatingPolicy) IsNamespaced() bool {
+	return false
+}
+
+func (p *ValidatingPolicy) MetaObject() metav1.Object {
+	return &p.policy
+}
+
+func NewValidatingPolicy(pol kyvernov2alpha1.ValidatingPolicy) engineapi.GenericPolicy {
+	return &ValidatingPolicy{
+		policy: pol,
+	}
+}
 
 type Engine interface {
 	Handle(context.Context, EngineRequest, ...policy.CompiledPolicy) (EngineResponse, error)
 }
 
-type engine struct{}
-
-func NewEngine() *engine {
-	return &engine{}
+type engine struct {
+	provider Provider
 }
 
-func (e *engine) Handle(ctx context.Context, request EngineRequest, policies ...policy.CompiledPolicy) (EngineResponse, error) {
-	var response EngineResponse
+func NewEngine(provider Provider) *engine {
+	return &engine{
+		provider: provider,
+	}
+}
+
+func (e *engine) Handle(ctx context.Context, request EngineRequest) (EngineResponse, error) {
+	response := EngineResponse{
+		Resource: request.Resource,
+	}
+	policies, err := e.provider.CompiledPolicies(ctx)
+	if err != nil {
+		return response, err
+	}
 	for _, policy := range policies {
-		// TODO
-		_, err := policy.Evaluate(ctx, request.Resource, request.NamespaceLabels)
-		if err != nil {
-			return response, nil
-		}
+		response.Policies = append(response.Policies, e.handlePolicy(ctx, policy, request))
 	}
 	return response, nil
+}
+
+func (e *engine) handlePolicy(ctx context.Context, policy policy.CompiledPolicy, request EngineRequest) PolicyResponse {
+	var rules []engineapi.RuleResponse
+	ok, err := policy.Evaluate(ctx, request.Resource, request.NamespaceLabels)
+	// TODO: evaluation should be per rule
+	if err != nil {
+		rules = handlers.WithResponses(engineapi.RuleError("todo", engineapi.Validation, "failed to load context", err, nil))
+	} else if ok {
+		rules = handlers.WithResponses(engineapi.RulePass("todo", engineapi.Validation, "success", nil))
+	} else {
+		rules = handlers.WithResponses(engineapi.RuleFail("todo", engineapi.Validation, "failure", nil))
+	}
+	return PolicyResponse{
+		// TODO
+		Policy: kyvernov2alpha1.ValidatingPolicy{},
+		Rules:  rules,
+	}
 }
