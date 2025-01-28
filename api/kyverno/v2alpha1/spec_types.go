59abc56596b49d9f007e58f5597f01bf505a0fad
Mariam Fahmy <mariam.fahmy@nirmata.com>
2025-01-28 12:36:16 +0200

    feat: add validation action to VPs
    Signed-off-by: Mariam Fahmy <mariam.fahmy@nirmata.com>

--- api/kyverno/v2alpha1/spec_types.go
+++ api/kyverno/v2alpha1/spec_types.go
@@ -8,6 +8,9 @@ import (
 type ValidatingPolicySpec struct {
 	admissionregistrationv1.ValidatingAdmissionPolicySpec `json:",inline"`
 
+	// ValidationAction specifies the action to be taken when the matched resource violates the policy.
+	ValidationAction []admissionregistrationv1.ValidationAction `json:"validationActions,omitempty"`
+
 	// WebhookConfiguration defines the configuration for the webhook.
 	// +optional
 	WebhookConfiguration *WebhookConfiguration `json:"webhookConfiguration,omitempty"`
