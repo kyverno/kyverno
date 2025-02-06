a4c10f6bb4522fe070cf7405d0a8a99d002e8b73
Mariam Fahmy <mariam.fahmy@nirmata.com>
2025-02-06 15:30:59 +0200

    feat: compile and evaluate polex's match conditions (#12113)
    Signed-off-by: Mariam Fahmy <mariam.fahmy@nirmata.com>

--- pkg/cel/engine/provider.go
+++ pkg/cel/engine/provider.go
@@ -7,12 +7,18 @@ import (
 
 	kyvernov2alpha1 "github.com/kyverno/kyverno/api/kyverno/v2alpha1"
 	"github.com/kyverno/kyverno/pkg/cel/policy"
+	kyvernov2alpha1listers "github.com/kyverno/kyverno/pkg/client/listers/kyverno/v2alpha1"
 	"golang.org/x/exp/maps"
 	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/client-go/util/workqueue"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
 type CompiledPolicy struct {
@@ -34,7 +40,7 @@ func (f ProviderFunc) CompiledPolicies(ctx context.Context) ([]CompiledPolicy, e
 func NewProvider(compiler policy.Compiler, policies ...kyvernov2alpha1.ValidatingPolicy) (ProviderFunc, error) {
 	compiled := make([]CompiledPolicy, 0, len(policies))
 	for _, vp := range policies {
-		policy, err := compiler.Compile(&vp)
+		policy, err := compiler.Compile(&vp, nil)
 		if err != nil {
 			return nil, fmt.Errorf("failed to compile policy %s (%w)", vp.GetName(), err.ToAggregate())
 		}
@@ -54,27 +60,84 @@ func NewProvider(compiler policy.Compiler, policies ...kyvernov2alpha1.Validatin
 	return provider, nil
 }
 
-func NewKubeProvider(compiler policy.Compiler, mgr ctrl.Manager) (Provider, error) {
-	r := newPolicyReconciler(compiler, mgr.GetClient())
-	if err := ctrl.NewControllerManagedBy(mgr).For(&kyvernov2alpha1.ValidatingPolicy{}).Complete(r); err != nil {
+func NewKubeProvider(
+	compiler policy.Compiler,
+	mgr ctrl.Manager,
+	polexLister kyvernov2alpha1listers.CELPolicyExceptionLister,
+) (Provider, error) {
+	r := newPolicyReconciler(compiler, mgr.GetClient(), polexLister)
+	err := ctrl.NewControllerManagedBy(mgr).
+		For(&kyvernov2alpha1.ValidatingPolicy{}).
+		Watches(&kyvernov2alpha1.CELPolicyException{}, &handler.Funcs{
+			CreateFunc: func(
+				ctx context.Context,
+				tce event.TypedCreateEvent[client.Object],
+				trli workqueue.TypedRateLimitingInterface[reconcile.Request],
+			) {
+				polex := tce.Object.(*kyvernov2alpha1.CELPolicyException)
+				for _, ref := range polex.Spec.PolicyRefs {
+					trli.Add(reconcile.Request{
+						NamespacedName: client.ObjectKey{
+							Name: ref.Name,
+						},
+					})
+				}
+			},
+			UpdateFunc: func(
+				ctx context.Context,
+				tue event.TypedUpdateEvent[client.Object],
+				trli workqueue.TypedRateLimitingInterface[reconcile.Request],
+			) {
+				polex := tue.ObjectNew.(*kyvernov2alpha1.CELPolicyException)
+				for _, ref := range polex.Spec.PolicyRefs {
+					trli.Add(reconcile.Request{
+						NamespacedName: client.ObjectKey{
+							Name: ref.Name,
+						},
+					})
+				}
+			},
+			DeleteFunc: func(
+				ctx context.Context,
+				tde event.TypedDeleteEvent[client.Object],
+				trli workqueue.TypedRateLimitingInterface[reconcile.Request],
+			) {
+				polex := tde.Object.(*kyvernov2alpha1.CELPolicyException)
+				for _, ref := range polex.Spec.PolicyRefs {
+					trli.Add(reconcile.Request{
+						NamespacedName: client.ObjectKey{
+							Name: ref.Name,
+						},
+					})
+				}
+			},
+		}).
+		Complete(r)
+	if err != nil {
 		return nil, fmt.Errorf("failed to construct manager: %w", err)
 	}
 	return r, nil
 }
 
 type policyReconciler struct {
-	client   client.Client
-	compiler policy.Compiler
-	lock     *sync.RWMutex
-	policies map[string]CompiledPolicy
+	client      client.Client
+	compiler    policy.Compiler
+	lock        *sync.RWMutex
+	policies    map[string]CompiledPolicy
+	polexLister kyvernov2alpha1listers.CELPolicyExceptionLister
 }
 
-func newPolicyReconciler(compiler policy.Compiler, client client.Client) *policyReconciler {
+func newPolicyReconciler(
+	compiler policy.Compiler,
+	client client.Client,
+	polexLister kyvernov2alpha1listers.CELPolicyExceptionLister,
+) *policyReconciler {
 	return &policyReconciler{
-		client:   client,
-		compiler: compiler,
-		lock:     &sync.RWMutex{},
-		policies: map[string]CompiledPolicy{},
+		client:      client,
+		compiler:    compiler,
+		lock:        &sync.RWMutex{},
+		policies:    map[string]CompiledPolicy{},
+		polexLister: polexLister,
 	}
 }
 
@@ -90,7 +153,12 @@ func (r *policyReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 	if err != nil {
 		return ctrl.Result{}, err
 	}
-	compiled, errs := r.compiler.Compile(&policy)
+	// get exceptions that match the policy
+	exceptions, err := r.ListExceptions(policy.GetName())
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	compiled, errs := r.compiler.Compile(&policy, exceptions)
 	if len(errs) > 0 {
 		fmt.Println(errs)
 		// No need to retry it
@@ -114,4 +182,20 @@ func (r *policyReconciler) CompiledPolicies(ctx context.Context) ([]CompiledPoli
 	r.lock.RLock()
 	defer r.lock.RUnlock()
 	return maps.Values(r.policies), nil
+}
+
+func (r *policyReconciler) ListExceptions(policyName string) ([]kyvernov2alpha1.CELPolicyException, error) {
+	polexList, err := r.polexLister.List(labels.Everything())
+	if err != nil {
+		return nil, err
+	}
+	var exceptions []kyvernov2alpha1.CELPolicyException
+	for _, polex := range polexList {
+		for _, ref := range polex.Spec.PolicyRefs {
+			if ref.Name == policyName {
+				exceptions = append(exceptions, *polex)
+			}
+		}
+	}
+	return exceptions, nil
 }
