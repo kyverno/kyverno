af0200617708ef7c8a175dd2a39f4ae94effad1e
Vishal Choudhary <vishal.choudhary@nirmata.com>
2025-02-10 20:13:30 +0530

    feat: add notary repository
    Signed-off-by: Vishal Choudhary <vishal.choudhary@nirmata.com>

--- pkg/imagedataloader/utils.go
+++ pkg/imagedataloader/utils.go
@@ -2,6 +2,7 @@ package imagedataloader
 
 import (
 	gcrv1 "github.com/google/go-containerregistry/pkg/v1"
+	"github.com/google/go-containerregistry/pkg/v1/types"
 	"github.com/opencontainers/go-digest"
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 )
@@ -25,4 +26,30 @@ func GCRtoOCISpecDesc(v1desc gcrv1.Descriptor) ocispec.Descriptor {
 		}
 	}
 	return ociDesc
+}
+
+func OCISpectoGCRDesc(ocidesc ocispec.Descriptor) (*gcrv1.Descriptor, error) {
+	gcrDesc := &gcrv1.Descriptor{
+		MediaType:    types.MediaType(ocidesc.MediaType),
+		Size:         ocidesc.Size,
+		URLs:         ocidesc.URLs,
+		Annotations:  ocidesc.Annotations,
+		Data:         ocidesc.Data,
+		ArtifactType: ocidesc.ArtifactType,
+	}
+
+	digest, err := gcrv1.NewHash(ocidesc.Digest.String())
+	if err != nil {
+		return nil, err
+	}
+
+	gcrDesc.Digest = digest
+	if ocidesc.Platform != nil {
+		gcrDesc.Platform = &gcrv1.Platform{
+			Architecture: ocidesc.Platform.Architecture,
+			OS:           ocidesc.Platform.OS,
+			OSVersion:    ocidesc.Platform.OSVersion,
+		}
+	}
+	return gcrDesc, nil
 }
