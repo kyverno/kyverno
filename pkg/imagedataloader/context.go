27bf08e7d8fbb4384d4bdc565938108435f5d208
Vishal Choudhary <vishal.choudhary@nirmata.com>
2025-02-14 22:09:27 +0530

    fix: make it an interface
    Signed-off-by: Vishal Choudhary <vishal.choudhary@nirmata.com>

--- pkg/imagedataloader/context.go
+++ pkg/imagedataloader/context.go
@@ -6,26 +6,31 @@ import (
 	k8scorev1 "k8s.io/client-go/kubernetes/typed/core/v1"
 )
 
-// ImageContext is a struct that stores a list of imagedata, it lives as long as
-// the admission request. Get request for images either returned a prefetched image or
-// fetches it from the registry. It is used to share image data for a policy across policies
-type ImageContext struct {
+type imageContext struct {
 	f    Fetcher
 	list map[string]*ImageData
 }
 
-func NewImageContext(lister k8scorev1.SecretInterface, opts ...Option) (*ImageContext, error) {
+// ImageContext stores a list of imagedata, it lives as long as
+// the admission request. Get request for images either returned a prefetched image or
+// fetches it from the registry. It is used to share image data for a policy across policies
+type ImageContext interface {
+	AddImages(ctx context.Context, images []string, opts ...Option) error
+	Get(ctx context.Context, image string, opts ...Option) (*ImageData, error)
+}
+
+func NewImageContext(lister k8scorev1.SecretInterface, opts ...Option) (ImageContext, error) {
 	idl, err := New(lister, opts...)
 	if err != nil {
 		return nil, err
 	}
-	return &ImageContext{
+	return &imageContext{
 		f:    idl,
 		list: make(map[string]*ImageData),
 	}, nil
 }
 
-func (idc *ImageContext) AddImages(ctx context.Context, images []string, opts ...Option) error {
+func (idc *imageContext) AddImages(ctx context.Context, images []string, opts ...Option) error {
 	for _, img := range images {
 		if _, found := idc.list[img]; found {
 			continue
@@ -40,7 +45,7 @@ func (idc *ImageContext) AddImages(ctx context.Context, images []string, opts ..
 	return nil
 }
 
-func (idc *ImageContext) Get(ctx context.Context, image string, opts ...Option) (*ImageData, error) {
+func (idc *imageContext) Get(ctx context.Context, image string, opts ...Option) (*ImageData, error) {
 	if data, found := idc.list[image]; found {
 		return data, nil
 	}
