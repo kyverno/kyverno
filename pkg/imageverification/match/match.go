8d915b52ce58651a9839e859b481939d8201ede1
Vishal Choudhary <vishal.choudhary@nirmata.com>
2025-02-27 12:49:11 +0530

    feat: add evaluator for image verification policies (#12251)
    * feat: add variables
    
    Signed-off-by: Vishal Choudhary <vishal.choudhary@nirmata.com>
    
    * feat: implement evaluator
    
    Signed-off-by: Vishal Choudhary <vishal.choudhary@nirmata.com>
    
    * fix: build
    
    Signed-off-by: Vishal Choudhary <vishal.choudhary@nirmata.com>
    
    * fix: linter
    
    Signed-off-by: Vishal Choudhary <vishal.choudhary@nirmata.com>
    
    * fix: unit tests
    
    Signed-off-by: Vishal Choudhary <vishal.choudhary@nirmata.com>
    
    ---------
    
    Signed-off-by: Vishal Choudhary <vishal.choudhary@nirmata.com>

--- pkg/imageverification/match/match.go
+++ pkg/imageverification/match/match.go
@@ -8,6 +8,7 @@ import (
 	"github.com/google/cel-go/cel"
 	"github.com/kyverno/kyverno/api/policies.kyverno.io/v1alpha1"
 	"github.com/kyverno/kyverno/pkg/cel/utils"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 )
 
 type CompiledMatch struct {
@@ -37,6 +38,9 @@ func (c *CompiledMatch) Match(image string) (bool, error) {
 }
 
 func Match(c []*CompiledMatch, image string) (bool, error) {
+	if len(c) == 0 {
+		return true, nil
+	}
 	for _, v := range c {
 		if matched, err := v.Match(image); err != nil {
 			return false, err
@@ -47,31 +51,34 @@ func Match(c []*CompiledMatch, image string) (bool, error) {
 	return false, nil
 }
 
-func CompileMatches(matches []v1alpha1.ImageRule) ([]*CompiledMatch, error) {
+func CompileMatches(path *field.Path, matches []v1alpha1.ImageRule) ([]*CompiledMatch, field.ErrorList) {
+	var allErrs field.ErrorList
 	compiledMatches := make([]*CompiledMatch, 0, len(matches))
 	e, err := cel.NewEnv(
 		cel.Variable("ref", cel.StringType),
 	)
 	if err != nil {
-		return nil, err
+		return nil, append(allErrs, field.Invalid(path, matches, err.Error()))
 	}
 
-	for _, m := range matches {
+	for i, m := range matches {
 		c := &CompiledMatch{}
 		if m.Glob != "" {
+			path := path.Index(i).Child("glob")
 			g, err := glob.Compile(m.Glob)
 			if err != nil {
-				return nil, err
+				return nil, append(allErrs, field.Invalid(path, m.Glob, err.Error()))
 			}
 			c.g = g
 		} else if m.CELExpression != "" {
+			path := path.Index(i).Child("expression")
 			ast, iss := e.Compile(m.CELExpression)
 			if iss.Err() != nil {
-				return nil, iss.Err()
+				return nil, append(allErrs, field.Invalid(path, m.CELExpression, iss.Err().Error()))
 			}
 			prg, err := e.Program(ast)
 			if err != nil {
-				return nil, err
+				return nil, append(allErrs, field.Invalid(path, m.CELExpression, err.Error()))
 			}
 			c.e = prg
 		}
