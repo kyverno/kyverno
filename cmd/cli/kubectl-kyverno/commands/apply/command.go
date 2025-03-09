0e0c19ad4e5e73dd64812876a59168dcdb527728
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2025-03-09 18:22:49 +0100

    implement get cm mock
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- cmd/cli/kubectl-kyverno/commands/apply/command.go
+++ cmd/cli/kubectl-kyverno/commands/apply/command.go
@@ -43,7 +43,9 @@ import (
 	admissionv1 "k8s.io/api/admission/v1"
 	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
 	corev1 "k8s.io/api/core/v1"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -333,10 +335,51 @@ func (c *ApplyCommandConfig) applyValidatingAdmissionPolicies(
 	return responses, nil
 }
 
-type fakeContextProvider struct{}
+type fakeContextProvider struct {
+	resources map[string]map[string]map[string]*unstructured.Unstructured
+}
 
-func (cp *fakeContextProvider) GetConfigMap(string, string) (unstructured.Unstructured, error) {
-	panic("")
+func newFakeContextProvider() *fakeContextProvider {
+	return &fakeContextProvider{
+		resources: map[string]map[string]map[string]*unstructured.Unstructured{},
+	}
+}
+
+func (cp *fakeContextProvider) addResource(gvr schema.GroupVersionResource, obj runtime.Object) error {
+	object, err := runtime.DefaultUnstructuredConverter.ToUnstructured(obj)
+	if err != nil {
+		return err
+	}
+	resource := &unstructured.Unstructured{Object: object}
+	resources := cp.resources[gvr.String()]
+	if resources == nil {
+		resources = map[string]map[string]*unstructured.Unstructured{}
+		cp.resources[gvr.String()] = resources
+	}
+	namespace := resources[resource.GetNamespace()]
+	if namespace == nil {
+		namespace = map[string]*unstructured.Unstructured{}
+		resources[resource.GetNamespace()] = namespace
+	}
+	namespace[resource.GetName()] = resource
+	return nil
+}
+
+func (cp *fakeContextProvider) GetConfigMap(ns, n string) (unstructured.Unstructured, error) {
+	gvr := schema.GroupVersionResource{Version: "v1", Resource: "configmaps"}
+	configmaps := cp.resources[gvr.String()]
+	if configmaps == nil {
+		return unstructured.Unstructured{}, kerrors.NewNotFound(gvr.GroupResource(), n)
+	}
+	namespace := configmaps[ns]
+	if namespace == nil {
+		return unstructured.Unstructured{}, kerrors.NewNotFound(gvr.GroupResource(), n)
+	}
+	resource := namespace[n]
+	if resource == nil {
+		return unstructured.Unstructured{}, kerrors.NewNotFound(gvr.GroupResource(), n)
+	}
+	return *resource, nil
 }
 
 func (cp *fakeContextProvider) GetGlobalReference(string, string) (any, error) {
@@ -394,7 +437,20 @@ func (c *ApplyCommandConfig) applyValidatingPolicies(
 		}
 		restMapper = restmapper.NewDiscoveryRESTMapper(apiGroupResources)
 	} else {
-		contextProvider = &fakeContextProvider{}
+		fakeContextProvider := newFakeContextProvider()
+		if err := fakeContextProvider.addResource(
+			schema.GroupVersionResource{Version: "v1", Resource: "configmaps"},
+			&corev1.ConfigMap{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: "default",
+					Name:      "policy-cm",
+				},
+				Data: map[string]string{},
+			},
+		); err != nil {
+			return nil, err
+		}
+		contextProvider = fakeContextProvider
 		apiGroupResources, err := data.APIGroupResources()
 		if err != nil {
 			return nil, err
