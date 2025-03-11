f0f194bf77aea214f23b88170e94f988d489f2e7
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2025-03-11 17:33:14 +0100

    use Any
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- cmd/cli/kubectl-kyverno/apis/v1alpha1/context.go
+++ cmd/cli/kubectl-kyverno/apis/v1alpha1/context.go
@@ -1,9 +1,9 @@
 package v1alpha1
 
 import (
+	"github.com/kyverno/kyverno-json/pkg/apis/policy/v1alpha1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/runtime"
 )
 
 // +genclient
@@ -24,13 +24,13 @@ type ContextSpec struct {
 }
 
 type ImageData struct {
-	Image         string               `json:"image"`
-	ResolvedImage string               `json:"resolvedImage"`
-	Registry      string               `json:"registry"`
-	Repository    string               `json:"repository"`
-	Tag           string               `json:"tag,omitempty"`
-	Digest        string               `json:"digest,omitempty"`
-	ImageIndex    runtime.RawExtension `json:"imageIndex,omitempty"`
-	Manifest      runtime.RawExtension `json:"manifest"`
-	Config        runtime.RawExtension `json:"config"`
+	Image         string       `json:"image"`
+	ResolvedImage string       `json:"resolvedImage"`
+	Registry      string       `json:"registry"`
+	Repository    string       `json:"repository"`
+	Tag           string       `json:"tag,omitempty"`
+	Digest        string       `json:"digest,omitempty"`
+	ImageIndex    v1alpha1.Any `json:"imageIndex,omitempty"`
+	Manifest      v1alpha1.Any `json:"manifest,omitempty"`
+	ConfigData    v1alpha1.Any `json:"config,omitempty"`
 }
