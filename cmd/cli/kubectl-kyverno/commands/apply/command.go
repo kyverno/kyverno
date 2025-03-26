e17264caf25d4f3a264ddfb12e7eb7922adbe9c6
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2025-03-26 08:53:46 +0100

    chore: remove unused field in vap processor (#12545)
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    Signed-off-by: Martijn Evers <mevers@gk-software.com>

--- cmd/cli/kubectl-kyverno/commands/apply/command.go
+++ cmd/cli/kubectl-kyverno/commands/apply/command.go
@@ -406,7 +406,6 @@ func (c *ApplyCommandConfig) applyValidatingAdmissionPolicies(
 			Bindings:             vapBindings,
 			Resource:             resource,
 			NamespaceSelectorMap: namespaceSelectorMap,
-			PolicyReport:         c.PolicyReport,
 			Rc:                   rc,
 			Client:               dClient,
 		}
