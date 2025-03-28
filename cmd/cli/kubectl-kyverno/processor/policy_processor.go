771709d95cfe23e92f9f81d19f368728cbe482f1
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2025-03-28 09:49:58 +0000

    fix: use correct resource in cli processor (#12575)
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- cmd/cli/kubectl-kyverno/processor/policy_processor.go
+++ cmd/cli/kubectl-kyverno/processor/policy_processor.go
@@ -82,7 +82,7 @@ func (p *PolicyProcessor) ApplyPoliciesOnResource() ([]engineapi.EngineResponse,
 	cfg := config.NewDefaultConfiguration(false)
 	jp := jmespath.New(cfg)
 	resource := p.Resource
-	namespaceLabels := p.NamespaceSelectorMap[p.Resource.GetNamespace()]
+	namespaceLabels := p.NamespaceSelectorMap[resource.GetNamespace()]
 	policyExceptionLister := &policyExceptionLister{
 		exceptions: p.PolicyExceptions,
 	}
@@ -225,7 +225,7 @@ func (p *PolicyProcessor) ApplyPoliciesOnResource() ([]engineapi.EngineResponse,
 		vapResponses = append(vapResponses, validateResponse)
 		p.Rc.addValidatingAdmissionResponse(validateResponse)
 	}
-	// validating admission policies
+	// validating policies
 	if len(p.ValidatingPolicies) != 0 {
 		ctx := context.TODO()
 		compiler := celpolicy.NewCompiler()
@@ -277,7 +277,7 @@ func (p *PolicyProcessor) ApplyPoliciesOnResource() ([]engineapi.EngineResponse,
 			}
 			contextProvider = fakeContextProvider
 		}
-		if p.Resource.Object != nil {
+		if resource.Object != nil {
 			eng := celengine.NewEngine(provider, p.Variables.Namespace, matching.NewMatcher())
 			// get gvk from resource
 			gvk := resource.GroupVersionKind()
