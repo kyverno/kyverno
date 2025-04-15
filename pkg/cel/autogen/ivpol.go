e5532a88415c159f4ef89d005ce2527bc5ae55ce
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2025-04-15 19:57:23 +0200

    feat: improve ivpol autogen API (#12781)
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    Signed-off-by: Martijn Evers <mevers@gk-software.com>

--- pkg/cel/autogen/ivpol.go
+++ pkg/cel/autogen/ivpol.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"strings"
 
-	"github.com/kyverno/kyverno/api/kyverno"
 	policiesv1alpha1 "github.com/kyverno/kyverno/api/policies.kyverno.io/v1alpha1"
 	"k8s.io/apimachinery/pkg/util/sets"
 )
@@ -13,25 +12,20 @@ func GetAutogenRulesImageVerify(policy *policiesv1alpha1.ImageValidatingPolicy) 
 	if policy == nil {
 		return nil, nil
 	}
-
-	applyAutoGen, desiredControllers := CanAutoGen(policy.GetSpec().MatchConstraints)
+	applyAutoGen, desiredControllers := CanAutoGen(policy.Spec.MatchConstraints)
 	if !applyAutoGen {
 		return nil, nil
 	}
-
-	var actualControllers sets.Set[string]
-	ann := policy.GetAnnotations()
-	actualControllersString, ok := ann[kyverno.AnnotationAutogenControllers]
-	if !ok {
-		actualControllers = desiredControllers
-	} else {
-		actualControllers = sets.New(strings.Split(actualControllersString, ",")...)
+	actualControllers := desiredControllers
+	if policy.Spec.AutogenConfiguration != nil &&
+		policy.Spec.AutogenConfiguration.PodControllers != nil &&
+		policy.Spec.AutogenConfiguration.PodControllers.Controllers != nil {
+		actualControllers = sets.New(policy.Spec.AutogenConfiguration.PodControllers.Controllers...)
 	}
 	genRules, err := autogenIvPols(policy, actualControllers)
 	if err != nil {
 		return nil, err
 	}
-
 	return genRules, nil
 }
 
