e1bc461a743837995374246d118f0e36412a2322
Your Name <you@example.com>
2025-04-17 02:19:57 +0000

    fix formatting
    Signed-off-by: Denish Tomar <denishtomar3436@gmail.com>

--- pkg/controllers/cleanup/controller_test.go
+++ pkg/controllers/cleanup/controller_test.go
@@ -1,44 +1,44 @@
 package cleanup
 
 import (
-    "testing"
+	"testing"
 
-    "github.com/golang/mock/gomock"
-    "github.com/kyverno/kyverno/pkg/config/mocks"
-    "github.com/stretchr/testify/assert"
-    "k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-    "k8s.io/apimachinery/pkg/runtime/schema"
+	"github.com/golang/mock/gomock"
+	"github.com/kyverno/kyverno/pkg/config/mocks"
+	"github.com/stretchr/testify/assert"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
 func Test_SkipResourceDueToFilter(t *testing.T) {
-    ctrl := gomock.NewController(t)
-    defer ctrl.Finish()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 
-    mockConfig := mocks.NewMockConfiguration(ctrl)
+	mockConfig := mocks.NewMockConfiguration(ctrl)
 
-    gvk := schema.GroupVersionKind{
-        Group:   "",
-        Version: "v1",
-        Kind:    "ConfigMap",
-    }
+	gvk := schema.GroupVersionKind{
+		Group:   "",
+		Version: "v1",
+		Kind:    "ConfigMap",
+	}
 
-    mockConfig.EXPECT().
-        ToFilter(gvk, "ConfigMap", "kube-system", "filtered-cm").
-        Return(true).
-        AnyTimes()
+	mockConfig.EXPECT().
+		ToFilter(gvk, "ConfigMap", "kube-system", "filtered-cm").
+		Return(true).
+		AnyTimes()
 
-    c := &controller{
-        configuration: mockConfig,
-    }
+	c := &controller{
+		configuration: mockConfig,
+	}
 
-    resource := unstructured.Unstructured{}
-    resource.SetKind("ConfigMap")
-    resource.SetNamespace("kube-system")
-    resource.SetName("filtered-cm")
+	resource := unstructured.Unstructured{}
+	resource.SetKind("ConfigMap")
+	resource.SetNamespace("kube-system")
+	resource.SetName("filtered-cm")
 
-    filtered := c.configuration.ToFilter(
-        gvk, resource.GetKind(), resource.GetNamespace(), resource.GetName(),
-    )
+	filtered := c.configuration.ToFilter(
+		gvk, resource.GetKind(), resource.GetNamespace(), resource.GetName(),
+	)
 
-    assert.True(t, filtered, "Expected resource to be filtered and skipped")
+	assert.True(t, filtered, "Expected resource to be filtered and skipped")
 }
