3894ec0a1103c82e5f46b9741c3cae5b3581640c
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2025-04-18 06:55:13 +0200

    refactor: autogen cel package (#12811)
    * refactor: autogen cel package
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    
    * fix
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    
    * fix tests
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    
    * fix tests
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    
    * fix tests
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    
    * fix tests
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    
    * fix tests
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    
    * fix tests
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    
    ---------
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/cel/autogen/rule_test.go
+++ pkg/cel/autogen/rule_test.go
@@ -2,11 +2,10 @@ package autogen
 
 import (
 	"encoding/json"
-	"reflect"
 	"testing"
 
 	policiesv1alpha1 "github.com/kyverno/kyverno/api/policies.kyverno.io/v1alpha1"
-	"gotest.tools/assert"
+	"github.com/stretchr/testify/assert"
 	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
 	"k8s.io/apimachinery/pkg/util/sets"
 )
@@ -16,7 +15,7 @@ func TestGenerateRuleForControllers(t *testing.T) {
 		name          string
 		controllers   sets.Set[string]
 		policySpec    []byte
-		generatedRule policiesv1alpha1.AutogenRule
+		generatedRule []policiesv1alpha1.AutogenRule
 	}{
 		{
 			name:        "autogen rule for deployments",
@@ -47,7 +46,7 @@ func TestGenerateRuleForControllers(t *testing.T) {
 					}
 				]
 			}`),
-			generatedRule: policiesv1alpha1.AutogenRule{
+			generatedRule: []policiesv1alpha1.AutogenRule{{
 				MatchConstraints: &admissionregistrationv1.MatchResources{
 					ResourceRules: []admissionregistrationv1.NamedRuleWithOperations{
 						{
@@ -70,7 +69,7 @@ func TestGenerateRuleForControllers(t *testing.T) {
 						Expression: "object.spec.template.spec.containers.all(container, has(container.securityContext) && has(container.securityContext.allowPrivilegeEscalation) && container.securityContext.allowPrivilegeEscalation == false)",
 					},
 				},
-			},
+			}},
 		},
 		{
 			name:        "autogen rule for deployments and daemonsets",
@@ -101,7 +100,7 @@ func TestGenerateRuleForControllers(t *testing.T) {
 					}
 				]
 			}`),
-			generatedRule: policiesv1alpha1.AutogenRule{
+			generatedRule: []policiesv1alpha1.AutogenRule{{
 				MatchConstraints: &admissionregistrationv1.MatchResources{
 					ResourceRules: []admissionregistrationv1.NamedRuleWithOperations{
 						{
@@ -124,7 +123,7 @@ func TestGenerateRuleForControllers(t *testing.T) {
 						Expression: "object.spec.template.spec.containers.all(container, has(container.securityContext) && has(container.securityContext.allowPrivilegeEscalation) && container.securityContext.allowPrivilegeEscalation == false)",
 					},
 				},
-			},
+			}},
 		},
 		{
 			name:        "autogen rule for deployments, daemonsets, statefulsets and replicasets",
@@ -161,7 +160,7 @@ func TestGenerateRuleForControllers(t *testing.T) {
 					}
 				]
 			}`),
-			generatedRule: policiesv1alpha1.AutogenRule{
+			generatedRule: []policiesv1alpha1.AutogenRule{{
 				MatchConstraints: &admissionregistrationv1.MatchResources{
 					ResourceRules: []admissionregistrationv1.NamedRuleWithOperations{
 						{
@@ -182,7 +181,7 @@ func TestGenerateRuleForControllers(t *testing.T) {
 				MatchConditions: []admissionregistrationv1.MatchCondition{
 					{
 						Name:       "autogen-only for production",
-						Expression: "!(object.kind =='Deployment' || object.kind =='ReplicaSet' || object.kind =='StatefulSet' || object.kind =='DaemonSet') || has(object.spec.template.metadata.labels) && has(object.spec.template.metadata.labels.prod) && object.spec.template.metadata.labels.prod == 'true'",
+						Expression: "!((object.apiVersion == 'apps/v1' && object.kind =='DaemonSet') || (object.apiVersion == 'apps/v1' && object.kind =='Deployment') || (object.apiVersion == 'apps/v1' && object.kind =='ReplicaSet') || (object.apiVersion == 'apps/v1' && object.kind =='StatefulSet')) || (has(object.spec.template.metadata.labels) && has(object.spec.template.metadata.labels.prod) && object.spec.template.metadata.labels.prod == 'true')",
 					},
 				},
 				Validations: []admissionregistrationv1.Validation{
@@ -190,22 +189,17 @@ func TestGenerateRuleForControllers(t *testing.T) {
 						Expression: "object.spec.template.spec.containers.all(container, has(container.securityContext) && has(container.securityContext.allowPrivilegeEscalation) && container.securityContext.allowPrivilegeEscalation == false)",
 					},
 				},
-			},
+			}},
 		},
 	}
-
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			var spec *policiesv1alpha1.ValidatingPolicySpec
 			err := json.Unmarshal(test.policySpec, &spec)
-			assert.NilError(t, err)
-
-			genRule, err := generatePodControllerRule(spec, test.controllers)
-			assert.NilError(t, err)
-
-			if !reflect.DeepEqual(genRule, &test.generatedRule) {
-				t.Errorf("generatePodControllerRule() = %v, want %v", genRule, test.generatedRule)
-			}
+			assert.NoError(t, err)
+			genRule, err := generateRuleForControllers(spec, test.controllers)
+			assert.NoError(t, err)
+			assert.Equal(t, test.generatedRule, genRule)
 		})
 	}
 }
@@ -213,7 +207,7 @@ func TestGenerateRuleForControllers(t *testing.T) {
 func TestGenerateCronJobRule(t *testing.T) {
 	tests := []struct {
 		policySpec    []byte
-		generatedRule policiesv1alpha1.AutogenRule
+		generatedRule []policiesv1alpha1.AutogenRule
 	}{
 		{
 			policySpec: []byte(`{
@@ -242,7 +236,7 @@ func TestGenerateCronJobRule(t *testing.T) {
         }
     ]
 }`),
-			generatedRule: policiesv1alpha1.AutogenRule{
+			generatedRule: []policiesv1alpha1.AutogenRule{{
 				MatchConstraints: &admissionregistrationv1.MatchResources{
 					ResourceRules: []admissionregistrationv1.NamedRuleWithOperations{
 						{
@@ -265,7 +259,7 @@ func TestGenerateCronJobRule(t *testing.T) {
 						Expression: "object.spec.jobTemplate.spec.template.spec.containers.all(container, has(container.securityContext) && has(container.securityContext.allowPrivilegeEscalation) && container.securityContext.allowPrivilegeEscalation == false)",
 					},
 				},
-			},
+			}},
 		},
 		{
 			policySpec: []byte(`{
@@ -300,7 +294,7 @@ func TestGenerateCronJobRule(t *testing.T) {
         }
     ]
 }`),
-			generatedRule: policiesv1alpha1.AutogenRule{
+			generatedRule: []policiesv1alpha1.AutogenRule{{
 				MatchConstraints: &admissionregistrationv1.MatchResources{
 					ResourceRules: []admissionregistrationv1.NamedRuleWithOperations{
 						{
@@ -321,7 +315,7 @@ func TestGenerateCronJobRule(t *testing.T) {
 				MatchConditions: []admissionregistrationv1.MatchCondition{
 					{
 						Name:       "autogen-cronjobs-only for production",
-						Expression: "!(object.kind =='CronJob') || has(object.spec.jobTemplate.spec.template.metadata.labels) && has(object.spec.jobTemplate.spec.template.metadata.labels.prod) && object.spec.jobTemplate.spec.template.metadata.labels.prod == 'true'",
+						Expression: "!((object.apiVersion == 'batch/v1' && object.kind =='CronJob')) || (has(object.spec.jobTemplate.spec.template.metadata.labels) && has(object.spec.jobTemplate.spec.template.metadata.labels.prod) && object.spec.jobTemplate.spec.template.metadata.labels.prod == 'true')",
 					},
 				},
 				Validations: []admissionregistrationv1.Validation{
@@ -329,7 +323,7 @@ func TestGenerateCronJobRule(t *testing.T) {
 						Expression: "object.spec.jobTemplate.spec.template.spec.containers.all(container, has(container.securityContext) && has(container.securityContext.allowPrivilegeEscalation) && container.securityContext.allowPrivilegeEscalation == false)",
 					},
 				},
-			},
+			}},
 		},
 		{
 			policySpec: []byte(`{
@@ -365,7 +359,7 @@ func TestGenerateCronJobRule(t *testing.T) {
         }
     ]
 }`),
-			generatedRule: policiesv1alpha1.AutogenRule{
+			generatedRule: []policiesv1alpha1.AutogenRule{{
 				MatchConstraints: &admissionregistrationv1.MatchResources{
 					ResourceRules: []admissionregistrationv1.NamedRuleWithOperations{
 						{
@@ -395,70 +389,65 @@ func TestGenerateCronJobRule(t *testing.T) {
 						Message:    "labels must be env=prod",
 					},
 				},
-			},
+			}},
 		},
 	}
-
-	for i, tt := range tests {
-		var spec *policiesv1alpha1.ValidatingPolicySpec
-		err := json.Unmarshal(tt.policySpec, &spec)
-		assert.NilError(t, err)
-
-		genRule, err := generateCronJobRule(spec, sets.New("cronjobs"))
-		assert.NilError(t, err)
-
-		if !reflect.DeepEqual(genRule, &tt.generatedRule) {
-			t.Errorf("%v: generateCronJobRule() = %v, want %v", i, genRule, tt.generatedRule)
-		}
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			var spec *policiesv1alpha1.ValidatingPolicySpec
+			err := json.Unmarshal(tt.policySpec, &spec)
+			assert.NoError(t, err)
+			genRule, err := generateRuleForControllers(spec, sets.New("cronjobs"))
+			assert.NoError(t, err)
+			assert.Equal(t, tt.generatedRule, genRule)
+		})
 	}
 }
 
 func TestUpdateGenRuleByte(t *testing.T) {
 	tests := []struct {
-		pbyte    []byte
-		resource autogencontroller
-		want     []byte
+		pbyte  []byte
+		config string
+		want   []byte
 	}{
 		{
-			pbyte:    []byte("object.spec"),
-			resource: "pods",
-			want:     []byte("object.spec.template.spec"),
+			pbyte:  []byte("object.spec"),
+			config: "deployments",
+			want:   []byte("object.spec.template.spec"),
 		},
 		{
-			pbyte:    []byte("oldObject.spec"),
-			resource: "pods",
-			want:     []byte("oldObject.spec.template.spec"),
+			pbyte:  []byte("oldObject.spec"),
+			config: "deployments",
+			want:   []byte("oldObject.spec.template.spec"),
 		},
 		{
-			pbyte:    []byte("object.spec"),
-			resource: "cronjobs",
-			want:     []byte("object.spec.jobTemplate.spec.template.spec"),
+			pbyte:  []byte("object.spec"),
+			config: "cronjobs",
+			want:   []byte("object.spec.jobTemplate.spec.template.spec"),
 		},
 		{
-			pbyte:    []byte("oldObject.spec"),
-			resource: "cronjobs",
-			want:     []byte("oldObject.spec.jobTemplate.spec.template.spec"),
+			pbyte:  []byte("oldObject.spec"),
+			config: "cronjobs",
+			want:   []byte("oldObject.spec.jobTemplate.spec.template.spec"),
 		},
 		{
-			pbyte:    []byte("object.metadata"),
-			resource: "pods",
-			want:     []byte("object.spec.template.metadata"),
+			pbyte:  []byte("object.metadata"),
+			config: "deployments",
+			want:   []byte("object.spec.template.metadata"),
 		},
 		{
-			pbyte:    []byte("object.spec.containers.all(container, has(container.securityContext) && has(container.securityContext.allowPrivilegeEscalation) && container.securityContext.allowPrivilegeEscalation == false)"),
-			resource: "cronjobs",
-			want:     []byte("object.spec.jobTemplate.spec.template.spec.containers.all(container, has(container.securityContext) && has(container.securityContext.allowPrivilegeEscalation) && container.securityContext.allowPrivilegeEscalation == false)"),
+			pbyte:  []byte("object.spec.containers.all(container, has(container.securityContext) && has(container.securityContext.allowPrivilegeEscalation) && container.securityContext.allowPrivilegeEscalation == false)"),
+			config: "cronjobs",
+			want:   []byte("object.spec.jobTemplate.spec.template.spec.containers.all(container, has(container.securityContext) && has(container.securityContext.allowPrivilegeEscalation) && container.securityContext.allowPrivilegeEscalation == false)"),
 		},
 		{
-			pbyte:    []byte("object.spec.containers.all(container, has(container.securityContext) && has(container.securityContext.allowPrivilegeEscalation) && container.securityContext.allowPrivilegeEscalation == false)"),
-			resource: "pods",
-			want:     []byte("object.spec.template.spec.containers.all(container, has(container.securityContext) && has(container.securityContext.allowPrivilegeEscalation) && container.securityContext.allowPrivilegeEscalation == false)"),
+			pbyte:  []byte("object.spec.containers.all(container, has(container.securityContext) && has(container.securityContext.allowPrivilegeEscalation) && container.securityContext.allowPrivilegeEscalation == false)"),
+			config: "deployments",
+			want:   []byte("object.spec.template.spec.containers.all(container, has(container.securityContext) && has(container.securityContext.allowPrivilegeEscalation) && container.securityContext.allowPrivilegeEscalation == false)"),
 		},
 	}
 	for _, tt := range tests {
-		got := updateFields(tt.pbyte, tt.resource)
-		if !reflect.DeepEqual(got, tt.want) {
-			t.Errorf("updateGenRuleByte() = %v, want %v", string(got), string(tt.want))
-		}
+		got := updateFields(tt.pbyte, replacementsMap[configsMap[tt.config].replacementsRef]...)
+		assert.Equal(t, tt.want, got)
 	}
 }
