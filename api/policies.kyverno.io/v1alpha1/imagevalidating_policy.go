0dd9ee54f574be2a0585f4825c85cb161210ce3a
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2025-04-23 00:55:00 +0200

    fix: missing docs for new policy types
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- api/policies.kyverno.io/v1alpha1/imagevalidating_policy.go
+++ api/policies.kyverno.io/v1alpha1/imagevalidating_policy.go
@@ -21,6 +21,7 @@ import (
 // +kubebuilder:printcolumn:name="AGE",type="date",JSONPath=".metadata.creationTimestamp"
 // +kubebuilder:printcolumn:name="READY",type=string,JSONPath=`.status.conditionStatus.ready`
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
 type ImageValidatingPolicy struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
