e36a09a58bda33a44114707b8a75a3c4c7be4bda
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2025-04-27 10:25:15 +0200

    chore: add cel/engine unit tests (#12904)
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/cel/engine/exception.go
+++ pkg/cel/engine/exception.go
@@ -2,22 +2,25 @@ package engine
 
 import (
 	policiesv1alpha1 "github.com/kyverno/kyverno/api/policies.kyverno.io/v1alpha1"
-	policiesv1alpha1listers "github.com/kyverno/kyverno/pkg/client/listers/policies.kyverno.io/v1alpha1"
 	"k8s.io/apimachinery/pkg/labels"
 )
 
-func ListExceptions(polexLister policiesv1alpha1listers.PolicyExceptionLister, policyName, policyKind string) ([]*policiesv1alpha1.PolicyException, error) {
-	polexList, err := polexLister.List(labels.Everything())
+type policyExceptionLister interface {
+	List(labels.Selector) ([]*policiesv1alpha1.PolicyException, error)
+}
+
+func ListExceptions(lister policyExceptionLister, kind, name string) ([]*policiesv1alpha1.PolicyException, error) {
+	exceptions, err := lister.List(labels.Everything())
 	if err != nil {
 		return nil, err
 	}
-	var exceptions []*policiesv1alpha1.PolicyException
-	for _, polex := range polexList {
-		for _, ref := range polex.Spec.PolicyRefs {
-			if ref.Name == policyName && ref.Kind == policyKind {
-				exceptions = append(exceptions, polex)
+	var out []*policiesv1alpha1.PolicyException
+	for _, exception := range exceptions {
+		for _, ref := range exception.Spec.PolicyRefs {
+			if ref.Name == name && ref.Kind == kind {
+				out = append(out, exception)
 			}
 		}
 	}
-	return exceptions, nil
+	return out, nil
 }
