d2fee3888f1af56de547ff151db6406358998c40
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2025-04-29 14:19:08 +0200

    refactor: centralize CEL keys (#12950)
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/imageverification/evaluator/policy.go
+++ pkg/imageverification/evaluator/policy.go
@@ -111,23 +111,23 @@ func (c *compiledPolicy) Evaluate(ctx context.Context, ictx imagedataloader.Imag
 		if err != nil {
 			return nil, fmt.Errorf("failed to prepare oldObject variable for evaluation: %w", err)
 		}
-		data[NamespaceObjectKey] = namespaceVal
-		data[RequestKey] = requestVal.Object
-		data[ObjectKey] = objectVal
-		data[OldObjectKey] = oldObjectVal
+		data[engine.NamespaceObjectKey] = namespaceVal
+		data[engine.RequestKey] = requestVal.Object
+		data[engine.ObjectKey] = objectVal
+		data[engine.OldObjectKey] = oldObjectVal
 		data[engine.VariablesKey] = vars
 		data[engine.GlobalContextKey] = globalcontext.Context{ContextInterface: context}
 		data[engine.ImageDataKey] = imagedata.Context{ContextInterface: context}
 		data[engine.ResourceKey] = resource.Context{ContextInterface: context}
 	} else {
-		data[ObjectKey] = request
+		data[engine.ObjectKey] = request
 	}
 	images, err := variables.ExtractImages(c.imageExtractors, data)
 	if err != nil {
 		return nil, err
 	}
-	data[ImagesKey] = images
-	data[AttestationKey] = c.attestationList
+	data[engine.ImagesKey] = images
+	data[engine.AttestationKey] = c.attestationList
 	attestors := make(map[string]policiesv1alpha1.Attestor)
 	for _, att := range c.attestors {
 		data, err := att.Evaluate(data)
@@ -136,7 +136,7 @@ func (c *compiledPolicy) Evaluate(ctx context.Context, ictx imagedataloader.Imag
 		}
 		attestors[data.Name] = data
 	}
-	data[AttestorKey] = attestors
+	data[engine.AttestorKey] = attestors
 
 	imgList := []string{}
 	for _, v := range images {
@@ -220,12 +220,12 @@ func (p *compiledPolicy) match(
 		if err != nil {
 			return false, fmt.Errorf("failed to prepare oldObject variable for evaluation: %w", err)
 		}
-		data[NamespaceObjectKey] = namespaceVal
-		data[RequestKey] = requestVal.Object
-		data[ObjectKey] = objectVal
-		data[OldObjectKey] = oldObjectVal
+		data[engine.NamespaceObjectKey] = namespaceVal
+		data[engine.RequestKey] = requestVal.Object
+		data[engine.ObjectKey] = objectVal
+		data[engine.OldObjectKey] = oldObjectVal
 	} else {
-		data[ObjectKey] = request
+		data[engine.ObjectKey] = request
 	}
 	var errs []error
 	for _, matchCondition := range matchConditions {
