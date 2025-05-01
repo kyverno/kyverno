588fa10786ec20df227af56909aa016216603bb4
Mariam Fahmy <mariam.fahmy@nirmata.com>
2025-05-01 11:50:08 +0100

    chore: use GetRESTMapper() (#12987)
    Signed-off-by: Yogi Suthari <yrsuthari@gmail.com>

--- cmd/cli/kubectl-kyverno/processor/utils.go
+++ cmd/cli/kubectl-kyverno/processor/utils.go
@@ -2,10 +2,6 @@ package processor
 
 import (
 	kyvernov1 "github.com/kyverno/kyverno/api/kyverno/v1"
-	"github.com/kyverno/kyverno/cmd/cli/kubectl-kyverno/data"
-	"github.com/kyverno/kyverno/pkg/clients/dclient"
-	"k8s.io/apimachinery/pkg/api/meta"
-	"k8s.io/client-go/restmapper"
 )
 
 func policyHasValidateOrVerifyImageChecks(policy kyvernov1.PolicyInterface) bool {
@@ -16,23 +12,4 @@ func policyHasValidateOrVerifyImageChecks(policy kyvernov1.PolicyInterface) bool
 		}
 	}
 	return false
-}
-
-func getRESTMapper(client dclient.Interface, isFake bool) (meta.RESTMapper, error) {
-	var restMapper meta.RESTMapper
-	// check that it is not a fake client
-	if client != nil && !isFake {
-		apiGroupResources, err := restmapper.GetAPIGroupResources(client.GetKubeClient().Discovery())
-		if err != nil {
-			return nil, err
-		}
-		restMapper = restmapper.NewDiscoveryRESTMapper(apiGroupResources)
-	} else {
-		apiGroupResources, err := data.APIGroupResources()
-		if err != nil {
-			return nil, err
-		}
-		restMapper = restmapper.NewDiscoveryRESTMapper(apiGroupResources)
-	}
-	return restMapper, nil
 }
