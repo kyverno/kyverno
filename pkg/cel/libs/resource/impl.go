347ebd6f56fb92a3b8c9906d40765887529f174e
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2025-05-07 15:39:53 +0200

    more tests
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/cel/libs/resource/impl.go
+++ pkg/cel/libs/resource/impl.go
@@ -3,7 +3,6 @@ package resource
 import (
 	"github.com/google/cel-go/common/types"
 	"github.com/google/cel-go/common/types/ref"
-	"github.com/kyverno/kyverno/pkg/cel/utils"
 )
 
 type impl struct {
@@ -11,14 +10,17 @@ type impl struct {
 }
 
 func (c *impl) list_resources_string_string_string(args ...ref.Val) ref.Val {
-	if self, err := utils.ConvertToNative[Context](args[0]); err != nil {
-		return types.WrapErr(err)
-	} else if apiVersion, err := utils.ConvertToNative[string](args[1]); err != nil {
-		return types.WrapErr(err)
-	} else if resource, err := utils.ConvertToNative[string](args[2]); err != nil {
-		return types.WrapErr(err)
-	} else if namespace, err := utils.ConvertToNative[string](args[3]); err != nil {
-		return types.WrapErr(err)
+	if len(args) != 4 {
+		return types.NewErr("expected 4 arguments, got %d", len(args))
+	}
+	if self, err := getArg[Context](args, 0); err != nil {
+		return err
+	} else if apiVersion, err := getArg[string](args, 1); err != nil {
+		return err
+	} else if resource, err := getArg[string](args, 2); err != nil {
+		return err
+	} else if namespace, err := getArg[string](args, 3); err != nil {
+		return err
 	} else {
 		list, err := self.ListResources(apiVersion, resource, namespace)
 		if err != nil {
@@ -30,16 +32,19 @@ func (c *impl) list_resources_string_string_string(args ...ref.Val) ref.Val {
 }
 
 func (c *impl) get_resource_string_string_string_string(args ...ref.Val) ref.Val {
-	if self, err := utils.ConvertToNative[Context](args[0]); err != nil {
-		return types.WrapErr(err)
-	} else if apiVersion, err := utils.ConvertToNative[string](args[1]); err != nil {
-		return types.WrapErr(err)
-	} else if resource, err := utils.ConvertToNative[string](args[2]); err != nil {
-		return types.WrapErr(err)
-	} else if namespace, err := utils.ConvertToNative[string](args[3]); err != nil {
-		return types.WrapErr(err)
-	} else if name, err := utils.ConvertToNative[string](args[4]); err != nil {
-		return types.WrapErr(err)
+	if len(args) != 5 {
+		return types.NewErr("expected 5 arguments, got %d", len(args))
+	}
+	if self, err := getArg[Context](args, 0); err != nil {
+		return err
+	} else if apiVersion, err := getArg[string](args, 1); err != nil {
+		return err
+	} else if resource, err := getArg[string](args, 2); err != nil {
+		return err
+	} else if namespace, err := getArg[string](args, 3); err != nil {
+		return err
+	} else if name, err := getArg[string](args, 4); err != nil {
+		return err
 	} else {
 		res, err := self.GetResource(apiVersion, resource, namespace, name)
 		if err != nil {
@@ -51,16 +56,19 @@ func (c *impl) get_resource_string_string_string_string(args ...ref.Val) ref.Val
 }
 
 func (c *impl) post_resource_string_string_string_map(args ...ref.Val) ref.Val {
-	if self, err := utils.ConvertToNative[Context](args[0]); err != nil {
-		return types.WrapErr(err)
-	} else if apiVersion, err := utils.ConvertToNative[string](args[1]); err != nil {
-		return types.WrapErr(err)
-	} else if resource, err := utils.ConvertToNative[string](args[2]); err != nil {
-		return types.WrapErr(err)
-	} else if namespace, err := utils.ConvertToNative[string](args[3]); err != nil {
-		return types.WrapErr(err)
-	} else if data, err := utils.ConvertToNative[map[string]any](args[4]); err != nil {
-		return types.WrapErr(err)
+	if len(args) != 5 {
+		return types.NewErr("expected 5 arguments, got %d", len(args))
+	}
+	if self, err := getArg[Context](args, 0); err != nil {
+		return err
+	} else if apiVersion, err := getArg[string](args, 1); err != nil {
+		return err
+	} else if resource, err := getArg[string](args, 2); err != nil {
+		return err
+	} else if namespace, err := getArg[string](args, 3); err != nil {
+		return err
+	} else if data, err := getArg[map[string]any](args, 4); err != nil {
+		return err
 	} else {
 		res, err := self.PostResource(apiVersion, resource, namespace, data)
 		if err != nil {
@@ -72,20 +80,8 @@ func (c *impl) post_resource_string_string_string_map(args ...ref.Val) ref.Val {
 }
 
 func (c *impl) post_resource_string_string_map(args ...ref.Val) ref.Val {
-	if self, err := utils.ConvertToNative[Context](args[0]); err != nil {
-		return types.WrapErr(err)
-	} else if apiVersion, err := utils.ConvertToNative[string](args[1]); err != nil {
-		return types.WrapErr(err)
-	} else if resource, err := utils.ConvertToNative[string](args[2]); err != nil {
-		return types.WrapErr(err)
-	} else if data, err := utils.ConvertToNative[map[string]any](args[3]); err != nil {
-		return types.WrapErr(err)
-	} else {
-		res, err := self.PostResource(apiVersion, resource, "", data)
-		if err != nil {
-			// Errors are not expected here since Parse is a more lenient parser than ParseRequestURI.
-			return types.NewErr("failed to create resource: %v", err)
-		}
-		return c.NativeToValue(res.UnstructuredContent())
+	if len(args) != 4 {
+		return types.NewErr("expected 4 arguments, got %d", len(args))
 	}
+	return c.post_resource_string_string_string_map(args[0], args[1], args[2], types.String(""), args[3])
 }
