cd2749fd2d2d96893f382e31001cdba92e9e31a5
Frank Jogeleit <frank.jogeleit@web.de>
2025-05-15 10:58:54 +0200

    remove unused function
    Signed-off-by: Frank Jogeleit <frank.jogeleit@web.de>

--- pkg/cel/policies/dpol/compiler/eval.go
+++ pkg/cel/policies/dpol/compiler/eval.go
@@ -1,12 +1,8 @@
 package compiler
 
 import (
-	"fmt"
-
 	policiesv1alpha1 "github.com/kyverno/kyverno/api/policies.kyverno.io/v1alpha1"
 	"github.com/kyverno/kyverno/pkg/cel/libs"
-	"github.com/kyverno/kyverno/pkg/cel/utils"
-	"k8s.io/apiserver/pkg/admission"
 	"k8s.io/apiserver/pkg/cel/lazy"
 )
 
@@ -21,15 +17,4 @@ type evaluationData struct {
 	Object    any
 	Context   libs.Context
 	Variables *lazy.MapValue
-}
-
-func prepareK8sData(attr admission.Attributes, context libs.Context) (evaluationData, error) {
-	objectVal, err := utils.ObjectToResolveVal(attr.GetObject())
-	if err != nil {
-		return evaluationData{}, fmt.Errorf("failed to prepare object variable for evaluation: %w", err)
-	}
-	return evaluationData{
-		Object:  objectVal,
-		Context: context,
-	}, nil
 }
