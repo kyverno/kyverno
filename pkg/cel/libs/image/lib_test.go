b23c6be98f80dca41d87db837fcff209879582c5
Denish Tomar <denishtomar3436@gmail.com>
2025-05-18 16:48:39 +0530

    rename ParseImageRef → parseImageReference + add deprecated
    Signed-off-by: Denish Tomar <denishtomar3436@gmail.com>

--- pkg/cel/libs/image/lib_test.go
+++ pkg/cel/libs/image/lib_test.go
@@ -98,12 +98,12 @@ func TestImage(t *testing.T) {
 	}{
 		{
 			name:        "parse",
-			expr:        `ParseImageRef("registry.k8s.io/kube-apiserver-arm64:latest")`,
+			expr:        `parseImageReference("registry.k8s.io/kube-apiserver-arm64:latest")`,
 			expectValue: image.Image{ImageReference: image.ConvertToImageRef(name.MustParseReference("registry.k8s.io/kube-apiserver-arm64:latest"))},
 		},
 		{
 			name:               "parse_invalid_image",
-			expr:               `ParseImageRef("registry.k8s.io/kube-apiserver-arm64:@")`,
+			expr:               `parseImageReference("registry.k8s.io/kube-apiserver-arm64:@")`,
 			expectedRuntimeErr: "could not parse reference: registry.k8s.io/kube-apiserver-arm64:@",
 		},
 		{
@@ -123,97 +123,97 @@ func TestImage(t *testing.T) {
 		},
 		{
 			name:        "contains_digest_no_identifier",
-			expr:        `ParseImageRef("registry.k8s.io/kube-apiserver-arm64").containsDigest()`,
+			expr:        `parseImageReference("registry.k8s.io/kube-apiserver-arm64").containsDigest()`,
 			expectValue: falseVal,
 		},
 		{
 			name:        "contains_digest_tag",
-			expr:        `ParseImageRef("registry.k8s.io/kube-apiserver-arm64:latest").containsDigest()`,
+			expr:        `parseImageReference("registry.k8s.io/kube-apiserver-arm64:latest").containsDigest()`,
 			expectValue: falseVal,
 		},
 		{
 			name:        "contains_digest_true",
-			expr:        `ParseImageRef("registry.k8s.io/kube-apiserver-arm64@sha256:6aefddb645ee6963afd681b1845c661d0ea4c3b20ab9db86d9e753b203d385f2").containsDigest()`,
+			expr:        `parseImageReference("registry.k8s.io/kube-apiserver-arm64@sha256:6aefddb645ee6963afd681b1845c661d0ea4c3b20ab9db86d9e753b203d385f2").containsDigest()`,
 			expectValue: trueVal,
 		},
 		{
 			name:        "contains_digest_with_tag_true",
-			expr:        `ParseImageRef("registry.k8s.io/kube-apiserver-arm64:latest@sha256:6aefddb645ee6963afd681b1845c661d0ea4c3b20ab9db86d9e753b203d385f2").containsDigest()`,
+			expr:        `parseImageReference("registry.k8s.io/kube-apiserver-arm64:latest@sha256:6aefddb645ee6963afd681b1845c661d0ea4c3b20ab9db86d9e753b203d385f2").containsDigest()`,
 			expectValue: trueVal,
 		},
 		{
 			name:        "registry",
-			expr:        `ParseImageRef("registry.k8s.io/kube-apiserver-arm64").registry() == "registry.k8s.io"`,
+			expr:        `parseImageReference("registry.k8s.io/kube-apiserver-arm64").registry() == "registry.k8s.io"`,
 			expectValue: trueVal,
 		},
 		{
 			name:        "registry_matches",
-			expr:        `ParseImageRef("registry.k8s.io/kube-apiserver-arm64").registry().matches("(registry.k8s.io|ghcr.io)")`,
+			expr:        `parseImageReference("registry.k8s.io/kube-apiserver-arm64").registry().matches("(registry.k8s.io|ghcr.io)")`,
 			expectValue: trueVal,
 		},
 		{
 			name:        "repository",
-			expr:        `ParseImageRef("registry.k8s.io/kube-apiserver-arm64").repository() == "kube-apiserver-arm64"`,
+			expr:        `parseImageReference("registry.k8s.io/kube-apiserver-arm64").repository() == "kube-apiserver-arm64"`,
 			expectValue: trueVal,
 		},
 		{
 			name:        "identifier_tag",
-			expr:        `ParseImageRef("registry.k8s.io/kube-apiserver-arm64:testtag").identifier()`,
+			expr:        `parseImageReference("registry.k8s.io/kube-apiserver-arm64:testtag").identifier()`,
 			expectValue: types.String("testtag"),
 		},
 		{
 			name:        "default_identifier",
-			expr:        `ParseImageRef("registry.k8s.io/kube-apiserver-arm64").identifier()`,
+			expr:        `parseImageReference("registry.k8s.io/kube-apiserver-arm64").identifier()`,
 			expectValue: types.String("latest"),
 		},
 		{
 			name:        "identifer_digest",
-			expr:        `ParseImageRef("registry.k8s.io/kube-apiserver-arm64@sha256:6aefddb645ee6963afd681b1845c661d0ea4c3b20ab9db86d9e753b203d385f2").identifier()`,
+			expr:        `parseImageReference("registry.k8s.io/kube-apiserver-arm64@sha256:6aefddb645ee6963afd681b1845c661d0ea4c3b20ab9db86d9e753b203d385f2").identifier()`,
 			expectValue: types.String("sha256:6aefddb645ee6963afd681b1845c661d0ea4c3b20ab9db86d9e753b203d385f2"),
 		},
 		{
 			name:        "identifer_digest_and_tag",
-			expr:        `ParseImageRef("registry.k8s.io/kube-apiserver-arm64:latest@sha256:6aefddb645ee6963afd681b1845c661d0ea4c3b20ab9db86d9e753b203d385f2").identifier()`,
+			expr:        `parseImageReference("registry.k8s.io/kube-apiserver-arm64:latest@sha256:6aefddb645ee6963afd681b1845c661d0ea4c3b20ab9db86d9e753b203d385f2").identifier()`,
 			expectValue: types.String("sha256:6aefddb645ee6963afd681b1845c661d0ea4c3b20ab9db86d9e753b203d385f2"),
 		},
 		{
 			name:        "tag",
-			expr:        `ParseImageRef("registry.k8s.io/kube-apiserver-arm64:testtag").tag()`,
+			expr:        `parseImageReference("registry.k8s.io/kube-apiserver-arm64:testtag").tag()`,
 			expectValue: types.String("testtag"),
 		},
 		{
 			name:        "default_tag",
-			expr:        `ParseImageRef("registry.k8s.io/kube-apiserver-arm64").tag()`,
+			expr:        `parseImageReference("registry.k8s.io/kube-apiserver-arm64").tag()`,
 			expectValue: types.String("latest"),
 		},
 		{
 			name:        "no_tag",
-			expr:        `ParseImageRef("registry.k8s.io/kube-apiserver-arm64@sha256:6aefddb645ee6963afd681b1845c661d0ea4c3b20ab9db86d9e753b203d385f2").tag()`,
+			expr:        `parseImageReference("registry.k8s.io/kube-apiserver-arm64@sha256:6aefddb645ee6963afd681b1845c661d0ea4c3b20ab9db86d9e753b203d385f2").tag()`,
 			expectValue: types.String(""),
 		},
 		{
 			name:        "identifier_tag",
-			expr:        `ParseImageRef("registry.k8s.io/kube-apiserver-arm64:testtag").identifier()`,
+			expr:        `parseImageReference("registry.k8s.io/kube-apiserver-arm64:testtag").identifier()`,
 			expectValue: types.String("testtag"),
 		},
 		{
 			name:        "no_digest",
-			expr:        `ParseImageRef("registry.k8s.io/kube-apiserver-arm64").digest()`,
+			expr:        `parseImageReference("registry.k8s.io/kube-apiserver-arm64").digest()`,
 			expectValue: types.String(""),
 		},
 		{
 			name:        "digest_tag",
-			expr:        `ParseImageRef("registry.k8s.io/kube-apiserver-arm64:testtag").digest()`,
+			expr:        `parseImageReference("registry.k8s.io/kube-apiserver-arm64:testtag").digest()`,
 			expectValue: types.String(""),
 		},
 		{
 			name:        "digest",
-			expr:        `ParseImageRef("registry.k8s.io/kube-apiserver-arm64@sha256:6aefddb645ee6963afd681b1845c661d0ea4c3b20ab9db86d9e753b203d385f2").digest()`,
+			expr:        `parseImageReference("registry.k8s.io/kube-apiserver-arm64@sha256:6aefddb645ee6963afd681b1845c661d0ea4c3b20ab9db86d9e753b203d385f2").digest()`,
 			expectValue: types.String("sha256:6aefddb645ee6963afd681b1845c661d0ea4c3b20ab9db86d9e753b203d385f2"),
 		},
 		{
 			name:        "digest_digest_and_tag",
-			expr:        `ParseImageRef("registry.k8s.io/kube-apiserver-arm64:latest@sha256:6aefddb645ee6963afd681b1845c661d0ea4c3b20ab9db86d9e753b203d385f2").digest() == "sha256:6aefddb645ee6963afd681b1845c661d0ea4c3b20ab9db86d9e753b203d385f2"`,
+			expr:        `parseImageReference("registry.k8s.io/kube-apiserver-arm64:latest@sha256:6aefddb645ee6963afd681b1845c661d0ea4c3b20ab9db86d9e753b203d385f2").digest() == "sha256:6aefddb645ee6963afd681b1845c661d0ea4c3b20ab9db86d9e753b203d385f2"`,
 			expectValue: trueVal,
 		},
 	}
