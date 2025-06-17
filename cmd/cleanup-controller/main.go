5e7905583fa4c547bac9860266bac1eb83f9c8eb
刘旭 <liuxu623@gmail.com>
2025-06-17 04:24:38 +0000

    feat(gctx): prefer use built-in informer for native resources, only use dynamic informer for CRD (#13364)
    Signed-off-by: liuxu <liuxu623@gmail.com>
    Co-authored-by: shuting <shuting@nirmata.com>

--- cmd/cleanup-controller/main.go
+++ cmd/cleanup-controller/main.go
@@ -173,6 +173,7 @@ func main() {
 			globalcontextcontroller.ControllerName,
 			globalcontextcontroller.NewController(
 				kyvernoInformer.Kyverno().V2alpha1().GlobalContextEntries(),
+				setup.KubeClient,
 				setup.KyvernoDynamicClient,
 				setup.KyvernoClient,
 				gcstore,
