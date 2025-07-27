9839d2d723e06022b77df7ed51605149f511b774
aerosouund <aerosound161@gmail.com>
2025-07-28 00:24:53 +0300

    fix linter complaints
    Signed-off-by: aerosouund <aerosound161@gmail.com>

--- pkg/controllers/report/resource/controller.go
+++ pkg/controllers/report/resource/controller.go
@@ -201,7 +201,7 @@ func (c *controller) GetAllResourceKeys() []string {
 func (c *controller) UpdateResourceHash(gvr schema.GroupVersionResource, uid types.UID, hash Resource) {
 	c.lock.Lock()
 	defer c.lock.Unlock()
-	w, _ := c.dynamicWatchers[gvr] // we must have a dynamic watcher for the resource, otherwise wouldn't end up here from the beginning
+	w := c.dynamicWatchers[gvr] // we must have a dynamic watcher for the resource, otherwise wouldn't end up here from the beginning
 	w.hashes[uid] = hash
 }
 
@@ -240,13 +240,17 @@ func (c *controller) startWatcher(ctx context.Context, logger logr.Logger, gvr s
 		}
 		// we started watcher for this resource before, use the previously existing hashes
 	} else {
+		// fetch the metadata to get the resource version
+		metadata, err := c.metaClient.Resource(gvr).List(ctx, metav1.ListOptions{})
+		if err != nil {
+			return nil, err
+		}
+		resourceVersion = metadata.GetResourceVersion()
+
 		hashes = w.hashes
-	}
-	// fetch the metadata to get the resource version
-	metadata, err := c.metaClient.Resource(gvr).List(ctx, metav1.ListOptions{})
-	resourceVersion = metadata.GetResourceVersion()
-	for uid := range hashes {
-		c.notify(Added, uid, gvk, hashes[uid])
+		for uid := range hashes {
+			c.notify(Added, uid, gvk, hashes[uid])
+		}
 	}
 
 	logger = logger.WithValues("resourceVersion", resourceVersion)
