26103ff8027547edb2bd86d62c3519b16346d700
Yugandhar Suthari <yrsuthari@gmail.com>
2025-09-03 19:14:27 +0530

    refactor: address PR feedback for GlobalContextEntry v2beta1 migration
    - Remove unnecessary conversion functions since v2alpha1 and v2beta1 schemas are identical
    - Update v2alpha1 to use type aliases referencing v2beta1 types to avoid duplication
    - Change storage version back to v2alpha1 for version 1.16 compatibility (v2beta1 will be storage in 1.17)
    - Remove CLI completion command changes as they are out of scope for this PR
    - Regenerate CRDs and client code to reflect the changes
    - Update tests to remove conversion function tests
    
    Addresses feedback from: https://github.com/kyverno/kyverno/pull/13912
    
    Signed-off-by: Yugandhar Suthari <yrsuthari@gmail.com>

--- api/kyverno/v2alpha1/global_context_entry_status.go
+++ api/kyverno/v2alpha1/global_context_entry_status.go
@@ -1,54 +1,19 @@
 package v2alpha1
 
 import (
-	"k8s.io/apimachinery/pkg/api/meta"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	kyvernov2beta1 "github.com/kyverno/kyverno/api/kyverno/v2beta1"
 )
 
 const (
 	// PolicyConditionReady means that the globalcontextentry is ready
-	GlobalContextEntryConditionReady = "Ready"
+	GlobalContextEntryConditionReady = kyvernov2beta1.GlobalContextEntryConditionReady
 )
 
 const (
 	// GlobalContextEntryReasonSucceeded is the reason set when the globalcontextentry is ready
-	GlobalContextEntryReasonSucceeded = "Succeeded"
+	GlobalContextEntryReasonSucceeded = kyvernov2beta1.GlobalContextEntryReasonSucceeded
 	// GlobalContextEntryReasonFailed is the reason set when the globalcontextentry is not ready
-	GlobalContextEntryReasonFailed = "Failed"
+	GlobalContextEntryReasonFailed = kyvernov2beta1.GlobalContextEntryReasonFailed
 )
 
-type GlobalContextEntryStatus struct {
-	// Deprecated in favor of Conditions
-	Ready *bool `json:"ready,omitempty"`
-	// +optional
-	Conditions []metav1.Condition `json:"conditions,omitempty"`
-	// Indicates the time when the globalcontextentry was last refreshed successfully for the API Call
-	// +optional
-	LastRefreshTime metav1.Time `json:"lastRefreshTime,omitempty"`
-}
-
-func (status *GlobalContextEntryStatus) SetReady(ready bool, message string) {
-	condition := metav1.Condition{
-		Type:    GlobalContextEntryConditionReady,
-		Message: message,
-	}
-	if ready {
-		condition.Status = metav1.ConditionTrue
-		condition.Reason = GlobalContextEntryReasonSucceeded
-	} else {
-		condition.Status = metav1.ConditionFalse
-		condition.Reason = GlobalContextEntryReasonFailed
-	}
-	status.Ready = nil
-	meta.SetStatusCondition(&status.Conditions, condition)
-}
-
-func (status *GlobalContextEntryStatus) UpdateRefreshTime() {
-	status.LastRefreshTime = metav1.Now()
-}
-
-// IsReady indicates if the globalcontextentry has loaded
-func (status *GlobalContextEntryStatus) IsReady() bool {
-	condition := meta.FindStatusCondition(status.Conditions, GlobalContextEntryConditionReady)
-	return condition != nil && condition.Status == metav1.ConditionTrue
-}
+type GlobalContextEntryStatus = kyvernov2beta1.GlobalContextEntryStatus
