629e72aa960ec185f792d9dc27d1e6a1c7565706
slashexx <dhruvpuri.35@gmail.com>
2025-09-29 19:26:35 +0530

    regen with new tools
    Signed-off-by: slashexx <dhruvpuri.35@gmail.com>

--- pkg/client/clientset/versioned/typed/policies.kyverno.io/v1alpha1/policies.kyverno.io_client.go
+++ pkg/client/clientset/versioned/typed/policies.kyverno.io/v1alpha1/policies.kyverno.io_client.go
@@ -75,9 +75,7 @@ func (c *PoliciesV1alpha1Client) ValidatingPolicies() ValidatingPolicyInterface 
 // where httpClient was generated with rest.HTTPClientFor(c).
 func NewForConfig(c *rest.Config) (*PoliciesV1alpha1Client, error) {
 	config := *c
-	if err := setConfigDefaults(&config); err != nil {
-		return nil, err
-	}
+	setConfigDefaults(&config)
 	httpClient, err := rest.HTTPClientFor(&config)
 	if err != nil {
 		return nil, err
@@ -89,9 +87,7 @@ func NewForConfig(c *rest.Config) (*PoliciesV1alpha1Client, error) {
 // Note the http client provided takes precedence over the configured transport values.
 func NewForConfigAndClient(c *rest.Config, h *http.Client) (*PoliciesV1alpha1Client, error) {
 	config := *c
-	if err := setConfigDefaults(&config); err != nil {
-		return nil, err
-	}
+	setConfigDefaults(&config)
 	client, err := rest.RESTClientForConfigAndClient(&config, h)
 	if err != nil {
 		return nil, err
@@ -114,7 +110,7 @@ func New(c rest.Interface) *PoliciesV1alpha1Client {
 	return &PoliciesV1alpha1Client{c}
 }
 
-func setConfigDefaults(config *rest.Config) error {
+func setConfigDefaults(config *rest.Config) {
 	gv := policieskyvernoiov1alpha1.SchemeGroupVersion
 	config.GroupVersion = &gv
 	config.APIPath = "/apis"
@@ -123,8 +119,6 @@ func setConfigDefaults(config *rest.Config) error {
 	if config.UserAgent == "" {
 		config.UserAgent = rest.DefaultKubernetesUserAgent()
 	}
-
-	return nil
 }
 
 // RESTClient returns a RESTClient that is used to communicate
--- pkg/client/informers/externalversions/policies.kyverno.io/v1alpha1/namespaceddeletingpolicy.go
+++ pkg/client/informers/externalversions/policies.kyverno.io/v1alpha1/namespaceddeletingpolicy.go
@@ -62,13 +62,25 @@ func NewFilteredNamespacedDeletingPolicyInformer(client versioned.Interface, nam
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.PoliciesV1alpha1().NamespacedDeletingPolicies(namespace).List(context.TODO(), options)
+				return client.PoliciesV1alpha1().NamespacedDeletingPolicies(namespace).List(context.Background(), options)
 			},
 			WatchFunc: func(options v1.ListOptions) (watch.Interface, error) {
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.PoliciesV1alpha1().NamespacedDeletingPolicies(namespace).Watch(context.TODO(), options)
+				return client.PoliciesV1alpha1().NamespacedDeletingPolicies(namespace).Watch(context.Background(), options)
+			},
+			ListWithContextFunc: func(ctx context.Context, options v1.ListOptions) (runtime.Object, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.PoliciesV1alpha1().NamespacedDeletingPolicies(namespace).List(ctx, options)
+			},
+			WatchFuncWithContext: func(ctx context.Context, options v1.ListOptions) (watch.Interface, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.PoliciesV1alpha1().NamespacedDeletingPolicies(namespace).Watch(ctx, options)
 			},
 		},
 		&apipolicieskyvernoiov1alpha1.NamespacedDeletingPolicy{},
