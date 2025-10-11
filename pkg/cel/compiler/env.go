1384a78435d1c917064538bac9f55f5f37385f33
aerosouund <aerosound161@gmail.com>
2025-10-11 17:14:30 +0300

    default env options
    Signed-off-by: aerosouund <aerosound161@gmail.com>

--- pkg/cel/compiler/env.go
+++ pkg/cel/compiler/env.go
@@ -36,28 +36,7 @@ func DefaultEnvOptions() []cel.EnvOption {
 func NewBaseEnv() (*cel.Env, error) {
 	// create new cel env
 	return cel.NewEnv(
-		// configure env
-		cel.HomogeneousAggregateLiterals(),
-		cel.EagerlyValidateDeclarations(true),
-		cel.DefaultUTCTimeZone(true),
-		cel.CrossTypeNumericComparisons(true),
-		// register common libs
-		cel.OptionalTypes(),
-		ext.Bindings(),
-		ext.Encoders(),
-		ext.Lists(),
-		ext.Math(),
-		ext.Protos(),
-		ext.Sets(),
-		ext.Strings(),
-		// register kubernetes libs
-		library.CIDR(),
-		library.Format(),
-		library.IP(),
-		library.Lists(),
-		library.Regex(),
-		library.URLs(),
-		library.Quantity(),
+		DefaultEnvOptions()...,
 	)
 }
 
