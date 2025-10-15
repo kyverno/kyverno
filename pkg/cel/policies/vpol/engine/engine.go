2d2291d93a7727fa352a4c96ff1abc11968678b6
Mariam Fahmy <mariam.fahmy@nirmata.com>
2025-10-15 16:34:40 +0700

    feat: configure report result via exceptions (#14175)
    * feat: configure report result via exceptions
    
    Signed-off-by: Mariam Fahmy <mariam.fahmy@nirmata.com>
    
    * fix chainsaw tests
    
    Signed-off-by: Mariam Fahmy <mariam.fahmy@nirmata.com>
    
    * chore: add new test
    
    Signed-off-by: Mariam Fahmy <mariam.fahmy@nirmata.com>
    
    * fix chainsaw tests
    
    Signed-off-by: Mariam Fahmy <mariam.fahmy@nirmata.com>
    
    ---------
    
    Signed-off-by: Mariam Fahmy <mariam.fahmy@nirmata.com>

--- pkg/cel/policies/vpol/engine/engine.go
+++ pkg/cel/policies/vpol/engine/engine.go
@@ -2,6 +2,8 @@ package engine
 
 import (
 	"context"
+	"fmt"
+	"strconv"
 	"strings"
 	"time"
 
@@ -13,6 +15,7 @@ import (
 	engineapi "github.com/kyverno/kyverno/pkg/engine/api"
 	"github.com/kyverno/kyverno/pkg/engine/handlers"
 	admissionutils "github.com/kyverno/kyverno/pkg/utils/admission"
+	reportutils "github.com/kyverno/kyverno/pkg/utils/report"
 	admissionv1 "k8s.io/api/admission/v1"
 	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -137,17 +140,58 @@ func (e *engineImpl) handlePolicy(ctx context.Context, policy Policy, jsonPayloa
 		response.Rules = append(response.Rules, *engineapi.RuleSkip("", engineapi.Validation, "skip", nil))
 	} else if len(result.Exceptions) > 0 {
 		exceptions := make([]engineapi.GenericException, 0, len(result.Exceptions))
-		var keys []string
-		for i := range result.Exceptions {
-			key, err := cache.MetaNamespaceKeyFunc(result.Exceptions[i])
+		keys := make([]string, 0, len(result.Exceptions))
+
+		var (
+			highestPriority int
+			selectedIndex   int
+		)
+		for i, ex := range result.Exceptions {
+			key, err := cache.MetaNamespaceKeyFunc(ex)
 			if err != nil {
-				response.Rules = handlers.WithResponses(engineapi.RuleError("exception", engineapi.Validation, "failed to compute exception key", err, nil))
+				response.Rules = handlers.WithResponses(
+					engineapi.RuleError(
+						"exception",
+						engineapi.Validation,
+						"failed to compute exception key",
+						err,
+						nil,
+					),
+				)
 				return response
 			}
+
 			keys = append(keys, key)
-			exceptions = append(exceptions, engineapi.NewCELPolicyException(result.Exceptions[i]))
+			exceptions = append(exceptions, engineapi.NewCELPolicyException(ex))
+
+			// evaluate exception priority from label
+			if val, ok := ex.GetLabels()[reportutils.LabelPolicyExceptionPriority]; ok {
+				if p, err := strconv.Atoi(val); err == nil && p > highestPriority {
+					highestPriority = p
+					selectedIndex = i
+				}
+			}
 		}
-		response.Rules = handlers.WithResponses(engineapi.RuleSkip("exception", engineapi.Validation, "rule is skipped due to policy exception: "+strings.Join(keys, ", "), nil).WithExceptions(exceptions))
+		// determine final result based on highest-priority exception
+		selectedException := result.Exceptions[selectedIndex]
+		reportResult := selectedException.Spec.ReportResult
+
+		joinedKeys := strings.Join(keys, ", ")
+		msgPrefix := "rule is %s due to policy exception: " + joinedKeys
+		switch reportResult {
+		case string(engineapi.RuleStatusPass):
+			response.Rules = handlers.WithResponses(
+				engineapi.RulePass("exception", engineapi.Validation,
+					fmt.Sprintf(msgPrefix, "passed"), nil,
+				).WithExceptions(exceptions),
+			)
+		default:
+			response.Rules = handlers.WithResponses(
+				engineapi.RuleSkip("exception", engineapi.Validation,
+					fmt.Sprintf(msgPrefix, "skipped"), nil,
+				).WithExceptions(exceptions),
+			)
+		}
 	} else {
 		// TODO: do we want to set a rule name?
 		ruleName := ""
