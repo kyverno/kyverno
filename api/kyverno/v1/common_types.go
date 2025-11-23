01f1d98f71b8bbe3b5fd41285e2b997b5d861c65
Pradeep Lakshmi Narasimha <pradeep.vaishnav4@gmail.com>
2025-11-23 15:49:06 +0530

    add kyverno CLI test cases for the new global flag behaviour
    Signed-off-by: Pradeep Lakshmi Narasimha <pradeep.vaishnav4@gmail.com>

--- api/kyverno/v1/common_types.go
+++ api/kyverno/v1/common_types.go
@@ -483,6 +483,7 @@ type Validation struct {
 	FailureActionOverrides []ValidationFailureActionOverride `json:"failureActionOverrides,omitempty"`
 
 	// AllowExistingViolations allows prexisting violating resources to continue violating a policy.
+	// If set, this overrides the global defaultAllowExistingViolations configuration.
 	// +kubebuilder:validation:Optional
 	AllowExistingViolations *bool `json:"allowExistingViolations,omitempty"`
 
