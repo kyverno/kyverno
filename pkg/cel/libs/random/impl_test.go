cf3fc86042c5be21119bbeb51273d32ccc10e303
aerosouund <aerosound161@gmail.com>
2025-12-29 15:13:59 +0200

    add no param test
    Signed-off-by: aerosouund <aerosound161@gmail.com>

--- pkg/cel/libs/random/impl_test.go
+++ pkg/cel/libs/random/impl_test.go
@@ -38,3 +38,33 @@ func Test_random(t *testing.T) {
 		assert.True(t, matched, "generated string %s should match pattern %s", value, pattern)
 	})
 }
+
+func Test_random_no_param(t *testing.T) {
+	base, err := compiler.NewBaseEnv()
+	assert.NoError(t, err)
+	assert.NotNil(t, base)
+	options := []cel.EnvOption{
+		Lib(nil),
+	}
+	env, err := base.Extend(options...)
+	assert.NoError(t, err)
+	assert.NotNil(t, env)
+
+	t.Run("random_string_no_parameter", func(t *testing.T) {
+		defaultPattern := "[0-9a-z]{8}"
+		ast, issues := env.Compile(`random()`)
+		assert.Nil(t, issues)
+		assert.NotNil(t, ast)
+		prog, err := env.Program(ast)
+		assert.NoError(t, err)
+		assert.NotNil(t, prog)
+		out, _, err := prog.Eval(map[string]any{})
+		assert.NoError(t, err)
+		value := out.Value().(string)
+
+		// verify the output matches the pattern
+		matched, err := regexp.MatchString("^"+defaultPattern+"$", value)
+		assert.NoError(t, err)
+		assert.True(t, matched, "generated string %s should match pattern %s", value, defaultPattern)
+	})
+}
