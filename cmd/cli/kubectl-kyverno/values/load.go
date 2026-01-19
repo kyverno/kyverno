d36b25f6c79cc9acf137115abb26d7832eba4a27
shuting <shuting@nirmata.com>
2026-01-19 17:48:43 +0800

    feat: support variable lookup for cpol (#14653)
    * feat: support configmap context lookup for cpol
    
    Signed-off-by: ShutingZhao <shuting@nirmata.com>
    
    * feat: enable CLI API call lookup
    
    Signed-off-by: ShutingZhao <shuting@nirmata.com>
    
    * chore: add missing files
    
    Signed-off-by: ShutingZhao <shuting@nirmata.com>
    
    * chore: linter fixes
    
    Signed-off-by: ShutingZhao <shuting@nirmata.com>
    
    * chore: remove duplicate code
    
    Signed-off-by: ShutingZhao <shuting@nirmata.com>
    
    * chore: update tests
    
    Signed-off-by: ShutingZhao <shuting@nirmata.com>
    
    * chore: update apis
    
    Signed-off-by: ShutingZhao <shuting@nirmata.com>
    
    * chore: fix codegen
    
    Signed-off-by: ShutingZhao <shuting@nirmata.com>
    
    * chore: fix CLI tests
    
    Signed-off-by: ShutingZhao <shuting@nirmata.com>
    
    * chore: fix configmap lookup
    
    Signed-off-by: ShutingZhao <shuting@nirmata.com>
    
    ---------
    
    Signed-off-by: ShutingZhao <shuting@nirmata.com>

--- cmd/cli/kubectl-kyverno/values/load.go
+++ cmd/cli/kubectl-kyverno/values/load.go
@@ -1,34 +1,13 @@
 package values
 
 import (
-	"io"
-	"os"
-
 	"github.com/go-git/go-billy/v5"
 	"github.com/kyverno/kyverno/cmd/cli/kubectl-kyverno/apis/v1alpha1"
-	"k8s.io/apimachinery/pkg/util/yaml"
+	"github.com/kyverno/kyverno/cmd/cli/kubectl-kyverno/utils/common"
 )
 
 func Load(f billy.Filesystem, filepath string) (*v1alpha1.Values, error) {
-	yamlBytes, err := readFile(f, filepath)
-	if err != nil {
-		return nil, err
-	}
-	vals := &v1alpha1.Values{}
-	if err := yaml.UnmarshalStrict(yamlBytes, vals); err != nil {
-		return nil, err
-	}
-	return vals, nil
-}
-
-func readFile(f billy.Filesystem, filepath string) ([]byte, error) {
-	if f != nil {
-		file, err := f.Open(filepath)
-		if err != nil {
-			return nil, err
-		}
-		defer file.Close()
-		return io.ReadAll(file)
-	}
-	return os.ReadFile(filepath)
+	return common.LoadYAML(f, filepath, func() *v1alpha1.Values {
+		return &v1alpha1.Values{}
+	})
 }
