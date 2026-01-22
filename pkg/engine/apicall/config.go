730b68925320313123d2bd6fe66beb54ec51edab
remo-lab <remopanda7@gmail.com>
2026-01-23 00:41:57 +0530

    fix: propagate context and add HTTP timeout in API/registry calls
    Signed-off-by: remo-lab <remopanda7@gmail.com>

--- pkg/engine/apicall/config.go
+++ pkg/engine/apicall/config.go
@@ -1,11 +1,32 @@
 package apicall
 
+import "time"
+
+// DefaultAPICallTimeout is the default timeout for external API calls
+const DefaultAPICallTimeout = 30 * time.Second
+
 type APICallConfiguration struct {
 	maxAPICallResponseLength int64
+	timeout                  time.Duration
 }
 
 func NewAPICallConfiguration(maxLen int64) APICallConfiguration {
 	return APICallConfiguration{
 		maxAPICallResponseLength: maxLen,
+		timeout:                  DefaultAPICallTimeout,
 	}
+}
+
+func NewAPICallConfigurationWithTimeout(maxLen int64, timeout time.Duration) APICallConfiguration {
+	return APICallConfiguration{
+		maxAPICallResponseLength: maxLen,
+		timeout:                  timeout,
+	}
+}
+
+func (c APICallConfiguration) GetTimeout() time.Duration {
+	if c.timeout == 0 {
+		return DefaultAPICallTimeout
+	}
+	return c.timeout
 }
