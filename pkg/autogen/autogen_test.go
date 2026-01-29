6f26360475d1c8483d9ad7f8711a8fe3f17d849e
Anushka Sharan <anushkasharan05@gmail.com>
2026-01-29 19:58:51 +0530

    fix: use gotest.tools/assert and consolidate redundant tests
    Signed-off-by: Anushka Sharan <anushkasharan05@gmail.com>

--- pkg/autogen/autogen_test.go
+++ pkg/autogen/autogen_test.go
@@ -3,15 +3,15 @@ package autogen
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"gotest.tools/assert"
 )
 
 func TestV1_IsNotNil(t *testing.T) {
-	assert.NotNil(t, V1, "V1 autogen should be initialized")
+	assert.Assert(t, V1 != nil, "V1 autogen should be initialized")
 }
 
 func TestV2_IsNotNil(t *testing.T) {
-	assert.NotNil(t, V2, "V2 autogen should be initialized")
+	assert.Assert(t, V2 != nil, "V2 autogen should be initialized")
 }
 
 func TestDefault_IsV1(t *testing.T) {
@@ -26,51 +26,31 @@ func TestAutogenInterface_V2ImplementsInterface(t *testing.T) {
 	var _ Autogen = V2
 }
 
-func TestV1_GetAutogenRuleNames_NilPolicy(t *testing.T) {
-	// V1.GetAutogenRuleNames panics with nil policy
-	assert.Panics(t, func() {
-		V1.GetAutogenRuleNames(nil)
-	}, "V1.GetAutogenRuleNames should panic with nil policy")
+func TestNilPolicy_Panics(t *testing.T) {
+	tests := []struct {
+		name string
+		fn   func()
+	}{
+		{"V1.GetAutogenRuleNames", func() { V1.GetAutogenRuleNames(nil) }},
+		{"V2.GetAutogenRuleNames", func() { V2.GetAutogenRuleNames(nil) }},
+		{"V1.GetAutogenKinds", func() { V1.GetAutogenKinds(nil) }},
+		{"V2.GetAutogenKinds", func() { V2.GetAutogenKinds(nil) }},
+		{"V1.ComputeRules", func() { V1.ComputeRules(nil, "") }},
+		{"V2.ComputeRules", func() { V2.ComputeRules(nil, "") }},
+		{"Default.GetAutogenRuleNames", func() { Default.GetAutogenRuleNames(nil) }},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			defer func() {
+				if r := recover(); r == nil {
+					t.Errorf("%s should panic with nil policy", tt.name)
+				}
+			}()
+			tt.fn()
+		})
+	}
 }
 
-func TestV2_GetAutogenRuleNames_NilPolicy(t *testing.T) {
-	assert.Panics(t, func() {
-		V2.GetAutogenRuleNames(nil)
-	}, "V2.GetAutogenRuleNames should panic with nil policy")
-}
-
-func TestV1_GetAutogenKinds_NilPolicy(t *testing.T) {
-	assert.Panics(t, func() {
-		V1.GetAutogenKinds(nil)
-	}, "V1.GetAutogenKinds should panic with nil policy")
-}
-
-func TestV2_GetAutogenKinds_NilPolicy(t *testing.T) {
-	assert.Panics(t, func() {
-		V2.GetAutogenKinds(nil)
-	}, "V2.GetAutogenKinds should panic with nil policy")
-}
-
-func TestV1_ComputeRules_NilPolicy(t *testing.T) {
-	assert.Panics(t, func() {
-		V1.ComputeRules(nil, "")
-	}, "V1.ComputeRules should panic with nil policy")
-}
-
-func TestV2_ComputeRules_NilPolicy(t *testing.T) {
-	assert.Panics(t, func() {
-		V2.ComputeRules(nil, "")
-	}, "V2.ComputeRules should panic with nil policy")
-}
-
-func TestDefault_GetAutogenRuleNames_NilPolicy(t *testing.T) {
-	assert.Panics(t, func() {
-		Default.GetAutogenRuleNames(nil)
-	}, "Default.GetAutogenRuleNames should panic with nil policy")
-}
-
-func TestAutogenVersions_AreDifferentInstances(t *testing.T) {
-	// V1 and V2 should be different implementations
-	assert.NotNil(t, V1)
-	assert.NotNil(t, V2)
+func TestAutogenVersions_AreDifferent(t *testing.T) {
+	assert.Assert(t, V1 != V2, "V1 and V2 should be different implementations")
 }
