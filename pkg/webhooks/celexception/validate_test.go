7f06791caa1e11e145fa3ad1b5a20e4ff876831f
Anushka Sharan <anushkasharan05@gmail.com>
2026-01-29 18:05:49 +0530

    fix: use types.UID instead of admissionv1.UID for consistency
    Signed-off-by: Anushka Sharan <anushkasharan05@gmail.com>

--- pkg/webhooks/celexception/validate_test.go
+++ pkg/webhooks/celexception/validate_test.go
@@ -12,6 +12,7 @@ import (
 	admissionv1 "k8s.io/api/admission/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 func TestNewHandlers(t *testing.T) {
@@ -134,7 +135,7 @@ func TestValidate_MultipleScenarios(t *testing.T) {
 			
 			request := handlers.AdmissionRequest{
 				AdmissionRequest: admissionv1.AdmissionRequest{
-					UID:       admissionv1.UID(tt.expectedUID),
+					UID:       types.UID(tt.expectedUID),
 					Kind:      metav1.GroupVersionKind{Group: "kyverno.io", Version: "v2alpha1", Kind: "CELPolicyException"},
 					Operation: admissionv1.Create,
 					Object:    runtime.RawExtension{Raw: []byte(tt.objectJSON)},
@@ -182,7 +183,7 @@ func TestValidate_DifferentOperations(t *testing.T) {
 		t.Run(string(op), func(t *testing.T) {
 			request := handlers.AdmissionRequest{
 				AdmissionRequest: admissionv1.AdmissionRequest{
-					UID:       admissionv1.UID("uid-" + string(op)),
+					UID:       types.UID("uid-" + string(op)),
 					Kind:      metav1.GroupVersionKind{Group: "kyverno.io", Version: "v2alpha1", Kind: "CELPolicyException"},
 					Operation: op,
 					Object:    runtime.RawExtension{Raw: []byte(`{}`)},
