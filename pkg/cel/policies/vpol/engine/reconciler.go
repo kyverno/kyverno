61613389cd51f465c2c1ac49d5a5579cbd6aeb73
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2026-02-11 13:08:24 +0100

    fix
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/cel/policies/vpol/engine/reconciler.go
+++ pkg/cel/policies/vpol/engine/reconciler.go
@@ -15,7 +15,6 @@ import (
 	"k8s.io/apimachinery/pkg/util/sets"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 type reconciler struct {
