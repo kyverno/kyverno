4b910eda158c8c434e40b9477c8165323e3aba28
ShutingZhao <shuting@nirmata.com>
2026-02-12 00:28:52 +0800

    chore: linter fixes
    Signed-off-by: ShutingZhao <shuting@nirmata.com>

--- cmd/cli/kubectl-kyverno/utils/common/fetch.go
+++ cmd/cli/kubectl-kyverno/utils/common/fetch.go
@@ -229,7 +229,6 @@ func (rf *ResourceFetcher) getKindsFromPolicy(
 	matchResources *admissionregistrationv1.MatchResources,
 	info *resourceTypeInfo,
 ) {
-
 	restMapper, err := utils.GetRESTMapper(rf.Client)
 	if err != nil {
 		log.Log.V(3).Info("failed to get rest mapper", "error", err)
