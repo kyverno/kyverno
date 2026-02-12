bf46933457638d1475df0f9bb1cae4ee3f23f9f2
Mohd Kamaal <102820439+Mohdcode@users.noreply.github.com>
2026-02-12 17:10:04 +0530

    Fix incorrect policy exclusion reporting for Pod-targeted policies with autogen in vpol/mpol (#13645)
    * add checks for rules
    
    Signed-off-by: Mohd Kamaal <mohdcode@MBA.local>
    
    * update test to adhere to skip result
    
    Signed-off-by: Mohd Kamaal <mohdcode@MBA.local>
    
    ---------
    
    Signed-off-by: Mohd Kamaal <mohdcode@MBA.local>
    Co-authored-by: Mohd Kamaal <mohdcode@MBA.local>
    Co-authored-by: shuting <shuting@nirmata.com>
    Co-authored-by: Ammar Yasser <aerosound161@gmail.com>
    Co-authored-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- cmd/cli/kubectl-kyverno/processor/policy_processor.go
+++ cmd/cli/kubectl-kyverno/processor/policy_processor.go
@@ -325,6 +325,9 @@ func (p *PolicyProcessor) ApplyPoliciesOnResource() ([]engineapi.EngineResponse,
 				return nil, fmt.Errorf("failed to apply mutating policies on resource %s (%w)", resource.GetName(), err)
 			}
 			for _, r := range reps.Policies {
+				if len(r.Rules) == 0 {
+					continue
+				}
 				patched := *reps.Resource
 				if reps.PatchedResource != nil {
 					patched = *reps.PatchedResource
@@ -444,6 +447,9 @@ func (p *PolicyProcessor) ApplyPoliciesOnResource() ([]engineapi.EngineResponse,
 				return nil, fmt.Errorf("failed to apply validating policies on resource %s (%w)", resource.GetName(), err)
 			}
 			for _, r := range reps.Policies {
+				if len(r.Rules) == 0 {
+					continue
+				}
 				response := engineapi.EngineResponse{
 					Resource: *reps.Resource,
 					PolicyResponse: engineapi.PolicyResponse{
