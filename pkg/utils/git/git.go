89a71045b696ab3d6ff29355a67f206f0e97f35a
Jagjeevan Kashid <jagjeevandev97@gmail.com>
2026-02-13 18:55:50 +0530

    fix: made changes according to eddy's suggestions
    Signed-off-by: Jagjeevan Kashid <jagjeevandev97@gmail.com>

--- pkg/utils/git/git.go
+++ pkg/utils/git/git.go
@@ -1,12 +1,10 @@
 package git
 
 import (
-	"context"
 	"fmt"
 	"io/fs"
 	"os"
 	"path/filepath"
-	"time"
 
 	"github.com/go-git/go-billy/v5"
 	"github.com/go-git/go-git/v5"
@@ -15,26 +13,12 @@ import (
 	"github.com/go-git/go-git/v5/storage/memory"
 )
 
-// defaultCloneTimeout is the maximum time allowed for a git clone operation.
-const defaultCloneTimeout = 2 * time.Minute
-
 // CloneFunc is a function type for cloning a git repository into a billy.Filesystem.
 // It can be replaced in tests to avoid real network calls while still exercising
 // the git-URL policy loading code path.
 type CloneFunc func(path string, fs billy.Filesystem, branch string, auth http.BasicAuth) (*git.Repository, error)
 
 func Clone(path string, fs billy.Filesystem, branch string, auth http.BasicAuth) (*git.Repository, error) {
-	return CloneWithContext(context.Background(), path, fs, branch, auth)
-}
-
-func CloneWithContext(ctx context.Context, path string, fs billy.Filesystem, branch string, auth http.BasicAuth) (*git.Repository, error) {
-	// If the context doesn't already have a deadline, apply a default timeout
-	// to prevent indefinite hangs on network operations.
-	if _, hasDeadline := ctx.Deadline(); !hasDeadline {
-		var cancel context.CancelFunc
-		ctx, cancel = context.WithTimeout(ctx, defaultCloneTimeout)
-		defer cancel()
-	}
 	co := &git.CloneOptions{
 		URL:           path,
 		ReferenceName: plumbing.ReferenceName(fmt.Sprintf("refs/heads/%s", branch)),
@@ -45,7 +29,7 @@ func CloneWithContext(ctx context.Context, path string, fs billy.Filesystem, bra
 	if auth.Username != "" && auth.Password != "" {
 		co.Auth = &auth
 	}
-	return git.CloneContext(ctx, memory.NewStorage(), fs, co)
+	return git.Clone(memory.NewStorage(), fs, co)
 }
 
 func ListFiles(fs billy.Filesystem, path string, predicate func(fs.FileInfo) bool) ([]string, error) {
