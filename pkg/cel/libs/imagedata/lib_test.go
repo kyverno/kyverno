de43b7fbc873f00884c64d3a4ff0fd78bea75cd7
aerosouund <aerosound161@gmail.com>
2026-02-19 15:09:35 +0200

    refactor: make the image data library use the globals pattern
    Signed-off-by: aerosouund <aerosound161@gmail.com>

--- pkg/cel/libs/imagedata/lib_test.go
+++ pkg/cel/libs/imagedata/lib_test.go
@@ -3,7 +3,6 @@ package imagedata
 import (
 	"testing"
 
-	"github.com/google/cel-go/cel"
 	"github.com/kyverno/kyverno/pkg/cel/compiler"
 	"github.com/stretchr/testify/assert"
 )
@@ -12,11 +11,10 @@ func TestLib(t *testing.T) {
 	base, err := compiler.NewBaseEnv()
 	assert.NoError(t, err)
 	assert.NotNil(t, base)
-	options := []cel.EnvOption{
-		cel.Variable("image", ContextType),
-		Lib(nil),
-	}
-	env, err := base.Extend(options...)
+
+	env, err := base.Extend(
+		Lib(nil, nil),
+	)
 	assert.NoError(t, err)
 	assert.NotNil(t, env)
 }
