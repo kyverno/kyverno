f03ef16567b2e4140d0b1ff1ac5ea27a94dec46a
Omar Nasser <omarnasserjr@gmail.com>
2026-04-15 23:26:39 +0300

    Support the usage of oldObject in Validating Policies CLI tests
    Signed-off-by: Omar Nasser <omarnasserjr@gmail.com>

--- cmd/cli/kubectl-kyverno/commands/test/test.go
+++ cmd/cli/kubectl-kyverno/commands/test/test.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"path/filepath"
 	"reflect"
+	"slices"
 	"strings"
 
 	authv3 "github.com/envoyproxy/go-control-plane/envoy/service/auth/v3"
@@ -131,6 +132,23 @@ func runTest(out io.Writer, testCase test.TestCase, registryAccess bool) (*TestR
 			fmt.Fprintln(out, "  warning: found duplicated resource", dup.Kind, dup.Name, dup.Namespace)
 		}
 	}
+
+	oldResourceMap := map[string]*unstructured.Unstructured{}
+	if len(testCase.Test.OldResources) > 0 {
+		fmt.Fprintln(out, "  Loading old resources", "...")
+		oldResourceFullPath := path.GetFullPaths(testCase.Test.OldResources, testDir, isGit)
+		oldResources, err := common.GetResourceAccordingToResourcePath(out, testCase.Fs, oldResourceFullPath, false, genericPolicies, dClient, "", false, false, testDir, loader.ResourceOptions{}, false)
+		if err != nil {
+			return nil, fmt.Errorf("error: failed to load old resources (%s)", err)
+		}
+		oldResources = ProcessResources(oldResources)
+		for _, r := range oldResources {
+			if r != nil {
+				oldResourceMap[processor.GenerateResourceKey(r)] = r
+			}
+		}
+	}
+
 	uniquesObjectArr := []runtime.Object{}
 	for _, t := range uniques {
 		uniquesObjectArr = append(uniquesObjectArr, t)
@@ -387,6 +405,7 @@ func runTest(out io.Writer, testCase test.TestCase, registryAccess bool) (*TestR
 			}
 		}
 	}
+
 	// validate policies
 	validPolicies := make([]kyvernov1.PolicyInterface, 0, len(results.Policies))
 	skippedPolicyNames := make(map[string]string)
@@ -410,8 +429,25 @@ func runTest(out io.Writer, testCase test.TestCase, registryAccess bool) (*TestR
 		SkippedPolicies: skippedPolicyNames,
 	}
 	for _, resource := range uniques {
+		// Look up the matching old resource for this specific resource
+		var oldResource *unstructured.Unstructured
+		if len(oldResourceMap) > 0 {
+			oldResource = oldResourceMap[processor.GenerateResourceKey(resource)]
+		}
+
+		policiesByOperation := map[string][]string{}
+		for _, res := range testCase.Test.Results {
+			if !slices.Contains(res.Resources, resource.GetName()) {
+				continue
+			}
+			op := res.ResourceOperation
+			if op == "" {
+				op = "CREATE"
+			}
+			policiesByOperation[op] = append(policiesByOperation[op], res.Policy)
+		}
 		// the policy processor is for multiple policies at once
-		processor := processor.PolicyProcessor{
+		policyProcessor := processor.PolicyProcessor{
 			Store:                             &store,
 			Policies:                          validPolicies,
 			ValidatingAdmissionPolicies:       results.VAPs,
@@ -423,6 +459,7 @@ func runTest(out io.Writer, testCase test.TestCase, registryAccess bool) (*TestR
 			MutatingAdmissionPolicyBindings:   results.MAPBindings,
 			TargetResources:                   targetResources,
 			Resource:                          *resource,
+			OldResource:                       oldResource,
 			PolicyExceptions:                  polexLoader.Exceptions,
 			CELExceptions:                     polexLoader.CELExceptions,
 			ParameterResources:                paramObjectsArr,
@@ -442,8 +479,9 @@ func runTest(out io.Writer, testCase test.TestCase, registryAccess bool) (*TestR
 			Out:                               io.Discard,
 			ConfigMapResolver:                 cmResolver,
 			RESTMapper:                        restMapper,
+			TestPoliciesByOperation:           policiesByOperation,
 		}
-		ers, err := processor.ApplyPoliciesOnResource()
+		ers, err := policyProcessor.ApplyPoliciesOnResource()
 		if err != nil {
 			return nil, fmt.Errorf("failed to apply policies on resource %v (%w)", resource.GetName(), err)
 		}
@@ -492,13 +530,13 @@ func runTest(out io.Writer, testCase test.TestCase, registryAccess bool) (*TestR
 			ers = append(ers, dpols...)
 		}
 
-		resourceKey := generateResourceKey(resource)
+		resourceKey := processor.GenerateResourceKey(resource)
 		engineResponses = append(engineResponses, ers...)
 		testResponse.Trigger[resourceKey] = ers
 	}
 
 	for _, jp := range jsonPayloads {
-		processor := processor.PolicyProcessor{
+		policyProcessor := processor.PolicyProcessor{
 			Store:                             &store,
 			Policies:                          validPolicies,
 			ValidatingAdmissionPolicies:       results.VAPs,
@@ -526,9 +564,10 @@ func runTest(out io.Writer, testCase test.TestCase, registryAccess bool) (*TestR
 			Client:                            dClient,
 			Subresources:                      vars.Subresources(),
 			Out:                               io.Discard,
+			ConfigMapResolver:                 cmResolver,
 			RESTMapper:                        restMapper,
 		}
-		ers, err := processor.ApplyPoliciesOnResource()
+		ers, err := policyProcessor.ApplyPoliciesOnResource()
 		if err != nil {
 			return nil, fmt.Errorf("failed to apply validating policies on JSON payload %s (%w)", jp.name, err)
 		}
@@ -606,7 +645,7 @@ func runTest(out io.Writer, testCase test.TestCase, registryAccess bool) (*TestR
 	for _, targetResource := range targetResources {
 		for _, engineResponse := range engineResponses {
 			if r, _ := extractPatchedTargetFromEngineResponse(targetResource.GetAPIVersion(), targetResource.GetKind(), targetResource.GetName(), targetResource.GetNamespace(), engineResponse); r != nil {
-				resourceKey := generateResourceKey(targetResource)
+				resourceKey := processor.GenerateResourceKey(targetResource)
 				testResponse.Target[resourceKey] = append(testResponse.Target[resourceKey], engineResponse)
 			}
 		}
@@ -834,10 +873,6 @@ func applyDeletingPolicies(
 	}
 
 	return responses, nil
-}
-
-func generateResourceKey(resource *unstructured.Unstructured) string {
-	return resource.GetAPIVersion() + "," + resource.GetKind() + "," + resource.GetNamespace() + "," + resource.GetName()
 }
 
 // convertNumericValuesToFloat64 recursively converts all numeric values in the object to float64.
