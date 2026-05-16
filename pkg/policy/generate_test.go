c626cf2efd602ab58679d07bbd20ed7ef4437652
Karthik <karthikgudaru72@gmail.com>
2026-05-16 06:36:49 +0000

    test: improve buildPolicyWithDeletedRules tests
    Signed-off-by: Karthik <karthikgudaru72@gmail.com>

--- pkg/policy/generate_test.go
+++ pkg/policy/generate_test.go
@@ -54,18 +54,45 @@ func Test_buildPolicyWithDeletedRules(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-		    originalRuleCount := len(tt.policy.GetSpec().Rules)
+			originalRules := make([]string, len(tt.policy.GetSpec().Rules))
+
+			for i, r := range tt.policy.GetSpec().Rules {
+				originalRules[i] = r.Name	
+			}
 			
 			returnedPolicy := buildPolicyWithDeletedRules(tt.policy, tt.deletedRules)
-
-			if len(returnedPolicy.GetSpec().Rules) != len(tt.wantRules) {
+            
+			returnedRules := returnedPolicy.GetSpec().Rules
+			if len(returnedRules) != len(tt.wantRules) {
 				t.Errorf("buildPolicyWithDeletedRules() rules len = %d, want %d",
 				 len(returnedPolicy.GetSpec().Rules), len(tt.wantRules))
 			}
+            
+            for i, wantRule := range tt.wantRules {
+				if i >= len(returnedRules) {
+					break
+				}
+				if returnedRules[i].Name != wantRule.Name {
+					t.Errorf("buildPolicyWithDeletedRules() rule[%d].Name = %q, want = %q",
+					 i, returnedRules[i].Name, wantRule.Name)
+				}
+			}
+            
+			currentRules := tt.policy.GetSpec().Rules
+			if len(currentRules) != len(originalRules) {
+				t.Errorf("buildPolicyWithDeletedRules() changed original policy rule count : was %d, now %d",
+				 len(originalRules), len(currentRules))
+			}
 
-			if len(tt.policy.GetSpec().Rules) != originalRuleCount {
-				t.Errorf("buildPolicyWithDeletedRules() mutated the orginal policy: had %d rules, now %d",
-				 originalRuleCount, len(tt.policy.GetSpec().Rules))
+			for i, originalRule := range originalRules {
+				if i >= len(originalRules) {
+					break
+				}
+
+				if currentRules[i].Name != originalRule {
+					t.Errorf("buildPolicyWithDeletedRules() mutated original policy rule[%d]: was %q, now %q",
+					 i, originalRule, currentRules[i].Name)
+				}
 			}
 		})
 	}
