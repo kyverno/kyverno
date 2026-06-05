af8950f5c58c714c7f6d4d9d42b4cdac1aacbee1
KrishnaParihar1 <163585208+KrishnaParihar1@users.noreply.github.com>
2026-06-05 13:22:48 +0530

    Add test for EnvOptions homogeneous aggregate behavior
    Replaced old tests with new test for homogeneous aggregate behavior in environment options.
    
    Signed-off-by: KrishnaParihar1 <163585208+KrishnaParihar1@users.noreply.github.com>

--- pkg/cel/compiler/env_test.go
+++ pkg/cel/compiler/env_test.go
@@ -3,17 +3,23 @@ package compiler
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/google/cel-go/cel"
+	"github.com/stretchr/testify/require"
 )
 
-func TestNewBaseEnv(t *testing.T) {
-	got, err := NewBaseEnv()
-	assert.NoError(t, err)
-	assert.NotNil(t, got)
-}
+func TestEnvOptions_HomogeneousAggregateBehavior(t *testing.T) {
+	defaultEnv, err := cel.NewEnv(DefaultEnvOptions()...)
+	require.NoError(t, err)
 
-func TestNewMatchImageEnv(t *testing.T) {
-	got, err := NewMatchImageEnv()
-	assert.NoError(t, err)
-	assert.NotNil(t, got)
+	dynamicEnv, err := cel.NewEnv(DynamicResourceEnvOptions()...)
+	require.NoError(t, err)
+
+	// heterogeneous aggregate literal
+	expr := `[1, "two"]`
+
+	_, issues := defaultEnv.Compile(expr)
+	require.Error(t, issues.Err())
+
+	_, issues = dynamicEnv.Compile(expr)
+	require.NoError(t, issues.Err())
 }
