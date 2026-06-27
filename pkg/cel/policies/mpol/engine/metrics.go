6401b91611a2adcc9f0a5d2fcbf6844792e89de6
Aditi Pandey <aditip149209@gmail.com>
2026-06-27 20:48:11 +0530

    Fixed botched merge conflict-no new additions
    Signed-off-by: Aditi Pandey <aditip149209@gmail.com>

--- pkg/cel/policies/mpol/engine/metrics.go
+++ pkg/cel/policies/mpol/engine/metrics.go
@@ -27,10 +27,11 @@ func (w *metricWrapper) Evaluate(ctx context.Context, attr admission.Attributes,
 				continue
 			}
 
-		w.metrics.RecordDuration(ctx, policy.Rules[0].Stats().ProcessingTime().Seconds(), string(policy.Rules[0].Status()), w.ruleExecutionCause, policy.Policy, response.Resource, string(request.Operation))
+			w.metrics.RecordDuration(ctx, policy.Rules[0].Stats().ProcessingTime().Seconds(), string(policy.Rules[0].Status()), w.ruleExecutionCause, policy.Policy, response.Resource, string(request.Operation))
 
-		for _, rule := range policy.Rules {
-			w.metrics.RecordResult(ctx, string(rule.Status()), w.ruleExecutionCause, policy.Policy, response.Resource, string(request.Operation))
+			for _, rule := range policy.Rules {
+				w.metrics.RecordResult(ctx, string(rule.Status()), w.ruleExecutionCause, policy.Policy, response.Resource, string(request.Operation))
+			}
 		}
 	}
 
@@ -49,13 +50,13 @@ func (w *metricWrapper) Handle(ctx context.Context, request engine.EngineRequest
 				continue
 			}
 
-		w.metrics.RecordDuration(ctx, policy.Rules[0].Stats().ProcessingTime().Seconds(), string(policy.Rules[0].Status()), w.ruleExecutionCause, policy.Policy, response.Resource, string(request.Request.Operation))
+			w.metrics.RecordDuration(ctx, policy.Rules[0].Stats().ProcessingTime().Seconds(), string(policy.Rules[0].Status()), w.ruleExecutionCause, policy.Policy, response.Resource, string(request.Request.Operation))
 
-		for _, rule := range policy.Rules {
-			w.metrics.RecordResult(ctx, string(rule.Status()), w.ruleExecutionCause, policy.Policy, response.Resource, string(request.Request.Operation))
+			for _, rule := range policy.Rules {
+				w.metrics.RecordResult(ctx, string(rule.Status()), w.ruleExecutionCause, policy.Policy, response.Resource, string(request.Request.Operation))
+			}
 		}
 	}
-
 	return response, nil
 }
 
