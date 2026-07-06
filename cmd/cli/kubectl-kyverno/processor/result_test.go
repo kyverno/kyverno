703b084f1849014c1c6c698354acb96bf925f98c
Gaurav Chaudhary <chaudharygaurav2004@gmail.com>
2026-07-06 16:51:49 +0530

    fix(cli): capture loop variable in GPOL result parallel subtests
    Signed-off-by: Gaurav Chaudhary <chaudharygaurav2004@gmail.com>

--- cmd/cli/kubectl-kyverno/processor/result_test.go
+++ cmd/cli/kubectl-kyverno/processor/result_test.go
@@ -71,6 +71,7 @@ func TestAddGenerateResponse_GeneratingPolicy(t *testing.T) {
 	}
 
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 			rc := &ResultCounts{}
