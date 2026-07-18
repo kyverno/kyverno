6520ef88defc43449aa8eb95fd98ec11148b4d97
Aditi Pandey <aditip149209@gmail.com>
2026-07-18 19:55:36 +0530

    Added identifier field to Validation struct
    Signed-off-by: Aditi Pandey <aditip149209@gmail.com>

--- pkg/cel/compiler/validation.go
+++ pkg/cel/compiler/validation.go
@@ -5,6 +5,10 @@ import (
 )
 
 type Validation struct {
+	// Identifier is the stable name of the validation, taken from the
+	// source spec's Identifier field when set. It is used to build
+	// autogen rule names that survive reordering of the validations list.
+	Identifier        string
 	Message           string
 	MessageExpression cel.Program
 	Program           cel.Program
