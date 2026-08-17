kyverno/kyverno

commit 716611b7ea484bc2b36783c7a8bb09b87b8220f3

fix: return all the exceptions that match the incoming resource (#10722)
* fix: return all the exceptions that match the incoming resource

Signed-off-by: Mariam Fahmy <mariam.fahmy@nirmata.com>

* fix: modify log messages

Signed-off-by: Mariam Fahmy <mariam.fahmy@nirmata.com>

---------

Signed-off-by: Mariam Fahmy <mariam.fahmy@nirmata.com>

pkg/controllers/report/background/controller.go
@@ -2,6 +2,7 @@ package background
 
 import (
 	"context"
+	"strings"
 	"time"
 
 	"github.com/go-logr/logr"
@@ -361,8 +362,8 @@ func (c *controller) reconcileReport(
 			}
 			policyNameToLabel[key] = reportutils.PolicyLabel(policy)
 		}
-		for _, exception := range exceptions {
-			key, err := cache.MetaNamespaceKeyFunc(exception)
+		for i, exception := range exceptions {
+			key, err := cache.MetaNamespaceKeyFunc(&exceptions[i])
 			if err != nil {
 				return err
 			}
@@ -376,13 +377,24 @@ func (c *controller) reconcileReport(
 			policyNameToLabel[key] = reportutils.ValidatingAdmissionPolicyBindingLabel(binding)
 		}
 		for _, result := range observed.GetResults() {
-			// if the policy did not change, keep the result
+			// The result is kept as it is if:
+			// 1. The Kyverno policy and its matched exceptions are unchanged
+			// 2. The ValidatingAdmissionPolicy and its matched binding are unchanged
+			keepResult := true
+			exception := result.Properties["exceptions"]
+			exceptions := strings.Split(exception, ",")
+			for _, exception := range exceptions {
+				exceptionLabel := policyNameToLabel[exception]
+				if exceptionLabel != "" && expected[exceptionLabel] != actual[exceptionLabel] {
+					keepResult = false
+					break
+				}
+			}
+
 			label := policyNameToLabel[result.Policy]
-			exceptionLabel := policyNameToLabel[result.Properties["exception"]]
 			vapBindingLabel := policyNameToLabel[result.Properties["binding"]]
 			if (label != "" && expected[label] == actual[label]) ||
-				(exceptionLabel != "" && expected[exceptionLabel] == actual[exceptionLabel]) ||
-				(vapBindingLabel != "" && expected[vapBindingLabel] == actual[vapBindingLabel]) {
+				(vapBindingLabel != "" && expected[vapBindingLabel] == actual[vapBindingLabel]) || keepResult {
 				ruleResults = append(ruleResults, result)
 			}
 		}

cmd/cli/kubectl-kyverno/commands/test/output.go
@@ -90,7 +90,7 @@ func printCheckResult(
 					// patchedTargetSubresourceName string
 					// podSecurityChecks contains pod security checks (only if this is a pod security rule)
 					"podSecurityChecks": rule.PodSecurityChecks(),
-					"exception ":        rule.Exception(),
+					"exceptions":        rule.Exceptions(),
 				}
 				if check.Assert.Value != nil {
 					errs, err := assert.Assert(ctx, nil, assert.Parse(ctx, check.Assert.Value), data, nil)

pkg/engine/api/ruleresponse.go
@@ -43,8 +43,8 @@ type RuleResponse struct {
 	patchedTargetSubresourceName string
 	// podSecurityChecks contains pod security checks (only if this is a pod security rule)
 	podSecurityChecks *PodSecurityChecks
-	// exception is the exception applied (if any)
-	exception *kyvernov2.PolicyException
+	// exceptions are the exceptions applied (if any)
+	exceptions []kyvernov2.PolicyException
 	// binding is the validatingadmissionpolicybinding (if any)
 	binding *v1alpha1.ValidatingAdmissionPolicyBinding
 	// emitWarning enable passing rule message as warning to api server warning header
@@ -88,8 +88,8 @@ func RuleFail(name string, ruleType RuleType, msg string) *RuleResponse {
 	return NewRuleResponse(name, ruleType, msg, RuleStatusFail)
 }
 
-func (r RuleResponse) WithException(exception *kyvernov2.PolicyException) *RuleResponse {
-	r.exception = exception
+func (r RuleResponse) WithExceptions(exceptions []kyvernov2.PolicyException) *RuleResponse {
+	r.exceptions = exceptions
 	return &r
 }
 
@@ -129,8 +129,8 @@ func (r *RuleResponse) Stats() ExecutionStats {
 	return r.stats
 }
 
-func (r *RuleResponse) Exception() *kyvernov2.PolicyException {
-	return r.exception
+func (r *RuleResponse) Exceptions() []kyvernov2.PolicyException {
+	return r.exceptions
 }
 
 func (r *RuleResponse) ValidatingAdmissionPolicyBinding() *v1alpha1.ValidatingAdmissionPolicyBinding {
@@ -138,7 +138,7 @@ func (r *RuleResponse) ValidatingAdmissionPolicyBinding() *v1alpha1.ValidatingAd
 }
 
 func (r *RuleResponse) IsException() bool {
-	return r.exception != nil
+	return len(r.exceptions) > 0
 }
 
 func (r *RuleResponse) PodSecurityChecks() *PodSecurityChecks {

pkg/engine/background.go
@@ -2,6 +2,7 @@ package engine
 
 import (
 	"context"
+	"strings"
 
 	"github.com/go-logr/logr"
 	kyvernov1 "github.com/kyverno/kyverno/api/kyverno/v1"
@@ -64,17 +65,21 @@ func (e *engine) filterRule(
 		logger.Error(err, "failed to get exceptions")
 		return nil
 	}
-	// check if there is a policy exception matches the incoming resource
-	exception := engineutils.MatchesException(exceptions, policyContext, logger)
-	if exception != nil {
-		key, err := cache.MetaNamespaceKeyFunc(exception)
-		if err != nil {
-			logger.Error(err, "failed to compute policy exception key", "namespace", exception.GetNamespace(), "name", exception.GetName())
-			return engineapi.RuleError(rule.Name, ruleType, "failed to compute exception key", err)
-		} else {
-			logger.V(3).Info("policy rule skipped due to policy exception", "exception", key)
-			return engineapi.RuleSkip(rule.Name, ruleType, "rule skipped due to policy exception "+key).WithException(exception)
+	// check if there are policy exceptions that match the incoming resource
+	matchedExceptions := engineutils.MatchesException(exceptions, policyContext, logger)
+	if len(matchedExceptions) > 0 {
+		var keys []string
+		for i, exception := range matchedExceptions {
+			key, err := cache.MetaNamespaceKeyFunc(&matchedExceptions[i])
+			if err != nil {
+				logger.Error(err, "failed to compute policy exception key", "namespace", exception.GetNamespace(), "name", exception.GetName())
+				return engineapi.RuleError(rule.Name, ruleType, "failed to compute exception key", err)
+			}
+			keys = append(keys, key)
 		}
+
+		logger.V(3).Info("policy rule is skipped due to policy exceptions", "exceptions", keys)
+		return engineapi.RuleSkip(rule.Name, ruleType, "rule is skipped due to policy exception "+strings.Join(keys, ", ")).WithExceptions(matchedExceptions)
 	}
 
 	newResource := policyContext.NewResource()

pkg/engine/handlers/mutation/mutate_existing.go
@@ -2,6 +2,7 @@ package mutation
 
 import (
 	"context"
+	"strings"
 
 	"github.com/go-logr/logr"
 	kyvernov1 "github.com/kyverno/kyverno/api/kyverno/v1"
@@ -37,19 +38,23 @@ func (h mutateExistingHandler) Process(
 	contextLoader engineapi.EngineContextLoader,
 	exceptions []*kyvernov2.PolicyException,
 ) (unstructured.Unstructured, []engineapi.RuleResponse) {
-	// check if there is a policy exception matches the incoming resource
-	exception := engineutils.MatchesException(exceptions, policyContext, logger)
-	if exception != nil {
-		key, err := cache.MetaNamespaceKeyFunc(exception)
-		if err != nil {
-			logger.Error(err, "failed to compute policy exception key", "namespace", exception.GetNamespace(), "name", exception.GetName())
-			return resource, handlers.WithError(rule, engineapi.Mutation, "failed to compute exception key", err)
-		} else {
-			logger.V(3).Info("policy rule skipped due to policy exception", "exception", key)
-			return resource, handlers.WithResponses(
-				engineapi.RuleSkip(rule.Name, engineapi.Mutation, "rule skipped due to policy exception "+key).WithException(exception),
-			)
+	// check if there are policy exceptions that match the incoming resource
+	matchedExceptions := engineutils.MatchesException(exceptions, policyContext, logger)
+	if len(matchedExceptions) > 0 {
+		var keys []string
+		for i, exception := range matchedExceptions {
+			key, err := cache.MetaNamespaceKeyFunc(&matchedExceptions[i])
+			if err != nil {
+				logger.Error(err, "failed to compute policy exception key", "namespace", exception.GetNamespace(), "name", exception.GetName())
+				return resource, handlers.WithError(rule, engineapi.Mutation, "failed to compute exception key", err)
+			}
+			keys = append(keys, key)
 		}
+
+		logger.V(3).Info("policy rule is skipped due to policy exceptions", "exceptions", keys)
+		return resource, handlers.WithResponses(
+			engineapi.RuleSkip(rule.Name, engineapi.Mutation, "rule is skipped due to policy exceptions"+strings.Join(keys, ", ")).WithExceptions(matchedExceptions),
+		)
 	}
 
 	var responses []engineapi.RuleResponse

pkg/engine/handlers/mutation/mutate_image.go
@@ -2,6 +2,7 @@ package mutation
 
 import (
 	"context"
+	"strings"
 
 	json_patch "github.com/evanphx/json-patch/v5"
 	"github.com/go-logr/logr"
@@ -68,19 +69,23 @@ func (h mutateImageHandler) Process(
 	contextLoader engineapi.EngineContextLoader,
 	exceptions []*kyvernov2.PolicyException,
 ) (unstructured.Unstructured, []engineapi.RuleResponse) {
-	// check if there is a policy exception matches the incoming resource
-	exception := engineutils.MatchesException(exceptions, policyContext, logger)
-	if exception != nil {
-		key, err := cache.MetaNamespaceKeyFunc(exception)
-		if err != nil {
-			logger.Error(err, "failed to compute policy exception key", "namespace", exception.GetNamespace(), "name", exception.GetName())
-			return resource, handlers.WithError(rule, engineapi.Mutation, "failed to compute exception key", err)
-		} else {
-			logger.V(3).Info("policy rule skipped due to policy exception", "exception", key)
-			return resource, handlers.WithResponses(
-				engineapi.RuleSkip(rule.Name, engineapi.Mutation, "rule skipped due to policy exception "+key).WithException(exception),
-			)
+	// check if there are policy exceptions that match the incoming resource
+	matchedExceptions := engineutils.MatchesException(exceptions, policyContext, logger)
+	if len(matchedExceptions) > 0 {
+		var keys []string
+		for i, exception := range matchedExceptions {
+			key, err := cache.MetaNamespaceKeyFunc(&matchedExceptions[i])
+			if err != nil {
+				logger.Error(err, "failed to compute policy exception key", "namespace", exception.GetNamespace(), "name", exception.GetName())
+				return resource, handlers.WithError(rule, engineapi.Mutation, "failed to compute exception key", err)
+			}
+			keys = append(keys, key)
 		}
+
+		logger.V(3).Info("policy rule is skipped due to policy exceptions", "exceptions", keys)
+		return resource, handlers.WithResponses(
+			engineapi.RuleSkip(rule.Name, engineapi.Mutation, "rule is skipped due to policy exceptions"+strings.Join(keys, ", ")).WithExceptions(matchedExceptions),
+		)
 	}
 
 	jsonContext := policyContext.JSONContext()

pkg/engine/handlers/mutation/mutate_resource.go
@@ -2,6 +2,7 @@ package mutation
 
 import (
 	"context"
+	"strings"
 
 	"github.com/go-logr/logr"
 	kyvernov1 "github.com/kyverno/kyverno/api/kyverno/v1"
@@ -30,19 +31,23 @@ func (h mutateResourceHandler) Process(
 	contextLoader engineapi.EngineContextLoader,
 	exceptions []*kyvernov2.PolicyException,
 ) (unstructured.Unstructured, []engineapi.RuleResponse) {
-	// check if there is a policy exception matches the incoming resource
-	exception := engineutils.MatchesException(exceptions, policyContext, logger)
-	if exception != nil {
-		key, err := cache.MetaNamespaceKeyFunc(exception)
-		if err != nil {
-			logger.Error(err, "failed to compute policy exception key", "namespace", exception.GetNamespace(), "name", exception.GetName())
-			return resource, handlers.WithError(rule, engineapi.Mutation, "failed to compute exception key", err)
-		} else {
-			logger.V(3).Info("policy rule skipped due to policy exception", "exception", key)
-			return resource, handlers.WithResponses(
-				engineapi.RuleSkip(rule.Name, engineapi.Mutation, "rule skipped due to policy exception "+key).WithException(exception),
-			)
+	// check if there are policy exceptions that match the incoming resource
+	matchedExceptions := engineutils.MatchesException(exceptions, policyContext, logger)
+	if len(matchedExceptions) > 0 {
+		var keys []string
+		for i, exception := range matchedExceptions {
+			key, err := cache.MetaNamespaceKeyFunc(&matchedExceptions[i])
+			if err != nil {
+				logger.Error(err, "failed to compute policy exception key", "namespace", exception.GetNamespace(), "name", exception.GetName())
+				return resource, handlers.WithError(rule, engineapi.Mutation, "failed to compute exception key", err)
+			}
+			keys = append(keys, key)
 		}
+
+		logger.V(3).Info("policy rule is skipped due to policy exceptions", "exceptions", keys)
+		return resource, handlers.WithResponses(
+			engineapi.RuleSkip(rule.Name, engineapi.Mutation, "rule is skipped due to policy exceptions"+strings.Join(keys, ", ")).WithExceptions(matchedExceptions),
+		)
 	}
 
 	_, subresource := policyContext.ResourceKind()

pkg/engine/handlers/validation/validate_cel.go
@@ -3,6 +3,7 @@ package validation
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/go-logr/logr"
 	kyvernov1 "github.com/kyverno/kyverno/api/kyverno/v1"
@@ -47,19 +48,23 @@ func (h validateCELHandler) Process(
 	_ engineapi.EngineContextLoader,
 	exceptions []*kyvernov2.PolicyException,
 ) (unstructured.Unstructured, []engineapi.RuleResponse) {
-	// check if there is a policy exception matches the incoming resource
-	exception := engineutils.MatchesException(exceptions, policyContext, logger)
-	if exception != nil {
-		key, err := cache.MetaNamespaceKeyFunc(exception)
-		if err != nil {
-			logger.Error(err, "failed to compute policy exception key", "namespace", exception.GetNamespace(), "name", exception.GetName())
-			return resource, handlers.WithError(rule, engineapi.Validation, "failed to compute exception key", err)
-		} else {
-			logger.V(3).Info("policy rule skipped due to policy exception", "exception", key)
-			return resource, handlers.WithResponses(
-				engineapi.RuleSkip(rule.Name, engineapi.Validation, "rule skipped due to policy exception "+key).WithException(exception),
-			)
+	// check if there are policy exceptions that match the incoming resource
+	matchedExceptions := engineutils.MatchesException(exceptions, policyContext, logger)
+	if len(matchedExceptions) > 0 {
+		var keys []string
+		for i, exception := range matchedExceptions {
+			key, err := cache.MetaNamespaceKeyFunc(&matchedExceptions[i])
+			if err != nil {
+				logger.Error(err, "failed to compute policy exception key", "namespace", exception.GetNamespace(), "name", exception.GetName())
+				return resource, handlers.WithError(rule, engineapi.Validation, "failed to compute exception key", err)
+			}
+			keys = append(keys, key)
 		}
+
+		logger.V(3).Info("policy rule is skipped due to policy exceptions", "exceptions", keys)
+		return resource, handlers.WithResponses(
+			engineapi.RuleSkip(rule.Name, engineapi.Validation, "rule is skipped due to policy exceptions"+strings.Join(keys, ", ")).WithExceptions(matchedExceptions),
+		)
 	}
 
 	// check if a corresponding validating admission policy is generated

commit 0b141b29e3c5a6c6370749cfd57d34dc39e658c1

fix(reports): drop stale background scan results when the owning policy changes
Motivation:
Editing a ClusterPolicy/Policy (for example, adding
`exclude.any.resources.namespaces: [kube-system]`) did not remove stale
"pass" results already recorded in background scan reports for
resources that should now be excluded. The stale entries lingered
until the next full background rescan (default interval: one hour),
making it look like the exclude was not respected.

Approach:
In `pkg/controllers/report/background/controller.go`, `reconcileReport`'s
partial-reconcile path decides per-result whether to carry a previously
observed result into the new report, or drop it so the resource gets
re-scanned. The decision used a `keepResult` boolean that defaulted to
`true` and was only set `false` when the result had a non-empty
`exceptions` property whose matched exception's version had changed.
For an ordinary pass/fail result with no exceptions, `keepResult` stayed
`true` forever, and the trailing `... || keepResult` in the append
condition made the whole check always true - so a stale result was kept
regardless of whether the owning policy's ResourceVersion had changed.
This is a regression from commit 716611b7e (PR #10722, 2024); the code
before that commit correctly dropped a result once its policy's version
no longer matched.

The fix extracts the decision into a small pure helper,
shouldKeepStaleResult, which keeps a result only when everything that
could have produced it - the owning policy/binding label, and any
exceptions referenced by the result - is unchanged. This matches the
existing doc comment describing the intended (but previously
unimplemented) behavior. When the policy changed, the stale result is
now dropped and the resource is re-scanned under the current policy on
the same reconcile pass.

Impact: this is a bookkeeping fix in the background-scan report
controller, not a change to admission-time policy enforcement - a

pkg/controllers/report/background/controller.go
@@ -675,25 +675,7 @@ func (c *controller) reconcileReport(
 			policyNameToLabel[key] = reportutils.MutatingAdmissionPolicyBindingLabel(&binding)
 		}
 		for _, result := range observed.GetResults() {
-			// The result is kept as it is if:
-			// 1. The Kyverno policy and its matched exceptions are unchanged
-			// 2. The ValidatingAdmissionPolicy and its matched binding are unchanged
-			keepResult := true
-			exception := result.Properties["exceptions"]
-			exceptions := strings.Split(exception, ",")
-			for _, exception := range exceptions {
-				exceptionLabel := policyNameToLabel[exception]
-				if exceptionLabel != "" && expected[exceptionLabel] != actual[exceptionLabel] {
-					keepResult = false
-					break
-				}
-			}
-			label := policyNameToLabel[result.Policy]
-			vapBindingLabel := policyNameToLabel[result.Properties["binding"]]
-			mapBindingLabel := policyNameToLabel[result.Properties["mapBinding"]]
-			if (label != "" && expected[label] == actual[label]) ||
-				(vapBindingLabel != "" && expected[vapBindingLabel] == actual[vapBindingLabel]) ||
-				(mapBindingLabel != "" && expected[mapBindingLabel] == actual[mapBindingLabel]) || keepResult {
+			if shouldKeepStaleResult(result, policyNameToLabel, expected, actual) {
 				ruleResults = append(ruleResults, result)
 			}
 		}
@@ -769,6 +751,37 @@ func (c *controller) reconcileReport(
 		return c.storeReport(ctx, observed, desired)
 	}
 	return nil
+}
+
+// shouldKeepStaleResult reports whether a previously observed rule result can be carried
+// over as-is into the new report without a rescan. This is only safe when everything that
+// could have influenced the result - the owning policy or binding, and any exceptions that
+// matched it - still has the resource version it had when the result was produced. If any of
+// those changed (e.g. a policy was edited to exclude the resource's namespace), the stale
+// result must be dropped so the resource is re-evaluated against the current policy.
+func shouldKeepStaleResult(result openreportsv1alpha1.ReportResult, policyNameToLabel, expected, actual map[string]string) bool {
+	label := policyNameToLabel[result.Policy]
+	vapBindingLabel := policyNameToLabel[result.Properties["binding"]]
+	mapBindingLabel := policyNameToLabel[result.Properties["mapBinding"]]
+	if label == "" && vapBindingLabel == "" && mapBindingLabel == "" {
+		return false
+	}
+	if label != "" && expected[label] != actual[label] {
+		return false
+	}
+	if vapBindingLabel != "" && expected[vapBindingLabel] != actual[vapBindingLabel] {
+		return false
+	}
+	if mapBindingLabel != "" && expected[mapBindingLabel] != actual[mapBindingLabel] {
+		return false
+	}
+	for _, exception := range strings.Split(result.Properties["exceptions"], ",") {
+		exceptionLabel := policyNameToLabel[exception]
+		if exceptionLabel != "" && expected[exceptionLabel] != actual[exceptionLabel] {
+			return false
+		}
+	}
+	return true
 }
 
 func (c *controller) storeReport(ctx context.Context, observed, desired reportsv1.ReportInterface) error {

pkg/controllers/report/background/controller_test.go
@@ -1,0 +1,66 @@
+package background
+
+import (
+	"testing"
+
+	openreportsv1alpha1 "github.com/openreports/reports-api/apis/openreports.io/v1alpha1"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestShouldKeepStaleResult_PolicyUnchanged(t *testing.T) {
+	policyNameToLabel := map[string]string{
+		"require-requests-limits": "policy.kyverno.io/require-requests-limits",
+	}
+	expected := map[string]string{"policy.kyverno.io/require-requests-limits": "1"}
+	actual := map[string]string{"policy.kyverno.io/require-requests-limits": "1"}
+
+	result := openreportsv1alpha1.ReportResult{Policy: "require-requests-limits"}
+
+	assert.True(t, shouldKeepStaleResult(result, policyNameToLabel, expected, actual))
+}
+
+// Reproduces the scenario from the reported issue: a ClusterPolicy is edited to add
+// `exclude.any.resources.namespaces: [kube-system]`, bumping its resource version. A stale
+// "pass" result recorded for a kube-system Pod under the old version must be dropped so the
+// resource is rescanned (and correctly excluded), instead of surviving in the report forever.
+func TestShouldKeepStaleResult_PolicyChanged(t *testing.T) {
+	policyNameToLabel := map[string]string{
+		"require-requests-limits": "policy.kyverno.io/require-requests-limits",
+	}
+	expected := map[string]string{"policy.kyverno.io/require-requests-limits": "2"}
+	actual := map[string]string{"policy.kyverno.io/require-requests-limits": "1"}
+
+	result := openreportsv1alpha1.ReportResult{Policy: "require-requests-limits"}
+
+	assert.False(t, shouldKeepStaleResult(result, policyNameToLabel, expected, actual))
+}
+
+func TestShouldKeepStaleResult_ExceptionChanged(t *testing.T) {
+	policyNameToLabel := map[string]string{
+		"require-requests-limits": "policy.kyverno.io/require-requests-limits",
+		"default/my-exception":    "policy.kyverno.io/polexcp-default-my-exception",
+	}
+	expected := map[string]string{
+		"policy.kyverno.io/require-requests-limits":      "1",
+		"policy.kyverno.io/polexcp-default-my-exception": "2",
+	}
+	actual := map[string]string{
+		"policy.kyverno.io/require-requests-limits":      "1",
+		"policy.kyverno.io/polexcp-default-my-exception": "1",
+	}
+
+	result := openreportsv1alpha1.ReportResult{
+		Policy: "require-requests-limits",
+		Properties: map[string]string{
+			"exceptions": "default/my-exception",
+		},
+	}
+
+	assert.False(t, shouldKeepStaleResult(result, policyNameToLabel, expected, actual))
+}
+
+func TestShouldKeepStaleResult_NoMatchingLabel(t *testing.T) {
+	result := openreportsv1alpha1.ReportResult{Policy: "some-other-policy"}
+
+	assert.False(t, shouldKeepStaleResult(result, map[string]string{}, map[string]string{}, map[string]string{}))
+}