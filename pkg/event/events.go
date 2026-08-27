8c6ec55ee4048dbfdf032b4efdca24c592f9b730
Yashraj Shukla <shuklayashraj68@gmail.com>
2026-08-27 12:33:58 +0000

    fix: use dynamic APIVersion in event ObjectReferences instead of hardcoded values
    Signed-off-by: Yashraj Shukla <shuklayashraj68@gmail.com>

--- pkg/event/events.go
+++ pkg/event/events.go
@@ -170,8 +170,7 @@ func NewResourceGenerationEvent(policy, rule string, source Source, resource kyv
 func NewBackgroundFailedEvent(err error, policy engineapi.GenericPolicy, rule string, source Source, resource kyvernov1.ResourceSpec) []Info {
 	events := make([]Info, 0, 1)
 	regarding := corev1.ObjectReference{
-		// TODO: iirc it's not safe to assume api version is set
-		APIVersion: "kyverno.io/v1",
+		APIVersion: policy.GetAPIVersion(),
 		Kind:       policy.GetKind(),
 		Name:       policy.GetName(),
 		Namespace:  policy.GetNamespace(),
@@ -210,8 +209,7 @@ func NewBackgroundSuccessEvent(source Source, policy engineapi.GenericPolicy, re
 		action = ResourceMutated
 	}
 	regarding := corev1.ObjectReference{
-		// TODO: iirc it's not safe to assume api version is set
-		APIVersion: "kyverno.io/v1",
+		APIVersion: policy.GetAPIVersion(),
 		Kind:       policy.GetKind(),
 		Name:       policy.GetName(),
 		Namespace:  policy.GetNamespace(),
@@ -260,8 +258,7 @@ func NewPolicyExceptionEvents(engineResponse engineapi.EngineResponse, ruleResp 
 
 			exceptionEvent := Info{
 				Regarding: corev1.ObjectReference{
-					// TODO: iirc it's not safe to assume api version is set
-					APIVersion: "kyverno.io/v2",
+					APIVersion: exception.GetAPIVersion(),
 					Kind:       "PolicyException",
 					Name:       name,
 					Namespace:  ns,
@@ -285,7 +282,7 @@ func NewPolicyExceptionEvents(engineResponse engineapi.EngineResponse, ruleResp 
 		// build the policy events
 		policyMessage := fmt.Sprintf("resource %s was skipped from rule %s due to policy exceptions %s", resourceKey(engineResponse.PatchedResource), ruleResp.Name(), strings.Join(exceptionNames, ", "))
 		regarding := corev1.ObjectReference{
-			// TODO: iirc it's not safe to assume api version is set
+			// PolicyInterface (Policy/ClusterPolicy) is always kyverno.io/v1
 			APIVersion: "kyverno.io/v1",
 			Kind:       pol.GetKind(),
 			Name:       pol.GetName(),
@@ -313,8 +310,7 @@ func NewPolicyExceptionEvents(engineResponse engineapi.EngineResponse, ruleResp 
 
 func NewCleanupPolicyEvent(policy kyvernov2.CleanupPolicyInterface, resource unstructured.Unstructured, err error) Info {
 	regarding := corev1.ObjectReference{
-		// TODO: iirc it's not safe to assume api version is set
-		APIVersion: "kyverno.io/v2",
+		APIVersion: policy.GetAPIVersion(),
 		Kind:       policy.GetKind(),
 		Name:       policy.GetName(),
 		Namespace:  policy.GetNamespace(),
