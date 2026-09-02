7570c08d3b5618ce2dbf37c4b4da5bd4980e7a98
Mannan-Ali <alimannan.am244@gmail.com>
2026-09-02 13:28:55 +0000

    fix: address AI code review feedback
    Signed-off-by: Mannan-Ali <alimannan.am244@gmail.com>

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -605,6 +605,7 @@ func (cd *configuration) load(cm *corev1.ConfigMap) {
 		}
 	}
 	// load enableUpdateRequestCleanup
+	cd.enableUpdateRequestCleanup = false
 	enableCleanup, ok := data[enableUpdateRequestCleanup]
 	if !ok {
 		logger.V(2).Info("enableUpdateRequestCleanup not set")
@@ -618,6 +619,7 @@ func (cd *configuration) load(cm *corev1.ConfigMap) {
 		}
 	}
 	// load updateRequestCleanupTTL
+	cd.updateRequestCleanupTTL = ""
 	ttl, ok := data[updateRequestCleanupTTL]
 	if !ok {
 		logger.V(2).Info("updateRequestCleanupTTL not set")
